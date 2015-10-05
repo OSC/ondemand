a414533a9f9309738438916401f18fabf0a3f76c
Brian McMichael <bmcmichael@osc.edu>
2015-10-05 12:16:06 -0400

    Add status label helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,25 @@ module ApplicationHelper
     # and the above line cuts the digits.
     #attribs_submit_host.split('.')[0].capitalize
   end
+
+  def status_label(attribs_job_state)
+    case attribs_job_state
+      when "C"
+        label = "Completed"
+        labelclass = "label-success"
+      when "R"
+        label = "Running"
+        labelclass = "label-primary"
+      when "Q"
+        label = "Queued"
+        labelclass = "label-info"
+      when "H"
+        label = "Hold"
+        labelclass = "label-warning"
+      else
+        label = "Not Submitted"
+        labelclass = "label-default"
+    end
+    "<span class='label #{labelclass}'>#{label}</span>".html_safe
+  end
 end
