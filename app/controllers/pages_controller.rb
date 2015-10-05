50aa9e6c245330541167042bebba4c292fa65453
Brian McMichael <bmcmichael@osc.edu>
2015-10-05 11:02:33 -0400

    App displays all jobs in view table

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,14 @@
 class PagesController < ApplicationController
-  def index
-  end
 
-  def about
+  def index
+    c = PBS::Conn.batch 'oakley'
+    #q = PBS::Query.new conn: c, type: :node
+    q = PBS::Query.new conn: c, type: :job
+
+    # Get all
+    @activejobs = q.find
+
+    # Get user
+    # q.where.user('username').find
   end
 end
