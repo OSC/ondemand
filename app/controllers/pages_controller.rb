2d9d044dec7810f7c3715a768d74ba851e1812fd
Brian McMichael <bmcmichael@osc.edu>
2015-10-05 13:13:59 -0400

    Sort the user's jobs to the top of the array.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,22 +1,25 @@
 class PagesController < ApplicationController
+  include ApplicationHelper
 
   def index
-    c = PBS::Conn.batch 'oakley'
-    #q = PBS::Query.new conn: c, type: :node
-    q = PBS::Query.new conn: c, type: :job
 
-    # Get all Oakley
+    # Get all Oakley jobs
+    c = PBS::Conn.batch 'oakley'
+    q = PBS::Query.new conn: c, type: :job
     oakleyjobs = q.find
 
+    # Get all Ruby jobs
     c = PBS::Conn.batch 'ruby'
-    #q = PBS::Query.new conn: c, type: :node
     q = PBS::Query.new conn: c, type: :job
-
-    # Get all Ruby
     rubyjobs = q.find
 
+    # Join the arrays
     @activejobs = oakleyjobs.concat rubyjobs
-    # Get user
+
+    # Sort the user's jobs to the top
+    @activejobs.sort_by! { |user| [ username(user[:attribs][:Job_Owner]) == ENV['USER'] ? 0 : 1 ] }
+
+    # Get user example:
     # q.where.user('username').find
   end
 end
