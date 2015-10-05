6dda0d3325dee808cb152616d7723f3b9dc2f688
Brian McMichael <bmcmichael@osc.edu>
2015-10-05 11:50:26 -0400

    Add hostname helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,15 @@
 module ApplicationHelper
 
   def username(attribs_Job_Owner)
-    uname = attribs_Job_Owner.split('@')[0]
+    attribs_Job_Owner.split('@')[0]
+  end
+
+  def hostname(attribs_submit_host)
+    attribs_submit_host.split(/\d+/)[0].capitalize
+
+    # We may want to split after the number.
+    # PBS returns jobs running  on Websvcs and N000 nodes,
+    # and the above line cuts the digits.
+    #attribs_submit_host.split('.')[0].capitalize
   end
 end
