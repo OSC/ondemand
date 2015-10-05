189a1e0310cb52d9fb2240ff2307bd8ac0eda693
Brian McMichael <bmcmichael@osc.edu>
2015-10-05 12:28:38 -0400

    App shows oakley and ruby jobs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,9 +5,17 @@ class PagesController < ApplicationController
     #q = PBS::Query.new conn: c, type: :node
     q = PBS::Query.new conn: c, type: :job
 
-    # Get all
-    @activejobs = q.find
+    # Get all Oakley
+    oakleyjobs = q.find
 
+    c = PBS::Conn.batch 'ruby'
+    #q = PBS::Query.new conn: c, type: :node
+    q = PBS::Query.new conn: c, type: :job
+
+    # Get all Ruby
+    rubyjobs = q.find
+
+    @activejobs = oakleyjobs.concat rubyjobs
     # Get user
     # q.where.user('username').find
   end
