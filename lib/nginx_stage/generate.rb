8b680b8440a7e4d1d1f06311e80705df187f4f7a
Jeremy Nicklas <jnicklas@osc.edu>
2016-01-08 16:54:29 -0500

    forgot to clean up old code

--- lib/nginx_stage/generate.rb
+++ lib/nginx_stage/generate.rb
@@ -19,17 +19,6 @@ module NginxStage
       end
     end
 
-    attr_reader :options
-
-    attr_reader :user
-    attr_reader :skip_nginx
-
-    def initialize(opts)
-      @options    = opts.dup
-      @user       = opts.fetch(:user, nil)
-      @skip_nginx = opts.fetch(:skip_nginx, false)
-    end
-
     def invoke
       output = nil
       self.class.hooks.each {|k,v| output = self.instance_eval(&v)}
@@ -51,26 +40,5 @@ module NginxStage
         file.write data
       end
     end
-
-    add_hook :require_user do
-      raise MissingOption, "missing option: --user=USER" unless user
-    end
-
-    add_hook :validate_user do
-      check_user_exists(user)
-      check_user_is_special(user)
-    end
-
-    private
-      def check_user_exists(user)
-        Etc.getpwnam(user)
-      rescue ArgumentError
-        raise InvalidUser, "user doesn't exist: #{user}"
-      end
-
-      def check_user_is_special(user)
-        passwd = Etc.getpwnam(user)
-        raise InvalidUser, "user is special: #{user} (#{passwd.uid} < #{NginxStage.min_uid})" if passwd.uid < NginxStage.min_uid
-      end
   end
 end
