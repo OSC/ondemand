f5b3071c32aaf0baba41fce9020ec319b4b4b066
Brian McMichael <bmcmichael@osc.edu>
2016-02-29 09:34:07 -0500

    code comments

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ var http        = require('http'),
     homeDir     = require('os-homedir'),
     app         = express(),
     PORT        = 9001,
-    PREFIX      = '/pun/dev/cloudcmd',
-
+    PREFIX      = '/pun/dev/cloudcmd',          /* FIXME: Find a way to make this dynamic based on the system
+                                                 *        This needs to be updated to ex: 
+                                                 *        '/pun/shared/bmcmichael/cloudcmd',
+                                                 *        when used on the shared environment.
+                                                 */       
     server,
     socket;
 
@@ -18,8 +21,8 @@ socket = io.listen(server, {
 app.use(cloudcmd({
     socket: socket,                 /* used by Config, Edit (optional) and Console (required)   */
     config: {                       /* config data (optional)                                   */
-      root: homeDir(),              /* set the root path to the logged in user's HOME           */
-      prefix: PREFIX,               /* base URL or function which returns base URL (optional)   */
+        root: homeDir(),            /* set the root path to the logged in user's HOME           */
+        prefix: PREFIX,             /* base URL or function which returns base URL (optional)   */
     }
 }));
 
