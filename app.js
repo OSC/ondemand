0f3980db97c0ed13e9e4ae7bc21a57b39fe99da9
Brian McMichael <bmcmichael@osc.edu>
2016-03-02 12:57:14 -0500

    use static home path

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ var http        = require('http'),
     cloudcmd    = require('cloudcmd'),
     express     = require('express'),
     io          = require('socket.io'),
-    homeDir     = require('os-homedir'),
+    HOME        = require('os-homedir')(),
     app         = express(),
     PORT        = 9001,
-    PREFIX      = '/pun/dev/cloudcmd',          /* FIXME: Find a way to make this dynamic based on the system
+    PREFIX      = '/pun/dev/cloudcmd',
+                                                /* FIXME: Find a way to make this dynamic based on the system
                                                  *        This needs to be updated to ex: 
                                                  *        '/pun/shared/bmcmichael/cloudcmd',
                                                  *        when used on the shared environment.
@@ -14,17 +15,18 @@ var http        = require('http'),
     socket;
 
 server = http.createServer(app);
+
 socket = io.listen(server, {
-  path: PREFIX + '/socket.io'
+    path: PREFIX + '/socket.io'
 });
 
 app.use(cloudcmd({
-    socket: socket,                 /* used by Config, Edit (optional) and Console (required)   */
-    config: {                       /* config data (optional)                                   */
-        root: homeDir(),            /* set the root path to the logged in user's HOME           */
-        prefix: PREFIX,             /* base URL or function which returns base URL (optional)   */
+    socket: socket,                   /* used by Config, Edit (optional) and Console (required)   */
+    config: {                         /* config data (optional)                                   */
+        auth: false,                  /* this is the default setting, but using it here to reset  */
+        root: HOME,                   /* set the root path to the logged in user's HOME           */
+        prefix: PREFIX,               /* base URL or function which returns base URL (optional)   */
     }
 }));
-
 
 server.listen(PORT);
