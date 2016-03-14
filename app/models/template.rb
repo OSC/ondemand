a997271ea932131c4d23db56c8615b4c6d085072
Brian McMichael <bmcmichael@osc.edu>
2016-03-14 16:05:49 -0400

    Add a path builder for the file manager

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,2 +1,15 @@
 class Template < ActiveRecord::Base
+
+  # If the template exists but has no id, treat is as one of the system templates.
+  def system?
+    id.nil?
+  end
+
+  # Provide the http path to the file manager
+  def file_manager_path
+    # Use File.join because URI.join does not respect relative urls
+    # TODO: Refactor FileManager into an initializer or helper class.
+    #       This will be used elsewhere and often.
+    File.join(FileManager[:fs], path)
+  end
 end
