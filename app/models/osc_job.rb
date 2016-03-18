5e121b7d52fd09e02485015448fbcb7ea4795e0b
Brian McMichael <bmcmichael@osc.edu>
2016-03-18 15:11:35 -0400

    Use the staged script name for build_job

--- app/models/osc_job.rb
+++ app/models/osc_job.rb
@@ -8,8 +8,13 @@ class OscJob < ActiveRecord::Base
     "osc_job"
   end
 
+  def staged_script_name
+    # TODO allow set this to something else
+    File.basename(self.script_path)
+  end
+
   def staging_template_dir
-      File.dirname(self.script_path)
+    File.dirname(self.script_path)
   end
 
   # Define tasks to do after staging template directory typically copy over
@@ -20,11 +25,12 @@ class OscJob < ActiveRecord::Base
 
   # Build an array of Machete jobs that are then submitted to the batch server
   def build_jobs(staged_dir, job_list = [])
-    job_list << OSC::Machete::Job.new(script: staged_dir.join("main.sh"))
+    job_list << OSC::Machete::Job.new(script: staged_dir.join(staged_script_name))
   end
 
   # Make copy of workflow
   def copy
     self.dup
   end
+  
 end
