2198ba4869edbeca8863f69e666e74165f60217b
Eric Franz <efranz@osc.edu>
2016-03-22 12:46:24 -0400

    generate dashboard controller
        bin/rails g controller dashboard index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Dashboard::Application.routes.draw do
+  get "dashboard/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
