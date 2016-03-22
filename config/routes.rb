077828036eb2f314efef5db2c20a0369c663e878
Eric Franz <efranz@osc.edu>
2016-03-22 12:48:17 -0400

    set root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Dashboard::Application.routes.draw do
   get "dashboard/index"
+  root "dashboard#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
