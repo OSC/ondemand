0c0f0d44faa3ad3e11743b99bf6e4e59244ba1b9
Eric Franz <efranz@osc.edu>
2016-03-22 13:36:05 -0400

    add static top menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
+// require dataTables/jquery.dataTables
+// require dataTables/bootstrap/3/jquery.dataTables.bootstrap
 //= require_tree .
