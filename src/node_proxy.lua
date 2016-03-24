1e114ab89b2d0ef54578a4ca1107fefaffceed66
Jeremy Nicklas <jnicklas@osc.edu>
2016-03-24 15:51:45 -0400

    added support for proxying to backend compute nodes with suburi

---
+++ src/node_proxy.lua
@@ -0,0 +1,35 @@
+require 'user_map'
+require 'proxy_helpers'
+
+--[[
+  node_proxy_handler
+
+  Maps an authenticated user to a system user. Then proxies user's traffic to a
+  backend node with the host and port specified in the request URI.
+--]]
+function node_proxy_handler(r)
+  -- read in OOD specific settings defined in Apache config
+  local user_map_cmd = r.subprocess_env['OOD_USER_MAP_CMD']
+  local node_uri     = r.subprocess_env['OOD_NODE_URI']
+
+  -- get the system-level user name
+  local user = user_map(r, user_map_cmd)
+
+  -- get the host & port of webserver on backend node from request
+  local host, port = r.uri:match("^" .. node_uri .. "/([^/]+)/([^/]+)")
+
+  -- generate connection object used in setting the reverse proxy
+  local conn = {}
+  conn.user = user
+  conn.server = host .. ":" .. port
+  conn.uri = r.unparsed_uri
+
+  -- setup request for reverse proxy
+  set_reverse_proxy(r, conn)
+
+  -- handle if backend server is down
+  r:custom_response(503, "Failed to connect to " .. conn.server)
+
+  -- let the proxy handler do this instead
+  return apache2.DECLINED
+end
