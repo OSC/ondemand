73834838b0608291582cf09302904cf7f166062b
Brian McMichael <bmcmichael@osc.edu>
2016-03-31 11:07:38 -0400

    add source.rb

---
+++ app/models/source.rb
@@ -0,0 +1,27 @@
+class Source
+  attr_accessor :path, :name
+
+  def initialize(name, path)
+    @name = name
+    @path = path
+  end
+
+  def self.osc
+    Source.new("OSC's Templates", "/nfs/01/wiag/PZS0645/ood/jobconstructor/templates")
+  end
+
+  def self.my
+    Source.new("My Templates", AwesimRails.dataroot.join("templates").to_s)
+  end
+
+  def templates
+    return [] unless Pathname.new(path).directory?
+
+    folders = Dir.entries(path)
+    # Remove "." and ".."
+    folders.shift(2)
+
+    # create a template for each folder
+    folders.map {|f| Template.new(Pathname.new(path).join(f)) }
+  end
+end
