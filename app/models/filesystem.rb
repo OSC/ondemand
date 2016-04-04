73341c52f94cac78d61eebc6918634fb5d31143f
Brian McMichael <bmcmichael@osc.edu>
2016-04-04 10:16:47 -0400

    add regex check against filesystem

--- app/models/filesystem.rb
+++ app/models/filesystem.rb
@@ -1,15 +1,28 @@
 class Filesystem
 
+  # TODO Move this to a config file?
+  # /nfs/07 => false
+  # /nfs/gpfs/UNAME/template => true
+  # /nfs/08/bmcmichael/ood_templ/5/ => true
+  BASE_PATTERN = /\/nfs\/([0-9]{2}|gpfs)\/\w+\//
+
   def initialize
     @fs = FileManager[:fs]
     @api = FileManager[:api]
   end
 
+  # Returns an http URI path to the cloudcmd filesystem link
   def fs(path)
     File.join(@fs, path)
   end
 
+  # Returns an http URI path to the cloudcmd api link
   def api(path)
     File.join(@api, path)
+  end
+
+  # Matches a pathname on the system to prevent root file system copies.
+  def safe_path? (path)
+    path =~ BASE_PATTERN ? true : false
   end
 end
