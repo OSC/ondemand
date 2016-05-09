8a1d5b9cf79546a82f3fa6d3ee874adcbd747803
Brian McMichael <bmcmichael@osc.edu>
2016-05-09 14:29:20 -0400

    add detailed comment to the cachebusting solution

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ socket = io.listen(server, {
     path: BASE_URI + '/socket.io'
 });
 
+// Disable browser-side caching of assets by injecting expiry headers into all requests
+// Since the caching is being performed in the browser, we set several headers to
+// get the client to respect our intentions.
 app.use(function(req, res, next) {
     res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
     res.header('Expires', '-1');
