c335e023c2f49e5400c3ec751143f6108748d50b
Brian McMichael <bmcmichael@osc.edu>
2016-05-17 11:14:38 -0400

    display file content or directory contents or error message

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,18 @@ class PagesController < ApplicationController
 
   def index
     @editor_content = ""
-    @path = params[:path]
-    @path += params.has_key?(:format) ? ".#{params[:format]}" : ""
-
-    @editor_content = @path
+    path = "/#{params[:path]}"
+    path += params.has_key?(:format) ? ".#{params[:format]}" : ""
+    @pathname = Pathname.new(path)
+    if @pathname.file? && @pathname.readable?
+      @editor_content = @pathname.read
+    elsif @pathname.directory?
+      # TODO make these into links and display a separate view
+      @editor_content = Pathname.glob(@pathname + "*")
+    else
+      # TODO make an error page
+      @editor_content = "Unreadable location!"
+    end
   end
 
   def about
