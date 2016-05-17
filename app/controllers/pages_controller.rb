971799cc7928209184e9b81de4765be5c3557fe0
Brian McMichael <bmcmichael@osc.edu>
2016-05-17 11:49:47 -0400

    get the apiurl and send it to the view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,12 +4,13 @@ class PagesController < ApplicationController
     @editor_content = ""
     path = "/#{params[:path]}"
     path += params.has_key?(:format) ? ".#{params[:format]}" : ""
-    @pathname = Pathname.new(path)
-    if @pathname.file? && @pathname.readable?
-      @editor_content = @pathname.read
-    elsif @pathname.directory?
+    pathname = Pathname.new(path)
+    if pathname.file? && pathname.readable?
+      @editor_content = pathname.read
+      @file_api_url = OodApp.files.api(path: pathname)
+    elsif pathname.directory?
       # TODO make these into links and display a separate view
-      @editor_content = Pathname.glob(@pathname + "*")
+      @editor_content = Pathname.glob(pathname + "*")
     else
       # TODO make an error page
       @editor_content = "Unreadable location!"
