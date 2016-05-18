4da1cd824fb683d96faf0f9c737ffcf230f3db94
Brian McMichael <bmcmichael@osc.edu>
2016-05-18 12:25:26 -0400

    Make pathname available in view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,12 +4,12 @@ class PagesController < ApplicationController
     path = params[:path] || "/"
     path = "/" + path unless path.start_with?("/")
 
-    pathname = Pathname.new(path)
-    if pathname.file? && pathname.readable?
+    @pathname = Pathname.new(path)
+    if @pathname.file? && @pathname.readable?
       @editor_content = ""
-      @file_api_url = OodApp.files.api(path: pathname)
-    elsif pathname.directory?
-      @directory_content = Dir.glob(pathname + "*").sort
+      @file_api_url = OodApp.files.api(path: @pathname)
+    elsif @pathname.directory?
+      @directory_content = Dir.glob(@pathname + "*").sort
       @file_edit_url = Pathname.new(ENV['RAILS_RELATIVE_URL_ROOT']).join('edit')
     else
       @directory_content = Dir.glob(ENV['HOME'] + "/*").sort
