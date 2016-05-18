9aecd441e36bf141cab8bc5ffaabe1dfe79ad7ea
Brian McMichael <bmcmichael@osc.edu>
2016-05-18 11:47:53 -0400

    fix the HOME glob on default

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,9 @@
 class PagesController < ApplicationController
 
   def index
-
     path = params[:path] || "/"
     path = "/" + path unless path.start_with?("/")
 
-    # path = "/#{params[:path]}"
-    # path += params.has_key?(:format) ? ".#{params[:format]}" : ""
     pathname = Pathname.new(path)
     if pathname.file? && pathname.readable?
       @editor_content = ""
@@ -15,7 +12,7 @@ class PagesController < ApplicationController
       @directory_content = Dir.glob(pathname + "*").sort
       @file_edit_url = Pathname.new(ENV['RAILS_RELATIVE_URL_ROOT']).join('edit')
     else
-      @directory_content = Dir.glob(ENV['HOME'] + "*").sort
+      @directory_content = Dir.glob(ENV['HOME'] + "/*").sort
       @file_edit_url = Pathname.new(ENV['RAILS_RELATIVE_URL_ROOT']).join('edit')
     end
 
