913323496391e34c2ad5243535475981a9d24f30
Brian McMichael <bmcmichael@osc.edu>
2016-05-26 15:39:45 -0400

    do not attempt to load ace-specific code if the editor element is not present

--- app/assets/javascripts/ood_ace.js
+++ app/assets/javascripts/ood_ace.js
@@ -1,91 +1,95 @@
 $( document ).ready(function () {
 
-    // Initialize the ace editor
-    var editor = ace.edit("editor");
-    editor.setTheme( $( "#theme option:selected" ).val() );
-    editor.session.setMode( "ace/mode/" + $( "#mode option:selected" ).val() );
+    // Do not load the ace editor if the class is not available
+    // ex. for directory views
+    if ( $( "#editor" ).length ) {
 
-    // Load the file via ajax
-    var loadedContent = $.ajax({
-        url: apiUrl,
-        type: 'GET',
-        success: function (data) {
-            editorContent = data;
-            editor.setValue(editorContent, -1);
-        },
-        error: function (request, status, error) {
-            alert("An error occured attempting to load this file!\n" + error);
-        }
-    });
-
-    // Disables/enables the save button
-    editor.on("change", function () {
-        $( "#save-button" ).prop("disabled", editor.session.getUndoManager().isClean());
-    });
-
-    // Change the font size
-    $( "#fontsize" ).change(function() {
-        editor.setFontSize( $( "#fontsize option:selected" ).val() );
-        // TODO Save setting to cookie
-    });
-
-    // Change the key bindings
-    $( "#keybindings" ).change(function() {
-        editor.setKeyboardHandler( "ace/keyboard/" + $( "#keybindings option:selected" ).val() );
-        // TODO Save setting to cookie
-    });
-
-    // Change the theme
-    $( "#theme" ).change(function() {
+        // Initialize the ace editor
+        var editor = ace.edit("editor");
         editor.setTheme( $( "#theme option:selected" ).val() );
-        // TODO Save setting to cookie
-    });
+        editor.session.setMode( "ace/mode/" + $( "#mode option:selected" ).val() );
 
-    // Change the mode
-    $( "#mode" ).change(function() {
-        editor.getSession().setMode( "ace/mode/" + $( "#mode option:selected" ).val() );
-        // TODO Save setting to cookie
-    });
+        // Load the file via ajax
+        var loadedContent = $.ajax({
+            url: apiUrl,
+            type: 'GET',
+            success: function (data) {
+                editorContent = data;
+                editor.setValue(editorContent, -1);
+            },
+            error: function (request, status, error) {
+                alert("An error occured attempting to load this file!\n" + error);
+            }
+        });
 
-    // Change the word wrap setting
-    $( "#wordwrap" ).change(function() {
-        editor.getSession().setUseWrapMode(this.checked);
-        // TODO Save setting to cookie
-    });
+        // Disables/enables the save button
+        editor.on("change", function () {
+            $( "#save-button" ).prop("disabled", editor.session.getUndoManager().isClean());
+        });
 
-    // Save button onclick handler
-    // sends the content to the cloudcmd api via PUT request
-    $( "#save-button" ).click(function() {
-        if (apiUrl !== "") {
-            $.ajax({
-                url: apiUrl,
-                type: 'PUT',
-                data: editor.getValue(),
-                success: function (data) {
-                    $( "#save-button" ).fadeOut(function() {
-                        $( "#save-button" ).html('<span class="glyphicon glyphicon-saved" aria-hidden="true"></span> Saved!').fadeIn();
-                        setTimeout(function() {
-                            $( "#save-button" ).fadeOut(function() {
-                                $("#save-button").html('<span class="glyphicon glyphicon-save" aria-hidden="true"></span> Save').fadeIn();
-                            })
-                        }, 1000);
-                    });
-                    editor.session.getUndoManager().markClean();
-                    $( "#save-button" ).prop("disabled", editor.session.getUndoManager().isClean());
-                },
-                error: function (request, status, error) {
-                    alert("An error occured attempting to save this file!\n" + error);
-                }
-            })
-        } else {
-            console.log("Can't save this!");
-        }
-    });
+        // Change the font size
+        $( "#fontsize" ).change(function() {
+            editor.setFontSize( $( "#fontsize option:selected" ).val() );
+            // TODO Save setting to cookie
+        });
+
+        // Change the key bindings
+        $( "#keybindings" ).change(function() {
+            editor.setKeyboardHandler( "ace/keyboard/" + $( "#keybindings option:selected" ).val() );
+            // TODO Save setting to cookie
+        });
+
+        // Change the theme
+        $( "#theme" ).change(function() {
+            editor.setTheme( $( "#theme option:selected" ).val() );
+            // TODO Save setting to cookie
+        });
+
+        // Change the mode
+        $( "#mode" ).change(function() {
+            editor.getSession().setMode( "ace/mode/" + $( "#mode option:selected" ).val() );
+            // TODO Save setting to cookie
+        });
+
+        // Change the word wrap setting
+        $( "#wordwrap" ).change(function() {
+            editor.getSession().setUseWrapMode(this.checked);
+            // TODO Save setting to cookie
+        });
+
+        // Save button onclick handler
+        // sends the content to the cloudcmd api via PUT request
+        $( "#save-button" ).click(function() {
+            if (apiUrl !== "") {
+                $.ajax({
+                    url: apiUrl,
+                    type: 'PUT',
+                    data: editor.getValue(),
+                    success: function (data) {
+                        $( "#save-button" ).fadeOut(function() {
+                            $( "#save-button" ).html('<span class="glyphicon glyphicon-saved" aria-hidden="true"></span> Saved!').fadeIn();
+                            setTimeout(function() {
+                                $( "#save-button" ).fadeOut(function() {
+                                    $("#save-button").html('<span class="glyphicon glyphicon-save" aria-hidden="true"></span> Save').fadeIn();
+                                })
+                            }, 1000);
+                        });
+                        editor.session.getUndoManager().markClean();
+                        $( "#save-button" ).prop("disabled", editor.session.getUndoManager().isClean());
+                    },
+                    error: function (request, status, error) {
+                        alert("An error occured attempting to save this file!\n" + error);
+                    }
+                })
+            } else {
+                console.log("Can't save this!");
+            }
+        });
+    }
 
     // Disable the save button after the initial load
     // Modifying settings makes the UndoManager "dirty"
     // so we have to explicitly re-disable it.
     $( "#save-button" ).prop("disabled", true);
-
-})
+});
 
