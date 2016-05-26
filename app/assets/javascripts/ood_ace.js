e778edc3c918e67ba70e3329eb6e13d069c608f7
Brian McMichael <bmcmichael@osc.edu>
2016-05-26 12:53:31 -0400

    move all js event handlers to document.ready function

--- app/assets/javascripts/ood_ace.js
+++ app/assets/javascripts/ood_ace.js
@@ -1,57 +1,62 @@
-// Change the font size
-$( "#fontsize" ).change(function() {
-    editor.setFontSize( $( "#fontsize option:selected" ).val() );
-    // TODO Save setting to cookie
-});
+$( document ).ready(function () {
 
-// Change the key bindings
-$( "#keybindings" ).change(function() {
-    editor.setKeyboardHandler( "ace/keyboard/" + $( "#keybindings option:selected" ).val() );
-    // TODO Save setting to cookie
-});
+    // Change the font size
+    $( "#fontsize" ).change(function() {
+        editor.setFontSize( $( "#fontsize option:selected" ).val() );
+        // TODO Save setting to cookie
+    });
 
-$( "#theme" ).change(function() {
-    editor.setTheme( $( "#theme option:selected" ).val() );
-    // TODO Save setting to cookie
-});
+    // Change the key bindings
+    $( "#keybindings" ).change(function() {
+        editor.setKeyboardHandler( "ace/keyboard/" + $( "#keybindings option:selected" ).val() );
+        // TODO Save setting to cookie
+    });
 
-$( "#mode" ).change(function() {
-    editor.getSession().setMode( "ace/mode/" + $( "#mode option:selected" ).val() );
-    // TODO Save setting to cookie
-});
+    $( "#theme" ).change(function() {
+        editor.setTheme( $( "#theme option:selected" ).val() );
+        // TODO Save setting to cookie
+    });
 
-$( "#wordwrap" ).change(function() {
-    editor.getSession().setUseWrapMode(this.checked);
-    // TODO Save setting to cookie
-});
+    $( "#mode" ).change(function() {
+        editor.getSession().setMode( "ace/mode/" + $( "#mode option:selected" ).val() );
+        // TODO Save setting to cookie
+    });
 
-$( "#reset-button" ).click(function() {
-    editor.setValue(editorContent, -1);
-});
+    $( "#wordwrap" ).change(function() {
+        editor.getSession().setUseWrapMode(this.checked);
+        // TODO Save setting to cookie
+    });
 
-$( "#save-button" ).click(function() {
-    if (apiUrl !== "") {
-        $.ajax({
-            url: apiUrl,
-            type: 'PUT',
-            data: editor.getValue(),
-            success: function (data) {
-                $( "#save-button" ).fadeOut(function() {
-                    $( "#save-button" ).html('<span class="glyphicon glyphicon-saved" aria-hidden="true"></span> Saved!').fadeIn();
-                    setTimeout(function() {
-                        $( "#save-button" ).fadeOut(function() {
-                            $("#save-button").html('<span class="glyphicon glyphicon-save" aria-hidden="true"></span> Save').fadeIn();
-                        })
-                    }, 2000);
-                });
+    $( "#reset-button" ).click(function() {
+        editor.setValue(editorContent, -1);
+    });
+
+    $( "#save-button" ).click(function() {
+        if (apiUrl !== "") {
+            $.ajax({
+                url: apiUrl,
+                type: 'PUT',
+                data: editor.getValue(),
+                success: function (data) {
+                    $( "#save-button" ).fadeOut(function() {
+                        $( "#save-button" ).html('<span class="glyphicon glyphicon-saved" aria-hidden="true"></span> Saved!').fadeIn();
+                        setTimeout(function() {
+                            $( "#save-button" ).fadeOut(function() {
+                                $("#save-button").html('<span class="glyphicon glyphicon-save" aria-hidden="true"></span> Save').fadeIn();
+                            })
+                        }, 2000);
+                    });
 
 
-            },
-            error: function (request, status, error) {
-                alert("An error occured attempting to save this file!\n" + error);
-            }
-        })
-    } else {
-        console.log("Can't save this!");
-    }
-});
+                },
+                error: function (request, status, error) {
+                    alert("An error occured attempting to save this file!\n" + error);
+                }
+            })
+        } else {
+            console.log("Can't save this!");
+        }
+    });
+
+})
+
