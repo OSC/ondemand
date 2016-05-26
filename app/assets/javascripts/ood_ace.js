d6ef105261b98d323919dc66d9df17f6ec7d0b6a
Brian McMichael <bmcmichael@osc.edu>
2016-05-26 15:29:49 -0400

    move editor code to onload, disable save on load, enable on edit

--- app/assets/javascripts/ood_ace.js
+++ app/assets/javascripts/ood_ace.js
@@ -1,5 +1,26 @@
 $( document ).ready(function () {
 
+    var editor = ace.edit("editor");
+    editor.setTheme( $( "#theme option:selected" ).val() );
+    editor.session.setMode( "ace/mode/" + $( "#mode option:selected" ).val() );
+
+    var loadedContent = $.ajax({
+        url: apiUrl,
+        type: 'GET',
+        success: function (data) {
+            editorContent = data;
+            editor.setValue(editorContent, -1);
+        },
+        error: function (request, status, error) {
+            alert("An error occured attempting to load this file!\n" + error);
+        }
+    });
+
+    // Disables/enables the save button
+    editor.on("change", function () {
+        $( "#save-button" ).prop("disabled", editor.session.getUndoManager().isClean());
+    });
+
     // Change the font size
     $( "#fontsize" ).change(function() {
         editor.setFontSize( $( "#fontsize option:selected" ).val() );
@@ -44,10 +65,10 @@ $( document ).ready(function () {
                             $( "#save-button" ).fadeOut(function() {
                                 $("#save-button").html('<span class="glyphicon glyphicon-save" aria-hidden="true"></span> Save').fadeIn();
                             })
-                        }, 2000);
+                        }, 1000);
                     });
-
-
+                    editor.session.getUndoManager().markClean();
+                    $( "#save-button" ).prop("disabled", editor.session.getUndoManager().isClean());
                 },
                 error: function (request, status, error) {
                     alert("An error occured attempting to save this file!\n" + error);
@@ -57,6 +78,8 @@ $( document ).ready(function () {
             console.log("Can't save this!");
         }
     });
+
+    $( "#save-button" ).prop("disabled", true);
 
 })
 
