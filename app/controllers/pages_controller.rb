898cc5b4dbfa95f922b4deb986c204ad12bf4863
Brian McMichael <bmcmichael@osc.edu>
2016-05-26 16:52:35 -0400

    add a notification that the file is not editable if it is not a plain text or empty file

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,13 +6,20 @@ class PagesController < ApplicationController
 
     @pathname = Pathname.new(path)
     if @pathname.file? && @pathname.readable?
-      @editor_content = ""
-      @file_api_url = OodApp.files.api(path: @pathname)
+      fileinfo = %x[ file -i #{@pathname} ]
+      if fileinfo.include?("text/plain") || fileinfo.include?("application/x-empty")
+        @editor_content = ""
+        @file_api_url = OodApp.files.api(path: @pathname)
+      else
+        @invalid_file_type = fileinfo
+        render status: 404
+      end
     elsif @pathname.directory?
       @directory_content = Dir.glob(@pathname + "*").sort
       @file_edit_url = Pathname.new(ENV['RAILS_RELATIVE_URL_ROOT']).join('edit')
     else
       @not_found = true
+      render status: 404
     end
 
   end
