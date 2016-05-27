e28e8a2730ec14eb8e59bcdbf739e2e45afa73cb
Brian McMichael <bmcmichael@osc.edu>
2016-05-27 14:07:29 -0400

    save settings on user change, add defaults, load settings on reload

--- app/assets/javascripts/ood_ace.js
+++ app/assets/javascripts/ood_ace.js
@@ -5,8 +5,7 @@ $( document ).ready(function () {
     if ( $( "#editor" ).length ) {
         // Initialize the ace editor
         var editor = ace.edit("editor");
-        editor.setTheme( $( "#theme option:selected" ).val() );
-        editor.session.setMode( "ace/mode/" + $( "#mode option:selected" ).val() );
+        setOptions();
         $( "#loading-notice" ).toggle();
         var loading = true;
         // Load the file via ajax
@@ -56,31 +55,31 @@ $( document ).ready(function () {
         // Change the font size
         $( "#fontsize" ).change(function() {
             editor.setFontSize( $( "#fontsize option:selected" ).val() );
-            // TODO Save setting to cookie
+            setCookie( 'fontsize', $( "#fontsize option:selected" ).val(), 9999 );
         });
 
         // Change the key bindings
         $( "#keybindings" ).change(function() {
             editor.setKeyboardHandler( "ace/keyboard/" + $( "#keybindings option:selected" ).val() );
-            // TODO Save setting to cookie
+            setCookie( 'keybindings', $( "#keybindings option:selected" ).val(), 9999 );
         });
 
         // Change the theme
         $( "#theme" ).change(function() {
             editor.setTheme( $( "#theme option:selected" ).val() );
-            // TODO Save setting to cookie
+            setCookie( 'theme', $( "#theme option:selected" ).val(), 9999 );
         });
 
         // Change the mode
         $( "#mode" ).change(function() {
             editor.getSession().setMode( "ace/mode/" + $( "#mode option:selected" ).val() );
-            // TODO Save setting to cookie
+            setCookie( 'mode', $( "#mode option:selected" ).val(), 9999 );
         });
 
         // Change the word wrap setting
         $( "#wordwrap" ).change(function() {
             editor.getSession().setUseWrapMode(this.checked);
-            // TODO Save setting to cookie
+            setCookie( 'wordwrap', $( "#wordwrap" ).is(':checked'), 9999 );
         });
 
         // Save button onclick handler
@@ -113,6 +112,19 @@ $( document ).ready(function () {
                 console.log("Can't save this!");
             }
         });
+
+        function setOptions() {
+            $( "#keybindings" ).val(getCookie('keybindings') || 'ace');
+            editor.setKeyboardHandler( "ace/keyboard/" + $( "#keybindings option:selected" ).val() );
+            $( "#fontsize" ).val(getCookie('fontsize') || '12px');
+            editor.setFontSize( $( "#fontsize option:selected" ).val() );
+            $( "#mode" ).val(getCookie('mode') || "text");
+            editor.session.setMode( "ace/mode/" + $( "#mode option:selected" ).val() );
+            $( "#theme" ).val(getCookie('theme') || "ace/theme/solarized_light");
+            editor.setTheme( $( "#theme option:selected" ).val() );
+            $( "#wordwrap" ).prop("checked", getCookie('wordwrap') === "true");
+            editor.getSession().setUseWrapMode( $( "#wordwrap" ).is(':checked'));
+        }
 
         // Mark the editor as clean after load.
         editor.session.getUndoManager().markClean();
