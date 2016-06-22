3ea7a2f96b5ac38bdeb12ba0c4950d7a1403d408
Eric Franz <efranz@osc.edu>
2016-06-22 13:57:33 -0400

    fix logout url for websvcs08 and websvcs09
    the websvcs08 logout url is /oidc/ whereas the websvcs09 is /oidc
    the trailing slash is no longer used

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :set_user
+  before_filter :set_user, :set_logout_url
 
   def set_user
     @user = User.new
+  end
+
+  def set_logout_url
+    @logout_url = "/oidc?logout="
+
+    #FIXME: HACK - delete when no longer using websvcs08
+    @logout_url = "/oidc/?logout=" if request.base_url =~ /websvcs08\.osc\.edu/
+    #end HACK
+
+    @logout_url = @logout_url + ERB::Util.u(request.base_url)
   end
 end
