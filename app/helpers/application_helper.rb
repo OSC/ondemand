dfccf7166ba69d6b623414af8e0cc50e50b27fd4
Eric Franz <efranz@osc.edu>
2016-07-14 22:21:03 -0400

    set help urls using env

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def support_url
+    ENV['OOD_DASHBOARD_SUPPORT_URL'] || "#"
+  end
+
+  def docs_url
+    ENV['OOD_DASHBOARD_DOCS_URL'] || "#"
+  end
+
+  def passwd_url
+    ENV['OOD_DASHBOARD_PASSWD_URL'] || "#"
+  end
 end
