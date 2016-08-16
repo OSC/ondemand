ccc488b5c9d39d082c7bee97c975750df32d7cfb
Brian McMichael <bmcmichael@osc.edu>
2016-08-16 11:04:42 -0400

    code comments

--- app/models/source.rb
+++ app/models/source.rb
@@ -1,27 +1,35 @@
 class Source
   attr_accessor :path, :name
 
+  # Constructor
+  # @param [String] name the human readable name of the source
+  # @param [String] path the path to the source
   def initialize(name, path)
     @name = name
     @path = path
   end
 
+  # @return [Source] A source that has been initialized to the system path.
   def self.system
     Source.new("System Templates", Rails.root.join('templates').to_s)
   end
 
+  # @return [Source] A source that has been initialized to the user template path.
   def self.my
     Source.new("My Templates", OodAppkit.dataroot.join("templates").to_s)
   end
 
+  # @return [Source] A source that has been initialized to the default path.
   def self.default
     Source.new("Default Templates", Rails.root.join('templates').join("default").to_s)
   end
 
+  # @return [Template] The default template.
   def self.default_template
     Source.default.templates.first
   end
 
+  # @return [Array<Template>] The templates available on the path.
   def templates
     return [] unless Pathname.new(path).directory?
 
