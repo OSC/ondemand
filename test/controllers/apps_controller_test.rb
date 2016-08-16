138964677f8758d92756764a44a134b54925628e
Eric Franz <efranz@osc.edu>
2016-08-16 15:56:10 -0400

    add FIXME comment about writing test in controller

--- test/controllers/apps_controller_test.rb
+++ test/controllers/apps_controller_test.rb
@@ -1,4 +1,26 @@
 require 'test_helper'
 
 class AppsControllerTest < ActionController::TestCase
+  test "show sys app" do
+    #FIXME:
+    # When I do
+    #
+    #    app_url
+    #
+    # I rightfully get error: ActionController::UrlGenerationError: No route
+    # matches {:type=>:sys, :controller=>"apps", :action=>"show"} missing
+    # required keys: [:name]
+    #
+    # When I do
+    #
+    #    app_url(:myjobs)
+    #
+    # I get this error: ActionController::UrlGenerationError: No route matches
+    # {:controller=>"apps", :action=>"/apps/show/myjobs"}
+    #
+    # This prevents me from adding tests for these route helpers.
+
+    # get app_path(:activejobs)
+    # assert_redirected_to "/pun/sys/activejobs"
+  end
 end
