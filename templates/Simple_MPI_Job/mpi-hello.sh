d8582c4130bccfb8757fc6c886bf485e1f87c756
Brian McMichael <bmcmichael@osc.edu>
2016-08-25 11:32:34 -0400

    update to use tmpdir

--- templates/Simple_MPI_Job/mpi-hello.sh
+++ templates/Simple_MPI_Job/mpi-hello.sh
@@ -3,6 +3,11 @@
 #PBS -l nodes=4:ppn=12
 #PBS -j oe
 
-cd $PBS_O_WORKDIR
+cp $PBS_O_WORKDIR/* $TMPDIR
+cd $TMPDIR
+
 mpicc -O2 mpi-hello.c -o mpi-hello
+
 mpiexec ./mpi-hello
+
+cp $TMPDIR/* $PBS_O_WORKDIR
