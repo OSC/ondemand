df0d4a1c6b77b9615c558adf4f491c6a1b87e4db
Eric Franz <efranz@osc.edu>
2016-09-08 10:00:40 -0400

    remove IE11 from supported browsers

--- config/initializers/browser.rb
+++ config/initializers/browser.rb
@@ -14,7 +14,6 @@ Browser.modern_rules.tap do |rules|
   rules << -> (b) { b.safari? && b.version.to_i >= 8 }
   rules << -> (b) { b.webkit? && !b.chrome? && !b.safari? }
   rules << -> (b) { b.firefox? && b.version.to_i >= 19  }
-  rules << -> (b) { b.ie? && b.version.to_i >= 11 && !b.compatibility_view?  }
   rules << -> (b) { b.edge? && !b.compatibility_view?  }
   rules << -> (b) { b.opera? && b.version.to_i >= 12  }
   rules << -> (b) { b.firefox? && b.device.tablet? && b.platform.android? && b.version.to_i >= 14  } # rubocop:disable Metrics/LineLength
