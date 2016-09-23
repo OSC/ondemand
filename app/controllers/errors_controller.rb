61c08839c043e35e81d73784bec22c3b7f44430a
Eric Franz <efranz@osc.edu>
2016-09-23 14:46:41 -0400

    get rid of duplicate layout for errors

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -9,14 +9,17 @@ class ErrorsController < ApplicationController
   # the routes.rb is set to the app to handle exceptions in application.rb
   #
   def not_found
-    render status: 404, layout: "application"
+    render status: 404
   end
 
-  # for 500 errors we use the simpler layout with the application links missing
-  # because that is one likely area where some complexity in building those
-  # links could result in an exception
   def internal_server_error
     @exception  = env['action_dispatch.exception']
+
+
+    # FIXME: a better solution exists than this
+    # avoid rendering nav in case those introduce exceptions
+    @hidenav = true
+
     #TODO: log exception information you want to log here
     # after removing it from the normal logging via lograge
     render status: 500
