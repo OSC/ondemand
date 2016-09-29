50c11467564b893222c6468bc8a446a0a1a0365d
Jeremy Nicklas <jnicklas@osc.edu>
2016-09-29 14:11:11 -0400

    replace code lost in migration

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,8 @@ module Dashboard
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Custom error pages
+    config.exceptions_app = self.routes
   end
 end
