822b1a40a92616e5d6d9374eb0cbc94c5f5de431
Jeremy Nicklas <jnicklas@osc.edu>
2016-09-30 12:00:39 -0400

    further namespace product routes

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   match "/404", :to => "errors#not_found", :via => :all
   match "/500", :to => "errors#internal_server_error", :via => :all
 
-  scope ':type' do
+  scope 'admin/:type' do
     resources :products, constraints: { type: /dev|usr/ } do
       member do
         get 'preview'
