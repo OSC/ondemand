2a8d3bdefef6d16fce372da1e75a27664f4e2c03
Jeremy Nicklas <jnicklas@osc.edu>
2016-10-07 14:45:19 -0400

    redirect to show page after successful creation

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -33,7 +33,7 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to products_url(type: @type), notice: 'Product was successfully created.' }
+        format.html { redirect_to product_url(@product.name, type: @type), notice: 'Product was successfully created.' }
         format.json { render :show, status: :created, location: @product }
       else
         format.html { render :new }
