264e1da239e65becfc76dddd3fa9c5d536f3fb60
Eric Franz <efranz@osc.edu>
2016-10-12 14:36:03 -0400

    fix breaking test

--- test/models/app_router_test.rb
+++ test/models/app_router_test.rb
@@ -5,12 +5,12 @@ class AppRouterTest < ActiveSupport::TestCase
   test "DevRouter.apps" do
     dir = Pathname.new(File.realpath(Dir.mktmpdir))
     DevRouter.stubs(:base_path).returns(dir)
-    %w(a b c d.git).each {|d| dir.join(d).mkdir }
+    %w(a b c .git).each {|d| dir.join(d).mkdir }
     FileUtils.touch dir.join("d.txt").to_s
     dir.join("c").chmod(0600)
     dir.join("d.txt").chmod(0700)
 
-    assert_equal ["a", "b"].sort, DevRouter.apps.map(&:name).sort
+    assert_equal ["a", "b"].sort, DevRouter.apps(require_manifest: false).map(&:name).sort
 
     dir.rmtree()
   end
