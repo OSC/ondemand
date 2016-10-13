bae8cbd8af1cd8464a6873fa36201f7cb89b1d47
Eric Franz <efranz@osc.edu>
2016-10-13 16:46:25 -0400

    fix path_router to impl expected router interface

--- app/apps/path_router.rb
+++ app/apps/path_router.rb
@@ -1,12 +1,17 @@
-# essentially a "null" object
-# useful for instatiating App objects
-# with a router injected into it
+# A special router to use to instantiate an OodApp
+# object if all you have is the path to the app
 class PathRouter
-  def url_for(app: app_name)
-    "#"
+  attr_reader :category, :caption, :url, :type, :path
+
+  def initialize(path)
+    @caption = nil
+    @category = "App"
+    @url = "#"
+    @type = :path
+    @path = Pathname.new(path)
   end
 
-  def path_for(app: app_name)
-    app
+  def owner
+    @owner ||= Etc.getpwuid(path.stat.uid).name
   end
 end
