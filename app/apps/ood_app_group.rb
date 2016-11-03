179aaf18e90ce8e2f1708b843336b9a55be4e427
Eric Franz <efranz@osc.edu>
2016-11-03 13:45:09 -0400

    add support for sorting and selecting OodAppGroups
    an OodAppGroup has an array of groups and a title and a method to group
    a list of OodApp objects by the group according to OodApp#category
    
    this commit adds ability to group by any OodApp attribute AND the
    class method OodAppGroup.select which, given an array of OodAppGroups, lets
    you select a subset of those groups by specifying an array of titles

--- app/apps/ood_app_group.rb
+++ app/apps/ood_app_group.rb
@@ -10,11 +10,26 @@ class OodAppGroup
     apps.count > 0
   end
 
-  def self.groups_for(apps: [])
+  # given an array of apps, group those apps by app category (or the attribute)
+  # specified by 'group_by', sorting both groups and apps arrays by title
+  def self.groups_for(apps: [], group_by: :category)
     apps.group_by { |app|
-      app.category
+      app.send(group_by)
     }.map { |k,v|
       OodAppGroup.new(title: k, apps: v.sort_by { |a| a.title })
     }.sort_by { |g| g.title }
+  end
+
+  # select a subset of groups by the specified array of titles
+  # maintaining the order specified by the titles array
+  #
+  # This way we can get a list of deployed sys apps, then group them by category
+  # then select only the categories we want to display in the menu
+  def self.select(titles:[], groups:[])
+    # only one group per title; this just makes it easy to get the group
+    # Hash[ [:title1,:group1], [:title2,:group2]] => {title1: :group1, title2: :group2 }
+    h = Hash[groups.map {|g| [g.title, g]}]
+
+    titles.map { |t| h.has_key?(t) ? h[t] : nil }.compact
   end
 end
