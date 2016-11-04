cb589e6f58f8de7a43d8d0c25b1c12ac9d9a1d90
Brian McMichael <bmcmichael@osc.edu>
2016-11-04 09:52:42 -0400

    replace map! with map

--- app/models/motd_file.rb
+++ app/models/motd_file.rb
@@ -47,7 +47,7 @@ class MotdFile
 
     # get array of sections which are delimited by a row of ******
     sections = f.split(/^\*+$/).map(&:strip).select { |x| ! x.empty?  }
-    sections.map! { |s| MotdFile::Message.from(s) }.compact.sort_by {|s| s.date }.reverse
+    sections.map { |s| MotdFile::Message.from(s) }.compact.sort_by {|s| s.date }.reverse
   rescue Errno::ENOENT
     # The messages file does not exist on the system.
     Rails.logger.warn "MOTD File is missing; it was expected at #{motd_system_file}"
