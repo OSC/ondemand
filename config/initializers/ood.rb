9dc2ec1b02d623516191bb85d96b2fea205bd3e6
Eric Franz <efranz@osc.edu>
2016-11-07 10:20:30 -0500

    specify nav categories from initializer

---
+++ config/initializers/ood.rb
@@ -0,0 +1,5 @@
+Rails.application.configure do
+  config.x.ood.nav_categories = ["Files", "Jobs", "Clusters", "Desktops"]
+  # config.x.ood.nav_categories = ["Files", "Clusters"]
+  # config.x.ood.nav_categories = []
+end
