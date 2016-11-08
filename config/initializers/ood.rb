6c5ce0b43876c2d07f70b63ba841c02b81402401
Eric Franz <efranz@osc.edu>
2016-11-08 13:03:05 -0500

    use site spec.config for nav options
    specifying files "favorites" or shortcut paths and nav categories for
    what to display in the dropdown move to site specific initializers
    
    defaults are set in the classes NavConfig and OodFilesApp

--- config/initializers/ood.rb
+++
@@ -1,2 +0,0 @@
-OodAppGroup.nav_categories = ENV['OOD_NAV_CATEGORIES'].split(",") if ENV['OOD_NAV_CATEGORIES']
-OodFilesApp.candidate_favorite_paths = OodFilesApp.new.paths_from_template(ENV['OOD_FILES_PATHS'], basename_filter: ENV['OOD_FILES_PATHS_BASENAME_FILTER'])
