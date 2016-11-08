f9ca1a6039498e7460f1b1b31a90369f9fcaff72
Eric Franz <efranz@osc.edu>
2016-11-07 23:05:02 -0500

    convert template to pathnames in app inititializer

--- app/apps/ood_files_app.rb
+++ app/apps/ood_files_app.rb
@@ -1,14 +1,27 @@
 # utility class for dealing with apps that play "files" role
 class OodFilesApp
+  class << self
+    # an array of Pathname objects to check for the existence of and access to
+    # should always be []
+    # set in config/initializers/ood.rb
+    attr_accessor :candidate_favorite_paths
+  end
+
+  # esure that [] is returned if class variable is not set
+  def candidate_favorite_paths
+    self.class.candidate_favorite_paths || []
+  end
+
   # when showing a link to the file explorer we always show
   # a link to the user's home directory
   # returns an array of other paths provided as shortcuts to the user
   def favorite_paths
-    @favorite_paths ||= paths_from_template(ENV['OOD_FILES_PATHS'], basename_filter: ENV['OOD_FILES_PATHS_BASENAME_FILTER']).select {|p|
+    @favorite_paths ||= candidate_favorite_paths.select {|p|
       p.directory? && p.readable? && p.executable?
     }
   end
 
+  # build an array of Pathname objects from the specified template string
   def paths_from_template(templates, basename_filter: nil )
     return [] if templates.nil?
 
--- config/initializers/ood.rb
+++ config/initializers/ood.rb
@@ -1,3 +1,5 @@
 Rails.application.configure do
   config.x.ood.nav_categories = ENV['OOD_NAV_CATEGORIES'] ? ENV['OOD_NAV_CATEGORIES'].split(",") : ["Files", "Jobs", "Clusters", "Desktops"]
 end
+
+OodFilesApp.candidate_favorite_paths = OodFilesApp.new.paths_from_template(ENV['OOD_FILES_PATHS'], basename_filter: ENV['OOD_FILES_PATHS_BASENAME_FILTER'])
