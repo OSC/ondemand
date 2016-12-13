b48fdc4aa198219bd48c1b62061231aed48bedaf
Eric Franz <efranz@osc.edu>
2016-12-13 03:55:23 -0500

    make dev dropdown visibility config option

--- app/apps/nav_config.rb
+++ app/apps/nav_config.rb
@@ -1,6 +1,8 @@
 class NavConfig
   class << self
     attr_accessor :categories
+    attr_accessor :show_develop_dropdown
   end
   self.categories = ["Files", "Jobs", "Clusters", "Desktops"]
+  self.show_develop_dropdown = ENV['OOD_APP_SHARING'].present?
 end
