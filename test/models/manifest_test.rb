a072254a91cbdca5e4ea7d31809e0aa01afd0456
Brian McMichael <bmcmichael@osc.edu>
2016-12-22 10:23:14 -0500

    more tests of manifest

--- test/models/manifest_test.rb
+++ test/models/manifest_test.rb
@@ -2,6 +2,11 @@ require 'test_helper'
 
 class ManifestTest < ActiveSupport::TestCase
 
+  @@hash1 = { :name => "brian", :description => "ginger", :role => "developer" }
+  @@hash2 = { "name" => "jeremy", "description" => nil, "role" => "monsignor", "url" => nil }
+  @@hash3 = { name: "eric", description: "senior developer", role: "visionary" }
+  @@hash4 = { name: nil, description: nil, role: nil, url: "http://127.0.0.1" }
+
   test "load a valid manifest" do
     manifest = Manifest.load("test/fixtures/files/manifest_valid")
     assert_instance_of Manifest, manifest, "Not a Manifest Object"
@@ -27,7 +32,7 @@ class ManifestTest < ActiveSupport::TestCase
     manifest = Manifest.load("test/fixtures/files/manifest_valid")
     save_result = manifest.save(manifest_file)
     new_manifest = Manifest.load(manifest_file)
-    assert_equal save_result, true, "Result should save"
+    assert_equal true, save_result, "Result should save"
     assert_instance_of Manifest, new_manifest, "Not a Manifest Object"
     assert_equal manifest.to_yaml, new_manifest.to_yaml, "The saved manifest does not match the original"
   end
@@ -37,7 +42,7 @@ class ManifestTest < ActiveSupport::TestCase
     manifest = Manifest.load("test/fixtures/files/manifest_invalid")
     save_result = manifest.save(manifest_file + "empty")
     new_manifest = Manifest.load(manifest_file + "empty")
-    assert_equal save_result, false, "Result should not save"
+    assert_equal false, save_result, "Result should not save"
     assert_instance_of MissingManifest, new_manifest, "Should not exist"
   end
 
@@ -46,8 +51,36 @@ class ManifestTest < ActiveSupport::TestCase
     manifest = Manifest.load("test/fixtures/files/manifest_missing")
     save_result = manifest.save(manifest_file + "empty")
     new_manifest = Manifest.load(manifest_file + "empty")
-    assert_equal save_result, false, "Result should not save"
+    assert_equal false, save_result, "Result should not save"
     assert_instance_of MissingManifest, new_manifest, "Should not exist"
+  end
+
+  test "merge manifests" do
+    manifest_one = Manifest.new @@hash1
+    manifest_two = Manifest.new @@hash2
+
+    returned_manifest = manifest_one.merge manifest_two
+    assert_instance_of Manifest, returned_manifest, "Merge did not return a manifest"
+    assert_equal "jeremy", returned_manifest.name, "name string did not update"
+    assert_equal "jeremy", manifest_one.name, "name string did not update in place"
+    assert_equal "", manifest_one.description, "nil should return empty string for description"
+    assert_nil manifest_one.url, "manifest url should be nil"
+    assert_equal "monsignor", manifest_one.role, "role was not added"
+  end
+
+  test "merge hash into manifest" do
+    manifest = Manifest.new @@hash4
+
+    manifest.merge @@hash3
+
+    assert_equal @@hash3[:role], manifest.role, "role does not match"
+    assert_equal @@hash4[:url], manifest.url, "url does not match"
+
+    manifest.merge @@hash4
+
+    assert_equal "", manifest.name, "nil name should return empty string"
+    assert_equal "", manifest.description, "nil description should return empty string"
+    assert_nil manifest.role, "nil role should return nil"
   end
 
 end
