ba7c173772ca1fb216a924d1a22e8e79662aa883
Eric Franz <efranz@osc.edu>
2017-01-13 15:11:38 -0500

    add dashboard site-wide announcement

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :set_user, :set_logout_url, :set_nav_groups
+  before_filter :set_user, :set_logout_url, :set_nav_groups, :set_announcement
 
   def set_user
     @user = User.new
@@ -20,5 +20,12 @@ class ApplicationController < ActionController::Base
   def set_nav_groups
     #TODO: for AweSim, what if we added the shared apps here?
     @nav_groups = OodAppGroup.select(titles: NavConfig.categories, groups: OodAppGroup.groups_for(apps: SysRouter.apps))
+  end
+
+  def set_announcement
+    path = Pathname.new(ENV["OOD_ANNOUNCEMENT_PATH"] || "/etc/ood/config/announcement.md")
+    @announcement = path.read if path.file?
+  rescue => e
+    logger.warn "Failed to read announcement file at: #{path} with error: #{e.message}"
   end
 end
