117e54685f1072a508eac37c26ac68cdc4d3b2dc
Eric Franz <efranz@osc.edu>
2017-01-19 16:15:09 -0500

    show home link only if dev dd shows

--- app/apps/nav_config.rb
+++ app/apps/nav_config.rb
@@ -2,6 +2,12 @@ class NavConfig
   class << self
     attr_accessor :categories
     attr_accessor :show_develop_dropdown
+
+    def show_home_link
+      # only show home link if there are other pages
+      # you can navigate to
+      show_develop_dropdown
+    end
   end
   self.categories = ["Files", "Jobs", "Clusters", "Desktops"]
   self.show_develop_dropdown = ENV['OOD_APP_SHARING'].present?
