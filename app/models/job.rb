f567383a6bd4ac3ba2962bc08eaae176f2dca808
Eric Franz <efranz@osc.edu>
2017-02-02 13:23:04 -0500

    inline Job#job

--- app/models/job.rb
+++ app/models/job.rb
@@ -7,4 +7,10 @@ class Job < ActiveRecord::Base
   # def results_valid?
   #   # CODE GOES HERE
   # end
+
+  # TODO: we inline the construction of this job object
+  # so we can include a custom TorqueAdapter
+  def job
+    OSC::Machete::Job.new(job_cache.symbolize_keys)
+  end
 end
