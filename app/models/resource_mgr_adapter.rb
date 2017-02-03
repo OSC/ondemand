35d9464d623d651f1ed3d2db309f95d3f3a3856b
Eric Franz <efranz@osc.edu>
2017-02-03 15:57:21 -0500

    privatize ResourceMgrAdapter cluster_for_host_id and adapter

--- app/models/resource_mgr_adapter.rb
+++ app/models/resource_mgr_adapter.rb
@@ -4,16 +4,6 @@
 # OodJob errors will be caught and re-raised as PBS::Error objects
 class ResourceMgrAdapter
 
-  def cluster_for_host_id(host)
-    raise PBS::Error, "host nil" if host.nil?
-    raise PBS::Error, "host is invalid value: #{host}" unless OODClusters.has_key?(host.to_sym)
-
-    OODClusters[host.to_sym]
-  end
-
-  def adapter
-    OodJob::Adapters::Torque
-  end
 
   # TODO: this adapter could ignore the host argument and use the one that is
   # specified when it is instantiated.
@@ -22,7 +12,6 @@ class ResourceMgrAdapter
   def qsub(script_path, host: nil, depends_on: {}, account_string: nil)
     cluster = cluster_for_host_id(host)
     script = OodJob::Script.new(content: script_path, accounting_id: account_string)
-
     adapter.new(cluster: cluster).submit(script: script, **depends_on)
 
   rescue OodJob::Adapter::Error => e
@@ -46,6 +35,19 @@ class ResourceMgrAdapter
 
   rescue OodJob::Adapter::Error => e
     raise PBS::Error, e.message
+  end
+
+  private
+
+  def cluster_for_host_id(host)
+    raise PBS::Error, "host nil" if host.nil?
+    raise PBS::Error, "host is invalid value: #{host}" unless OODClusters.has_key?(host.to_sym)
+
+    OODClusters[host.to_sym]
+  end
+
+  def adapter
+    OodJob::Adapters::Torque
   end
 
   def status_for_ood_job_status(status)
