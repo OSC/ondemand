ea60f68eef15a4193dbe246694f40fa229cc898d
Brian McMichael <bmcmichael@osc.edu>
2017-02-08 16:16:44 -0500

    enhance comments

--- app/models/filter.rb
+++ app/models/filter.rb
@@ -5,15 +5,29 @@ class Filter
     attr_accessor :list
   end
 
+  # Apply the current filtering proc to an array of jobs.
+  #
+  # @return [Array] The filtered array.
   def apply(job_array)
     self.filter_block ? job_array.select(&filter_block) : job_array
   end
 
   # Provide the cookie_id to be used for the default filter.
+  #
+  # @return [String] The id of the default filter
   def self.default_id
     "user"
   end
 
+  # Maintains a list of Filter objects.
+  #
+  # Add new filter objects via the format:
+  # self.list << Filter.new.tap { |f|
+  #   user = OodSupport::User.new.name
+  #   f.title = "Your Jobs"
+  #   f.cookie_id = "user"
+  #   f.filter_block = Proc.new { |id, attr| attr[:Job_Owner] =~ /^#{user}@/ }
+  # }
   self.list = []
 
   # Add a filter by user option.
