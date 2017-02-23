5c9e728657da81f30ae498c60261176a1c42ba44
Eric Franz <efranz@osc.edu>
2017-02-22 19:10:06 -0500

    remove unused configurable logout url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,18 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :set_user, :set_logout_url, :set_nav_groups, :set_announcement
+  before_filter :set_user, :set_nav_groups, :set_announcement
 
   def set_user
     @user = User.new
-  end
-
-  def set_logout_url
-    if ENV['OOD_DASHBOARD_LOGOUT_URL'].present?
-      @logout_url = (ENV['OOD_DASHBOARD_LOGOUT_URL'] % { login: ERB::Util.u(request.base_url) })
-    else
-      @logout_url = nil
-    end
   end
 
   def set_nav_groups
