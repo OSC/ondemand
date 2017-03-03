6aff165029e7c6f97d7c4d65697a1a4ab27558f1
Brian McMichael <bmcmichael@osc.edu>
2017-03-03 10:27:01 -0500

    increase max default to 10GB

--- app.js
+++ app.js
@@ -135,7 +135,7 @@ app.use(cloudcmd({
         // treeroottitle: "Project Space"
         treeroot:               HOME,
         treeroottitle:          "Home Directory",
-        upload_max:             process.env.FILE_UPLOAD_MAX || 2097152000,
+        upload_max:             process.env.FILE_UPLOAD_MAX || 10485760000,
         file_editor:            process.env.OOD_FILE_EDITOR || '/pun/sys/file-editor/edit',
         shell:                  process.env.OOD_SHELL || '/pun/sys/shell/ssh/default',
         fileexplorer_version:   gitSync.tag()
