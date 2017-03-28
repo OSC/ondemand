42f6ec175f48e313672e62e98ceb32475a053d80
Brian McMichael <bmcmichael@osc.edu>
2017-03-28 10:11:19 -0400

    Update the initializer for ood_core

--- config/initializers/ood_appkit.rb
+++ config/initializers/ood_appkit.rb
@@ -1,7 +1,16 @@
 # config/initializers/ood_appkit.rb
+require "ood_core"
 
-OODClusters = OodAppkit.clusters.select do |c|
-  c.valid? &&
-    c.resource_mgr_server? &&
-    c.resource_mgr_server.is_a?(OodCluster::Servers::Torque)
-end.each_with_object({}) { |c, h| h[c.id] = c }
+OodAppkit.configure do |config|
+  config.clusters = OodCore::Clusters.new(
+      OodCore::Clusters.load_file("/etc/ood/config/clusters.d").select(&:job_allow?)
+  )
+end
+
+#OODClusters = OodAppkit.clusters.select do |c|
+#  c.valid? &&
+#    c.resource_mgr_server? &&
+#    c.resource_mgr_server.is_a?(OodCluster::Servers::Torque)
+#end.each_with_object({}) { |c, h| h[c.id] = c }
+
+OODClusters = OodAppkit.clusters
