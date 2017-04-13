4f721bec992c706e416acb527ac8bd386524ba92
Brian McMichael <bmcmichael@osc.edu>
2017-04-13 15:22:16 -0400

    Update view to follow the key/value attrib model

--- app/models/jobstatusdata.rb
+++ app/models/jobstatusdata.rb
@@ -7,8 +7,10 @@
 # @version 0.0.1
 class Jobstatusdata
   include ApplicationHelper
+  attr_reader :pbsid, :jobname, :username, :account, :status, :cluster, :nodes, :starttime, :walltime, :walltime_used, :submit_args, :output_path, :nodect, :ppn, :total_cpu, :queue, :cput, :mem, :vmem, :terminal_path, :fs_path, :extended_available, :native_attribs
 
-  attr_reader :pbsid, :jobname, :username, :account, :status, :cluster, :nodes, :starttime, :walltime, :walltime_used, :submit_args, :output_path, :nodect, :ppn, :total_cpu, :queue, :cput, :mem, :vmem, :terminal_path, :fs_path, :extended_available
+  Attribute = Struct.new(:name, :value)
+
 
   # Define an object containing only necessary data to send to client.
   #
@@ -52,24 +54,27 @@ class Jobstatusdata
   # @return [Jobstatusdata] self
   def extended_data_torque(info)
     return unless info.native
-    self.walltime = info.native.fetch(:Resource_List, {})[:walltime].presence || "00:00:00"
+    attributes = []
+    attributes.push Attribute.new "Walltime", (info.native.fetch(:Resource_List, {})[:walltime].presence || "00:00:00")
+    node_count = info.native.fetch(:Resource_List, {})[:nodect].to_i
+    attributes.push Attribute.new "Node Count", node_count
+    ppn = info.native[:Resource_List][:nodes].to_s.split("ppn=").second || '0'
+    attributes.push Attribute.new "PPN", ppn
+    attributes.push Attribute.new "Total CPUs", ppn.to_i * node_count.to_i
+    attributes.push Attribute.new "CPU Time", info.native.fetch(:resources_used, {})[:cput].presence || '0'
+    mem = info.native.fetch(:resources_used, {})[:mem].presence || "0 b"
+    attributes.push Attribute.new "Memory", Filesize.from(mem).pretty
+    vmem = info.native.fetch(:resources_used, {})[:vmem].presence || "0 b"
+    attributes.push Attribute.new "Virtual Memory", Filesize.from(vmem).pretty
+    attributes.push Attribute.new "Node Count", info.native.fetch(:Resource_List, {})[:nodect].to_i
+    self.native_attribs = attributes
+
     self.submit_args = info.native[:submit_args].presence || "None"
     self.output_path = info.native[:Output_Path].to_s.split(":").second || info.native[:Output_Path]
-    self.nodect = info.native.fetch(:Resource_List, {})[:nodect].to_i
-    self.ppn = info.native[:Resource_List][:nodes].to_s.split("ppn=").second || '0'
-    self.total_cpu = self.ppn[/\d+/].to_i * self.nodect.to_i
-    self.cput = info.native.fetch(:resources_used, {})[:cput].presence || '0'
-    mem = info.native.fetch(:resources_used, {})[:mem].presence || "0 b"
-    self.mem = Filesize.from(mem).pretty
-    vmem = info.native.fetch(:resources_used, {})[:vmem].presence || "0 b"
-    self.vmem = Filesize.from(vmem).pretty
+
     output_pathname = Pathname.new(self.output_path).dirname
     self.terminal_path = OodAppkit.shell.url(path: (output_pathname.writable? ? output_pathname : ENV["HOME"])).to_s
     self.fs_path = OodAppkit.files.url(path: (output_pathname.writable? ? output_pathname : ENV["HOME"])).to_s
-    if self.status == :running || self.status == :completed
-      self.nodes = node_array(info.allocated_nodes)
-      self.starttime = info.dispatch_time.to_i
-    end
     self
   end
 
@@ -152,6 +157,6 @@ class Jobstatusdata
       node_info_array.map { |n| n.name }
     end
 
-    attr_writer :pbsid, :jobname, :username, :account, :status, :cluster, :nodes, :starttime, :walltime, :walltime_used, :submit_args, :output_path, :nodect, :ppn, :total_cpu, :queue, :cput, :mem, :vmem, :terminal_path, :fs_path, :extended_available
+    attr_writer :pbsid, :jobname, :username, :account, :status, :cluster, :nodes, :starttime, :walltime, :walltime_used, :submit_args, :output_path, :nodect, :ppn, :total_cpu, :queue, :cput, :mem, :vmem, :terminal_path, :fs_path, :extended_available, :native_attribs
 
 end
