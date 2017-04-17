a4917b8ebce5d510f73793187663b50111378ffb
Jeremy Nicklas <jnicklas@osc.edu>
2017-04-17 15:54:24 -0400

    use latest ood_appkit

--- config/initializers/ood_appkit.rb
+++ config/initializers/ood_appkit.rb
@@ -1,10 +1,6 @@
 # config/initializers/ood_appkit.rb
 require "ood_core"
 
-OodAppkit.configure do |config|
-  config.clusters = OodCore::Clusters.new(
-      OodCore::Clusters.load_file(ENV['OOD_CLUSTERS'] || '/etc/ood/config/clusters.d').select(&:job_allow?)
-  )
-end
-
-OODClusters = OodAppkit.clusters
+OODClusters = OodCore::Clusters.new(
+  OodAppkit.clusters.select(&:job_allow?)
+)
