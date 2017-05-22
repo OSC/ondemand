5d1954ecf710b2a596e2c7ebac3e9d903e4fe58a
Brian McMichael <bmcmichael@osc.edu>
2017-05-22 12:55:42 -0400

    Use abstractions when setting/getting keys

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,14 +22,24 @@ $(document).ready(function(){
 var KEY_PREFIX = "ood_editor_store_";
 
 // Set localStorage. Adds a key prefix to reduce overlap likelihood.
-function setUserPreference(key, value) {
+function setLocalStorage(key, value) {
     var ood_key = KEY_PREFIX + key;
     localStorage.setItem(ood_key, value);
     return null;
 }
 
 // Get localStorage. Adds a key prefix added by setter.
-function getUserPreference(key) {
+function getLocalStorage(key) {
     var ood_key = KEY_PREFIX + key;
     return localStorage.getItem(ood_key);
+}
+
+// Set a user preference key to a specific value.
+function setUserPreference(key, value) {
+    return setLocalStorage(key, value);
+}
+
+// Get the current value of the key from preferences.
+function getUserPreference(key) {
+    return getLocalStorage(key);
 }
