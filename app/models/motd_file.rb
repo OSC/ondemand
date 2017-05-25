da4114825007a935139a881a058809c845ba1c2b
Brian McMichael <bmcmichael@osc.edu>
2017-05-25 17:01:46 -0400

    Preload content in inialiazer so we don't make uri calls on each check

--- app/models/motd_file.rb
+++ app/models/motd_file.rb
@@ -2,26 +2,33 @@ require 'open-uri'
 
 class MotdFile
 
-  attr_reader :motd_uri
-
   # Initialize the Motd Controller object based on the current user.
   #
   # @param [String] path The path to the motd file as a URI
   def initialize(path = ENV['MOTD_PATH'])
-    @motd_uri = path
+    @content = load(path)
   end
 
   # Checks the path URI to see if it can be opened
   #
   # Uses open-uri to check local or remote path for contents
   def content
-      motd_uri ? open(motd_uri).read : nil
+    @content
+  end
+
+  private
+
+  def load(motd_uri)
+    motd_uri ? open(motd_uri).read : nil
   rescue Errno::ENOENT
     Rails.logger.warn "MOTD File is missing; it was expected at #{motd_uri}"
+    nil
   rescue OpenURI::HTTPError
     Rails.logger.warn "MOTD File is not available at #{motd_uri}"
+    nil
   rescue StandardError => ex
     Rails.logger.warn "Error opening MOTD at #{motd_uri}\nException: #{ex}"
+    nil
   end
 end
 
