164c7abeb107e9285c1bb0090314e90cdaeab72a
Brian McMichael <bmcmichael@osc.edu>
2017-05-26 17:04:40 -0400

    Add test ssuite and fixtures

---
+++ test/models/motd_formatter_osc_test.rb
@@ -0,0 +1,64 @@
+require 'test_helper'
+
+class MotdTest < ActiveSupport::TestCase
+  # test date order of example Motd file
+  #
+  test "motd message date format" do
+
+    date = Date.new(2016, 5, 4)
+
+    # assume year month day
+    msg = "2016/05/04\n--- NEW CLUSTER\n\nSomething good!"
+    assert_equal date, MotdFormatterOsc::Message.from(msg).date
+    msg = "2016-05-04\n--- NEW CLUSTER\n\nSomething good!"
+    assert_equal date, MotdFormatterOsc::Message.from(msg).date
+    msg = "2016.05.04\n--- NEW CLUSTER\n\nSomething good!"
+    assert_equal date, MotdFormatterOsc::Message.from(msg).date
+    msg = "2016 05 04\n--- NEW CLUSTER\n\nSomething good!"
+    assert_nil MotdFormatterOsc::Message.from(msg)
+    msg = "2016+05+04\n--- NEW CLUSTER\n\nSomething good!"
+    assert_nil MotdFormatterOsc::Message.from(msg)
+  end
+
+  test "test when motd formatter_osc_valid" do
+
+    path = "#{Rails.root}/test/fixtures/files/motd_valid"
+    motd_file = MotdFile.new(path)
+    formatted_motd = MotdFormatterOsc.new motd_file
+    expected_file = File.open(path).read
+
+    assert_equal true, motd_file.exist?
+    assert_equal path, motd_file.motd_path
+    assert_equal expected_file, motd_file.content
+    assert_equal 3, formatted_motd.messages.count
+  end
+
+  test "test when motd_formatter_osc empty" do
+    path = "#{Rails.root}/test/fixtures/files/motd_empty"
+    motd_file = MotdFile.new(path)
+    formatted_motd = MotdFormatterOsc.new motd_file
+
+    assert_equal true, motd_file.exist?
+    assert_equal path, motd_file.motd_path
+    assert_equal '', motd_file.content
+    assert_equal 0, formatted_motd.messages.count
+  end
+
+  test "test when motd formatter_osc_missing" do
+    path = "#{Rails.root}/test/fixtures/files/motd_missing"
+    motd_file = MotdFile.new(path)
+    formatted_motd = MotdFormatterOsc.new motd_file
+
+    assert_equal false, motd_file.exist?
+    assert_equal path, motd_file.motd_path
+    assert_equal '', motd_file.content
+    assert_equal 0, formatted_motd.messages.count
+  end
+
+  test "test when motd formatter_osc_nil" do
+    motd_file = nil
+    formatted_motd = MotdFormatterOsc.new motd_file
+
+    assert_not_nil formatted_motd.content
+  end
+end
---
+++ test/models/motd_formatter_rss_test.rb
@@ -0,0 +1,44 @@
+require 'test_helper'
+
+class MotdTest < ActiveSupport::TestCase
+
+  test "test when motd formatter_rss_valid" do
+
+    path = "#{Rails.root}/test/fixtures/files/motd_rss"
+    motd_file = MotdFile.new(path)
+    formatted_motd = MotdFormatterRss.new motd_file
+
+    assert formatted_motd.content.items.is_a? Enumerable
+  end
+
+  test "test when motd_formatter_rss invalid" do
+    path = "#{Rails.root}/test/fixtures/files/motd_rss_invalid"
+    motd_file = MotdFile.new(path)
+    formatted_motd = MotdFormatterRss.new motd_file
+
+    assert_nil formatted_motd.content
+  end
+
+  test "test when motd_formatter_rss empty" do
+    path = "#{Rails.root}/test/fixtures/files/motd_empty"
+    motd_file = MotdFile.new(path)
+    formatted_motd = MotdFormatterRss.new motd_file
+
+    assert_nil formatted_motd.content
+  end
+
+  test "test when motd formatter_rss_missing" do
+    path = "#{Rails.root}/test/fixtures/files/motd_missing"
+    motd_file = MotdFile.new(path)
+    formatted_motd = MotdFormatterRss.new motd_file
+
+    assert_nil formatted_motd.content
+  end
+
+  test "test when motd formatter_rss_nil" do
+    motd_file = nil
+    formatted_motd = MotdFormatterRss.new motd_file
+
+    assert_nil formatted_motd.content
+  end
+end
