dc3605d7178f5ef3f3c38c7f9b4c2f9b9ead04c8
Eric Franz <efranz@osc.edu>
2017-05-31 17:07:54 -0400

    remove home link

--- app/apps/nav_config.rb
+++ app/apps/nav_config.rb
@@ -2,12 +2,6 @@ class NavConfig
   class << self
     attr_accessor :categories
     attr_accessor :show_develop_dropdown
-
-    def show_home_link
-      # only show home link if there are other pages
-      # you can navigate to
-      show_develop_dropdown
-    end
   end
   self.categories = ["Files", "Jobs", "Clusters", "Desktops", "Desktop Apps", "Interactive Apps"]
   self.show_develop_dropdown = ENV['OOD_APP_SHARING'].present?
