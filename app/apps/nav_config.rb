e8d6f83a837526a1bd9050de4acc70a8d4cbc254
Eric Franz <efranz@osc.edu>
2017-06-13 10:22:45 -0400

    rm Desktops and Desktop Apps from default nav

--- app/apps/nav_config.rb
+++ app/apps/nav_config.rb
@@ -3,6 +3,6 @@ class NavConfig
     attr_accessor :categories
     attr_accessor :show_develop_dropdown
   end
-  self.categories = ["Files", "Jobs", "Clusters", "Desktops", "Desktop Apps", "Interactive Apps"]
+  self.categories = ["Files", "Jobs", "Clusters", "Interactive Apps"]
   self.show_develop_dropdown = ENV['OOD_APP_SHARING'].present?
 end
