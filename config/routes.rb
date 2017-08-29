ac5b11df38e171a7818875943ae4cdefd63d6d3a
Eric Franz <efranz@osc.edu>
2017-08-29 14:38:09 -0400

    enable shared apps routes when app sharing enabled

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,11 @@ Rails.application.routes.draw do
   get "apps/icon/:name(/:type(/:owner))" => "apps#icon", as: "app_icon", defaults: { type: "sys" }
 
   if ENV['OOD_APP_SHARING'].present?
+    # TODO:
+    # is there a cleaner approach to this? an app should be a resource
+    get "apps(/index(/:type(/:owner)))" => "apps#index", as: "apps", defaults: { type: "usr" }
+    get "apps/restart" => "apps#restart"
+
     root "apps#index", defaults: { type: "usr" }
   else
     root "dashboard#index"
@@ -22,11 +27,6 @@ Rails.application.routes.draw do
 
   #FIXME: undo when ready to deploy app sharing to production, remove?
   if ENV['OOD_APP_DEVELOPMENT'].present?
-    # TODO:
-    # is there a cleaner approach to this? an app should be a resource
-    get "apps(/index(/:type(/:owner)))" => "apps#index", as: "apps", defaults: { type: "usr" }
-    get "apps/restart" => "apps#restart"
-
     # App administration
     scope 'admin/:type' do
       resources :products, param: :name, constraints: { type: /dev|usr/ } do
