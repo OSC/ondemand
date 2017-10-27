3c36906525614d8279544c9779877bcb96031041
Eric Franz <efranz@osc.edu>
2017-10-27 13:29:27 -0400

    add colors to configuration obj

--- config/configuration.rb
+++ config/configuration.rb
@@ -4,6 +4,9 @@ class Configuration
     attr_writer :app_development_enabled
     attr_writer :app_sharing_enabled
 
+    attr_accessor :banner_bg
+    attr_accessor :banner_color
+
     # FIXME: temporary
     attr_accessor :app_sharing_facls_enabled
     alias_method :app_sharing_facls_enabled?, :app_sharing_facls_enabled
@@ -20,4 +23,22 @@ class Configuration
     end
     alias_method :app_sharing_enabled, :app_sharing_enabled?
   end
+
+  # setup default values for banner
+  # self.banner_bg = '#e9e9e9'
+  # self.banner_color = '#737373'
+  self.banner_bg = lambda {
+    if ENV['OOD_NAVBAR_TYPE'] == 'default'
+      ENV['BOOTSTRAP_NAVBAR_DEFAULT_BG'] || '#f8f8f8'
+    else
+      ENV['BOOTSTRAP_NAVBAR_INVERSE_BG'] || '#53565a'
+    end
+  }.call
+  self.banner_color = lambda {
+    if ENV['OOD_NAVBAR_TYPE'] == 'default'
+      ENV['BOOTSTRAP_NAVBAR_DEFAULT_LINK_COLOR'] || '#777'
+    else
+      ENV['BOOTSTRAP_NAVBAR_INVERSE_LINK_COLOR'] || '#fff'
+    end
+  }.call
 end
