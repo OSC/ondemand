bb99acc680e536e10307a0d1f00b9ab0d8ae412c
Eric Franz <efranz@osc.edu>
2017-10-30 19:45:09 -0400

    load custom /etc initializers first

--- config/configuration.rb
+++ config/configuration.rb
@@ -67,7 +67,7 @@ end
 # support custom initializers and views in /etc
 if defined?(Rails) && defined?(Rails.application)
   Rails.application.configure do |config|
-    config.paths["config/initializers"] << Configuration.config_root.join("config", "initializers").to_s
+    config.paths["config/initializers"].unshift Configuration.config_root.join("config", "initializers").to_s
     config.paths["app/views"].unshift Configuration.config_root.join("app", "views").to_s
   end
 end
