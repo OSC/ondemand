231e620a0e2e46b9ad86ddc0decd11458cb94afe
Eric Franz <efranz@osc.edu>
2017-10-31 14:21:32 -0400

    clean up code and revert boot.rb change

--- config/boot.rb
+++ config/boot.rb
@@ -1,7 +1,7 @@
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
-require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+require 'bundler/setup' # Set up gems listed in the Gemfile.
 
 # load dotenv files before "before_configuration" callback
 require File.expand_path('../config_root', __FILE__)
