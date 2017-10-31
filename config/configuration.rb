ec704cf0de4e5ff657cd75edfa3808c9aad4e831
Eric Franz <efranz@osc.edu>
2017-10-30 20:10:33 -0400

    refactor dotenv loading

--- config/configuration.rb
+++ config/configuration.rb
@@ -34,35 +34,47 @@ class Configuration
       @custom_brand_bg_color = ENV.values_at('OOD_BRAND_BG_COLOR', 'BOOTSTRAP_NAVBAR_DEFAULT_BG', 'BOOTSTRAP_NAVBAR_INVERSE_BG').compact.first
     end
 
-    # what is the correct name for this option?
     def custom_brand_link_active_bg_color
       return @custom_brand_link_active_bg_color if defined? @custom_brand_link_active_bg_color
       @custom_brand_link_active_bg_color = ENV.values_at('OOD_BRAND_LINK_ACTIVE_BG_COLOR', 'BOOTSTRAP_NAVBAR_DEFAULT_LINK_ACTIVE_BG','BOOTSTRAP_NAVBAR_INVERSE_LINK_ACTIVE_BG' ).compact.first
     end
+
+    def app_root
+      Pathname.new(File.expand_path("../../",  __FILE__))
+    end
+
+    def load_dotenv_files
+      Dir.chdir app_root do
+        # .env.local first, so it can override OOD_APP_CONFIG
+        Dotenv.load(*dotenv_local_files) unless dotenv_local_files.empty?
+
+        # load the rest of the dotenv files
+        Dotenv.load(*dotenv_files)
+      end
+    end
+
+    def rails_env
+      (defined?(Rails) && Rails.env) || ENV['RAILS_ENV']
+    end
+
+    def dotenv_local_files
+      [
+        (app_root.join(".env.#{rails_env}.local") unless rails_env.nil?),
+        (app_root.join(".env.local") unless rails_env == "test"),
+      ].compact
+    end
+
+    def dotenv_files
+      [
+        config_root.join("env"),
+        (app_root.join(".env.#{rails_env}") unless rails_env.nil?),
+        app_root.join(".env")
+      ].compact
+    end
   end
 end
 
-# load dotenv files to initialize environment
-Pathname.new(File.expand_path("../../",  __FILE__)).tap do |root|
-  Dir.chdir root do
-    rails_env = (defined?(Rails) && Rails.env) || ENV['RAILS_ENV']
-
-    # .env.local first, so it can override OOD_APP_CONFIG
-    dotenv_files = [
-      (root.join(".env.#{rails_env}.local") unless rails_env.nil?),
-      (root.join(".env.local") unless rails_env == "test"),
-    ].compact
-    Dotenv.load(*dotenv_files) unless dotenv_files.empty?
-
-    # load the rest of the dotenv files
-    dotenv_files = [
-      Configuration.config_root.join("env"),
-      (root.join(".env.#{rails_env}") unless rails_env.nil?),
-      root.join(".env")
-    ].compact
-    Dotenv.load(*dotenv_files)
-  end
-end
+Configuration.load_dotenv_files
 
 # support custom initializers and views in /etc
 if defined?(Rails) && defined?(Rails.application)
