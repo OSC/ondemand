325b20d2c62acbdc437298c4d72a5a29e080a172
Eric Franz <efranz@osc.edu>
2017-10-30 21:31:46 -0400

    set relative url root in config/boot.rb

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,7 @@
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
+# FIXME: move to bin/setup instead, and remove this
+ENV['RAILS_RELATIVE_URL_ROOT'] = "/pun/sys/myjobs" if ENV['RAILS_ENV'] == 'production' && ENV['RAILS_RELATIVE_URL_ROOT'].nil?
+
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
