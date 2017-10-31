e61a596dc07845696c7eff092afa3011bb888f13
Eric Franz <efranz@osc.edu>
2017-10-31 08:49:42 -0400

    update dotenv loading similar to job composer
    load dotenv in config/boot
    include in Configuration module
    remove unnecessary guard clauses
    load /etc.../.env and /etc.../.env.production files

--- config/boot.rb
+++ config/boot.rb
@@ -1,9 +1,8 @@
+# Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
-# TODO: File.basename(File.expand_path('../../', __FILE__)) == "dashboard"
-# assume /pun/sys since core app... but for other apps, a solution that
-# determines what app type could be a useful alternative...
-# TODO: could just drop this and add it to the bin/setup instead (that is probably best)
-ENV['RAILS_RELATIVE_URL_ROOT'] = "/pun/sys/dashboard" if ENV['RAILS_ENV'] == 'production' && ENV['RAILS_RELATIVE_URL_ROOT'].nil?
+require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
 
-require 'bundler/setup' # Set up gems listed in the Gemfile.
+# load dotenv files before "before_configuration" callback
+require File.expand_path('../config_root', __FILE__)
+ConfigRoot.load_dotenv_files
--- config/configuration.rb
+++ config/configuration.rb
@@ -3,6 +3,8 @@ require 'dotenv'
 
 # dashboard app specific configuration
 class Configuration
+  extend ConfigRoot
+
   class << self
     attr_writer :app_development_enabled
     attr_writer :app_sharing_enabled
@@ -23,12 +25,6 @@ class Configuration
     end
     alias_method :app_sharing_enabled, :app_sharing_enabled?
 
-    # The app's configuration root directory
-    # @return [Pathname] path to configuration root
-    def config_root
-      Pathname.new(ENV["OOD_APP_CONFIG"] || "/etc/ood/config/apps/dashboard")
-    end
-
     def custom_brand_bg_color
       return @custom_brand_bg_color if defined? @custom_brand_bg_color
       @custom_brand_bg_color = ENV.values_at('OOD_BRAND_BG_COLOR', 'BOOTSTRAP_NAVBAR_DEFAULT_BG', 'BOOTSTRAP_NAVBAR_INVERSE_BG').compact.first
@@ -38,48 +34,11 @@ class Configuration
       return @custom_brand_link_active_bg_color if defined? @custom_brand_link_active_bg_color
       @custom_brand_link_active_bg_color = ENV.values_at('OOD_BRAND_LINK_ACTIVE_BG_COLOR', 'BOOTSTRAP_NAVBAR_DEFAULT_LINK_ACTIVE_BG','BOOTSTRAP_NAVBAR_INVERSE_LINK_ACTIVE_BG' ).compact.first
     end
-
-    def app_root
-      Pathname.new(File.expand_path("../../",  __FILE__))
-    end
-
-    def load_dotenv_files
-      Dir.chdir app_root do
-        # .env.local first, so it can override OOD_APP_CONFIG
-        Dotenv.load(*dotenv_local_files) unless dotenv_local_files.empty?
-
-        # load the rest of the dotenv files
-        Dotenv.load(*dotenv_files)
-      end
-    end
-
-    def rails_env
-      (defined?(Rails) && Rails.env) || ENV['RAILS_ENV']
-    end
-
-    def dotenv_local_files
-      [
-        (app_root.join(".env.#{rails_env}.local") unless rails_env.nil?),
-        (app_root.join(".env.local") unless rails_env == "test"),
-      ].compact
-    end
-
-    def dotenv_files
-      [
-        config_root.join("env"),
-        (app_root.join(".env.#{rails_env}") unless rails_env.nil?),
-        app_root.join(".env")
-      ].compact
-    end
   end
 end
 
-Configuration.load_dotenv_files
-
 # support custom initializers and views in /etc
-if defined?(Rails) && defined?(Rails.application)
-  Rails.application.configure do |config|
-    config.paths["config/initializers"].unshift Configuration.config_root.join("config", "initializers").to_s
-    config.paths["app/views"].unshift Configuration.config_root.join("app", "views").to_s
-  end
+Rails.application.configure do |config|
+  config.paths["config/initializers"].unshift Configuration.config_root.join("config", "initializers").to_s
+  config.paths["app/views"].unshift Configuration.config_root.join("app", "views").to_s
 end
