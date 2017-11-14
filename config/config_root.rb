0ea5a331ffeb457f13b73fec2e1edb457decfc8f
Eric Franz <efranz@osc.edu>
2017-11-14 11:52:09 -0500

    rename OOD_APP_CONFIG to OOD_APP_CONFIG_ROOT

--- config/config_root.rb
+++ config/config_root.rb
@@ -10,19 +10,18 @@ module ConfigRoot
   # The app's configuration root directory
   # @return [Pathname] path to configuration root
   def config_root
-    Pathname.new(ENV["OOD_APP_CONFIG"] || "/etc/ood/config/apps/dashboard")
+    Pathname.new(ENV["OOD_APP_CONFIG_ROOT"] || "/etc/ood/config/apps/dashboard")
   end
-
 
   # Load the dotenv local files first, then the /etc dotenv files and
   # the .env and .env.production or .env.development files.
   #
-  # Doing this in two separate loads means OOD_APP_CONFIG can be specified in
+  # Doing this in two separate loads means OOD_APP_CONFIG_ROOT can be specified in
   # the .env.local file, which will specify where to look for the /etc dotenv
-  # files. The default for OOD_APP_CONFIG is /etc/ood/config/apps/myjobs and
+  # files. The default for OOD_APP_CONFIG_ROOT is /etc/ood/config/apps/myjobs and
   # both .env and .env.production will be searched for there.
   def load_dotenv_files
-    # .env.local first, so it can override OOD_APP_CONFIG
+    # .env.local first, so it can override OOD_APP_CONFIG_ROOT
     Dotenv.load(*dotenv_local_files) unless dotenv_local_files.empty?
 
     # load the rest of the dotenv files
