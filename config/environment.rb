ac01428068c1fc99db412e7aa6d0a0c53bd98a66
Eric Franz <efranz@osc.edu>
2017-11-20 10:53:35 -0500

    remove superfluous require stmt

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-# Load the dashboard specific configuration.
-require File.expand_path('../configuration', __FILE__)
-
 # Initialize the Rails application.
 Rails.application.initialize!
