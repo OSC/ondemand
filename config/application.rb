68e21d118dd201b1af6a8d06af8684cd9aff8cb4
Eric Franz <efranz@osc.edu>
2017-12-08 20:44:14 -0500

    remove superfluous whitespace

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-
 module JobConstructor
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
