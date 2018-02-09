b6fe2eb5b6d75d5eaa214be23fb9b78c95bdba02
Jeremy Nicklas <jeremywnicklas@gmail.com>
2018-02-09 13:24:02 -0500

    Add Travis support (#2)

---
+++ tests.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# Get dependencies
+yum install -y centos-release-scl
+yum install -y \
+  make \
+  curl \
+  sqlite-devel \
+  rh-ruby22 \
+  rh-ruby22-rubygem-rake \
+  rh-ruby22-rubygem-bundler \
+  rh-ruby22-ruby-devel \
+  nodejs010 \
+  git19
+
+# Setup environment
+source scl_source enable rh-ruby22 nodejs010 git19 || :
+
+# Build and install
+rake -mj ${NUM_TASKS:-$(nproc)} && rake install
