b1e0c3ba05dcf798a33242999fa7caa0ee07200f
Jeremy Nicklas <jnicklas@osc.edu>
2018-04-03 15:22:09 -0400

    update configuration singleton tests

--- test/system/configuration_singleton_test.rb
+++ test/system/configuration_singleton_test.rb
@@ -1,99 +1,187 @@
 require 'test_helper'
 
 class ConfigurationSingletonTest < ActiveSupport::TestCase
-  #FIXME: this approach is ugly and difficult to follow
-  #
-  # Marshaling the ConfigurationSingleton doesn't work because the methods are evaluated when
-  # you call them: ConfigurationSingleton doesn't store data for most of its options.
-  #
-  # Using Bundler.with_clean_env to provide several unit tests, one for loading
-  # dotenv, and one for ConfigurationSingleton.new once expected env is loaded, doesn't
-  # let us test dataroot because of OodAppkit's approach to storing the dataroot
-  # as an instance variable.
-  #
-  # One solution to clean up the below code would be to implement a
-  # ConfigurationSingleton#to_h method.
-  def runner(code, env: 'development', envvars: '')
-    Tempfile.open('runnerbin') do |f|
-      Bundler.with_clean_env do
-        f.write(code)
-        f.close
-        `RAILS_ENV=#{env} #{envvars} bin/rails runner -e #{env} #{f.path}`
+  # Restore the state of ENV after each test
+  def setup
+    @env = ENV.to_h
+    ENV.delete("OOD_DATAROOT")
+  end
+
+  def teardown
+    ENV.clear
+    ENV.update(@env)
+  end
+
+  test "should have default config root" do
+    assert_equal Pathname.new("/etc/ood/config/apps/dashboard"), ConfigurationSingleton.new.config_root
+  end
+
+  test "can configure config root" do
+    ENV["OOD_APP_CONFIG_ROOT"] = "/path/to/config"
+    assert_equal Pathname.new("/path/to/config"), ConfigurationSingleton.new.config_root
+  end
+
+  test "should not load external configuration by default if not production" do
+    refute ConfigurationSingleton.new.load_external_config?
+  end
+
+  test "can enable external configuration if not production" do
+    ENV["OOD_LOAD_EXTERNAL_CONFIG"] = "true"
+    assert ConfigurationSingleton.new.load_external_config?
+  end
+
+  test "should load external configuration by default if production" do
+    ENV["RAILS_ENV"] = "production"
+    assert ConfigurationSingleton.new.load_external_config?
+  end
+
+  test "can disable external configuration if production" do
+    ENV["RAILS_ENV"] = "production"
+    ENV["OOD_LOAD_EXTERNAL_CONFIG"] = "false"
+    refute ConfigurationSingleton.new.load_external_config?
+  end
+
+  test "should load environment variables" do
+    Dir.mktmpdir do |dir|
+      env = Pathname.new(dir).join("env")
+      File.open env, "w" do |f|
+        f.write <<-EOT
+          TEST_UNQUOTE=test_123
+          TEST_QUOTE="another test"
+          TEST_NUMBER=123
+        EOT
       end
+
+      ENV["OOD_APP_CONFIG_ROOT"] = env.dirname.to_s
+      ENV["OOD_LOAD_EXTERNAL_CONFIG"] = "true"
+      ConfigurationSingleton.new.load_dotenv_files
+      assert_equal "test_123", ENV["TEST_UNQUOTE"]
+      assert_equal "another test", ENV["TEST_QUOTE"]
+      assert_equal "123", ENV["TEST_NUMBER"]
+      assert_nil ENV["TEST_UNDEFINED"]
     end
   end
 
-  # Use bin/rails runner to get values set in Configuration after initialization
-  # using marshal to pass the OpenStruct between processes
-  #
-  # @return [OpenStruct] attrs have values set in Configuration after initialization
-  def config_via_runner(env: 'development', envvars: '')
-    code = %q(require 'ood_appkit'
-      puts Marshal.dump(
-        OpenStruct.new(
-          brand_bg_color: Configuration.brand_bg_color,
-          dataroot: Configuration.dataroot,
-          load_external_config: Configuration.load_external_config?,
-          ood_appkit_dataroot: OodAppkit.dataroot
-        )
-      )
+  test "should have app development disabled by default if sandbox does not exist" do
+    Dir.mktmpdir do |dir|
+      sandbox = Pathname.new(dir)
+      DevRouter.stubs(:base_path).returns(sandbox)
+    end
+    refute ConfigurationSingleton.new.app_development_enabled?
+  end
+
+  test "should have app development enabled by default if sandbox exists" do
+    Dir.mktmpdir do |dir|
+      sandbox = Pathname.new(dir)
+      DevRouter.stubs(:base_path).returns(sandbox)
+      assert ConfigurationSingleton.new.app_development_enabled?
+    end
+  end
+
+  test "can configure whether app development is enabled or disabled" do
+    ENV["OOD_APP_DEVELOPMENT"] = "true"
+    assert ConfigurationSingleton.new.app_development_enabled?
+
+    ENV["OOD_APP_DEVELOPMENT"] = "false"
+    refute ConfigurationSingleton.new.app_development_enabled?
+  end
+
+  test "should have app sharing disabled by default" do
+    refute ConfigurationSingleton.new.app_sharing_enabled?
+  end
+
+  test "can configure whether app sharing is enabled or disabled" do
+    ENV["OOD_APP_SHARING"] = "true"
+    assert ConfigurationSingleton.new.app_sharing_enabled?
+
+    ENV["OOD_APP_SHARING"] = "false"
+    refute ConfigurationSingleton.new.app_sharing_enabled?
+  end
+
+  test "should have default announcement paths" do
+    assert_equal(
+      [
+        Pathname.new("/etc/ood/config/announcement.md"),
+        Pathname.new("/etc/ood/config/announcement.yml"),
+        Pathname.new("/etc/ood/config/announcements.d")
+      ],
+      ConfigurationSingleton.new.announcement_path
     )
-    Marshal.load(runner(code, env: env, envvars: envvars))
   end
 
-  test "tests should not be run with .env.local* files in directory" do
-    assert Dir.glob(".env.local{.development,.production,}").none?, "these tests should not be run with a .env.local or .env.local.development or .env.local.production"
+  test "can configure announcement path" do
+    ENV["OOD_ANNOUNCEMENT_PATH"] = "/path/to/announcement"
+    assert_equal Pathname.new("/path/to/announcement"), ConfigurationSingleton.new.announcement_path
   end
 
-  test "configuration defaults in development env" do
-    config = config_via_runner
-
-    assert_nil config.brand_bg_color
-    assert_equal Rails.root.join("data").to_s, config.dataroot.to_s
-    assert_equal false, config.load_external_config
+  test "should not have default brand bg color" do
+    assert_nil ConfigurationSingleton.new.brand_bg_color
   end
 
-  test "loading custom OSC external config in production env" do
-    config_root = Rails.root.join('config','examples','osc')
-    config = config_via_runner(env: 'production', envvars: "OOD_APP_CONFIG_ROOT=#{config_root}")
-
-    assert_equal '#c8102e', config.brand_bg_color
-    assert_equal File.expand_path("~/ondemand/data/sys/dashboard"), config.dataroot.to_s
-    assert_equal config.dataroot, config.ood_appkit_dataroot
-    assert_equal true, config.load_external_config
+  test "can configure brand bg color" do
+    ENV["OOD_BRAND_BG_COLOR"] = "MY_COLOR"
+    assert_equal "MY_COLOR", ConfigurationSingleton.new.brand_bg_color
   end
 
-  test "loading custom AweSim external config in production env" do
-    config_root = Rails.root.join('config','examples','awesim')
-    config = config_via_runner(env: 'production', envvars: "OOD_APP_CONFIG_ROOT=#{config_root}")
-
-    assert_nil config.brand_bg_color
-    assert_equal File.expand_path("~/awesim/data/sys/dashboard"), config.dataroot.to_s
-    assert_equal config.dataroot, config.ood_appkit_dataroot
-    assert_equal true, config.load_external_config
+  test "should not have default brand link active bg color" do
+    assert_nil ConfigurationSingleton.new.brand_link_active_bg_color
   end
 
-  test "disable app sharing with falsy env value" do
-    Bundler.with_clean_env do
-      # default is false
-      assert_equal false, ConfigurationSingleton.new.app_sharing_enabled?, 'default app sharing enabled should be false'
-
-      # enabling is true
-      ENV['OOD_APP_SHARING'] = '1'
-      assert_equal true, ConfigurationSingleton.new.app_sharing_enabled?, 'OOD_APP_SHARING=1 should result in app sharing enabled'
-
-      # disabling with a falsy value is false
-      ENV['OOD_APP_SHARING'] = '0'
-      assert_equal false, ConfigurationSingleton.new.app_sharing_enabled?, 'OOD_APP_SHARING=0 should result in app sharing disabled'
-    end
+  test "can configure brand link active bg color" do
+    ENV["OOD_BRAND_LINK_ACTIVE_BG_COLOR"] = "MY_COLOR"
+    assert_equal "MY_COLOR", ConfigurationSingleton.new.brand_link_active_bg_color
   end
 
-  test "default dataroot" do
-    Bundler.with_clean_env do
-      ENV['OOD_DATAROOT'] = nil
-      ENV['RAILS_ENV'] = 'production'
+  test "should not have default logo img" do
+    assert_nil ConfigurationSingleton.new.logo_img
+  end
 
-      assert_equal File.expand_path("~/ondemand/data/sys/dashboard"), ConfigurationSingleton.new.dataroot.to_s
-    end
+  test "can configure logo img" do
+    ENV["OOD_DASHBOARD_LOGO"] = "MY_LOGO"
+    assert_equal "MY_LOGO", ConfigurationSingleton.new.logo_img
+  end
+
+  test "should try to display logo img by default" do
+    assert ConfigurationSingleton.new.logo_img?
+  end
+
+  test "can disable the display of logo img" do
+    ENV["DISABLE_DASHBOARD_LOGO"] = "true"
+    refute ConfigurationSingleton.new.logo_img?
+  end
+
+  test "should hide the all apps link by default" do
+    refute ConfigurationSingleton.new.show_all_apps_link?
+  end
+
+  test "can enable the all apps link" do
+    ENV["SHOW_ALL_APPS_LINK"] = "true"
+    assert ConfigurationSingleton.new.show_all_apps_link?
+  end
+
+  test "should have default dataroot under app if not production" do
+    assert_equal Rails.root.join("data"), ConfigurationSingleton.new.dataroot
+  end
+
+  test "should have default dataroot under home dir if production" do
+    ENV["RAILS_ENV"] = "production"
+    assert_equal Pathname.new("~/ondemand/data/sys/dashboard").expand_path, ConfigurationSingleton.new.dataroot
+  end
+
+  test "can configure full path of dataroot" do
+    ENV["OOD_DATAROOT"] = "/path/to/dataroot"
+    assert_equal Pathname.new("/path/to/dataroot"), ConfigurationSingleton.new.dataroot
+  end
+
+  test "can configure portal component of dataroot if production" do
+    ENV["RAILS_ENV"] = "production"
+    ENV["OOD_PORTAL"] = "MY_PORTAL"
+    assert_equal Pathname.new("~/MY_PORTAL/data/sys/dashboard").expand_path, ConfigurationSingleton.new.dataroot
+  end
+
+  test "can configure app token component of dataroot if production" do
+    ENV["RAILS_ENV"] = "production"
+    ENV["APP_TOKEN"] = "MY/APP/TOKEN"
+    assert_equal Pathname.new("~/ondemand/data/MY/APP/TOKEN").expand_path, ConfigurationSingleton.new.dataroot
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,4 +7,4 @@ class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
 end
 
-require 'mocha/mini_test'
+require 'mocha/minitest'
