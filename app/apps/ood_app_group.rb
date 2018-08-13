8b34e6042e6cae3916c55b6f188c6cfe568d0e19
AriettaZ <azhu@osc.edu>
2018-08-13 14:21:04 -0400

    add controller test

--- app/apps/ood_app_group.rb
+++ app/apps/ood_app_group.rb
@@ -34,15 +34,11 @@ class OodAppGroup
     # only one group per title; this just makes it easy to get the group
     # Hash[ [:title1,:group1], [:title2,:group2]] => {title1: :group1, title2: :group2 }
     h = Hash[groups.map {|g| [g.title, g]}]
-
     titles.map { |t| h.has_key?(t) ? h[t] : nil }.compact
   end
   
   def self.order(titles:[], groups:[])
       h = Hash[groups.map {|g| [g.title, g]}]
-      unselected =[]
-      selected = []
-      titles.each { |t| h.has_key?(t) ? selected.push(h[t]) : unselected.push(h[t]) }
-      selected.concat(unselected.sort)
+      titles.concat(h.keys.sort).uniq.map { |t| h.has_key?(t) ? h[t] : nil }.compact 
   end
 end
