495fd8adf474d6723caebd86ca2befe37b3359af
Eric Franz <efranz@osc.edu>
2019-01-29 11:25:21 -0500

    further simplify Configuration#locale

--- config/configuration_singleton.rb
+++ config/configuration_singleton.rb
@@ -147,7 +147,7 @@ class ConfigurationSingleton
   end
 
   def locale
-    @locale ||= ENV['OOD_LOCALE'].try(:to_sym) || I18n.default_locale
+    (ENV['OOD_LOCALE'] || I18n.default_locale).to_sym
   end
 
   def locales_root
