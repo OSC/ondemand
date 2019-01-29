6fa6d39f0a394bf5adb6b29f20b41f64114b008f
Eric Franz <efranz@osc.edu>
2019-01-29 11:02:36 -0500

    use before_action instead of before_filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :set_user, :set_nav_groups, :set_announcements
-  
-  before_action :set_locale
-  
+  before_action :set_user, :set_nav_groups, :set_announcements, :set_locale
+
   def set_locale
     I18n.locale = ::Configuration.locale
   end
