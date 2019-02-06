7a75a2b85315ac8ac7b892472d9ec837b5f8ade2
Tyler Harris <tharris@osc.edu>
2019-02-06 17:08:38 -0500

    Add one batch connect translation and do quota translations

--- app/models/quota.rb
+++ app/models/quota.rb
@@ -165,13 +165,13 @@ class Quota
 
   def to_s
     if @resource_type == "file"
-      msg = "Using #{number_to_human(@total_usage).downcase} files of quota #{number_to_human(@limit).downcase} files"
+      msg = I18n.translate('dashboard.quota.file', used: number_to_human(@total_usage).downcase, available: number_to_human(@limit).downcase)
       return msg unless self.shared?
-      return msg + " (#{number_to_human(@user_usage).downcase} files are yours)"
+      return msg + " #{I18n.translate('dashboard.quota.file_shared', used_exclusive: number_to_human(@user_usage).downcase)}"
     elsif @resource_type == "block"
-      msg = "Using #{number_to_human_size(@total_usage * BLOCK_SIZE)} of quota #{number_to_human_size(@limit * BLOCK_SIZE)}"
+      msg = I18n.translate('dashboard.quota.block', used: number_to_human_size(@total_usage * BLOCK_SIZE), available: number_to_human_size(@limit * BLOCK_SIZE))
       return msg unless self.shared?
-      return msg + " (#{number_to_human_size(@user_usage * BLOCK_SIZE)} are yours)"
+      return msg + " #{I18n.translate('dashboard.quota.block_shared', used_exclusive: number_to_human_size(@user_usage * BLOCK_SIZE))}"
     end
   end
 end
