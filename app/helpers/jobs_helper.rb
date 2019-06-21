95f9314f3dc8809ac9b11db6bf73ce4b71fd6fe8
kinanAlAttar <alattar.2@osu.edu>
2019-06-21 13:58:30 -0400

    Update job's helper & Add _job_details_node_view partial to override _ganglia_graphs_table partial per node

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -2,15 +2,12 @@ module JobsHelper
 
 	def build_ganglia_link( host, start_seconds, report_type, node_num, size )
     ganglia_uri = ""
-    OODClusters.each do |c|
-      if host.eql?(c.id.to_s)
-        if c.custom_allow?(:ganglia)
-          ganglia_cluster = OodCluster::Servers::Ganglia.new(c.custom_config(:ganglia))
-          ganglia_base = ganglia_cluster.uri.to_s
-          ganglia_server = ganglia_cluster.opt_query.fetch(:h, '') % {h: node_num }
-          ganglia_uri = ("#{ganglia_base}&z=#{size}&cs=#{start_seconds.to_s}&g=#{report_type}&h=#{ganglia_server}").html_safe
-        end
-      end
+    c = OODClusters[host]
+    if c && c.custom_allow?(:ganglia)
+      ganglia_cluster = OodCluster::Servers::Ganglia.new(c.custom_config(:ganglia))
+      ganglia_base = ganglia_cluster.uri.to_s
+      ganglia_server = ganglia_cluster.opt_query.fetch(:h, '') % {h: node_num }
+      ganglia_uri = ("#{ganglia_base}&z=#{size}&cs=#{start_seconds.to_s}&g=#{report_type}&h=#{ganglia_server}").html_safe
     end
     ganglia_uri
   rescue
