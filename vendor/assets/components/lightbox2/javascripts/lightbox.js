114842dd4a1ffcb274ec597280563f716274f51f
Ariel Zhu <azhu@osc.edu>
2019-09-13 12:52:53 -0400

    clean up code

--- vendor/assets/components/lightbox2/javascripts/lightbox.js
+++ vendor/assets/components/lightbox2/javascripts/lightbox.js
@@ -22,8 +22,8 @@
       this.alwaysShowNavOnTouchDevices = false;
       this.fadeDuration                = 20;
       this.fitImagesInViewport         = true;
-    //   this.maxWidth                    = 800;
-    //   this.maxHeight                   = 600;
+      // this.maxWidth                    = 800;
+      // this.maxHeight                   = 600;
       this.positionFromTop             = 50;
       this.resizeDuration              = 20;
       this.showImageNumberLabel        = true;
