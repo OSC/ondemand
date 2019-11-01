2465678ef187d00f463d9573b49d4093559c0fe3
Morgan Rodgers <mrodgers@osc.edu>
2019-11-01 10:27:58 -0400

    State post webpacker:install

--- apps/dashboard/config/initializers/content_security_policy.rb
+++ apps/dashboard/config/initializers/content_security_policy.rb
@@ -23,3 +23,6 @@
 # For further information see the following documentation:
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
 # Rails.application.config.content_security_policy_report_only = true
+
+# You need to allow webpack-dev-server host as allowed origin for connect-src.
+policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
