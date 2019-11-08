43f0a988d10b72c0cef6d55564ac42253199cfc5
Trey Dockendorf <tdockendorf@osc.edu>
2019-11-08 12:28:38 -0500

    Refactor update_ood_portal to use Ruby

--- ood-portal-generator/lib/ood_portal_generator/application.rb
+++ ood-portal-generator/lib/ood_portal_generator/application.rb
@@ -1,4 +1,8 @@
+require "digest"
+require "fileutils"
 require "optparse"
+require "tempfile"
+require "time"
 require "yaml"
 
 module OodPortalGenerator
@@ -8,7 +12,7 @@ module OodPortalGenerator
       # The yaml configuration file used as context for template
       # @return [Pathname] path to yaml config
       def config
-        Pathname.new(@config || "/etc/ood/config/ood_portal.yml")
+        Pathname.new(@config || ENV['CONFIG'] || "/etc/ood/config/ood_portal.yml")
       end
 
       # The erb template file that will be rendered
@@ -29,6 +33,160 @@ module OodPortalGenerator
         config.file? ? YAML.load_file(config) : {}
       end
 
+      def rpm
+        @rpm.nil? ? false : @rpm
+      end
+
+      def force
+        @force.nil? ? false : @force
+      end
+
+      def detailed_exitcodes
+        @detailed_exitcodes.nil? ? false : @detailed_exitcodes
+      end
+
+      def change_exit
+        detailed_exitcodes ? 3 : 0
+      end
+
+      def skip_exit
+        detailed_exitcodes ? 4 : 0
+      end
+
+      def apache
+        ENV['APACHE'] || OodPortalGenerator.scl_apache ? '/opt/rh/httpd24/root/etc/httpd/conf.d/ood-portal.conf' : '/etc/httpd/conf.d/ood-portal.conf'
+      end
+
+      def apache_bak
+        "#{apache}.#{Time.now.strftime('%Y%m%dT%H%M%S')}"
+      end
+
+      def apache_services
+        if OodPortalGenerator.scl_apache
+          ['httpd24-httpd', 'httpd24-htcacheclean']
+        else
+          ['httpd', 'htcacheclean']
+        end
+      end
+
+      def sum
+        ENV['SUM'] || '/etc/ood/config/ood_portal.sha256sum'
+      end
+
+      def parse_input(input)
+        lines = File.readlines(input)
+        new_lines = []
+        lines.each do |line|
+          next if line =~ /^\s*#/
+          new_lines << line
+        end
+        str = new_lines.join('')
+        str
+      end
+
+      def save_checksum(input)
+        str = parse_input(input)
+        checksum = Digest::SHA256.hexdigest(str)
+        open(sum, 'w') do |f|
+          f.puts "#{checksum} #{apache}\n"
+        end
+      end
+
+      def checksum_matches?(input)
+        checksum_str = File.readlines(sum)[0]
+        checksum = checksum_str.split(' ')[0]
+
+        str = parse_input(input)
+        new_checksum = Digest::SHA256.hexdigest(str)
+
+        checksum == new_checksum
+      end
+
+      def checksum_exists?
+        return false unless File.exist?(sum)
+        File.readlines(sum).grep(apache).size == 0
+      end
+
+      def update_replace?
+        # If checksum of ood-portal.conf matches or ood-portal.conf doesn't exit, replace is possible.
+        # If the checksum matches this means a site has not changed ood-portal.conf outside ood-portal-generator
+        if checksum_matches?(apache)
+          replace = true
+        else
+          replace = false
+        end
+        replace = true if force
+        return replace
+      end
+
+      def exit!(code)
+        case code
+        when TrueClass
+          exit(0)
+        when FalseClass
+          exit(1)
+        else
+          exit(code)
+        end
+      end
+
+      def update_ood_portal
+        ret = 0
+        changed = false
+        new_apache = Tempfile.new('new_apache')
+        @output = new_apache.path
+        self.start('generate')
+
+        # Create checksum file if the path to ood-portal.conf not in checksum file
+        # Checksum is based on mktemp generated ood-portal.conf but using path of real ood-portal.conf
+        if ! checksum_exists?
+          puts "Generating Apache config checksum file: '#{sum}'"
+          save_checksum(new_apache.path)
+        end
+
+        replace = update_replace?
+
+        if ! FileUtils.cmp(new_apache.path, apache)
+          if replace
+            if File.exist?(apache)
+              puts "Backing up previous Apache config to: '#{apache_bak}'"
+              FileUtils.mv(apache, apache_bak)
+            end
+            puts "Generating new Apache config at: '#{apache}'"
+            `cat "#{new_apache.path}" > "#{apache}"`
+            puts "Generating Apache config checksum file: '#{sum}'"
+            save_checksum(apache)
+            ret = change_exit
+            changed = true
+          else
+            puts "WARNING: Checksum of #{apache} does not match previous value, not replacing."
+            puts "Generating new Apache config at: '#{apache}.new'"
+            `cat "#{new_apache.path}" > "#{apache}.new"`
+            ret = skip_exit
+          end
+        else
+          puts "No change in Apache config."
+        end
+
+        new_apache.unlink
+
+        if rpm || ! replace
+          return ret
+        end
+
+        puts "Completed successfully!"
+        if changed
+          puts ""
+          puts "Restart the #{apache_services[0]} service now."
+          puts ""
+          puts "Suggested command:"
+          puts "    sudo systemctl try-restart #{apache_services[0]}.service #{apache_services[1]}.service"
+          puts ""
+        end
+
+        return ret
+      end
+
       # Starts the OodPortalGenerator CLI
       # @return [void]
       def start(mode)
@@ -36,36 +194,53 @@ module OodPortalGenerator
         Process.setproctitle("#{mode} #{ARGV.join(" ")}")
 
         # Parse CLI arguments
-        parser.parse!
+        parser(mode).parse!
 
         # Render Apache portal config
         if mode == 'generate'
           view = View.new(context)
           rendered_template = view.render(template.read)
           output.write(rendered_template)
+        elsif mode == 'update_ood_portal'
+          exitcode = update_ood_portal()
+          exit!(exitcode)
         end
       rescue
         $stderr.puts "#{$!.to_s}"
         $stderr.puts "Run '#{mode} --help' to see a full list of available options."
-        exit(false)
+        exit!(false)
       end
 
       # Parser used for parsing CLI options
       # @return [OptionParser] the option parser object
-      def parser
+      def parser(mode)
         OptionParser.new do |parser|
-          parser.banner = "Usage: generate [options]"
+          parser.banner = "Usage: #{mode} [options]"
 
-          parser.on("-c", "--config CONFIG", String, "YAML config file used to render template") do |v|
-            @config = v
-          end
+          if mode == 'generate'
+            parser.on("-c", "--config CONFIG", String, "YAML config file used to render template") do |v|
+              @config = v
+            end
 
-          parser.on("-t", "--template TEMPLATE", String, "ERB template that is rendered") do |v|
-            @template = v
-          end
+            parser.on("-t", "--template TEMPLATE", String, "ERB template that is rendered") do |v|
+              @template = v
+            end
 
-          parser.on("-o", "--output OUTPUT", String, "File that rendered template is output to") do |v|
-            @output = v
+            parser.on("-o", "--output OUTPUT", String, "File that rendered template is output to") do |v|
+              @output = v
+            end
+          elsif mode == 'update_ood_portal'
+            parser.on("-r", "--rpm", TrueClass, "Execution performed during RPM install") do |v|
+              @rpm = v
+            end
+
+            parser.on("-f", "--force", TrueClass, "Force replacement of configs even if checksums differ") do |v|
+              @force = v
+            end
+
+            parser.on("--detailed-exitcodes", TrueClass, "Exit with 3 when changes are made and 4 when changes skipped") do |v|
+              @detailed_exitcodes = v
+            end
           end
 
           parser.on("-v", "--version", "Print current version") do
--- ood-portal-generator/spec/application_spec.rb
+++ ood-portal-generator/spec/application_spec.rb
@@ -1,16 +1,132 @@
 require 'spec_helper'
 require File.expand_path '../../lib/ood_portal_generator', __FILE__
+require 'tempfile'
 
 describe OodPortalGenerator::Application do
   let(:argv) do
     %W()
   end
 
-  before(:each) do
-    stub_const('ARGV', argv)
+  let(:sum) do
+    Tempfile.new('sum')
   end
 
-  it 'runs generate' do
-    expect { described_class.start('generate') }.to output(/VirtualHost/).to_stdout
+  let(:apache) do
+    Tempfile.new('apache')
+  end
+
+  let(:apache_bak) do
+    Tempfile.new('apache_bak')
+  end
+
+  before(:each) do
+    stub_const('ARGV', argv)
+    allow(described_class).to receive(:sum).and_return(sum.path)
+  end
+
+  after(:each) do
+    sum.unlink
+    apache.unlink
+  end
+
+  describe 'generate' do
+    it 'runs generate' do
+      expect { described_class.start('generate') }.to output(/VirtualHost/).to_stdout
+    end
+  end
+
+  describe 'save_checksum' do
+    it 'saves checksum file' do
+      allow(File).to receive(:readlines).with('/dne.conf').and_return(["# comment\n", "foo\n", "  #comment\n"])
+      described_class.save_checksum('/dne.conf')
+      expect(File.read(sum.path)).to eq("b5bb9d8014a0f9b1d61e21e796d78dccdf1352f23cd32812f4850b878ae4944c /opt/rh/httpd24/root/etc/httpd/conf.d/ood-portal.conf\n")
+    end
+  end
+
+  describe 'checksum_matches?' do
+    it 'matches' do
+      allow(File).to receive(:readlines).with(sum.path).and_return(["b5bb9d8014a0f9b1d61e21e796d78dccdf1352f23cd32812f4850b878ae4944c /opt/rh/httpd24/root/etc/httpd/conf.d/ood-portal.conf\n"])
+      allow(File).to receive(:readlines).with('/dne.conf').and_return(["# comment\n", "foo\n", "  #comment\n"])
+      expect(described_class.checksum_matches?('/dne.conf')).to eq(true)
+    end
+
+    it 'does not match' do
+      allow(File).to receive(:readlines).with(sum.path).and_return(["b5bb9d8014a0f9b1d61e21e796d78dccdf1352f23cd32812f4850b878ae4944c /opt/rh/httpd24/root/etc/httpd/conf.d/ood-portal.conf\n"])
+      allow(File).to receive(:readlines).with('/dne.conf').and_return(["# comment\n", "bar\n", "  #comment\n"])
+      expect(described_class.checksum_matches?('/dne.conf')).to eq(false)
+    end
+  end
+
+  describe 'checksum_exists?' do
+    it 'returns true' do
+      allow(File).to receive(:readlines).with(sum.path).and_return(["b5bb9d8014a0f9b1d61e21e796d78dccdf1352f23cd32812f4850b878ae4944c /opt/rh/httpd24/root/etc/httpd/conf.d/ood-portal.conf\n"])
+      expect(described_class.checksum_exists?).to eq(true)
+    end
+
+    it 'returns false' do
+      allow(File).to receive(:readlines).with(sum.path).and_return(["b5bb9d8014a0f9b1d61e21e796d78dccdf1352f23cd32812f4850b878ae4944c /foo/bar\n"])
+      expect(described_class.checksum_exists?).to eq(true)
+    end
+
+    it 'returns false if checksum does not exist' do
+      allow(File).to receive(:readlines).with(sum.path).and_return(nil)
+      sum.unlink
+      expect(described_class.checksum_exists?).to eq(false)
+    end
+  end
+
+  describe 'update_ood_portal' do
+    it 'does not replace if no changes detected' do
+      allow(described_class).to receive(:checksum_exists?).and_return(true)
+      allow(described_class).to receive(:update_replace?).and_return(false)
+      allow(FileUtils).to receive(:cmp).and_return(true)
+      ret = described_class.update_ood_portal()
+      expect(ret).to eq(0)
+    end
+
+    it 'does not replace if checksums do not match and cmp is true' do
+      allow(described_class).to receive(:detailed_exitcodes).and_return(true)
+      allow(described_class).to receive(:apache).and_return(apache.path)
+      allow(described_class).to receive(:checksum_exists?).and_return(true)
+      allow(described_class).to receive(:update_replace?).and_return(true)
+      allow(FileUtils).to receive(:cmp).and_return(true)
+      ret = described_class.update_ood_portal()
+      expect(ret).to eq(0)
+    end
+
+    it 'does replace if checksums match and cmp is false' do
+      allow(described_class).to receive(:detailed_exitcodes).and_return(true)
+      allow(described_class).to receive(:apache).and_return(apache.path)
+      allow(described_class).to receive(:checksum_exists?).and_return(true)
+      allow(described_class).to receive(:update_replace?).and_return(true)
+      allow(FileUtils).to receive(:cmp).and_return(false)
+      expect(described_class).to receive(:save_checksum).with(apache.path)
+      ret = described_class.update_ood_portal()
+      expect(ret).to eq(3)
+    end
+
+    it 'creates backup of Apache when replacing' do
+      allow(described_class).to receive(:detailed_exitcodes).and_return(true)
+      allow(described_class).to receive(:apache).and_return(apache.path)
+      allow(described_class).to receive(:apache_bak).and_return("#{apache.path}.bak")
+      allow(described_class).to receive(:checksum_exists?).and_return(true)
+      allow(described_class).to receive(:update_replace?).and_return(true)
+      allow(FileUtils).to receive(:cmp).and_return(false)
+      expect(described_class).to receive(:save_checksum).with(apache.path)
+      ret = described_class.update_ood_portal()
+      expect(ret).to eq(3)
+      expect(File.exist?("#{apache.path}.bak")).to eq(true)
+    end
+
+    it 'does not replace if checksums do not match and cmp is false' do
+      allow(described_class).to receive(:detailed_exitcodes).and_return(true)
+      allow(described_class).to receive(:apache).and_return(apache.path)
+      allow(described_class).to receive(:checksum_exists?).and_return(true)
+      allow(described_class).to receive(:update_replace?).and_return(false)
+      allow(FileUtils).to receive(:cmp).and_return(false)
+      ret = described_class.update_ood_portal()
+      expect(ret).to eq(4)
+      expect(File.exist?("#{apache.path}.new")).to eq(true)
+    end
   end
 end
