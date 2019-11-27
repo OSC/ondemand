18296f517992df09280ceba7cd1afcb770fb6acf
Eric Franz <efranz@osc.edu>
2019-11-27 11:30:41 -0500

    Revert "add mock data for xdmod api"
    This reverts commit 7df8efd5887662e2128694825faef90eb097c1e5.

--- apps/dashboard/app/controllers/dashboard_controller.rb
+++ apps/dashboard/app/controllers/dashboard_controller.rb
@@ -6,16 +6,4 @@ class DashboardController < ApplicationController
 
   def logout
   end
-
-  def jobs
-    render file: Rails.root.join("jobs.json"), content_type: 'application/json', layout: false
-  end
-
-  def perf
-    render file: Rails.root.join("performance.json"), content_type: 'application/json', layout: false
-  end
-
-  def role
-    render file: Rails.root.join("roles.json"), content_type: 'application/json', layout: false
-  end
 end
