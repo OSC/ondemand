a057c1745e215f9a28ed9f0bd247c7d3a2fc4a62
Kinan AlAttar <alattar.2@osu.edu>
2019-12-17 16:23:59 -0500

    Update dashboard/app/helpers/batch_connect/session_contexts_helper.rb

--- apps/dashboard/app/helpers/batch_connect/session_contexts_helper.rb
+++ apps/dashboard/app/helpers/batch_connect/session_contexts_helper.rb
@@ -10,7 +10,6 @@ module BatchConnect::SessionContextsHelper
     when "select"
       form.select attrib.id, attrib.select_choices, field_options, attrib.html_options
     when "resolution_field"
-      # resolution_field(form, attrib.id, all_options)
       render partial: "batch_connect/sessions/connections/resolution_field", locals: {form: form, id: attrib.id, opts: all_options}
     when "check_box"
       form.form_group attrib.id, help: field_options[:help] do
@@ -21,94 +20,5 @@ module BatchConnect::SessionContextsHelper
     else
       form.send widget, attrib.id, all_options
     end
-  end
-
-  def resolution_field(form, id, opts = {})
-    # content_tag(:div, id: "#{id}_group", class: "form-group") do
-    #   concat form.label(id, opts[:label])
-    #   concat form.hidden_field(id, id: "#{id}_field")
-    #   concat(
-    #     content_tag(:div, class: "row") do
-    #       concat (
-    #         content_tag(:div, class: "col-sm-6") do
-    #           concat (
-    #             content_tag(:div, class: "input-group") do
-    #               concat content_tag(:div, "width", class: "input-group-addon", style: "width: 70px")
-    #               concat number_field_tag("#{id}_x_field", nil, class: "form-control", min: 100, required: opts[:required])
-    #               concat content_tag(:div, "px", class: "input-group-addon")
-    #             end
-    #           )
-    #         end
-    #       )
-    #       concat (
-    #         content_tag(:div, class: "col-sm-6") do
-    #           concat (
-    #             content_tag(:div, class: "input-group") do
-    #               concat content_tag(:div, "height", class: "input-group-addon", style: "width: 70px")
-    #               concat number_field_tag("#{id}_y_field", nil, class: "form-control", min: 100, required: opts[:required])
-    #               concat content_tag(:div, "px", class: "input-group-addon")
-    #             end
-    #           )
-    #         end
-    #       )
-    #     end
-    #   )
-    #   concat content_tag(:span, opts[:help], class: "help-block") if opts[:help]
-    #   concat button_tag(t('dashboard.batch_connect_form_reset_resolution'), id: "#{id}_reset", type: "button", class: "btn btn-default")
-    #   concat(
-    #     content_tag(:script) do
-    #       <<-EOT.html_safe
-    #         (function() {
-    #           // Target elements
-    #           var $target  = $('##{id}_field');
-    #           var $targetX = $('##{id}_x_field');
-    #           var $targetY = $('##{id}_y_field');
-    #           var $targetR = $('##{id}_reset');
-
-    #           // Helper methods
-    #           var setX = function(x) {
-    #             y = getY();
-    #             $target.val(x + 'x' + y);
-    #             $targetX.val(x);
-    #           };
-    #           var setY = function(y) {
-    #             x = getX();
-    #             $target.val(x + 'x' + y);
-    #             $targetY.val(y);
-    #           };
-    #           var getX = function() {
-    #             return $target.val().split('x')[0];
-    #           };
-    #           var getY = function() {
-    #             return $target.val().split('x')[1];
-    #           };
-    #           var resetXY = function() {
-    #             setX(window.screen.width  * 0.8 | 0);
-    #             setY(window.screen.height * 0.8 | 0);
-    #           };
-
-    #           // Set defaults if not already set
-    #           if ( !$target.val() ) {
-    #             resetXY();
-    #           } else {
-    #             setX(getX());
-    #             setY(getY());
-    #           }
-
-    #           // Event listeners
-    #           $targetX.change(function() {
-    #             setX($(this).val());
-    #           });
-    #           $targetY.change(function() {
-    #             setY($(this).val());
-    #           });
-    #           $targetR.click(function() {
-    #             resetXY();
-    #           });
-    #         })();
-    #       EOT
-    #     end
-    #   )
-    # end
   end
 end
