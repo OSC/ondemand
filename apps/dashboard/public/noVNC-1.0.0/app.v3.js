c9cd8b266e5be277976e64187ed70d4fe0ecb1e0
Morgan Rodgers <mrodgers@osc.edu>
2019-12-20 11:45:30 -0500

    Undo bugged copy-paste hack

--- apps/dashboard/public/noVNC-1.0.0/app.v3.js
+++ apps/dashboard/public/noVNC-1.0.0/app.v3.js
@@ -489,8 +489,6 @@ var UI = {
         document.getElementById("noVNC_clipboard_button").addEventListener('click', UI.toggleClipboardPanel);
         document.getElementById("noVNC_clipboard_text").addEventListener('change', UI.clipboardSend);
         document.getElementById("noVNC_clipboard_clear_button").addEventListener('click', UI.clipboardClear);
-
-        document.getElementById("noVNC_clipboard_text").addEventListener('input', UI.syncClipboardPanelToLocalClipboard);
     },
 
     // Add a call to save settings when the element changes,
@@ -1054,49 +1052,6 @@ var UI = {
      *   CLIPBOARD
      * ------v------*/
 
-    // Read and write text to local clipboard is currently only supported in Chrome 66+ and Opera53+
-    // further information at https://developer.mozilla.org/en-US/docs/Web/API/Clipboard
-
-    writeLocalClipboard(text) {
-        if (typeof navigator.clipboard !== "undefined" && typeof navigator.clipboard.writeText !== "undefined" &&
-            typeof navigator.permissions !== "undefined" && typeof navigator.permissions.query !== "undefined"
-           ) {
-            navigator.permissions.query({name: 'clipboard-write'})
-            .then(() => navigator.clipboard.writeText(text))
-            .then(() => {
-                let debugMessage = text.substr(0, 40) + "...";
-                Log.Debug('>> UI.setClipboardText: navigator.clipboard.writeText with ' + debugMessage);
-            })
-            .catch((err) => {
-                if(err.name !== 'TypeError'){
-                    Log.Error(">> UI.setClipboardText: Failed to write system clipboard (trying to copy from NoVNC clipboard)");
-                }
-            });
-        }
-    },
-
-    readLocalClipboard() {
-        // navigator.clipboard and navigator.clipbaord.readText is not available in all browsers
-        if (typeof navigator.clipboard !== "undefined" && typeof navigator.clipboard.readText !== "undefined" &&
-            typeof navigator.permissions !== "undefined" && typeof navigator.permissions.query !== "undefined"
-           ) {
-            navigator.permissions.query({name: 'clipboard-read'})
-            .then(() => navigator.clipboard.readText())
-            .then((clipboardText) => {
-                const text = document.getElementById('noVNC_clipboard_text').value;
-                if (clipboardText !== text) {
-                    document.getElementById('noVNC_clipboard_text').value = clipboardText;
-                    UI.clipboardSend();
-                }
-            })
-            .catch((err) => {
-                if(err.name !== 'TypeError'){
-                  Log.Warn("<< UI.readLocalClipboard: Failed to read system clipboard-: " + err);
-                }
-            });
-        }
-    },
-
     openClipboardPanel: function () {
         UI.closeAllPanels();
         UI.openControlbar();
@@ -1121,13 +1076,11 @@ var UI = {
     clipboardReceive: function (e) {
         Log.Debug(">> UI.clipboardReceive: " + e.detail.text.substr(0, 40) + "...");
         document.getElementById('noVNC_clipboard_text').value = e.detail.text;
-        UI.writeLocalClipboard(e.detail.text);
         Log.Debug("<< UI.clipboardReceive");
     },
 
     clipboardClear: function () {
         document.getElementById('noVNC_clipboard_text').value = "";
-        UI.writeLocalClipboard("");
         UI.rfb.clipboardPasteFrom("");
     },
 
@@ -1136,12 +1089,6 @@ var UI = {
         Log.Debug(">> UI.clipboardSend: " + text.substr(0, 40) + "...");
         UI.rfb.clipboardPasteFrom(text);
         Log.Debug("<< UI.clipboardSend");
-    },
-    syncClipboardPanelToLocalClipboard() {
-        // Reads text from clipboard panel and set it to local clipboard
-        // Mainly used to synchronize clipboard panel with local clipboard
-        const text = document.getElementById('noVNC_clipboard_text').value;
-        UI.writeLocalClipboard(text);
     },
 
     /* ------^-------
@@ -1212,9 +1159,6 @@ var UI = {
             UI.updatePowerButton();
         });
         UI.rfb.addEventListener("clipboard", UI.clipboardReceive);
-
-        UI.rfb.oncanvasfocus = UI.readLocalClipboard;
-
         UI.rfb.addEventListener("bell", UI.bell);
         UI.rfb.addEventListener("desktopname", UI.updateDesktopName);
         UI.rfb.clipViewport = UI.getSetting('view_clip');
@@ -6524,8 +6468,6 @@ function RFB(target, url, options) {
     // time to set up callbacks
     setTimeout(this._updateConnectionState.bind(this, 'connecting'));
 
-    this.oncanvasfocus = () => {}; // Handler for canvas focused
-
     Log.Debug("<< RFB.constructor");
 };
 
@@ -6673,7 +6615,6 @@ RFB.prototype = {
     },
 
     focus: function () {
-        this.oncanvasfocus();
         this._canvas.focus();
     },
 
