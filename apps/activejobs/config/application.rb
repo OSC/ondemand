4b79cb8d42f8d620d6512ee6e05dd4eecc75685e
Morgan Rodgers <mrodgers@osc.edu>
2020-01-10 16:22:52 -0500

    Upgrade Rails 4.2.11 to 5.0.7.2

--- apps/activejobs/config/application.rb
+++ apps/activejobs/config/application.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
 # Pick the frameworks you want:
 # require "active_record/railtie"
@@ -9,7 +9,7 @@ require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env)
+Bundler.require(*Rails.groups)
 
 module JobStatus
   class Application < Rails::Application
--- apps/activejobs/config/initializers/session_store.rb
+++ apps/activejobs/config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-JobStatus::Application.config.session_store :cookie_store, key: '_job_status_session'
+Rails.application.config.session_store :cookie_store, key: '_job_status_session'
