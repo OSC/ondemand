80a323860cf7c6fa8bb7d36f59e05e154124b5e6
Mario Squeo <msqueo@osc.edu>
2020-02-12 16:44:52 -0500

    Remove extraneous console logs.

--- apps/myjobs/app/assets/javascripts/joyride.js
+++ apps/myjobs/app/assets/javascripts/joyride.js
@@ -482,17 +482,6 @@
                         leftAdjustment = settings.tipSettings.tipAdjustmentX ? parseInt(settings.tipSettings.tipAdjustmentX) : 0;
 
                     if (methods.bottom()) {
-
-                        /*
-                        var topOffset;
-
-                        if (firstRun) {
-                            topOffset = 240;
-                            firstRun = false;
-                        } else {
-                            topOffset = (settings.$target.offset().top + nub_height + settings.$target.outerHeight() + topAdjustment)
-                        }*/
-
                         settings.$next_tip.css({
                             top: (settings.$target.offset().top + nub_height + settings.$target.outerHeight() + topAdjustment),
                             left: settings.$target.offset().left + leftAdjustment});
@@ -506,7 +495,7 @@
                     } else if (methods.top()) {
 
                         settings.$next_tip.css({
-                            top: topOffset,
+                            top: settings.$target.offset().top + topAdjustment,
                             left: settings.$target.offset().left + leftAdjustment});
 
                         methods.nub_position($nub, settings.tipSettings.nubPosition, 'bottom');
@@ -642,7 +631,7 @@
                 }
                 expose = $(settings.template.expose);
                 settings.$body.append(expose);
-                console.log('Test')
+
                 expose.css({
                     top: el.offset().top,
                     left: el.offset().left,
