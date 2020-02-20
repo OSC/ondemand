9ddc243fe598b9ba76c5b6b2b3c63a49daae2801
Trey Dockendorf <tdockendorf@osc.edu>
2020-02-20 10:44:52 -0500

    Remove dependency on ood_cluster gem

--- apps/activejobs/app/helpers/jobs_helper.rb
+++ apps/activejobs/app/helpers/jobs_helper.rb
@@ -4,10 +4,11 @@ module JobsHelper
     ganglia_uri = ""
     c = OODClusters[host]
     if c && c.custom_allow?(:ganglia)
-      ganglia_cluster = OodCluster::Servers::Ganglia.new(c.custom_config(:ganglia))
-      ganglia_base = ganglia_cluster.uri.to_s
-      ganglia_server = ganglia_cluster.opt_query.fetch(:h, '') % {h: node_num }
-      ganglia_uri = ("#{ganglia_base}&z=#{size}&cs=#{start_seconds.to_s}&g=#{report_type}&h=#{ganglia_server}").html_safe
+      server = c.custom_config(:ganglia)
+      ganglia_base = "#{server[:scheme]}#{server[:host]}/#{server[:segments].join('')}"
+      cluster_query = server.fetch(:req_query, {}).map { |k,v| "#{k}=#{v}" }[0]
+      ganglia_server = server.fetch(:opt_query, {}).fetch('h', '') % {h: node_num }
+      ganglia_uri = ("#{ganglia_base}?#{cluster_query}&z=#{size}&cs=#{start_seconds.to_s}&g=#{report_type}&h=#{ganglia_server}").html_safe
     end
     ganglia_uri
   rescue
