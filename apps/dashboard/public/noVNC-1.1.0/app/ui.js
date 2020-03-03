e5f507eb498aa13ef021bffff2b53e5e10dd80a4
Mario Squeo <me@mariosqueo.com>
2020-03-03 10:55:02 -0500

    Specify the WebSocket protocols to connect with

--- apps/dashboard/public/noVNC-1.1.0/app/ui.js
+++ apps/dashboard/public/noVNC-1.1.0/app/ui.js
@@ -1018,7 +1018,8 @@ const UI = {
         UI.rfb = new RFB(document.getElementById('noVNC_container'), url,
                          { shared: UI.getSetting('shared'),
                            repeaterID: UI.getSetting('repeaterID'),
-                           credentials: { password: password } });
+                           credentials: { password: password },
+                           wsProtocols: ['binary', 'base64'] });
         UI.rfb.addEventListener("connect", UI.connectFinished);
         UI.rfb.addEventListener("disconnect", UI.disconnectFinished);
         UI.rfb.addEventListener("credentialsrequired", UI.credentials);
