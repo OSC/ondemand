49d16c47bf237a3930b64f0db9197dd3777d8745
Eric Franz <efranz@osc.edu>
2020-03-19 11:47:05 -0400

    add climate control for testing purposes

--- apps/dashboard/Gemfile
+++ apps/dashboard/Gemfile
@@ -31,6 +31,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem 'climate_control', '0.2.0'
 end
 
 group :development do
--- apps/dashboard/test/test_helper.rb
+++ apps/dashboard/test/test_helper.rb
@@ -2,9 +2,14 @@ ENV['RAILS_ENV'] ||= 'test'
 ENV['OOD_LOCALES_ROOT'] = Rails.root.join('config/locales').to_s
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'climate_control'
 
 class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
+
+  def with_modified_env(options, &block)
+    ClimateControl.modify(options, &block)
+  end
 end
 
 require 'mocha/minitest'
