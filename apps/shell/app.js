1675a30448e7d6825802d6bba0c66438fdaf6f56
Jeff Ohrstrom <johrstrom@hotmail.com>
2020-03-27 10:47:55 -0400

    modify url regex to account for query parameters

--- apps/shell/app.js
+++ apps/shell/app.js
@@ -62,12 +62,12 @@ wss.on('connection', function connection (ws, req) {
   var dir;
   var term;
   var args;
+  var host_path_rx = '/ssh/([^\\/\\?]+)([^\\?]+)?(\\?.*)?$';
 
   console.log('Connection established');
 
-  // Determine host and dir from request URL path
-  path = url.parse(req.url).pathname;
-  if (match = path.match(process.env.PASSENGER_BASE_URI + '/ssh/([^\\/]+)(.+)?$')) {
+  // Determine host and dir from request URL
+  if (match = req.url.match(process.env.PASSENGER_BASE_URI + host_path_rx)) {
     if (match[1] !== 'default') host = match[1];
     if (match[2]) dir = decodeURIComponent(match[2]);
   }
