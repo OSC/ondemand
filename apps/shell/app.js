9e23de5e225760912cbb4d4b1cd784a67e7b4d91
Sammy Mansour <mansour.69@osu.edu>
2020-04-26 20:09:27 -0400

    Term Schemes that plays nice with directories just incase they do not exist.

--- apps/shell/app.js
+++ apps/shell/app.js
@@ -9,6 +9,8 @@ var dotenv    = require('dotenv');
 var Tokens    = require('csrf');
 var url       = require('url');
 var uuidv4    = require('uuid/v4');
+var os        = require('os');
+var termSchemes   = require('term-schemes');
 var port      = 3000;
 
 // Read in environment variables
@@ -21,6 +23,161 @@ if (process.env.NODE_ENV === 'production') {
 if (fs.existsSync('.env')) {
   console.warn('[DEPRECATION] The file \'.env\' is being deprecated. Please move this file to \'/etc/ood/config/apps/shell/env\'.');
   dotenv.config({path: '.env'});
+}
+
+//Start Terminal Color Scheme Implementation
+//declared constants for directory paths
+const xdg_config_dir = (process.env["XDG_CONFIG_HOME"] || path.join(os.homedir(), ".config"));
+const ood_app_config_root = (process.env["OOD_APP_CONFIG_ROOT"] || '/etc/ood/config/ondemand/apps/shell');
+const userDir = path.join(xdg_config_dir, "ondemand", "apps", "shell", "themes");
+const systemDir = path.join(ood_app_config_root, "themes");
+
+//declared global variables for color schemes
+var schemeObjects;
+
+checkDirSync(userDir);
+checkDirSync(systemDir);
+
+if (process.env["SYSTEM_SCHEMES"] === true) {
+    var user = getSchemeObjects(userDir);
+    var system = getSchemeObjects(systemDir);
+
+   schemeObjects = {...getSchemeObjects(userDir), ...getSchemeObjects(systemDir)};
+
+} else {
+   schemeObjects = {...getSchemeObjects(userDir)};
+}
+
+// helper functions
+function checkDirSync(dir) {  
+  try {
+    fs.statSync(dir);
+  } catch(e) {
+    if (dir === systemDir) {
+      process.env["SYSTEM_SCHEMES"] = false;
+    } else if (dir === userDir) {
+      fs.mkdirSync(dir);
+    }
+  }
+}
+
+function getSchemeObjects(dir) {
+  var schemes = {};
+
+  fs.readdirSync(dir).forEach(function(file) {
+    fileInfo = path.parse(file);
+    schemes[fileInfo.name] = {name: fileInfo.name, file: fileInfo.base, ext: fileInfo.ext, dir: dir}
+  });
+  return schemes;
+}
+
+function getSchemeFilesArray() {
+
+    return Object.keys(schemeObjects).map(i => schemeObjects[i])
+}
+
+
+function getSchemeFileObject(base) {
+    var userDir = path.join(xdg_config_dir, "apps", "shell", "themes");
+    fs.readdirSync(userDir).forEach(file => {
+        if (file === base) {
+            return path.parse(file);
+        }
+
+    })
+}
+
+function rgbToHexMath (num) { 
+  var hex = Number(num).toString(16);
+  if (hex.length < 2) {
+       hex = "0" + hex;
+  }
+  return hex;
+};
+
+function hexConverter (array) {
+    var red = array[0];
+    var green = array[1];
+    var blue = array[2];
+
+    return `#${rgbToHexMath(red)}${rgbToHexMath(green)}${rgbToHexMath(blue)}`.toUpperCase();
+}
+
+function findHost(uuid) {
+    sessions = terminals.instances;
+    var host = sessions[uuid].host;
+
+    return host;
+}
+
+function parseFile(fileObject) {
+    
+    const ext = fileObject.ext;
+    const file = fileObject.dir + "/" + fileObject.file;
+    const raw = String(fs.readFileSync(file));
+      
+    switch(ext) {
+        case ".itermcolors":
+            return termSchemes.iterm2(raw);
+        break;
+
+        case ".colorscheme":
+            return termSchemes.konsole(raw);
+        break;
+
+        case ".colors":
+            return termSchemes.remmina(raw);
+        break;
+
+        case ".terminal":
+            return termSchemes.terminal(raw);      
+        break;
+
+        case ".config":
+            return termSchemes.terminator(raw);
+        break;
+
+        case ".config_0":
+            return termSchemes.tilda(raw);
+        break;
+
+        case ".theme":
+            return termSchemes.xfce(raw);
+        break;
+
+        case ".txt":
+            return termSchemes.termite(raw);
+        break;
+
+        case ".xrdb" || ".Xresources":
+            return termSchemes.xresources(raw);
+        break;
+
+        default:
+            schemeError = {error: "unknown file type."}
+            return schemeError;
+        break; 
+
+    }
+}
+
+function convertSchemeObject(obj) {
+    newSchemeObj = {};
+    colorArray = [];
+    for (var key of Object.keys(obj)) {
+       if(isNaN(key) === false) {
+           
+           colorArray.push(hexConverter(obj[key]));
+       
+        } else if (isNaN(key)) {
+            newSchemeObj[key] = hexConverter(obj[key]);
+        }
+
+    }
+
+    newSchemeObj["colorPaletteOverrides"] = colorArray;
+
+    return newSchemeObj;
 }
 
 const tokens = new Tokens({});
@@ -40,19 +197,81 @@ router.get('/ssh/*', function (req, res) {
 
 });
 
+//For laumch page to start a new session with color scheme and host.
+router.get('/new-session', function(req, res, next) {
+    var id = uuidv4();
+
+
+    res.redirect(url.format({
+        pathname: req.baseUrl + "/custom-term",
+        query: {
+            "host": req.query.host + ".osc.edu",
+            "scheme": req.query.scheme,
+            "session": id
+        }
+    }));
+
+});
+
+router.get('/custom-term', function(req, res, next) {
+    res.locals.uuid = req.query.session;
+    var fileObject, schemeObject, schemeColorConvert;
+    var defaultObj = {
+        'use-default-window-copy': true,
+        'ctrl-v-paste': true,
+        'ctrl-c-copy': true,
+        'cursor-blink': true,
+        };
+
+    res.locals.schemeObject;
+    if (req.query.scheme === "default") {
+       if ('default' in schemeObjects) {
+        fileObject = schemeObjects[req.query.scheme];
+        schemeObject = parseFile(fileObject);
+        schemeColorConvert = convertSchemeObject(schemeObject);
+        res.locals.schemeObject = schemeColorConvert;
+       } else {
+
+        res.locals.schemeObject = defaultObj;
+       }
+    } else {
+     
+     fileObject = schemeObjects[req.query.scheme];
+     schemeObject = parseFile(fileObject);
+     schemeColorConvert = convertSchemeObject(schemeObject);
+     res.locals.schemeObject = schemeColorConvert;        
+    }
+
+    next();
+
+}, function(req, res, next) {
+    res.locals.host = req.query.host || findHost(req.query.session);
+    console.log(res.locals.schemeObject);
+    var cookieValue = JSON.stringify(res.locals.schemeObject);
+
+    res.cookie(res.locals.uuid, cookieValue, {expires: new Date(Date.now() + 8 * 3600000) });
+
+    next();   
+
+}, function(req, res, next) {
+
+    res.redirect(req.baseUrl + `/session/${req.query.session}/${res.locals.host}`)
+})
+
 router.get('/session/:id/*', function (req, res) {
 
   res.render('index',
     {
       baseURI: req.baseUrl,
       csrfToken: tokens.create(secret),
+      session: req.params.id
     });
 
 });
 
 router.get('/launch', function (req, res) {
 
-  res.render('launch', {baseURI: req.baseUrl, sessions: terminal.sessionsInfo()});
+  res.render('launch', {baseURI: req.baseUrl, sessions: terminals.sessionsInfo(), fileOptions: getSchemeFilesArray() || []});
 
 })
 
--- apps/shell/public/javascripts/ood_shell.1.js
+++ apps/shell/public/javascripts/ood_shell.1.js
@@ -1,3 +1,46 @@
+ function getCookie(uuid) {
+
+   var cookie = document.cookie.match('(^|[^;]+)\\s*' + uuid + '\\s*=\\s*([^;]+)')
+
+   return cookie ? cookie.pop() : '';
+
+ }
+
+ function CustomTerm(uuid) {
+  this.uuid = uuid;
+}
+
+CustomTerm.prototype.getTermPrefs = function () {
+
+  var defaultPref = {
+        'use-default-window-copy': true,
+        'ctrl-v-paste': true,
+        'ctrl-c-copy': true,
+        'cursor-blink': true,
+    };
+
+  if (getCookie(this.uuid)) {
+    var decodedTheme = decodeURIComponent(getCookie(this.uuid));
+    var jsonTheme = JSON.parse(decodedTheme);
+
+    pref = {
+        'use-default-window-copy': true,
+        'ctrl-v-paste': true,
+        'ctrl-c-copy': true,
+        'cursor-blink': true,
+        'background-color': jsonTheme['background'],
+        'foreground-color': jsonTheme['text'],
+        'color-palette-overrides': jsonTheme['colorPaletteOverrides'],
+        'cursor-color': jsonTheme['cursor'] + '50',
+    };
+
+    return pref;
+  }
+
+  return defaultPref;
+}
+
+
 
 // Object that defines a terminal element
 function OodShell(element, socket, prefs) {
