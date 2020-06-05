32ec6e0b74758f1c773cc7ee890194e4f73283e4
Matthew Hu <mhu@owens-rw02.ten.osc.edu>
2020-06-05 14:17:53 -0400

    remove leftover console.log

--- apps/shell/app.js
+++ apps/shell/app.js
@@ -68,7 +68,6 @@ glob.sync('/etc/ood/config/clusters.d/*.y*ml')
   .forEach((config) => {
     let host = config.v2.login.host; //Already did checking above
     let isDefault = config.v2.login.default;
-    console.log(isDefault)
     if(isDefault){
       whitelist.unshift(host);
     } else{
