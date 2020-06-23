49321b3b273f20de92f43fde415f8d2bb3aa4553
msquee <msqueo@osc.edu>
2020-06-23 16:15:46 -0400

    Make dashboard logo responsive
    * Fixes the logo being cut off on mobile devices
    
    * Image width is 50% of the container

--- apps/dashboard/app/helpers/dashboard_helper.rb
+++ apps/dashboard/app/helpers/dashboard_helper.rb
@@ -10,7 +10,11 @@ module DashboardHelper
     if url
       uri = Addressable::URI.parse(url)
       uri.query_values = (uri.query_values || {}).merge({timestamp: Time.now.to_i})
-      %(<img src="#{uri}" alt="logo" />).html_safe
+      content_tag(:div, class: "row") do
+        content_tag(:div, class: "col-sm-6 col-md-6 col-lg-6") do
+          image_tag("#{uri}", class: "img-responsive", alt: "logo").html_safe
+        end
+      end
     else # default logo image
       image_tag("OpenOnDemand_stack_RGB.svg", alt: "logo", height: "85", style: "margin-bottom: 10px")
     end
