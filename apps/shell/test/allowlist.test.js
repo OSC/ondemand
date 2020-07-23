5f5c7cba81b2ff680983c93c9c6ee845227d5263
msquee <msqueo@osc.edu>
2020-07-23 16:47:03 -0400

    Remove redundant helper function and add test case

--- apps/shell/test/allowlist.test.js
+++ apps/shell/test/allowlist.test.js
@@ -8,27 +8,14 @@ const helpers = require('../utils/helpers')
  */
 const ALLOW_LIST = new Set(['owens.osc.edu', 'pitzer.osc.edu', '*.ten.osc.edu'])
 
-describe('Helper function generateWildcardGlob()', () => {
-  test('it should generate wildcard glob', () => {
-    let wildcardGlob = helpers.generateWildcardGlob(Array.from(ALLOW_LIST))
-    expect(wildcardGlob).toBe('@(*.ten.osc.edu)')
-  })
-
-  test('it should not have trailing character in wildcard glob', () => {
-    let wildcardGlob = helpers.generateWildcardGlob(Array.from(ALLOW_LIST))
-    expect(wildcardGlob).not.toBe('@(*.ten.osc.edu|)')
-  })
-
-  test('it should not parse hosts with without wildcards in the FQDN', () => {
-    let wildcardGlob = helpers.generateWildcardGlob(Array.from(ALLOW_LIST))
-    expect(wildcardGlob).not.toBe('@(owens.osc.edu|pitzer.osc.edu|*.ten.osc.edu)')
-  })
-})
-
 describe('Helper function hostInAllowList()', () => {
   test('it should be true for hostnames in allowlist', () => {
     expect(helpers.hostInAllowList(ALLOW_LIST, 'pitzer.osc.edu')).toBeTruthy()
     expect(helpers.hostInAllowList(ALLOW_LIST, 'owens.osc.edu')).toBeTruthy()
+  })
+
+  test('it should be false for hostname not in the allowlist', () => {
+    expect(helpers.hostInAllowList(ALLOW_LIST, 'localhost')).not.toBeTruthy()
   })
 
   test('it should be true for hostname that matches wildcard FQDN', () => {
