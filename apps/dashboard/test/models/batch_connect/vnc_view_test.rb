fbd349b6f8abb6febe20ad69273bc12fb40fb5e1
msquee <msqueo@osc.edu>
2020-08-07 11:48:04 -0400

    Update native instructions for Windows noVNC sessions
    * Add tests

---
+++ apps/dashboard/test/models/batch_connect/vnc_view_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class BatchConnect::VncViewTest < ActiveSupport::TestCase
+  test "should have Windows VNC native instructions partial" do
+    assert Rails.root.join('app/views/batch_connect/sessions/connections/_native_vnc_windows.html.erb').file?, "_native_vnc_windows.html.erb partial is required to exist so we can override it in /etc for OSC Connect directions"
+  end
+end
