db730dba37d16ce30e686c24355dd7038e2cb544
Matthew Hu <matt.hu017@gmail.com>
2020-08-08 14:44:16 -0400

    Moved nav_config_test to the correct directory Now executes with rake test

--- apps/dashboard/app/apps/nav_config.rb
+++ apps/dashboard/app/apps/nav_config.rb
@@ -1,9 +1,9 @@
 class NavConfig
-    class << self
-      attr_accessor :categories, :show_only_specified_categories
-      alias_method :show_only_specified_categories?, :show_only_specified_categories
-      alias_method :categories_whitelist=, :show_only_specified_categories=
-    end
-    self.categories = ["Files", "Jobs", "Clusters", "Interactive Apps"]
-    self.show_only_specified_categories = false
+  class << self
+    attr_accessor :categories, :show_only_specified_categories
+    alias_method :show_only_specified_categories?, :show_only_specified_categories
+    alias_method :categories_whitelist=, :show_only_specified_categories=
+  end
+  self.categories = ["Files", "Jobs", "Clusters", "Interactive Apps"]
+  self.show_only_specified_categories = false
 end
---
+++ apps/dashboard/test/models/nav_config_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+require_relative '../../app/apps/nav_config'
+
+class NavConfigTest < ActiveSupport::TestCase
+    test 'inital value of show_only_specified_categories? is false' do
+        assert_equal(false, NavConfig.show_only_specified_categories?)
+    end
+
+    test 'show_only_specified_categories? true if categories_whitelist is true' do
+        NavConfig.categories_whitelist = true
+        assert_equal(NavConfig.show_only_specified_categories?, true)
+    end
+
+    test 'show_only_specified_categories? true if show_only_specified_categories is true' do 
+        NavConfig.show_only_specified_categories = true
+        assert_equal(NavConfig.show_only_specified_categories?, true)
+    end
+
+    test 'show_only_specified_categories? false if categories_whitelist is false' do
+        NavConfig.categories_whitelist = false
+        assert_equal(NavConfig.show_only_specified_categories?, false)
+    end
+ 
+    test 'show_only_specified_categories? false if show_only_specified_categories is false' do 
+        NavConfig.show_only_specified_categories = false
+        assert_equal(NavConfig.show_only_specified_categories?, false)
+    end
+end
