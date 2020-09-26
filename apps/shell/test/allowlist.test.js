eea7b061cfcf53bb08b77c0cb459fde795af47e8
Matthew Hu <matt.hu017@gmail.com>
2020-09-26 14:00:21 -0400

    Added HostAllowlist test methods

--- apps/shell/test/allowlist.test.js
+++ apps/shell/test/allowlist.test.js
@@ -2,6 +2,7 @@
  * Allowlist helper function tests
  */
 const helpers = require('../utils/helpers')
+const { HostAllowlist } = require('../utils/helpers')
 
 /**
  * Constants used during test
@@ -24,5 +25,55 @@ describe('Helper function hostInAllowList()', () => {
 
   test('it should be false for hostname not matched by wildcard FQDN', () => {
     expect(helpers.hostInAllowList(ALLOW_LIST, 'p1001.eleven.osc.edu')).not.toBeTruthy()
+  })
+})
+
+describe('Helper function hostAndDirFromURL()', () => {
+  const default_sshhost = 'owens.osc.edu';
+  test('it should capture hostname and directory path is undefined', () => {
+    let url = 'https://ondemand-test.osc.edu/pun/dev/shell/ssh/pitzer.osc.edu';
+    expect(helpers.hostAndDirFromURL(url, default_sshhost)).toMatchObject(['pitzer.osc.edu', null]);
+  })
+
+  test('it should set hostname to default_sshhost', () => {
+    let url = 'https://ondemand-test.osc.edu/pun/dev/shell/ssh/default';
+    expect(helpers.hostAndDirFromURL(url, default_sshhost)).toMatchObject(['owens.osc.edu', null]);
+  })
+
+  test('it should capture directory path', () => {
+    let url = 'https://ondemand-test.osc.edu/pun/dev/shell/ssh/default/path/to/directory';
+    expect(helpers.hostAndDirFromURL(url, default_sshhost)).toMatchObject(['owens.osc.edu', '/path/to/directory']);
+  })
+}) 
+
+describe('Class HostAllowlist', () => {
+  let cluster_path = "./test/clusters.d"
+
+  test('generates allowlist and default_sshhost correctly', () => {
+    let ood_default_sshhost = "owens.osc.edu"
+    let ood_sshhost_allowlist = "owens.osc.edu:pitzer.osc.edu:*.ten.osc.edu"
+    let host_allowlist = new HostAllowlist(ood_sshhost_allowlist, cluster_path, ood_default_sshhost);
+
+    expect(host_allowlist.allowlist).toMatchObject(new Set(['owens.osc.edu', 'pitzer.osc.edu', 'ruby.osc.edu', "*.ten.osc.edu"]))
+    expect(host_allowlist.default_sshhost).toBe("owens.osc.edu")
+
+  })
+
+  test('if default_sshhost is not declared', () => {
+    let ood_default_sshhost
+    let ood_sshhost_allowlist = "owens.osc.edu:pitzer.osc.edu:*.ten.osc.edu"
+    let host_allowlist = new HostAllowlist(ood_sshhost_allowlist, cluster_path, ood_default_sshhost);
+
+    expect(host_allowlist.allowlist).toMatchObject(new Set(['owens.osc.edu', 'pitzer.osc.edu', 'ruby.osc.edu', "*.ten.osc.edu"]))
+    expect(host_allowlist.default_sshhost).toBe("owens.osc.edu")
+  })
+
+  test('allowlist not declared, allowlist generated from default_sshhost and cluster configs', () => {
+    let ood_default_sshhost = "armstrong.osc.edu"
+    let ood_sshhost_allowlist
+    let host_allowlist = new HostAllowlist(ood_sshhost_allowlist, cluster_path, ood_default_sshhost);
+
+    expect(host_allowlist.allowlist).toMatchObject(new Set(['armstrong.osc.edu', 'owens.osc.edu', 'pitzer.osc.edu', 'ruby.osc.edu']))
+    expect(host_allowlist.default_sshhost).toBe("armstrong.osc.edu")
   })
 })
