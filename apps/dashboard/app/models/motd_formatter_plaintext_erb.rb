c8457bbc4fbaf6a60c661464c22ce95d8e5f5084
KinanAlAttar <alattar.2@osu.edu>
2020-10-20 10:09:24 -0400

    Remove render method in plaintext_erb formatter

--- apps/dashboard/app/models/motd_formatter_plaintext_erb.rb
+++ apps/dashboard/app/models/motd_formatter_plaintext_erb.rb
@@ -4,17 +4,9 @@ class MotdFormatterPlaintextErb
   def initialize(motd_file)
     motd_file = MotdFile.new unless motd_file
     @title = motd_file.title
-    @content = render(motd_file)
+    @content = ERB.new(motd_file.content).result
   end
 
-  def render(motd_file)
-    begin
-      ERB.new(motd_file.content).result
-    rescue Exception => e
-      raise "ERB rendering failed with: #{e.message}\n #{content}"
-    end
-  end
- 
   def to_partial_path
     "dashboard/motd_plaintext"
   end
