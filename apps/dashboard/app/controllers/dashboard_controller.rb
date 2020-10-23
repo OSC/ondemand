1eb21463e96cb599f7a9191f095f44bee90ded71
KinanAlAttar <alattar.2@osu.edu>
2020-10-23 10:58:15 -0400

    Catch StandardError instead of Exception

--- apps/dashboard/app/controllers/dashboard_controller.rb
+++ apps/dashboard/app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
   def index
     begin
       @motd = MotdFile.new.formatter
-    rescue Exception => e
+    rescue StandardError => e
   	  flash.now[:alert] = t('dashboard.motd_erb_render_error', error_message: e.message)
     end
   end
