a4f08783faff636f839363c09b75731c06b8b593
msquee <msqueo@osc.edu>
2020-10-28 13:01:20 -0400

    Gracefully fail if we can't parse cluster configuration
    * Improve readability by utilizing object destructuring
    * Improve error handling

--- apps/shell/utils/host-allowlist.js
+++ apps/shell/utils/host-allowlist.js
@@ -5,27 +5,50 @@ const yaml      = require('js-yaml');
 const fs        = require('fs');
 
 class HostAllowlist {
-  constructor(ood_sshhost_allowlist, clusters_d_path, ood_default_sshhost){
-    this.allowlist = ood_sshhost_allowlist ? new Set(ood_sshhost_allowlist.split(':')) : new Set();
+  constructor(ood_sshhost_allowlist, clusters_d_path, ood_default_sshhost) {
+    this.allowlist = ood_sshhost_allowlist ? new Set(ood_sshhost_allowlist.split(':')) : new Set()
+    this.clusters = new Array()
 
-    //Filter through cluster configs, return array of hashes with host and default
-    let cluster_sshhosts = glob.sync(path.join((clusters_d_path || '/etc/ood/config/clusters.d'), '*.y*ml'))
-    .map(yml => yaml.safeLoad(fs.readFileSync(yml)))
-    .filter(config => (config.v2 && config.v2.login && config.v2.login.host) && ! (config.v2 && config.v2.metadata && config.v2.metadata.hidden))
-    .map(config => {
-      return { host: config.v2.login.host, default: config.v2.login.default ? true : false }
-    })
+    const directory = path.join((clusters_d_path || '/etc/ood/config/clusters.d'))
+    let yamlFiles = glob.sync(path.join(directory, '*.y*ml'))
+  
+    while (yamlFiles.length > 0) {
+      try {
+        // Destructure "v2" property out of yaml configuration.
+        let { v2: version } = yaml.safeLoad(fs.readFileSync(yamlFiles.shift(), 'utf-8'))
+  
+        // Destructure "login" and "metadata" properties out of cluster configuration.
+        let {
+          login: {
+            default: isDefault = version.login.default === undefined ? false : version.login.default, // If cluster configuration does not contain "default" property, set false.
+            host = version.login.host === undefined ? 'localhost' : version.login.host // If host is not defined, set to "localhost".
+          },
+          metadata: {
+            hidden = version.metadata.hidden === undefined ? true : version.metadata.hidden // If cluster configuration does not set "hidden" property, set true.
+          }
+        } = version
+ 
+        this.clusters.push(version)
 
-    //Add to allowlist
-    if (ood_default_sshhost) { 
-      this.addToAllowlist(ood_default_sshhost);
+        if (isDefault) this.default_sshhost = host // Set cluster as default if "login.default" is true.
+        if (!hidden) { // If the cluster is not hidden, add the host to the allowlist.
+          this.addToAllowlist(host)
+        }
+      } catch (err) {
+        const { name, reason, message } = err
+        if (name === 'YAMLException', reason, message) {
+          console.error(name, reason, message)
+        }
+      }
     }
-    cluster_sshhosts.forEach(cluster => {
-      this.addToAllowlist(cluster.host);
-    });
 
-    var cluster = cluster_sshhosts.find(cluster => cluster.default) || cluster_sshhosts[0];
-    this.default_sshhost = ood_default_sshhost || cluster.host;
+    // Find default cluster configuration.
+    let { login: { host } } = this.clusters.find(cluster => cluster.default) || this.clusters.shift() // Find cluster with "login.default", if not found then use first cluster found.
+    this.default_sshhost = ood_default_sshhost || host // ood_default_sshhost takes precedence over default cluster found.
+
+    if (ood_default_sshhost) {
+      this.addToAllowlist(ood_default_sshhost)
+    }
   }
 
   default_sshhost() {
