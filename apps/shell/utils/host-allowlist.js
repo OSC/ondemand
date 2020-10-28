07a915235273a2c9e570ccb8a5511d4a10ee4154
msquee <msqueo@osc.edu>
2020-10-28 11:50:44 -0400

    Indent with 2 spaces instead of tabs

--- apps/shell/utils/host-allowlist.js
+++ apps/shell/utils/host-allowlist.js
@@ -5,45 +5,45 @@ const yaml      = require('js-yaml');
 const fs        = require('fs');
 
 class HostAllowlist {
-    constructor(ood_sshhost_allowlist, clusters_d_path, ood_default_sshhost){
-        this.allowlist = ood_sshhost_allowlist ? new Set(ood_sshhost_allowlist.split(':')) : new Set();
-  
-        //Filter through cluster configs, return array of hashes with host and default
-        let cluster_sshhosts = glob.sync(path.join((clusters_d_path || '/etc/ood/config/clusters.d'), '*.y*ml'))
-        .map(yml => yaml.safeLoad(fs.readFileSync(yml)))
-        .filter(config => (config.v2 && config.v2.login && config.v2.login.host) && ! (config.v2 && config.v2.metadata && config.v2.metadata.hidden))
-        .map(config => {
-            return { host: config.v2.login.host, default: config.v2.login.default ? true : false }
-        })
+  constructor(ood_sshhost_allowlist, clusters_d_path, ood_default_sshhost){
+    this.allowlist = ood_sshhost_allowlist ? new Set(ood_sshhost_allowlist.split(':')) : new Set();
 
-        //Add to allowlist
-        if (ood_default_sshhost) { 
-            this.addToAllowlist(ood_default_sshhost);
-        }
-        cluster_sshhosts.forEach(cluster => {
-            this.addToAllowlist(cluster.host);
-        });
+    //Filter through cluster configs, return array of hashes with host and default
+    let cluster_sshhosts = glob.sync(path.join((clusters_d_path || '/etc/ood/config/clusters.d'), '*.y*ml'))
+    .map(yml => yaml.safeLoad(fs.readFileSync(yml)))
+    .filter(config => (config.v2 && config.v2.login && config.v2.login.host) && ! (config.v2 && config.v2.metadata && config.v2.metadata.hidden))
+    .map(config => {
+      return { host: config.v2.login.host, default: config.v2.login.default ? true : false }
+    })
 
-        var cluster = cluster_sshhosts.find(cluster => cluster.default) || cluster_sshhosts[0];
-        this.default_sshhost = ood_default_sshhost || cluster.host;
+    //Add to allowlist
+    if (ood_default_sshhost) { 
+      this.addToAllowlist(ood_default_sshhost);
     }
-  
-    default_sshhost() {
-        return this.default_sshhost;
-    }
-  
-    allowlist() {
-        return this.allowlist;
-    }
+    cluster_sshhosts.forEach(cluster => {
+      this.addToAllowlist(cluster.host);
+    });
 
-    addToAllowlist(host) {
-        this.allowlist.add(host);
-    }
+    var cluster = cluster_sshhosts.find(cluster => cluster.default) || cluster_sshhosts[0];
+    this.default_sshhost = ood_default_sshhost || cluster.host;
+  }
 
-    hostInAllowlist(host) {
-        var arrAllowlist = Array.from(this.allowlist);
-        return arrAllowlist.some((value) => minimatch(host, value))
-    }
+  default_sshhost() {
+    return this.default_sshhost;
+  }
+
+  allowlist() {
+    return this.allowlist;
+  }
+
+  addToAllowlist(host) {
+    this.allowlist.add(host);
+  }
+
+  hostInAllowlist(host) {
+    var arrAllowlist = Array.from(this.allowlist);
+    return arrAllowlist.some((value) => minimatch(host, value))
+  }
 }
 
 module.exports = HostAllowlist
