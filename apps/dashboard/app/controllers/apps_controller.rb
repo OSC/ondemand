12da04609a62ce5b62139e699607225b6ff0eb73
KinanAlAttar <alattar.2@osu.edu>
2020-12-30 20:36:41 -0500

    Set SVG precedence in rendering icon

--- apps/dashboard/app/controllers/apps_controller.rb
+++ apps/dashboard/app/controllers/apps_controller.rb
@@ -53,10 +53,10 @@ class AppsController < ApplicationController
   def icon
     set_app
 
-    if @app.png_icon? 
-      send_file @app.icon_path, :type => 'image/png', :disposition => 'inline'
-    elsif @app.svg_icon?
+    if @app.svg_icon? 
       send_file @app.icon_path, :type => 'image/svg+xml', :disposition => 'inline'
+    elsif @app.png_icon?
+      send_file @app.icon_path, :type => 'image/png', :disposition => 'inline'
     else
       raise ActionController::RoutingError.new('Not Found')
     end
