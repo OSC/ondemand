b6279312dffc6d5adb82c9802f8d10089590ea5c
KinanAlAttar <alattar.2@osu.edu>
2021-01-06 15:03:35 -0500

    Revert "Revert "Add a class for general erb rendering""
    This reverts commit b70304431a2dab28ab29b975997210a97f679efd.

--- apps/dashboard/app/models/motd_formatter_markdown_erb.rb
+++ apps/dashboard/app/models/motd_formatter_markdown_erb.rb
@@ -1,10 +1,12 @@
+require 'erb_render'
+
 class MotdFormatterMarkdownErb
   attr_reader :content, :title
 
   def initialize(motd_file)
     motd_file ||= MotdFile.new unless motd_file
     @title = motd_file.title
-    @content = OodAppkit.markdown.render(ERB.new(motd_file.content).result)
+    @content = OodAppkit.markdown.render(ERBRender.motd_render_erb(motd_file))
   end
   
   def to_partial_path
