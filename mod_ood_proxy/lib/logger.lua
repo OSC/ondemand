273c25e7ee5f0a13bd529c827ab063347affaba9
Mario Squeo <msqueo@osc.edu>
2021-01-06 17:38:32 -0500

    Fix timestamp generation

--- mod_ood_proxy/lib/logger.lua
+++ mod_ood_proxy/lib/logger.lua
@@ -19,7 +19,8 @@ function logger(r)
     msg["log_hook"] = "ood"
 
     -- log
-    msg["log_time"] = os.date("!%Y-%m-%dT%T", time / 1000000) .. "." .. time % 1000000 .. "Z"
+    -- msg["log_time"] = os.date("!%Y-%m-%dT%T", time / 1000000) .. "." .. time % 1000000 .. "Z"
+    msg["log_time"] = os.date("!%Y-%m-%dT%TZ")
     msg["log_id"]   = r.log_id
 
     -- user
