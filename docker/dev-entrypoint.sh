7a291e132e5e774fd9f7f6f81903d0a681a3aa4e
Jeff Ohrstrom <johrstrom@hotmail.com>
2021-01-29 10:41:40 -0500

    setup in sudo context bc container mount points w/ higher kernel & podman versions wont let you read them

--- docker/dev-entrypoint.sh
+++ docker/dev-entrypoint.sh
@@ -8,18 +8,18 @@ export USER=$(whoami) # $USER isn't set!?
 APP_DEV_DIR="/home/$USER/ondemand/dev"
 OOD_DEV_DIR="/var/www/ood/apps/dev/$USER"
 
-sudo su root <<MKDEV
+sudo su root <<SETUP
   mkdir -p $OOD_DEV_DIR 
   chmod 755 $OOD_DEV_DIR
   cd $OOD_DEV_DIR
   ln -s $APP_DEV_DIR gateway
-MKDEV
 
-/opt/ood/ood-portal-generator/sbin/update_ood_portal
+  /opt/ood/ood-portal-generator/sbin/update_ood_portal
 
-if [ -n "$OOD_STATIC_USER" ] && [ -f "$OOD_STATIC_USER" ]; then
-  cat "$OOD_STATIC_USER" >>  /etc/ood/dex/config.yaml
-fi
+  if [ -n "$OOD_STATIC_USER" ] && [ -f "$OOD_STATIC_USER" ]; then
+    cat "$OOD_STATIC_USER" >>  /etc/ood/dex/config.yaml
+  fi
+SETUP
 
 sudo runuser -u ondemand-dex /usr/sbin/ondemand-dex serve /etc/ood/dex/config.yaml &
 sudo /usr/sbin/httpd -DFOREGROUND
