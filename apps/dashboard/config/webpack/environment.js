44701d7a5003b1b63967b71feca6d1734a2b9aa6
Mario Squeo <msqueo@osc.edu>
2021-02-05 11:49:22 -0500

    Add jquery webpack alias

--- apps/dashboard/config/webpack/environment.js
+++ apps/dashboard/config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+
+const config = environment.toWebpackConfig()
+
+config.resolve.alias = {
+ jquery: 'jquery/src/jquery'
+}
 
 module.exports = environment
