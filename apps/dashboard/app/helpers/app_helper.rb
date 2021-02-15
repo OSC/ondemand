6900135c6ca8bf1d791dc9af903f13d10d3a5216
Jeff Ohrstrom <johrstrom@hotmail.com>
2021-02-15 12:40:21 -0500

    add helper for install location lookup that's internationalized

--- apps/dashboard/app/helpers/app_helper.rb
+++ apps/dashboard/app/helpers/app_helper.rb
@@ -5,4 +5,17 @@ module AppHelper
   rescue
     text
   end
+
+  def install_location(app)
+    case app.type
+    when :sys
+      I18n.t('dashboard.all_apps_table_install_location_sys')
+    when :dev
+      I18n.t('dashboard.all_apps_table_install_location_dev')
+    when :usr
+      I18n.t('dashboard.all_apps_table_install_location_usr')
+    else
+      I18n.t('dashboard.unknown')
+    end
+  end
 end
