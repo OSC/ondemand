cafdd10259186fd0e7ae897824ea88ddf00d38cc
Eric Franz <efranz@osc.edu>
2021-02-23 14:22:45 -0500

    use hterm profiles for themes
    prepolulate hterm.defaultStorage with theme values, an hterm profile per
    theme, so that we can just use hterm specify the starting profile and switch
    by using term.setProfile

--- apps/shell/public/javascripts/ood_shell.1.js
+++ apps/shell/public/javascripts/ood_shell.1.js
@@ -1,9 +1,8 @@
-
 // Object that defines a terminal element
-function OodShell(element, url, prefs) {
+function OodShell(element, url, profile) {
   this.element = element;
   this.url     = url;
-  this.prefs   = prefs || {};
+  this.profile   = profile || "default";
   this.socket  = null;
   this.term    = null;
 }
@@ -15,28 +14,12 @@ OodShell.prototype.createTerminal = function () {
   this.socket.onclose   = this.closeTerminal.bind(this);
 };
 
+
 OodShell.prototype.runTerminal = function () {
   var that = this;
 
-  // Set backing store that hterm uses to read/write preferences
-  hterm.defaultStorage = new lib.Storage.Memory();
-
   // Create an instance of hterm.Terminal
-  this.term = new hterm.Terminal();
-
-  // FIXME:
-  // instead of changing theme here, idealy we would initialize
-  // the custom storage used by preferences to associate a profile_id with a set of preferences
-  // then above we would do new hterm.Terminal(profile_id)
-  // and when we change the theme we would do
-  // term.setProfile(profile_id)
-  //
-  // but as you see from https://gist.github.com/russelldavies/5745528 as an example
-  // there isn't a clear approach to doing this because the way to create a profile appears to be through a term object
-  // though we could create a throw-away term object as an experiment
-  //
-  // for now we change the theme here but add guard clauses to hterm when screen doesn't exist yet
-  this.changeTheme(this.prefs)
+  this.term = new hterm.Terminal(this.profile);
 
   // Handler that fires when terminal is initialized and ready for use
   this.term.onTerminalReady = function () {
@@ -107,9 +90,7 @@ OodShell.prototype.sendString = function (str) {
 };
 
 OodShell.prototype.changeTheme = function (theme) {
-  for (var k in theme) {
-    this.term.prefs_.set(k, theme[k]);
-  }
+  this.term.setProfile(theme);
 }
 
 OodShell.prototype.onTerminalResize = function (columns, rows) {
