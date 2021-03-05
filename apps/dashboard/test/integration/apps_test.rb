97af2b95860ea8a39bca90879dcdc1a1ea701141
Jeff Ohrstrom <johrstrom@hotmail.com>
2021-03-05 16:50:37 -0500

    move /apps/index tests to integration tests

--- apps/dashboard/test/integration/apps_test.rb
+++ apps/dashboard/test/integration/apps_test.rb
@@ -2,6 +2,100 @@ require 'test_helper'
 
 class AppsTest < ActionDispatch::IntegrationTest
 
+  UserDouble = Struct.new(:name)
+
+  def setup
+    SysRouter.stubs(:base_path).returns(Rails.root.join("test/fixtures/sys_with_interactive_apps"))
+    DevRouter.stubs(:base_path).returns(Rails.root.join("test/fixtures/dev"))
+    OodAppkit.stubs(:clusters).returns(OodCore::Clusters.load_file("test/fixtures/config/clusters.d"))
+
+    Router.instance_variable_set('@pinned_apps', nil)
+    OodSupport::Process.stubs(:user).returns(UserDouble.new('me'))
+    OodSupport::User.stubs(:new).returns(UserDouble.new('me'))
+    FileUtils.chmod 0000, 'test/fixtures/usr/cant_see/'
+    UsrRouter.stubs(:base_path).with(:owner => "me").returns(Pathname.new("test/fixtures/usr/me"))
+    UsrRouter.stubs(:base_path).with(:owner => 'shared').returns(Pathname.new("test/fixtures/usr/shared"))
+    UsrRouter.stubs(:base_path).with(:owner => 'cant_see').returns(Pathname.new("test/fixtures/usr/cant_see"))
+    UsrRouter.stubs(:owners).returns(['me', 'shared', 'cant_see'])
+  end
+
+  def teardown
+    FileUtils.chmod 0755, 'test/fixtures/usr/cant_see/'
+    Router.instance_variable_set('@pinned_apps', nil)
+  end
+
+  test "default table is correct" do
+    OodAppkit.stubs(:clusters).returns([]) # this gets rid of interactive apps
+    get "/apps/index"
+
+    headers = css_select('tr[id="all-apps-table-header"] th')
+    assert_equal I18n.t('dashboard.all_apps_table_app_column'), headers[0].text
+    assert_equal I18n.t('dashboard.all_apps_table_install_column'), headers[1].text
+    assert_equal I18n.t('dashboard.all_apps_table_category_column'), headers[2].text
+    assert_equal I18n.t('dashboard.all_apps_table_sub_category_column'), headers[3].text
+
+    data_rows = css_select('table[id="all-apps-table"] tr').slice(1, 100)
+
+    # ensure the table has only system installed apps
+    assert_equal 5, data_rows.size
+    system_install = I18n.t('dashboard.all_apps_table_install_location_sys')
+    data_rows.each do |row|
+      assert_equal system_install, row.xpath('./td[2]').text
+    end
+
+    row_ids = [
+      "pun-sys-shell-ssh-default",
+      "apps-show-systemstatus",
+      "pun-sys-files",
+      "apps-show-activejobs",
+      "apps-show-myjobs",
+    ]
+
+    row_ids.each do |id|
+      assert_select "tr[id='#{id}']", 1
+    end
+  end
+
+  test "table shows interactive sys apps with dev and usr apps" do
+    Configuration.stubs(:app_development_enabled?).returns(true)
+    Configuration.stubs(:app_sharing_enabled?).returns(true)
+    UsrRouter.stubs(:base_path).with(:owner => "me").returns(Pathname.new("test/fixtures/usr/me"))
+    UsrRouter.stubs(:base_path).with(:owner => 'shared').returns(Pathname.new("test/fixtures/usr/shared"))
+    UsrRouter.stubs(:base_path).with(:owner => 'cant_see').returns(Pathname.new("test/fixtures/usr/cant_see"))
+    UsrRouter.stubs(:owners).returns(['me', 'shared', 'cant_see'])
+
+    OodSupport::Process.stubs(:user).returns(UserDouble.new('me'))
+
+    get "/apps/index"
+
+    data_rows = css_select('table[id="all-apps-table"] tr').slice(1, 100)
+
+    assert_equal 14, data_rows.size
+
+    # difference here is shell apps have hosts in them (and there are 2 of them)
+    # all interactive apps are shown along with the dev and usr apps
+    row_ids = [
+      "pun-sys-shell-ssh-oakley.osc.edu",
+      "pun-sys-shell-ssh-owens.osc.edu",
+      "apps-show-systemstatus",
+      "pun-sys-files",
+      "batch_connect-sys-bc_desktop-oakley-session_contexts-new",
+      "batch_connect-sys-bc_jupyter-session_contexts-new",
+      "batch_connect-sys-bc_paraview-session_contexts-new",
+      "apps-show-activejobs",
+      "apps-show-myjobs",
+      "batch_connect-dev-bc_rstudio-session_contexts-new",  # the dev app
+      "apps-show-my_shared_app-usr-me",                     # the usr apps
+      "batch_connect-usr-shared-bc_app-session_contexts-new",
+      "batch_connect-usr-shared-bc_with_subapps-owens-session_contexts-new",
+      "batch_connect-usr-shared-bc_with_subapps-oakley-session_contexts-new"
+    ]
+
+    row_ids.each do |id|
+      assert_select "tr[id='#{id}']", 1
+    end
+  end
+
   test "index table adds metadata columns" do
     OodAppkit.stubs(:clusters).returns(OodCore::Clusters.load_file("test/fixtures/config/clusters.d"))
     Configuration.stubs(:app_development_enabled?).returns(true)
