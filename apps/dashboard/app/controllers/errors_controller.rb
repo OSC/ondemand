a8a8e178b0e512b1414f477b38f3e8106c5530c5
Eric Franz <efranz@osc.edu>
2021-03-31 09:47:25 -0400

    support json for default errors controller

--- apps/dashboard/app/controllers/errors_controller.rb
+++ apps/dashboard/app/controllers/errors_controller.rb
@@ -9,7 +9,10 @@ class ErrorsController < ApplicationController
   # the routes.rb is set to the app to handle exceptions in application.rb
   #
   def not_found
-    render status: 404
+    respond_to do |format|
+      format.html { render status: :not_found }
+      format.json { render nothing: true, status: :not_found }
+    end
   end
 
   def internal_server_error
@@ -22,6 +25,10 @@ class ErrorsController < ApplicationController
 
     #TODO: log exception information you want to log here
     # after removing it from the normal logging via lograge
-    render status: 500
+
+    respond_to do |format|
+      format.html { render status: :internal_server_error }
+      format.json { render nothing: true, status: :internal_server_error }
+    end
   end
 end
