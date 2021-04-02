08a921976180de53496f01753166f2602839978b
Eric Franz <efranz@osc.edu>
2021-04-02 15:21:40 -0400

    path breadcrumbs to suffix / to each segment

--- apps/dashboard/app/helpers/files_helper.rb
+++ apps/dashboard/app/helpers/files_helper.rb
@@ -3,4 +3,14 @@ module FilesHelper
   def sort_by_created_at(transfers)
     transfers.sort_by { |t| - t.created_at }
   end
+
+  def path_segment_with_slash(segment, counter, total)
+    # TODO: ucomment if we decide to omit trailing slash on current directory
+    # if counter == 0 || counter == total - 1
+    if counter == 0
+      segment
+    else
+      segment + " /"
+    end
+  end
 end
