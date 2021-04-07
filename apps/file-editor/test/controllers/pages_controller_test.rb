fc95736a2feef6b868faaf7bcede34c0920c79a9
Jeff Ohrstrom <johrstrom@osc.edu>
2021-04-07 10:30:39 -0400

    add file-editor files back to make the PR a little easier to understand

---
+++ apps/file-editor/test/controllers/pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+end
