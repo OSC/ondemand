3cf46d73192ea090f91dd67e5e0c45b632e73b66
Mario Squeo <msqueo@osc.edu>
2021-04-07 17:34:35 -0400

    Create shell_links helper in Files app

--- apps/dashboard/app/helpers/files_helper.rb
+++ apps/dashboard/app/helpers/files_helper.rb
@@ -4,6 +4,22 @@ module FilesHelper
     transfers.sort_by { |t| - t.created_at }
   end
 
+  def shell_links
+    html = ""
+
+    Configuration.login_clusters.each{ |cluster|
+      html.concat(tag.a(
+        cluster.title,
+        href: OodAppkit.shell.url(path: cluster.title.lower).to_s,
+        class: 'dropdown-item',
+        role: 'menuitem',
+        target: '_blank'
+      ))
+    }
+
+    html.html_safe
+  end
+
   def path_segment_with_slash(segment, counter, total)
     # TODO: ucomment if we decide to omit trailing slash on current directory
     # if counter == 0 || counter == total - 1
