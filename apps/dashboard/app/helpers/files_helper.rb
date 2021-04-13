b6a48cc0e30750ccbfb06f063ce4cea91722e6f5
Mario Squeo <msqueo@osc.edu>
2021-04-13 10:22:51 -0400

    Remove shell_links helper

--- apps/dashboard/app/helpers/files_helper.rb
+++ apps/dashboard/app/helpers/files_helper.rb
@@ -4,23 +4,6 @@ module FilesHelper
     transfers.sort_by { |t| - t.created_at }
   end
 
-  def shell_links
-    html = ""
-
-    Configuration.login_clusters.each{ |cluster|
-      link = OodAppLink.new(
-        title: cluster.title,
-        url: OodAppkit::Urls::Shell.new(base_url: '/').url(host: cluster.title.downcase),
-        icon_uri: 'fas://terminal',
-        new_tab: true
-      ).url
-
-      html.concat(link)
-    }
-
-    html.html_safe
-  end
-
   def path_segment_with_slash(segment, counter, total)
     # TODO: ucomment if we decide to omit trailing slash on current directory
     # if counter == 0 || counter == total - 1
