fd9274f8c7cdafcade0fca3d6bc5b7b8fccc48ad
Jeff Ohrstrom <johrstrom@osc.edu>
2021-04-20 11:04:31 -0400

    add redirects in the PUN config for the files and file-editor apps (#1056)

--- spec/e2e/browser_spec.rb
+++ spec/e2e/browser_spec.rb
@@ -38,10 +38,38 @@ describe 'OnDemand browser test' do
     expect(browser.table(id: 'job_status_table').present?).to be true
   end
 
-  it 'redirects from /pun/sys/activejobs preserve query params' do
+  it 'redirects from /pun/sys/activejobs and preserves query params' do
     browser.goto "#{ctr_base_url}/pun/sys/activejobs?jobcluster=all&jobfilter=all"
+    browser.screenshot.save 'activejobs.png'
     expect(browser.url).to eq("#{ctr_base_url}/pun/sys/dashboard/activejobs?jobcluster=all&jobfilter=all")
     expect(browser.title).to eq('Dashboard - Open OnDemand')
     expect(browser.table(id: 'job_status_table').present?).to be true
+  end
+
+  it 'redirects /pun/sys/file-editor#index' do
+    browser.goto "#{ctr_base_url}/pun/sys/file-editor"
+    expect(browser.url).to eq("#{ctr_base_url}/pun/sys/dashboard/files/fs/home/ood")
+    expect(browser.title).to eq('Dashboard - Open OnDemand')
+    expect(browser.table(id: 'directory-contents').present?).to be true
+  end
+
+  it 'redirects /pun/sys/file-editor and preserves the file' do
+    browser.goto "#{ctr_base_url}/pun/sys/file-editor/edit/home/ood/.bashrc"
+    expect(browser.url).to eq("#{ctr_base_url}/pun/sys/dashboard/files/edit/home/ood/.bashrc")
+    expect(browser.title).to eq('File Editor - Open OnDemand - .bashrc')
+  end
+
+  it 'redirects /pun/sys/files#index' do
+    browser.goto "#{ctr_base_url}/pun/sys/files"
+    expect(browser.url).to eq("#{ctr_base_url}/pun/sys/dashboard/files/fs/home/ood")
+    expect(browser.title).to eq('Dashboard - Open OnDemand')
+    expect(browser.table(id: 'directory-contents').present?).to be true
+  end
+
+  it 'redirects /pun/sys/files and preserves the path' do
+    browser.goto "#{ctr_base_url}/pun/sys/files/fs/var/www/ood"
+    expect(browser.url).to eq("#{ctr_base_url}/pun/sys/dashboard/files/fs/var/www/ood")
+    expect(browser.title).to eq('Dashboard - Open OnDemand')
+    expect(browser.table(id: 'directory-contents').present?).to be true
   end
 end
