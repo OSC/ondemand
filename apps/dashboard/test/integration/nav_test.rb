f0383a7f6edbbdbe7f6ae393a903c75c00ff980c
Eric Franz <efranz@osc.edu>
2021-05-01 21:41:42 -0400

    control open_apps_in_new_window? using manifest
    when default is to open in new window,
    any OodApp will open in a new window unless the
    manifest of that app specifies otherwise
    using new_window: true or new_window: false
    
    set activejobs and files to always open in same window using manifest
    set file-editor and shell to always open in new window using manifest
    
    specified default via Configuration.open_apps_in_new_window?
    default is true (unless default is changed using OOD_OPEN_APPS_IN_NEW_WINDOW)

---
+++ apps/dashboard/test/integration/nav_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class NavTest < ActionDispatch::IntegrationTest
+ test "default for app to open in new window" do
+   SysRouter.stubs(:base_path).returns(Rails.root.parent)
+   Configuration.stubs(:open_apps_in_new_window?).returns(true)
+
+   get '/'
+
+   link = css_select('a[title="Job Composer"]').first
+
+   assert link, "Job Composer link not found on index page"
+   assert_equal '_blank', link['target'], 'Job Composer link should be set to open in new window'
+ end
+
+ test "default for app to open in same window" do
+   SysRouter.stubs(:base_path).returns(Rails.root.parent)
+   Configuration.stubs(:open_apps_in_new_window?).returns(false)
+
+   get '/'
+
+   link = css_select('a[title="Job Composer"]').first
+
+   assert link, 'Job Composer link not found on index page'
+   refute link['target'], 'Job Composer link should be set to open in same window'
+ end
+end
