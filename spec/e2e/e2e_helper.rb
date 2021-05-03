b7dada5229769a8e52ba25ac9ce2695f75ce88db
Jeff Ohrstrom <johrstrom@osc.edu>
2021-05-03 12:19:29 -0400

    simplify browser_login retry logic

--- spec/e2e/e2e_helper.rb
+++ spec/e2e/e2e_helper.rb
@@ -14,20 +14,14 @@ end
 def browser_login(browser)
   # sometimes you need to retry to let the container start up, so retry to make the tests
   # a little less flaky
-  [*1..5].each do |try|
-    begin
-      browser.goto ctr_base_url
-      browser.text_field(id: 'username').set "ood@localhost"
-      browser.text_field(id: 'password').set "password"
-      browser.button(id: 'submit-login').click
-      break
-    rescue => e
-      puts "can't login. retry number #{try}"
-      raise e if try == 5
-
-      `sleep 3`
-    end
+  Watir::Wait.until(timeout: 30, interval: 3) do
+    `curl --head -w '%{http_code}' -o /dev/null -s http://localhost:8080/`.eql?('302')
   end
+
+  browser.goto ctr_base_url
+  browser.text_field(id: 'username').set "ood@localhost"
+  browser.text_field(id: 'password').set "password"
+  browser.button(id: 'submit-login').click
 end
 
 def hook_fixture(file)
