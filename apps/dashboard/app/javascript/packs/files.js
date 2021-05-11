a53f26c9dd9b500a552bd122956b76f037c7fe7d
Eric Franz <efranz@osc.edu>
2021-05-10 22:29:04 -0400

    disable all animations in Swal

--- apps/dashboard/app/javascript/packs/files.js
+++ apps/dashboard/app/javascript/packs/files.js
@@ -2,4 +2,13 @@ import ClipboardJS from 'clipboard'
 import Swal from 'sweetalert2'
 
 window.ClipboardJS = ClipboardJS
-window.Swal        = Swal
+window.Swal        = Swal.mixin({
+  showClass: {
+    popup: 'swal2-noanimation',
+    backdrop: 'swal2-noanimation'
+  },
+  hideClass: {
+    popup: '',
+    backdrop: ''
+  }
+});
