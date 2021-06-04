3b8d1d3e396a09bed02087e100384da6f7e4f0da
KinanAlAttar <alattar.2@osu.edu>
2021-06-04 18:20:00 -0400

    fix formatting

--- ood-portal-generator/spec/ood_portal_generator_view_spec.rb
+++ ood-portal-generator/spec/ood_portal_generator_view_spec.rb
@@ -2,12 +2,14 @@ require 'spec_helper'
 require File.expand_path '../../lib/ood_portal_generator', __FILE__
 
 describe OodPortalGenerator::View do
-  describe 'dodes config opts match with example opts?' do
+  describe 'does config opts match with example opts?' do
     it 'returns true if opts match' do
       config_opts = OodPortalGenerator::View.new
-        .instance_variables.map(&:to_s).map { |opt| opt.delete("@") }
-      example_config_opts = File.read(
-        './share/ood_portal_example.yml').scan(/#([\w_]+):/).flatten
+          .instance_variables
+          .map(&:to_s)
+          .map { |opt| opt.delete("@") }
+      
+      example_config_opts = File.read('./share/ood_portal_example.yml').scan(/#([\w_]+):/).flatten
 
       # remove dex as it's not part of the view
       example_config_opts -= %w(dex) 
@@ -15,8 +17,7 @@ describe OodPortalGenerator::View do
       # delete inst vars that are not actual options in the example file
       config_opts -= %w(protocol oidc_redirect_uri oidc_crypto_passphrase)
 
-      expect(
-        config_opts + example_config_opts - (config_opts & example_config_opts)).to be_empty
+      expect(config_opts + example_config_opts - (config_opts & example_config_opts)).to be_empty
     end
   end
 end
