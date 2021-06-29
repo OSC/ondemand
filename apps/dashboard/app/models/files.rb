8e41a523762c4919e2894217cb1a280c4fcefdef
Mario Squeo <6081892+msquee@users.noreply.github.com>
2021-06-29 12:03:52 -0400

    Fix files preview. Set charset=utf-8 in Content-Type header (#1254)
    * Fix files preview. Set charset=utf-8 in Content-Type header
    
    * Render text formats as text/plain

--- apps/dashboard/app/models/files.rb
+++ apps/dashboard/app/models/files.rb
@@ -132,11 +132,10 @@ class Files
     #     x === 'application/x-yaml'
     #     # => true
     #
-    case type
-    when 'text/javascript', 'text/css', 'text/yaml', 'text/yml'
-      'text/plain'
-    else
+    if %r{text/.*}.match(type).nil?
       type
+    else
+      'text/plain; charset=utf-8'
     end
   end
 
