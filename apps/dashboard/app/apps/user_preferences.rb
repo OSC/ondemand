6dcc4a7c3a52d12cd9729b94720c2c7be8355666
Samir Mansour <mansour.69@osu.edu>
2021-07-07 11:43:55 -0400

    Changed instance variable name, simplified nesting of yaml and changed directory where preferences could exist

--- apps/dashboard/app/apps/user_preferences.rb
+++ apps/dashboard/app/apps/user_preferences.rb
@@ -2,18 +2,8 @@ class UserPreferences
     require 'yaml'
   
     def initialize()
-      _prefs
-    end
-  
-    def preferences_file
-        @preferences_file ||= begin
-            file = ENV['OOD_PREFERENCES_FILE'] || "#{User.new.dir}/preferences.yml"
-            Pathname.new(file.to_s).expand_path
-        end
-    end
-    
-    private def _prefs
-        @_prefs ||= begin
+        FileUtils.mkdir_p("~/.config/ondemand/") unless File.exists?("~/.config/ondemand/")
+        @preferences ||= begin
             if preferences_file.file? && preferences_file.readable?
                 YAML.safe_load(preferences_file.read)
             else
@@ -21,23 +11,38 @@ class UserPreferences
             end
         rescue => e
             puts "#{e.message}"
-            @_prefs = {}
+            @preferences = {}
+        end
+    end
+
+    attr_accessor :preferences
+  
+    def preferences_file
+        @preferences_file ||= begin
+            file = ENV['OOD_PREFERENCES_FILE'] || "#{User.new.dir}/.config/ondemand/preferences.yml"
+            Pathname.new(file.to_s).expand_path
         end
     end
   
     def method_missing(method_name, *args, &block)
         method_string = method_name.to_s
-        if @_prefs.key?(method_string)
-            @_prefs.dig(method_string, *args)
+        if @preferences.key?(method_string)
+            @preferences.dig(method_string, *args)
         else
             super
         end
     end
 
-    def write_setting(app_name, settings_hash)
+    def write_setting(settings_hash)
         settings_hash.each do |key, value|
-            @_prefs[app_name][key] = value
+            @preferences[key] = value
         end
-        File.open(@preferences_file, "w") { |file| file.write(@_prefs.to_yaml) }
+        
+        begin
+            File.open(@preferences_file, "w") { |file| file.write(@preferences.to_yaml) }
+        rescue => e
+            puts "Failed to write to preferences file: #{e.message}"
+        end
+
     end
 end
