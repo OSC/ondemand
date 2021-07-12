da80a51e066fad16f85b5979fcd72495870e1cc5
Samir Mansour <mansour.69@osu.edu>
2021-07-12 09:34:14 -0400

    Fix for locale fallback bug and changed location of locale settings to config/application.rb

--- apps/dashboard/config/application.rb
+++ apps/dashboard/config/application.rb
@@ -35,6 +35,12 @@ module Dashboard
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.i18n.default_locale = :en
+    config.i18n.available_locales = %i(en zh-CN)
+    # Note: symbol is wrapped in single quotes because of escaped hyphen.
+    config.i18n.fallbacks = {
+      'zh-CN': :en
+    }
 
     # Custom error pages
     config.exceptions_app = self.routes
