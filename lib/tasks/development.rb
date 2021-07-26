74149baf44b9b09e869d3c460db2cc147b1f2a68
Jeff Ohrstrom <johrstrom@osc.edu>
2021-07-26 18:28:22 -0400

    use right rubocop and fix some warnings

--- lib/tasks/development.rb
+++ lib/tasks/development.rb
@@ -1,19 +1,18 @@
 # frozen_string_literal: true
 
 namespace :dev do
-
   require_relative 'build_utils'
   require 'yaml'
   require 'bcrypt'
   include BuildUtils
 
   def dev_container_name
-    'ood-dev' || "#{ENV['OOD_DEV_CONTAINER_NAME']}"
+    'ood-dev' || ENV['OOD_DEV_CONTAINER_NAME'].to_s
   end
 
   def init_ood_portal
     file = "#{config_directory}/config/ood_portal.yml"
-    return if File.exists?(file)
+    return if File.exist?(file)
 
     FileUtils.mkdir_p("#{config_directory}/config")
 
@@ -24,7 +23,7 @@ namespace :dev do
         'listen_addr_port': 8080,
         'oidc_remote_user_claim': 'email',
         'dex': {
-          'connectors':  [{
+          'connectors': [{
             'type': 'mockCallback',
             'id': 'mock',
             'name': 'Mock'
@@ -43,14 +42,14 @@ namespace :dev do
     plain_password = STDIN.noecho(&:gets).chomp
     bcrypted = BCrypt::Password.create(plain_password)
 
-    content = <<CONTENT
-enablePasswordDB: true
-staticPasswords:
-- email: "#{user.name}@localhost"
-  hash: "#{bcrypted}"
-  username: "#{user.name}"
-  userID: "71e63e31-7af3-41d7-add2-575568f4525f"
-CONTENT
+    content = <<~CONTENT
+      enablePasswordDB: true
+      staticPasswords:
+      - email: "#{user.name}@localhost"
+        hash: "#{bcrypted}"
+        username: "#{user.name}"
+        userID: "71e63e31-7af3-41d7-add2-575568f4525f"
+    CONTENT
 
     File.open(file, File::WRONLY|File::CREAT|File::EXCL) do |f|
       f.write(content)
@@ -68,7 +67,7 @@ CONTENT
   def podman_rt_args
     [
       '--userns', 'keep-id',
-      '--cap-add', 'sys_ptrace',
+      '--cap-add', 'sys_ptrace'
     ].freeze
   end
 
@@ -81,7 +80,7 @@ CONTENT
   end
 
   def dev_mounts
-    [ 
+    [
       '-v', "#{config_directory}:/etc/ood",
       '-v', "#{user.dir}/ondemand:#{user.dir}/ondemand"
     ]
@@ -89,14 +88,14 @@ CONTENT
 
   desc 'Start development container'
   task :start => ['package:dev_container', 'ensure_dev_files'] do
-    ctr_args = [ container_runtime, 'run', '-p 8080:8080', '-p 5556:5556' ]
-    ctr_args.concat ["--name #{dev_container_name}" ]
-    ctr_args.concat [ '--rm', '--detach' ]
-    ctr_args.concat [ '-e', 'OOD_STATIC_USER=/etc/ood/config/static_user.yml' ]
+    ctr_args = [container_runtime, 'run', '-p 8080:8080', '-p 5556:5556']
+    ctr_args.concat ["--name #{dev_container_name}"]
+    ctr_args.concat ['--rm', '--detach']
+    ctr_args.concat ['-e', 'OOD_STATIC_USER=/etc/ood/config/static_user.yml']
     ctr_args.concat dev_mounts
     ctr_args.concat container_rt_args
 
-    ctr_args.concat [ "#{dev_image_name}:latest" ]
+    ctr_args.concat ["#{dev_image_name}:latest"]
     sh ctr_args.join(' ')
   end
 
@@ -110,11 +109,11 @@ CONTENT
 
   desc 'Bash exec into the development container'
   task :exec do
-    ctr_args = [ container_runtime, 'exec', '-it' ]
+    ctr_args = [container_runtime, 'exec', '-it']
     # home is set to /root? could be bug for me
-    ctr_args.concat [ '-e', "HOME=#{user.dir}" ]
-    ctr_args.concat [ '--workdir', user.dir.to_s ]
-    ctr_args.concat [ dev_container_name, '/bin/bash' ]
+    ctr_args.concat ['-e', "HOME=#{user.dir}"]
+    ctr_args.concat ['--workdir', user.dir.to_s]
+    ctr_args.concat [dev_container_name, '/bin/bash']
 
     sh ctr_args.join(' ')
   end
@@ -123,11 +122,11 @@ CONTENT
 
   # let advanced users know this, not --tasks
   task :ensure_dev_files do
-      [
-        :init_ood_portal,
-        :init_ctr_user
-      ].each do |initer|
-        self.send(initer)
-      end
+    [
+      :init_ood_portal,
+      :init_ctr_user
+    ].each do |initer|
+      send(initer)
+    end
   end
 end
