e7e18858e164f7d5d802546da99946ecfc7eddb7
Mario Squeo <me@mariosqueo.com>
2021-08-12 12:21:45 +0000

    Update proxy rake tasks
    - build:proxy
    
    Docker specific:
    - proxy:start
    - proxy:stop
    - proxy:restart

--- lib/tasks/ood-proxy.rb
+++ lib/tasks/ood-proxy.rb
@@ -1,25 +1,27 @@
 # frozen_string_literal: true
 
-namespace :ood_proxy do
-  require_relative 'build_utils'
-  include BuildUtils
+require_relative 'build_utils'
+include BuildUtils
 
+namespace :build do
   def proxy_container_name
     'ood-proxy-rs' || ENV['OOD_PROXY_CONTAINER_NAME'].to_s
   end
 
-  task :build do
+  task :proxy do
     sh build_cmd("Dockerfile.proxy", proxy_container_name)
     sh tag_latest_container_cmd(proxy_container_name)
   end
+end
 
+namespace :proxy do
   task :start do
-    Rake::Task['ood_proxy:build'] unless image_exists?("#{proxy_container_name}:#{image_tag}")
+    Rake::Task['build:proxy'] unless image_exists?("#{proxy_container_name}:#{image_tag}")
 
     ctr_args = [container_runtime, 'run', "#{proxy_container_name}"]
     ctr_args.concat ["--name #{proxy_container_name}"]
     ctr_args.concat ["#{proxy_container_name}:latest"]
-
+  
     sh ctr_args.join(' ')
   end
 
