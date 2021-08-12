820a205d0c9c847d30a34fcc0fc4ed072dcf518c
Mario Squeo <me@mariosqueo.com>
2021-08-12 12:04:41 +0000

    Add example test

--- ood-proxy-rs/src/main.rs
+++ ood-proxy-rs/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello Open OnDemand!");
+  println!("Hello Open OnDemand!");
+}
+
+pub fn ood() -> String {
+  return format!("Open OnDemand");
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_ood() {
+    assert_eq!(ood(), "Open OnDemand");
+  }
 }
