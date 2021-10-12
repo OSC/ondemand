a47865ba9d3509b96d36322162b57220c3024766
Jeff Ohrstrom <johrstrom@osc.edu>
2021-10-12 10:20:38 -0400

    add min/max javascript functionality (#1441)

--- apps/dashboard/app/javascript/packs/batchConnect.js
+++ apps/dashboard/app/javascript/packs/batchConnect.js
@@ -5,12 +5,19 @@ const shortNameRex = new RegExp(`${bcPrefix}_([\\w\\-]+)`);
 const tokenRex = /([A-Z][a-z]+){1}([\w\-]+)/;
 
 // @example ['NodeType', 'Cluster']
-const optionTokens = [];
+const formTokens = [];
 
 // simple lookup table to indicate that the change handler is setup between two
 // elements. I.e., {'cluster': [ 'node_type' ] } means that changes to cluster
 // trigger changes to node_type
-const handlerCache = {};
+const optionForHandlerCache = {};
+
+
+// a simple array of elements that already have change handlers attached to them.
+const minMaxHandlerCache = [];
+
+// Lookup table for setting min & max values.
+const lookup = {};
 
 function bcElement(name) {
   return `${bcPrefix}_${name.toLowerCase()}`;
@@ -56,6 +63,8 @@ function shortId(elementId) {
 }
 
 function snakeCaseWords(str) {
+  if(str === undefined) return undefined;
+
   let snakeCase = "";
   let first = true;
 
@@ -63,6 +72,8 @@ function snakeCaseWords(str) {
     if (first){
       first = false;
       snakeCase += c.toLowerCase();
+    } else if(c === '-') {
+      snakeCase += '_';
     } else if(c == c.toUpperCase()) {
       snakeCase += `_${c.toLowerCase()}`;
     } else {
@@ -79,8 +90,8 @@ function snakeCaseWords(str) {
  */
 function memorizeElements(elements) {
   elements.each((_i, ele) => {
-    optionTokens.push(mountainCaseWords(shortId(ele['id'])));
-    handlerCache[ele['id']] = [];
+    formTokens.push(mountainCaseWords(shortId(ele['id'])));
+    optionForHandlerCache[ele['id']] = [];
   });
 };
 
@@ -100,7 +111,12 @@ function makeChangeHandlers(){
           let keys = Object.keys(data);
           if(keys.length !== 0) {
             keys.forEach((key) => {
-              addChangeHandler(parseOptions(key), element['id']);
+              if(key.startsWith('optionFor')) {
+                let token = key.replace('optionFor','');
+                addOptionForHandler(idFromToken(token), element['id']);
+              } else if(key.startsWith('max') || key.startsWith('min')) {
+                addMinMaxForHandler(element['id'], opt.value, key, data[key]);
+              }
             });
           }
       });
@@ -108,10 +124,157 @@ function makeChangeHandlers(){
   });
 };
 
-function addChangeHandler(causeId, targetId) {
+/**
+ *
+ * @param {*} changeOnId batch_connect_session_context_node_type
+ * @param {*} option gpu
+ * @param {*} configKey maxNumCoresForClusterAnnieOakley
+ * @param {*} configValue 42
+ *
+ * node_type:
+ *   widget: select
+ *   options:
+ *    - [
+ *        'gpu',
+ *        data-max-num-cores-for-cluster-annie-oakley: 42
+ *      ]
+ */
+function addMinMaxForHandler(optionId, option, key,  configValue) {
+  optionId = String(optionId || '');
+
+  const configObj = parseMinMaxFor(key);
+  const id = configObj['subjectId'];
+  // this is the id of the target object we're setting the min/max for.
+  // if it's undefined - there's nothing to do, it was likely configured wrong.
+  if(id === undefined) return;
+
+  const secondDimId = configObj['predicateId'];
+  const secondDimValue = configObj['predicateValue'];
+
+  if(lookup[id] === undefined) lookup[id] = new Table(optionId, secondDimId);
+
+  const table = lookup[id];
+  table.put(option, secondDimValue, {[minOrMax(key)] : configValue });
+
+  let cacheKey = `${optionId}_${secondDimId}`;
+  if(!minMaxHandlerCache.includes(cacheKey)) {
+    const changeElement = $(`#${optionId}`);
+
+    changeElement.on('change', (event) => {
+      toggleMinMax(event, id, secondDimId);
+    });
+
+    minMaxHandlerCache.push(cacheKey);
+  }
+
+  cacheKey = `${secondDimId}_${optionId}`;
+  if(secondDimId !== undefined && !minMaxHandlerCache.includes(cacheKey)){
+    const secondEle = $(`#${secondDimId}`);
+
+    secondEle.on('change', (event) => {
+      toggleMinMax(event, id, optionId);
+    });
+
+    minMaxHandlerCache.push(cacheKey);
+  }
+}
+
+/**
+ *
+ *  This is a simple table class to describe the relationship between
+ *  two different element types as a table with named columns.
+ *
+ *  table.get('gpu','owens') would return the value shown.
+ *
+ *      'oakley'   |                    |                |
+ *      'owens'    | { min: 3, max: 42} |                |
+ *                 |  'gpu'             |   'hugemem'    |
+ *
+ * In the simple case, it's a 1d vector instead of a 2d matrix. This
+ * allows for, say, gpu to have the same min & max across clusters.
+ */
+class Table {
+  constructor(x, y) {
+    this.x = x;
+    this.xIdxLookup = {};
+
+    this.y = y;
+    this.yIdxLookup = {};
+
+    this.table = [[]];
+  }
+
+  put(x, y, value) {
+    if(!x) return;
+
+    if(this.xIdxLookup[x] === undefined) this.xIdxLookup[x] = Object.keys(this.xIdxLookup).length;
+    if(y && this.yIdxLookup[y] === undefined) this.yIdxLookup[y] = Object.keys(this.yIdxLookup).length;
+
+    const xIdx = this.xIdxLookup[x];
+    const yIdx = this.yIdxLookup[y];
+
+    if(this.table[xIdx] === undefined ){
+      this.table[xIdx] = [];
+    }
+
+    // if y's index is defined, then it's a 2d matrix. Otherwise a 1d vector.
+    if(yIdx === undefined) {
+      if(this.table[xIdx] === undefined){
+        this.table[xIdx] = value;
+      } else {
+        const prev = this.table[xIdx];
+        const newer = value;
+        this.table[xIdx] = Object.assign(prev, newer);
+      }
+    } else {
+      if(this.table[xIdx][yIdx] === undefined){
+        this.table[xIdx][yIdx] = value;
+      } else {
+        const prev = this.table[xIdx][yIdx];
+        const newer = value;
+        this.table[xIdx][yIdx] = Object.assign(prev, newer);
+      }
+    }
+  }
+
+  get(x, y) {
+    const xIdx = this.xIdxLookup[x];
+    const yIdx = this.yIdxLookup[y];
+
+    if(this.table[xIdx] === undefined){
+      return undefined;
+    }else if(y === undefined){
+      return this.table[xIdx];
+    }else {
+      return this.table[xIdx][yIdx];
+    }
+  }
+}
+
+function toggleMinMax(event, changeId, otherId) {
+  let x = undefined, y = undefined;
+
+  // in the example of cluster & node_type, either element can trigger a change
+  // so let's figure out the axis' based on the change element's id.
+  if(event.target['id'] == lookup[changeId].x) {
+    x = snakeCaseWords(event.target.value);
+    y = snakeCaseWords($(`#${otherId}`).val());
+  } else {
+    y = snakeCaseWords(event.target.value);
+    x = snakeCaseWords($(`#${otherId}`).val());
+  }
+
+  const chageElement = $(`#${changeId}`);
+  const mm = lookup[changeId].get(x, y);
+
+  if(mm && mm['max'] !== undefined) chageElement.attr('max', mm['max']);
+  if(mm && mm['min'] !== undefined) chageElement.attr('min', mm['min']);
+}
+
+function addOptionForHandler(causeId, targetId) {
   const changeId = String(causeId || '');
 
-  if(changeId.length == 0 || handlerCache[causeId].includes(targetId)) {
+  if(changeId.length == 0 || optionForHandlerCache[causeId].includes(targetId)) {
     // nothing to do. invalid causeId or we already have a handler between the 2
     return;
   }
@@ -120,7 +283,7 @@ function addChangeHandler(causeId, targetId) {
 
   if(targetId && causeElement) {
     // cache the fact that there's a new handler here
-    handlerCache[causeId].push(targetId);
+    optionForHandlerCache[causeId].push(targetId);
 
     causeElement.on('change', (event) => {
       toggleOptionsFor(event, targetId);
@@ -132,22 +295,97 @@ function addChangeHandler(causeId, targetId) {
 };
 
 /**
- @example
-
-  optionForNodeTypeFoo ->
-    batch_connect_session_context_node_type
-    or
-    undefined if it can't parse anything
- 
-  @param {string} data - the option string to parse
-
+ *
+ * @param {*} key minNumCoresForClusterAnnieOakley
+ * @returns
+ *
+ *  {
+ *    'subjectId': 'batch_connect_session_context_num_cores',
+ *    'predicateId': 'batch_connect_session_context_cluster',
+ *    'predicateValue': 'annie_oakley'
+ *  }
  */
-function parseOptions(input) {
-  // looking for NodeType when i know i have node_type
-  const str = input.replace('optionFor','');
+function parseMinMaxFor(key) {
+  let k = undefined;
+  let predicateId = undefined;
+  let predicateValue = undefined;
+  let subjectId = undefined;
 
-  // just return the first one you find
-  return optionTokens.map((token) => {
+  if(key.startsWith('min')) {
+    k = key.replace(/^min/,'');
+  } else if(key.startsWith('max')) {
+    k = key.replace(/^max/, '')
+  }
+
+  //trying to parse maxNumCoresForClusterOwens
+  const tokens = k.match(/^(\w+)For(\w+)$/);
+
+  if(tokens == null) {
+    // the key is likely just maxNumCores with no For clause
+    subjectId = idFromToken(k);
+
+  } else if(tokens.length == 3) {
+    const subject = tokens[1];
+    const predicateFull = tokens[2];
+    subjectId = idFromToken(subject);
+
+    const predicateTokens = predicateFull.split(/(?=[A-Z])/);
+    if(predicateTokens && predicateTokens.length >= 2) {
+
+      // if there are only 2 tokens then it's like 'ClusterOwens' which is easy
+      if(predicateTokens.length == 2) {
+        predicateId = idFromToken(predicateTokens[0]);
+        predicateValue = predicateTokens[1];
+
+      // else it's like NodeTypeFooBar, so it's a little more difficult
+      } else {
+        let tokenString = '';
+        let done = false;
+        predicateTokens.forEach((pt, idx) => {
+          if(done) { return; }
+
+          tokenString = `${tokenString}${pt}`
+          let tokenId = idFromToken(tokenString);
+          if(tokenId !== undefined) {
+            done = true;
+            predicateId = tokenId;
+            predicateValue = predicateTokens.slice(idx+1).join('');
+          }
+        })
+      }
+    }
+  }
+
+  return {
+    'subjectId': subjectId,
+    'predicateId': predicateId,
+    'predicateValue': snakeCaseWords(predicateValue),
+  }
+}
+
+function minOrMax(key) {
+  if(key.startsWith('min')){
+    return 'min';
+  } else if(key.startsWith('max')){
+    return 'max';
+  } else {
+    return null;
+  }
+}
+
+
+/**
+ * Turn a MountainCase token into a form element id
+ *
+ * @example
+ *  NodeType -> batch_connect_session_context_node_type
+ *
+ * @param {*} str
+ * @returns
+ */
+
+function idFromToken(str) {
+  return formTokens.map((token) => {
     let match = str.match(`^${token}{1}`);
 
     if (match && match.length >= 1) {
--- apps/dashboard/test/system/batch_connect_test.rb
+++ apps/dashboard/test/system/batch_connect_test.rb
@@ -40,4 +40,81 @@ class BatchConnectTest < ApplicationSystemTestCase
     assert_equal 'display: none;', find_option_style('python_version', '3.2')
     assert_equal '3.6', find("##{bc_ele_id('python_version')}").value
   end
+
+  test 'changing node type changes mins & maxs' do
+    # max starts out at 20
+    visit new_batch_connect_session_context_url('sys/bc_jupyter')
+    assert_equal 20, find_max('bc_num_slots')
+    assert_equal 1, find_min('bc_num_slots')
+    select('owens', from: bc_ele_id('cluster'))
+
+    # change the node type and we should have some new max
+    select('gpu', from: bc_ele_id('node_type'))
+    assert_equal 28, find_max('bc_num_slots')
+    assert_equal 2, find_min('bc_num_slots')
+
+    select('hugemem', from: bc_ele_id('node_type'))
+    assert_equal 42, find_max('bc_num_slots')
+    assert_equal 42, find_min('bc_num_slots')
+  end
+
+  test 'changing the cluster changes max' do
+    # max starts out at 20
+    visit new_batch_connect_session_context_url('sys/bc_jupyter')
+    assert_equal 20, find_max('bc_num_slots')
+    select('owens', from: bc_ele_id('cluster'))
+
+    select('gpu', from: bc_ele_id('node_type'))
+    assert_equal 28, find_max('bc_num_slots')
+
+    # changing the cluster changes the max
+    select('oakley', from: bc_ele_id('cluster'))
+    assert_equal 40, find_max('bc_num_slots')
+  end
+
+  test 'using same node sets min/max' do
+    # max starts out at 20
+    visit new_batch_connect_session_context_url('sys/bc_jupyter')
+    assert_equal 20, find_max('bc_num_slots')
+
+    select('same', from: bc_ele_id('node_type'))
+    assert_equal 100, find_min('bc_num_slots')
+    assert_equal 200, find_max('bc_num_slots')
+
+    # toggle the cluster back and forth and it's still the same
+    select('oakley', from: bc_ele_id('cluster'))
+    select('owens', from: bc_ele_id('cluster'))
+    assert_equal 100, find_min('bc_num_slots')
+    assert_equal 200, find_max('bc_num_slots')
+
+    select('oakley', from: bc_ele_id('cluster'))
+    assert_equal 100, find_min('bc_num_slots')
+    assert_equal 200, find_max('bc_num_slots')
+  end
+
+  test 'nothing applied to any node type' do
+    visit new_batch_connect_session_context_url('sys/bc_jupyter')
+    assert_equal 20, find_max('bc_num_slots')
+    assert_equal 1, find_min('bc_num_slots')
+
+    # changing clusters does nothing.
+    select('owens', from: bc_ele_id('cluster'))
+    select('any', from: bc_ele_id('node_type'))
+    assert_equal 20, find_max('bc_num_slots')
+    assert_equal 1, find_min('bc_num_slots')
+
+    select('oakley', from: bc_ele_id('cluster'))
+    assert_equal 20, find_max('bc_num_slots')
+    assert_equal 1, find_min('bc_num_slots')
+
+    # choose same to get a min & max set. Change back to
+    # any and we keep the same min & max from same.
+    # TODO this is _current_ behaviour, will probably break
+    select('same', from: bc_ele_id('node_type'))
+    assert_equal 200, find_max('bc_num_slots')
+    assert_equal 100, find_min('bc_num_slots')
+    select('any', from: bc_ele_id('node_type'))
+    assert_equal 200, find_max('bc_num_slots')
+    assert_equal 100, find_min('bc_num_slots')
+  end
 end
