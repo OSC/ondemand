242ba99301a45ccb16c6a850faf0cbd5966b048e
Jeff Ohrstrom <johrstrom@osc.edu>
2021-11-05 16:41:46 -0400

    migrate product assets to webpack

---
+++ apps/dashboard/app/javascript/packs/products_index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+jQuery(function(){
+  $('#productTable').DataTable();
+});
---
+++ apps/dashboard/app/javascript/packs/products_show.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const id = 'product_cli_modal';
+const spinnerId = `${id}_spinner`;
+const headerId = `${id}_header`;
+const closeButton = `<button class="close float-right" data-dismiss="modal">&times;</button>`;
+
+jQuery(function(){
+  $('[data-toggle="cli"]').on('click', (event) => updateModal(event));
+
+  $(`#${headerId}`).replaceWith(`
+    <h2>
+      <span>no action</pan>
+      ${closeButton}
+    </h2>
+  `);
+});
+
+function updateModal(event){
+  const button = $(`#${event.target['id']}`);
+  if(button === undefined || button.data() === {}) { return; }
+
+  const title = button.data('title');
+  const cmd = button.data('cmd');
+  const target = button.data('target');
+  const header = `\$ <code><strong>${cmd}</strong></code>\n`
+
+  $(`#${headerId}`).replaceWith(`
+    <h2>
+      <span>${title}</pan>
+      ${closeButton}
+    </h2>
+  `);
+
+  const xhr = new XMLHttpRequest;
+  xhr.onreadystatechange = function() {
+    if(this.status == 200){
+      $(`#${id} .product-cli-body`).html(`${header}${this.responseText}`);
+      $(`#${id} .product-cli-body`).scrollTop($(`#${id} .product-cli-body`)[0].scrollHeight);
+    }
+  };
+
+  xhr.onloadend = function() {
+    $(`#${spinnerId}`).replaceWith(`
+      <button class="close float-right" data-dismiss="modal">&times;</button>
+    `);
+    if (this.status != 200) {
+      $(`#${id} .product-cli-body`).html(`${header}A fatal error has occurred`);
+    };
+  };
+
+  xhr.open('PATCH', target);
+  xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))
+  xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+  xhr.send();
+  $(`#${id}`).modal('show');
+}
