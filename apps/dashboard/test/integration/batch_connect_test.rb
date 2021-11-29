d46501eb2fe943a471bd6c2f6edc20974c63ab22
Gerald Byrket <gbyrket@osc.edu>
2021-11-29 17:14:29 -0500

    Changed Test Code to only navigate to site one time

--- apps/dashboard/test/integration/batch_connect_test.rb
+++ apps/dashboard/test/integration/batch_connect_test.rb
@@ -16,11 +16,7 @@ class BatchConnectTest < ActionDispatch::IntegrationTest
   test 'radio buttons and labels appear correctly' do
     get new_batch_connect_session_context_url('sys/bc_jupyter')
     assert_select 'form input[id="batch_connect_session_context_mode_0"]'
-
-    get new_batch_connect_session_context_url('sys/bc_jupyter')
     assert_select 'form input[id="batch_connect_session_context_mode_1"]'
-
-    get new_batch_connect_session_context_url('sys/bc_jupyter')
     assert_equal 'The Mode', css_select('label[for="batch_connect_session_context_mode"]').text
     assert_equal 'Jupyter Lab', css_select('label[for="batch_connect_session_context_mode_1"]').text
     assert_equal 'Jupyter Notebook', css_select('label[for="batch_connect_session_context_mode_0"]').text
