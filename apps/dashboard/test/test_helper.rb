3cd9d5335dc2bd02f0ecd5c96a763ad21ec51905
Jeff Ohrstrom <johrstrom@osc.edu>
2021-12-01 11:01:41 -0500

    add rest of test infrastructure

--- apps/dashboard/test/test_helper.rb
+++ apps/dashboard/test/test_helper.rb
@@ -15,7 +15,7 @@ require 'timecop'
 class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
 
-  UserDouble = Struct.new(:name)
+  UserDouble = Struct.new(:name, :groups)
 
   def with_modified_env(options, &block)
     ClimateControl.modify(options, &block)
@@ -30,13 +30,18 @@ class ActiveSupport::TestCase
   end
 
   def stub_usr_router
-    OodSupport::Process.stubs(:user).returns(UserDouble.new('me'))
-    OodSupport::User.stubs(:new).returns(UserDouble.new('me'))
+    OodSupport::Process.stubs(:user).returns(UserDouble.new('me', ['me']))
+    OodSupport::User.stubs(:new).returns(UserDouble.new('me', ['me']))
 
     UsrRouter.stubs(:base_path).with(:owner => "me").returns(Pathname.new("test/fixtures/usr/me"))
     UsrRouter.stubs(:base_path).with(:owner => 'shared').returns(Pathname.new("test/fixtures/usr/shared"))
     UsrRouter.stubs(:base_path).with(:owner => 'cant_see').returns(Pathname.new("test/fixtures/usr/cant_see"))
     UsrRouter.stubs(:owners).returns(['me', 'shared', 'cant_see'])
+  end
+
+  def stub_sys_apps
+    OodAppkit.stubs(:clusters).returns(OodCore::Clusters.load_file('test/fixtures/config/clusters.d'))
+    SysRouter.stubs(:base_path).returns(Rails.root.join('test/fixtures/sys_with_gateway_apps'))
   end
 
   def setup_usr_fixtures
@@ -45,6 +50,10 @@ class ActiveSupport::TestCase
 
   def teardown_usr_fixtures
     FileUtils.chmod 0755, 'test/fixtures/usr/cant_see/'
+  end
+
+  def bc_ele_id(ele)
+    "batch_connect_session_context_#{ele}"
   end
 end
 
