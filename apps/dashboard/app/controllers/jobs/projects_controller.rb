e9f44b6bca10cdee1a3e82e5381633e001530367
Travis Ravert <travert@osc.edu>
2021-12-08 12:37:12 -0500

    Placeholder.

--- apps/dashboard/app/controllers/jobs/projects_controller.rb
+++ apps/dashboard/app/controllers/jobs/projects_controller.rb
@@ -1,11 +1,11 @@
 class Jobs::ProjectsController < ApplicationController
   def show
     @pwd = Jobs::Project.new.pwd
-    @files = Jobs::Project.new.ls(@pwd)
+    @files = Jobs::Project.new.ls
   end
 
   def index
     @pwd = Jobs::Project.new.pwd
-    @files = Jobs::Project.new.ls(@pwd)
+    @files = Jobs::Project.new.ls
   end
 end
--- apps/dashboard/app/models/jobs/project.rb
+++ apps/dashboard/app/models/jobs/project.rb
@@ -1,19 +1,30 @@
 class Jobs::Project
-  def ls(dir)
-    Pathname.new(dir).each_child.map do |path|
-      Jobs::Project.stat(path)
-    end.sort_by { |p| p[:directory] ? 0 : 1 }
-  end
 
   def self.stat(dir)
+    # need to get:
+    # { name: path.basename 
+    # directory: path.stat.directory }
+    #hash back for ERB to work.
     path = Pathname.new(dir)
-      {
-        name: path.basename,
-        directory: path.stat.directory?
-      }
+    {
+      name: path.basename,
+      directory: path.stat.directory?
+    }
   end
 
+  def ls
+    dir_contents = files
+    dir_contents.each_child.map do |path|
+      Jobs::Project.stat(path)
+    end
+  end
+
+
   def pwd
-    Dir.pwd
+    @pwd = Pathname.new(Dir.pwd)
+  end
+
+  def files
+    @files = Dir.entries(pwd)
   end
 end
