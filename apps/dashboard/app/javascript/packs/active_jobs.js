ce904fc21173424284297b5e878db78e4f0d1961
Jeff Ohrstrom <johrstrom@osc.edu>
2022-01-04 10:47:07 -0500

    migrate activejobs javascript to webpack (#1748)
    * migrate activejobs javascript to webpack
    
    * fixup ready and imports

--- apps/dashboard/app/javascript/packs/active_jobs.js
+++ apps/dashboard/app/javascript/packs/active_jobs.js
@@ -1,9 +1,15 @@
-//= require oboe/dist/oboe-browser.min
-//= require datatables.net-plugins/api/processing()
 
-$(document).ready(function(){
+import oboe from 'oboe';
+import 'datatables.net-plugins/api/processing()';
+
+jQuery(function(){
     $('[data-toggle="tooltip"]').tooltip();
 });
+
+window.fetch_table_data = fetch_table_data;
+window.create_datatable = create_datatable;
+window.set_cluster_id = set_cluster_id;
+window.set_filter_id = set_filter_id;
 
 var entityMap = {
   '&': '&amp;',
