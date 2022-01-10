189683f5cfedd8d0aa8e986cc8c56cc00c7707c8
Gerald Byrket <93494625+gbyrketosc@users.noreply.github.com>
2022-01-10 09:14:57 -0600

    Added functionality to view yaml files inline with browser (#1758)
    Added functionality to view yaml files inline with browser

--- apps/dashboard/app/models/files.rb
+++ apps/dashboard/app/models/files.rb
@@ -132,11 +132,18 @@ class Files
     #     x === 'application/x-yaml'
     #     # => true
     #
-    if %r{text/.*}.match(type).nil?
-      type
-    else
-      'text/plain; charset=utf-8'
-    end
+
+    # NOTE:  due to how most modern browsers are handling the yaml mime-types,
+    # we have to cast the yaml mime-types to text/plain mime-type so we can
+    # view the yaml files inline with the browser.
+    # Would like to have a pattern match for the  yaml mime-types; something like application/*yml and application/*yaml
+    # but couldn't find one that worked. Therefore, for now we're using full mime-type names for yaml mime-types.
+
+    plain_cast_array = ['text/*', 'application/x-yml', 'application/x-yaml', 'application/yml', 'application/yaml']
+
+    type = 'text/plain; charset=utf-8' if plain_cast_array.any? { |mime| /#{mime}/.match?(type) }
+
+    type
   end
 
   # returns mime type string if found, "" otherwise
