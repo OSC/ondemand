691dfb77593ae8aff399c9bb503b5f3e93542a6b
Jeff Ohrstrom <johrstrom@osc.edu>
2022-01-12 13:09:23 -0500

    move all the js out of application.js to js packs (#1755)

--- apps/dashboard/app/javascript/packs/batch_connect_sessions.js
+++ apps/dashboard/app/javascript/packs/batch_connect_sessions.js
@@ -106,6 +106,36 @@ function makePollers(){
   });
 }
 
+function settingKey(id) {
+  return id + '_value';
+}
+
+function storeSetting(event) {
+  var key = settingKey(event.currentTarget.id);
+  var value = event.currentTarget.value;
+
+  localStorage.setItem(key, value);
+}
+
+function tryUpdateSetting(name) {
+  var saved_value = localStorage.getItem(settingKey(name));
+
+  if(saved_value) {
+    var selector = 'input[type="range"][name="' + name + '"]';
+    $(selector).val(saved_value);
+  }
+}
+
+function installSettingHandlers(name) {
+  var selector = 'input[type="range"][name="' + name + '"]';
+  $(selector).on('change', function(event){
+    storeSetting(event);
+  });
+}
+
+window.installSettingHandlers = installSettingHandlers;
+window.tryUpdateSetting = tryUpdateSetting;
+
 jQuery(function() {
   makePollers();
 });
