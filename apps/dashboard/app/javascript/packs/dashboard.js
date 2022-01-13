fc25626cce5c2b58e21972345364eea4e51f79c4
Jeff Ohrstrom <johrstrom@osc.edu>
2022-01-13 12:33:50 -0500

    migrate dashboard coffeescript to plain js in webpack

--- apps/dashboard/app/javascript/packs/dashboard.js
+++ apps/dashboard/app/javascript/packs/dashboard.js
@@ -80,3 +80,20 @@ var promiseLoggedIntoXDMoD = (function(){
 
 window.promiseLoginToXDMoD = promiseLoginToXDMoD;
 window.promiseLoggedIntoXDMoD = promiseLoggedIntoXDMoD;
+
+jQuery(function(){
+  $("a[target=_blank]").on("click", function(event) {
+    // open url using javascript, instead of following directly
+    event.preventDefault();
+
+    if(window.open($(this).attr("href")) == null){
+      // link was not opened in new window, so display error msg to user
+      const html = $("#js-alert-danger-template").html();
+      const msg = "This link is configured to open in a new window, but it doesn't seem to have opened. " +
+            "Please disable your popup blocker for this page and try again.";
+
+      // replace message in alert and add to main div of layout
+      $("div[role=main]").prepend(html.split("ALERT_MSG").join(msg));
+    }
+  });
+});
