29e530293a0afb4c491c83ff6bb82b351a82f42b
Travis Ravert <travert@osc.edu>
2022-01-14 11:32:29 -0500

    Breaking tests into :dev and :usr types.

---
+++ apps/dashboard/test/system/products_usr_test.rb
@@ -0,0 +1,25 @@
+require 'application_system_test_case'
+
+class ProductsUsrTest < ApplicationSystemTestCase
+  # Only testing show and index actions for type :usr
+  # ProductsDevTest used for testing page interactions
+
+  def setup
+    stub_usr_router
+    setup_usr_fixtures
+  end
+
+  def teardown
+    teardown_usr_fixtures
+  end
+
+  test 'Index of my_shared_app can be accessed' do
+    visit products_path(:usr)
+    teardown_usr_fixtures
+  end
+
+  test 'Show of my_shared_app url can be accessed' do
+    visit product_path(:usr, 'my_shared_app')
+    teardown_usr_fixtures
+  end
+end
