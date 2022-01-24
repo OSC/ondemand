d544707d80f788435c581d8323d4f0d36fb19b81
Jeff Ohrstrom <johrstrom@osc.edu>
2022-01-24 12:03:46 -0500

    files app needs datatables too

--- apps/dashboard/app/javascript/packs/files/index.js
+++ apps/dashboard/app/javascript/packs/files/index.js
@@ -3,6 +3,10 @@ import Swal from 'sweetalert2'
 import { Uppy, BasePlugin } from '@uppy/core'
 import Dashboard from '@uppy/dashboard'
 import XHRUpload from '@uppy/xhr-upload'
+import 'datatables.net';
+import 'datatables.net-bs4/js/dataTables.bootstrap4';
+import 'datatables.net-select';
+import 'datatables.net-select-bs4';
 
 window.ClipboardJS = ClipboardJS
 window.Uppy = Uppy
