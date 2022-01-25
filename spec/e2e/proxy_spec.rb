170f7dcc17da72356177feea37bf6273da99b21e
Travis Ravert <travert@osc.edu>
2022-01-25 10:53:01 -0500

    Testing e2e run of added tests for lua proxy changes.

--- spec/e2e/proxy_spec.rb
+++ spec/e2e/proxy_spec.rb
@@ -99,4 +99,98 @@ describe 'Node and Rnode proxies' do
     expect(browser.url).to eq("#{ctr_base_url}/node/localhost/5001/one/one-level-down")
     expect(browser.div(id: 'test-div').present?).to be true
   end
+
+  it 'node correctly returns 404 when no redirect is given on init' do
+    browser.goto "#{ctr_base_url}/nginx/init"
+    expect(response.status).to have_http_status(404)
+  end 
+
+  it 'redirects to /pun/sys/dashboard when redirect is given on init' do
+    browser.goto "#{ctr_base_url}/nginx/init?redir=/pun/sys/dashboard"
+    # TODO ensure it's a 302 response on redirect
+    expect(response.status).to have_http_status(302)
+    follow_redirect!
+    expect(browser.url).to eq("#{ctr_base_url}/pun/sys/dashboard")
+    expect(resonse.status).to have_http_status(200)
+  end
+
+  it 'node correctly returns 404 when redirect to "github.com" given on init' do
+    browser.goto "#{ctr_base_url}/nginx/init?redir=github.com"
+    expect(response.status).to have_http_status(404)
+  end
+
+  it 'node correctly returns 404 when redirect to "https://github.com" given on init' do
+    browser.goto "#{ctr_base_url}/nginx/init?redir=https://github.com"
+    expect(response.status).to have_http_status(404)
+  end
+
+  it 'node correctly returns 404 when redirect to "/node/localhost/5001" is given on init' do
+    browser.goto "#{ctr_base_url}/nginx/init?redir=/node/localhost/5001"
+    expect(response.status).to have_http_status(404)
+  end
+
+  # TODO returns 404 and error message when it can't correctly boot the PUN
+  # it 'returns 404 and error when PUN not booted on init' do
+  #   browser.goto "#{ctr_base_url}/nginx/init"
+  # end
+
+  it 'node correctly returns 404 when no redirect is given on stop' do
+    browser.goto "#{ctr_base_url}/nginx/stop"
+    expect(response.status).to have_http_status(404)
+  end 
+
+  # TODO returns correct error page on stop when no redirect given
+  # it 'returns correct error page when no redirect is given on stop' do
+    # browser.goto "#{ctr_base_url}/nginx/stop"    
+    # expect(browser.url).to eq(#route to whatever error page)
+    # expect(resonse.status).to have_http_status(200)
+  # end
+
+  it 'node correctly returns 404 when redirect to "github.com" given on stop' do
+    browser.goto "#{ctr_base_url}/nginx/stop?redir=github.com"
+    expect(response.status).to have_http_status(404)
+  end
+
+  it 'node correctly returns 404 when redirect to "https://github.com" given on stop' do
+    browser.goto "#{ctr_base_url}/nginx/stop?redir=https://github.com"
+    expect(response.status).to have_http_status(404)
+  end
+
+  it 'node correctly returns 404 when redirect to "/node/localhost/5001" is given on stop' do
+    browser.goto "#{ctr_base_url}/nginx/stop?redir=/node/localhost/5001"
+    expect(response.status).to have_http_status(404)
+  end
+
+  # TODO returns correct error page on stop when a redirect is given
+  # it 'returns 404 and error when PUN not booted' do
+  #  browser.goto "#{ctr_base_url}/nginx/init?redir=/nothing"
+  # end
+
+  it 'node correctly returns 200 when no redirect is given on noop' do
+    browser.goto "#{ctr_base_url}/nginx/noop"
+    expect(response.status).to have_http_status(200)
+  end 
+
+  it 'node correctly redirects to /pun/sys/dashboard with 307 when no redirect is given on noop' do
+    browser.goto "#{ctr_base_url}/nginx/noop"
+    expect(browser.url).to eq("#{ctr_base_url}/pun/sys/dashboard")
+    expect(response.status).to have_http_status(307)
+  end 
+
+  it 'node correctly returns 404 when redirect to "github.com" given on noop' do
+    browser.goto "#{ctr_base_url}/nginx/noop?redir=github.com"
+    expect(response.status).to have_http_status(404)
+  end
+
+  it 'node correctly returns 404 when redirect to "https://github.com" given on noop' do
+    browser.goto "#{ctr_base_url}/nginx/noop?redir=https://github.com"
+    expect(response.status).to have_http_status(404)
+  end
+
+  it 'node correctly returns 404 when redirect to "/node/localhost/5001" is given on noop' do
+    browser.goto "#{ctr_base_url}/nginx/noop?redir=/node/localhost/5001"
+    expect(response.status).to have_http_status(404)
+  end
+  
+  # TODO: check content of page.
 end
