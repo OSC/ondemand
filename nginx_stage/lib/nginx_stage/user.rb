f460dc2d998e6e5b2488cdff5dfad0cd6aabf5be
Jeff Ohrstrom <johrstrom@osc.edu>
2022-02-07 11:54:24 -0500

    specify what to set the config to

--- nginx_stage/lib/nginx_stage/user.rb
+++ nginx_stage/lib/nginx_stage/user.rb
@@ -41,7 +41,7 @@ module NginxStage
         err_msg = <<~HEREDOC
           Username '#{user}' is being mapped to '#{name}' in SSSD and they don't match.
           Users with domain names cannot be mapped correctly. If '#{name}' still has the
-          domain in it you'll need to set SSSD's full_name_format.
+          domain in it you'll need to set SSSD's full_name_format to '%1$s'.
 
           See https://github.com/OSC/ondemand/issues/1759 for more details.
         HEREDOC
--- nginx_stage/spec/user_spec.rb
+++ nginx_stage/spec/user_spec.rb
@@ -33,7 +33,7 @@ describe NginxStage::User do
       msg = <<~HEREDOC
         Username 'spec' is being mapped to 'spec@domain.edu' in SSSD and they don't match.
         Users with domain names cannot be mapped correctly. If 'spec@domain.edu' still has the
-        domain in it you'll need to set SSSD's full_name_format.
+        domain in it you'll need to set SSSD's full_name_format to '%1$s'.
       
         See https://github.com/OSC/ondemand/issues/1759 for more details.
       HEREDOC
