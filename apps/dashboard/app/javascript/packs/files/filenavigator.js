0443ff1950370e105b06d035e12140a8573fcafd
Gerald Byrket <gbyrket@osc.edu>
2022-02-08 16:10:28 -0600

    Intermediate step to get file app to open in new window with proper relative url

--- apps/dashboard/app/javascript/packs/files/filenavigator.js
+++ apps/dashboard/app/javascript/packs/files/filenavigator.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $("#btnFileNavigator").on("click", function(){
-        var url = '/pun/dev/dashboard/files/fs/home/gbyrket';
+        var url = $("#relative_root_id").val();
+        alert(url);
         window.open(url, "fileNavigator", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=800,height=600");
     });        
 });
