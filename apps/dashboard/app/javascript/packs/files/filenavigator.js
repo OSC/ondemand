fd4fb85f01878e55bcd5e10c8f9bf0817599b27c
Gerald Byrket <gbyrket@osc.edu>
2022-02-09 11:20:11 -0600

    Added Functionality to load files app in a modal window

--- apps/dashboard/app/javascript/packs/files/filenavigator.js
+++ apps/dashboard/app/javascript/packs/files/filenavigator.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
     $("#btnFileNavigator").on("click", function(){
-        var url = $("#relative_root_id").val();
-        alert(url);
-        window.open(url, "fileNavigator", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=800,height=600");
+        $('#divFileNavigator').show();
     });        
 });
