6dd19eabba18e5614c474f5bcf88cebda7caf2f0
Gerald Byrket <gbyrket@osc.edu>
2022-02-15 15:22:52 -0600

    Undo accidental commit

--- apps/dashboard/config/initializers/session_store.rb
+++ apps/dashboard/config/initializers/session_store.rb
@@ -1,6 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
 if Rails.application.config.session_store.nil?
-  # Rails.application.config.session_store :cookie_store, key: '_dashboard_session', secure: Rails.env.production?, same_site: :strict
-  Rails.application.config.session_store :cookie_store, key: '_dashboard_session', secure: false
+  Rails.application.config.session_store :cookie_store, key: '_dashboard_session', secure: Rails.env.production?, same_site: :strict
 end
