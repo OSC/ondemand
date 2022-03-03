ef35c71168340ca986c525d197325f6f5da0c7cc
Jeff Ohrstrom <johrstrom@osc.edu>
2022-03-03 12:22:41 -0500

    WIP, just committing to show

--- lib/tasks/release.rb
+++ lib/tasks/release.rb
@@ -8,11 +8,11 @@ namespace :release do
       raise StandardError.new('environment variables ZENODO_TOKEN and RELEASE_ARCHIVE have to be set')
     end
     token = ENV['ZENODO_TOKEN'].to_s
+    host = 'zenodo.org'
+    port = '443'
 
-    uri = URI("https://zenodo.org/api/deposit/depositions?access_token=#{token}")
-
-    data = Net::HTTP.start(uri.host, uri.port, :use_ssl => true) do |http|
-      request = Net::HTTP::Get.new(uri)
+    data = Net::HTTP.start(host, port, :use_ssl => true) do |http|
+      request = Net::HTTP::Get.new(URI("https://zenodo.org/api/deposit/depositions?access_token=#{token}"))
       request['Accept'] = 'application/json'
       
       response = http.request(request)
@@ -23,20 +23,41 @@ namespace :release do
     end 
 
     id = data['id']
-    uri = URI("https://zenodo.org/api/deposit/depositions/#{id}/files?access_token=#{token}")
 
-    upload_response = Net::HTTP.start(uri.host, uri.port, :use_ssl => true) do |http|
-      request = Net::HTTP::Post.new(uri)
+    Net::HTTP.start(host, port, :use_ssl => true) do |http|
+      # upload the new archive
+      request = Net::HTTP::Post.new(URI("https://zenodo.org/api/deposit/depositions/#{id}/files?access_token=#{token}"))
       request['Accept'] = 'application/json'
       p = Pathname.new(ENV['RELEASE_ARCHIVE'].to_s)
-      request.set_form([['file', File.open(p.to_s)], ['name', p.basename.to_s]], 'multipart/form-data')
+      request.set_form([['file', File.open(p.to_s)]], 'multipart/form-data')
+      response = http.request(request)
+      raise StandardError.new("Cannot upload archive. Zenodo Server responded with #{response.code}:#{response.body}") unless response.code.to_i == 201
+
+
+      # TODO remove the existing file
+
+      # edit the metadata to reflect new publish date and new version
+      version = /ondemand-(.+).zip/.match(p.basename.to_s)[1]
+      uri = URI("https://zenodo.org/api/deposit/depositions/#{id}?access_token=#{token}")
+      # uri = URI("https://zenodo.org/api/deposit/depositions/#{id}/actions/edit?access_token=#{token}")
+      request = Net::HTTP::Put.new(uri)
+      request.set_form_data("metadata" => {"version" => "#{version}-1", "publication_date" => Date.today})
+      response = http.request(request)
+      puts response.body
+      raise StandardError.new("Cannot edit new version. Zenodo Server responded with #{response.code}:#{response.body}") unless response.code.to_i == 201
+    end
+
+    uri = URI("#{data['links']['publish']}?access_token=#{token}")
+    
+    Net::HTTP.start(uri.host, uri.port, :use_ssl => true) do |http|
+      request = Net::HTTP::Post.new(uri)
+      request['Accept'] = 'application/json'
       
       response = http.request(request)
 
-      raise StandardError.new("Cannot upload archive. Zenodo Server responded with #{response.code}") unless response.code.to_i == 201
-      response
+      raise StandardError.new("Cannot publish archive. Zenodo Server responded with #{response.code}:#{response.body}") unless response.code.to_i == 202
     end
 
-    puts "successfully uploaded #{ENV['RELEASE_ARCHIVE']} to Zenodo's #{id}"
+    puts "sucessfully published #{ENV['RELEASE_ARCHIVE']} to zendo at #{data['doi_url']}"
   end
 end
