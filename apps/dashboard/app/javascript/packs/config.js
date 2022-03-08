1176cccebe0856d26ffc659228edc4d5896e9da6
Jeff Ohrstrom <johrstrom@osc.edu>
2022-03-08 11:01:34 -0500

    ensure navbar has default colors (#1895)
    ensure navbar has default colors

--- apps/dashboard/app/javascript/packs/config.js
+++ apps/dashboard/app/javascript/packs/config.js
@@ -10,11 +10,17 @@ function setNavbarColor() {
   const cfgData = configData();
   const styles = document.styleSheets[0];
 
-  styles.insertRule(navbar('light', cfgData['bgColor']), styles.rules.length);
-  styles.insertRule(navbar('dark', cfgData['bgColor']), styles.rules.length);
+  const bgLightColor = cfgData['bgColor'] === '' ? 'rgb(248, 248, 248)' : cfgData['bgColor'];
+  const bgDarkColor = cfgData['bgColor'] === '' ? 'rgb(83, 86, 90)' : cfgData['bgColor'];
+
+  const linkLightColor = cfgData['linkBgColor'] === '' ? 'rgb(231, 231, 231)' : cfgData['linkBgColor'];
+  const linkDarkColor = cfgData['linkBgColor'] === '' ? 'rgb(59, 61, 63)' : cfgData['linkBgColor'];
+
+  styles.insertRule(navbar('light', bgLightColor), styles.rules.length);
+  styles.insertRule(navbar('dark', bgDarkColor), styles.rules.length);
   
-  styles.insertRule(navbarHighlight('light', cfgData['linkBgColor']), styles.rules.length);
-  styles.insertRule(navbarHighlight('dark', cfgData['linkBgColor']), styles.rules.length);
+  styles.insertRule(navbarHighlight('light', linkLightColor), styles.rules.length);
+  styles.insertRule(navbarHighlight('dark', linkDarkColor), styles.rules.length);
 }
 
 function navbar(theme, color){
