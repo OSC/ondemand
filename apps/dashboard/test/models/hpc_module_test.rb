dc3b7a1df259a0493f003d8b6790972bae1eccb7
Jeff Ohrstrom <johrstrom@osc.edu>
2022-03-25 16:52:41 -0400

    add support for reading modules and dynamic string configs (#1925)
    add support for reading modules and dynamic string configs

---
+++ apps/dashboard/test/models/hpc_module_test.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class HpcModuleTest < ActiveSupport::TestCase
+  test 'all safely reads from inaccessabile directories' do
+    with_modified_env({ OOD_MODULE_FILE_DIR: '/dev/null' }) do
+      assert_equal [], HpcModule.all('owens')
+    end
+  end
+
+  test 'all safely reads invalid json' do
+    Dir.mktmpdir do |dir|
+      with_modified_env({ OOD_MODULE_FILE_DIR: dir }) do
+        `echo '{this is bad json}' > #{dir}/owens.json`
+        assert_equal [], HpcModule.all('owens')
+      end
+    end
+  end
+
+  test 'reads a simple file' do
+    with_modified_env({ OOD_MODULE_FILE_DIR: "#{Rails.root}/test/fixtures/modules/" }) do
+      # NOTE: that there are no duplicates and rstudio has no version
+      assert_equal(['jupyter/1', 'jupyter/2', 'rstudio'], HpcModule.all('simple').map(&:to_s))
+    end
+  end
+
+  test 'default version' do
+    m = HpcModule.new('test')
+    assert m.default?
+    assert m.version.nil?
+  end
+
+  test 'module with version version' do
+    m = HpcModule.new('test', version: 9001)
+    assert !m.default?
+    assert_equal m.version, '9001' # we gave an int, got back a string
+  end
+end
