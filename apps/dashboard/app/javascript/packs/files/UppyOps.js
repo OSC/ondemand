3917327ab8e0fcdd6d8fefccafcb37f76c9b5067
Gerald Byrket <88058640+gbsoftwaresolutions@users.noreply.github.com>
2022-03-31 14:54:46 -0500

    Move what was in document.ready to outside

--- apps/dashboard/app/javascript/packs/files/UppyOps.js
+++ apps/dashboard/app/javascript/packs/files/UppyOps.js
@@ -4,140 +4,134 @@ import XHRUpload from '@uppy/xhr-upload'
 import _ from 'lodash';
 
 
-let uppy = null;
+class EmptyDirCreator extends BasePlugin {
+  constructor (uppy, opts){
+    super(uppy, opts)
+    this.id = this.opts.id || 'EmptyDirUploaderCatcher';
+    this.type = 'acquirer';
 
-$(document).ready(function() {
+    this.empty_dirs = [];
+    this.last_entries = [];
 
-  class EmptyDirCreator extends BasePlugin {
-    constructor (uppy, opts){
-      super(uppy, opts)
-      this.id = this.opts.id || 'EmptyDirUploaderCatcher';
-      this.type = 'acquirer';
+    this.handleRootDrop = this.handleRootDrop.bind(this);
+    this.createEmptyDirs = this.createEmptyDirs.bind(this);
 
-      this.empty_dirs = [];
-      this.last_entries = [];
+    this.uppy = uppy;
+  }
 
-      this.handleRootDrop = this.handleRootDrop.bind(this);
-      this.createEmptyDirs = this.createEmptyDirs.bind(this);
 
-      this.uppy = uppy;
+
+  handleRootDrop (e) {
+    // from https://github.com/transloadit/uppy/blob/7ce58beeb620df3df0640cb369f5d71e3d3f751f/packages/%40uppy/utils/src/getDroppedFiles/index.js
+    if (e.dataTransfer.items && e.dataTransfer.items[0] && 'webkitGetAsEntry' in e.dataTransfer.items[0]) {
+      // toArray https://github.com/transloadit/uppy/blob/7ce58beeb620df3df0640cb369f5d71e3d3f751f/packages/%40uppy/utils/src/toArray.js#L4
+      let items = Array.prototype.slice.call(e.dataTransfer.items || [], 0);
+      let entries = items.map(i => i.webkitGetAsEntry()).filter(i => i);
+
+      return Promise.all(entries.map(i => getEmptyDirs(i))).then((dirs) => {
+        this.empty_dirs = this.empty_dirs.concat(_.flattenDeep(dirs));
+
+      });
     }
+    //else we don't have access to directory information
+  }
 
+  createEmptyDirs (ids) {
+    if(! this.uppy.getState().error){ // avoid creating empty dirs if error occurred during upload
 
+      //TODO: error checking and reporting
+      return Promise.all(this.empty_dirs.map((d) => {
+        // "fullPath" should actually be the path relative to the current directory
+        let filename = _.trimStart(d.fullPath, '/');
 
-    handleRootDrop (e) {
-      // from https://github.com/transloadit/uppy/blob/7ce58beeb620df3df0640cb369f5d71e3d3f751f/packages/%40uppy/utils/src/getDroppedFiles/index.js
-      if (e.dataTransfer.items && e.dataTransfer.items[0] && 'webkitGetAsEntry' in e.dataTransfer.items[0]) {
-        // toArray https://github.com/transloadit/uppy/blob/7ce58beeb620df3df0640cb369f5d71e3d3f751f/packages/%40uppy/utils/src/toArray.js#L4
-        let items = Array.prototype.slice.call(e.dataTransfer.items || [], 0);
-        let entries = items.map(i => i.webkitGetAsEntry()).filter(i => i);
+        return fetch(`${history.state.currentDirectoryUrl}/${encodeURI(filename)}?dir=true`, {method: 'put', headers: { 'X-CSRF-Token': csrf_token }})
+        //TODO: parse json response verify if there was an error creating directory and handle error
 
-        return Promise.all(entries.map(i => getEmptyDirs(i))).then((dirs) => {
-          this.empty_dirs = this.empty_dirs.concat(_.flattenDeep(dirs));
-
-        });
-      }
-      //else we don't have access to directory information
-    }
-
-    createEmptyDirs (ids) {
-      if(! this.uppy.getState().error){ // avoid creating empty dirs if error occurred during upload
-
-        //TODO: error checking and reporting
-        return Promise.all(this.empty_dirs.map((d) => {
-          // "fullPath" should actually be the path relative to the current directory
-          let filename = _.trimStart(d.fullPath, '/');
-
-          return fetch(`${history.state.currentDirectoryUrl}/${encodeURI(filename)}?dir=true`, {method: 'put', headers: { 'X-CSRF-Token': csrf_token }})
-          //TODO: parse json response verify if there was an error creating directory and handle error
-
-        })).then(() => this.empty_dirs = []);
-      }
-    }
-
-    install () {
-      this.uppy.addPostProcessor(this.createEmptyDirs);
-    }
-
-    uninstall () {
-      this.uppy.removePostProcessor(this.createEmptyDirs);
+      })).then(() => this.empty_dirs = []);
     }
   }
 
-  uppy = new Uppy({
-    restrictions: {
-      maxFileSize: maxFileSize,
-    }
-  });
-  
-  uppy.use(EmptyDirCreator);
-  uppy.use(Dashboard, {
-    trigger: '#upload-btn',
-    fileManagerSelectionType: 'both',
-    disableThumbnailGenerator: true,
-    showLinkToFileUploadResult: false,
-    closeModalOnClickOutside: true,
-    closeAfterFinish: true,
-    allowMultipleUploads: false,
-    onRequestCloseModal: () => closeAndResetUppyModal(uppy),
-    note: 'Empty directories will be included in the upload only when a directory upload is initiated via drag and drop. This is because the File and Directory Entries API is available only on a drop event, not during an input change event.'
-  });
-  uppy.use(XHRUpload, {
-    endpoint: filesUploadPath,
-    withCredentials: true,
-    fieldName: 'file',
-    limit: 1,
-    headers: { 'X-CSRF-Token': csrf_token },
-    timeout: 128 * 1000,
-  });
+  install () {
+    this.uppy.addPostProcessor(this.createEmptyDirs);
+  }
 
-  uppy.on('file-added', (file) => {
-    uppy.setFileMeta(file.id, { parent: history.state.currentDirectory });
-    if(file.meta.relativePath == null && file.data.webkitRelativePath){
-      uppy.setFileMeta(file.id, { relativePath: file.data.webkitRelativePath });
-    }
-  });
+  uninstall () {
+    this.uppy.removePostProcessor(this.createEmptyDirs);
+  }
+}
 
-  uppy.on('complete', (result) => {
-    if(result.successful.length > 0){
-      reloadTable();
-    }
-  });
+uppy = new Uppy({
+  restrictions: {
+    maxFileSize: maxFileSize,
+  }
+});
 
-  // https://stackoverflow.com/questions/6756583/prevent-browser-from-loading-a-drag-and-dropped-file
-  global.addEventListener("dragover",function(e){
-    e = e || event;
-    e.preventDefault();
-  },false);
-  
-  global.addEventListener("drop",function(e){
-    e = e || event;
-    e.preventDefault();
-  },false);
+uppy.use(EmptyDirCreator);
+uppy.use(Dashboard, {
+  trigger: '#upload-btn',
+  fileManagerSelectionType: 'both',
+  disableThumbnailGenerator: true,
+  showLinkToFileUploadResult: false,
+  closeModalOnClickOutside: true,
+  closeAfterFinish: true,
+  allowMultipleUploads: false,
+  onRequestCloseModal: () => closeAndResetUppyModal(uppy),
+  note: 'Empty directories will be included in the upload only when a directory upload is initiated via drag and drop. This is because the File and Directory Entries API is available only on a drop event, not during an input change event.'
+});
+uppy.use(XHRUpload, {
+  endpoint: filesUploadPath,
+  withCredentials: true,
+  fieldName: 'file',
+  limit: 1,
+  headers: { 'X-CSRF-Token': csrf_token },
+  timeout: 128 * 1000,
+});
 
-  $('#directory-contents').on('drop', function(e){
-    this.classList.remove('dragover');
-    // Prevent default behavior (Prevent file from being opened)
+uppy.on('file-added', (file) => {
+  uppy.setFileMeta(file.id, { parent: history.state.currentDirectory });
+  if(file.meta.relativePath == null && file.data.webkitRelativePath){
+    uppy.setFileMeta(file.id, { relativePath: file.data.webkitRelativePath });
+  }
+});
 
-    // pass drop event to uppy dashboard
-    uppy.getPlugin('Dashboard').openModal().then(() => uppy.getPlugin('Dashboard').handleDrop(e.originalEvent))
-  });
+uppy.on('complete', (result) => {
+  if(result.successful.length > 0){
+    reloadTable();
+  }
+});
 
-  $('#directory-contents').on('dragover', function(e){
-    this.classList.add('dragover');
+// https://stackoverflow.com/questions/6756583/prevent-browser-from-loading-a-drag-and-dropped-file
+global.addEventListener("dragover",function(e){
+  e = e || event;
+  e.preventDefault();
+},false);
 
-    // Prevent default behavior (Prevent file from being opened)
-    e.preventDefault();
+global.addEventListener("drop",function(e){
+  e = e || event;
+  e.preventDefault();
+},false);
 
-    // specifies what feedback will be shown to the user by setting the dropEffect attribute of the DataTransfer associated with the event
-    // too bad we can't show an indicator (no dragstart/end when dragging from OS to browser)
-    e.originalEvent.dataTransfer.dropEffect = 'copy';
-  });
+$('#directory-contents').on('drop', function(e){
+  this.classList.remove('dragover');
+  // Prevent default behavior (Prevent file from being opened)
 
-  $('#directory-contents').on('dragleave', function(e){
-    this.classList.remove('dragover');
-  });
+  // pass drop event to uppy dashboard
+  uppy.getPlugin('Dashboard').openModal().then(() => uppy.getPlugin('Dashboard').handleDrop(e.originalEvent))
+});
 
+$('#directory-contents').on('dragover', function(e){
+  this.classList.add('dragover');
+
+  // Prevent default behavior (Prevent file from being opened)
+  e.preventDefault();
+
+  // specifies what feedback will be shown to the user by setting the dropEffect attribute of the DataTransfer associated with the event
+  // too bad we can't show an indicator (no dragstart/end when dragging from OS to browser)
+  e.originalEvent.dataTransfer.dropEffect = 'copy';
+});
+
+$('#directory-contents').on('dragleave', function(e){
+  this.classList.remove('dragover');
 });
 
 function closeAndResetUppyModal(uppy){
