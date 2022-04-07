1d4a0355e11fef26fa81240cf0aa87c5d36796b5
Jeff Ohrstrom <johrstrom@osc.edu>
2022-04-07 16:09:11 -0400

    add a smart attribute for automatically loading modules (#1930)
    Add a smart attribute for automatically loading modules in the form: "auto_modules_#{app_name}" where app_name is something like matlab or jupyter.  These are also cluster aware meaning they'll hide & show if dynamic bc js is enabled.

--- apps/dashboard/test/models/hpc_module_test.rb
+++ apps/dashboard/test/models/hpc_module_test.rb
@@ -3,6 +3,13 @@
 require 'test_helper'
 
 class HpcModuleTest < ActiveSupport::TestCase
+
+  # owens.json and oakley.json in this directory are from real clusters
+  # (oakley is actually pitzer) from the time this was written.
+  def fixture_dir
+    "#{Rails.root}/test/fixtures/modules/"
+  end
+
   test 'all safely reads from inaccessabile directories' do
     with_modified_env({ OOD_MODULE_FILE_DIR: '/dev/null' }) do
       assert_equal [], HpcModule.all('owens')
@@ -18,10 +25,30 @@ class HpcModuleTest < ActiveSupport::TestCase
     end
   end
 
-  test 'reads a simple file' do
-    with_modified_env({ OOD_MODULE_FILE_DIR: "#{Rails.root}/test/fixtures/modules/" }) do
-      # NOTE: that there are no duplicates and rstudio has no version
-      assert_equal(['jupyter/1', 'jupyter/2', 'rstudio'], HpcModule.all('simple').map(&:to_s))
+  test 'all versions is corrrect, sorted and unique' do
+    stub_sys_apps
+    with_modified_env({ OOD_MODULE_FILE_DIR: fixture_dir }) do
+      expected = [
+        'app_jupyter/3.1.18', 'app_jupyter/3.0.17', 'app_jupyter/2.3.2', 'app_jupyter/2.2.10',
+        'app_jupyter/1.2.21', 'app_jupyter/1.2.16', 'app_jupyter/0.35.6'
+      ]
+      assert_equal(expected, HpcModule.all_versions('app_jupyter').map(&:to_s))
+    end
+  end
+
+  test 'all versions returns empty array when it cant find' do
+    stub_sys_apps
+    with_modified_env({ OOD_MODULE_FILE_DIR: fixture_dir }) do
+      assert_equal([], HpcModule.all_versions('wont_find').map(&:to_s))
+      assert_equal([], HpcModule.all_versions(nil).map(&:to_s))
+    end
+  end
+
+  test 'on_cluster? can find modules' do
+    stub_sys_apps
+    with_modified_env({ OOD_MODULE_FILE_DIR: fixture_dir }) do
+      assert HpcModule.new('app_jupyter', version: '0.35.6').on_cluster?('oakley')
+      assert !HpcModule.new('app_jupyter', version: '0.35.6').on_cluster?('owens')
     end
   end
 
--- apps/dashboard/test/system/batch_connect_test.rb
+++ apps/dashboard/test/system/batch_connect_test.rb
@@ -448,4 +448,51 @@ class BatchConnectTest < ApplicationSystemTestCase
     click_on('Launch')
     verify_bc_alert('sys/bc_jupyter', 'save', err_msg)
   end
+
+  test 'auto generated modules are dynamic' do
+    with_modified_env({ OOD_MODULE_FILE_DIR: 'test/fixtures/modules' }) do
+      visit new_batch_connect_session_context_url('sys/bc_jupyter')
+      intel_both_clusters  = []
+
+      # defaults
+      assert_equal '3.0.17', find_value('auto_modules_app_jupyter')
+      assert_equal '2021.3.0', find_value('auto_modules_intel')
+      assert_equal 'owens', find_value('cluster')
+      # versions not available on owens
+      assert_equal 'display: none;', find_option_style('auto_modules_app_jupyter', '3.1.18')
+      assert_equal 'display: none;', find_option_style('auto_modules_app_jupyter', '1.2.16')
+      assert_equal 'display: none;', find_option_style('auto_modules_app_jupyter', '0.35.6')
+      assert_equal 'display: none;', find_option_style('auto_modules_intel', '18.0.4')
+
+      # select oakley and now they're available
+      select('oakley', from: bc_ele_id('cluster'))
+      assert_equal '3.0.17', find_value('auto_modules_app_jupyter')
+      assert_equal '', find_option_style('auto_modules_app_jupyter', '3.1.18')
+      assert_equal '', find_option_style('auto_modules_app_jupyter', '1.2.16')
+      assert_equal '', find_option_style('auto_modules_app_jupyter', '0.35.6')
+
+      # and lots of intel versions aren't
+      assert_equal 'display: none;', find_option_style('auto_modules_intel', '18.0.2')
+      assert_equal 'display: none;', find_option_style('auto_modules_intel', '18.0.0')
+      assert_equal 'display: none;', find_option_style('auto_modules_intel', '17.0.5')
+      assert_equal 'display: none;', find_option_style('auto_modules_intel', '17.0.2')
+      assert_equal 'display: none;', find_option_style('auto_modules_intel', '16.0.8')
+      assert_equal 'display: none;', find_option_style('auto_modules_intel', '16.0.3')
+    end
+  end
 end
+
+
+# <select class="form-control" name="batch_connect_session_context[auto_modules]" id="batch_connect_session_context_auto_modules"><option value="2021.3.0">2021.3.0</option>
+# <option value="19.1.3">19.1.3</option>
+# <option value="19.0.5">19.0.5</option>
+# <option value="19.0.3">19.0.3</option>
+# <option data-option-for-cluster-owens="false" value="18.0.4" style="display: none;">18.0.4</option>
+# <option value="18.0.3">18.0.3</option>
+# <option data-option-for-cluster-pitzer="false" value="18.0.2" style="">18.0.2</option>
+# <option data-option-for-cluster-pitzer="false" value="18.0.0" style="">18.0.0</option>
+# <option value="17.0.7">17.0.7</option>
+# <option data-option-for-cluster-pitzer="false" value="17.0.5" style="">17.0.5</option>
+# <option data-option-for-cluster-pitzer="false" value="17.0.2" style="">17.0.2</option>
+# <option data-option-for-cluster-pitzer="false" value="16.0.8" style="">16.0.8</option>
+# <option data-option-for-cluster-pitzer="false" value="16.0.3" style="">16.0.3</option></select>
