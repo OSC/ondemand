6edfe470ba083376f55aaf8695e035b2aa8911d3
Jeff Ohrstrom <johrstrom@osc.edu>
2022-04-12 11:14:31 -0400

    allow for manifests to override the caption (#1941)
    Manifests can now override the default caption.  This means that icons showing apps can now show this text instead of the default 'System installed app' or similar.

--- apps/dashboard/app/apps/manifest.rb
+++ apps/dashboard/app/apps/manifest.rb
@@ -130,6 +130,13 @@ category: OSC
     @manifest_options[:new_window]
   end
 
+  # Return the app's caption
+  #
+  #  @return [String] caption as string
+  def caption
+    @manifest_options[:caption].to_s
+  end
+
   # Manifest objects are valid
   #
   # @return [true] Always return true
