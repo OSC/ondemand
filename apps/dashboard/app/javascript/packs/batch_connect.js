27cb1a7ace18c59cb724802d8a91046c8e0c3340
Jeff Ohrstrom <johrstrom@osc.edu>
2022-04-15 16:56:36 -0400

    add a comment to toggleMinMax

--- apps/dashboard/app/javascript/packs/batch_connect.js
+++ apps/dashboard/app/javascript/packs/batch_connect.js
@@ -361,6 +361,10 @@ function updateVisibility(event, changeId) {
   }
 }
 
+/**
+ * Update the min & max values of `changeId` based on the
+ * event, the `otherId` and the settings in minMaxLookup table.
+ */
 function toggleMinMax(event, changeId, otherId) {
   let x = undefined, y = undefined;
 
