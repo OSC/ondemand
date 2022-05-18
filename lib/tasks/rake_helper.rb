ff79b2464d5c5cc6830e35ad2e4e13ec5561367d
Jeff Ohrstrom <johrstrom@osc.edu>
2022-05-18 17:04:05 -0400

    build dev containers from nightly rpms instead

--- lib/tasks/rake_helper.rb
+++ lib/tasks/rake_helper.rb
@@ -159,6 +159,10 @@ module RakeHelper
     "#{version_major}.#{version_minor}.#{date}-#{id}.#{git_hash}.nightly"
   end
 
+  def today
+    Time.now.strftime("%Y%m%d")
+  end
+
   def numeric_tag
     @numeric_tag ||= git_tag.delete_prefix('v')
   end
@@ -175,16 +179,8 @@ module RakeHelper
     podman_runtime? ? "podman" : "docker"
   end
 
-  def test_image_name
-    "ood-test"
-  end
-
   def dev_image_name
     "ood-dev"
-  end
-
-  def image_name
-    "ood"
   end
 
   def user
@@ -195,7 +191,6 @@ module RakeHelper
     @image_names ||=
     {
       ood: "ood",
-      test: "ood-test",
       dev: "ood-dev"
     }.freeze
   end
