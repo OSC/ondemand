6ff860c7751c5db801ddcacf7dd1c1626b11fbaa
Travis Ravert <travert@osc.edu>
2022-05-25 17:15:22 -0400

    Initial commit to expand jobs app integration tests

--- apps/dashboard/test/integration/projects_controller_test.rb
+++ apps/dashboard/test/integration/projects_controller_test.rb
@@ -6,24 +6,88 @@ class ProjectsControllerTest < ActionDispatch::IntegrationTest
     OodAppkit.stubs(:dataroot).returns(Rails.root.join('test/fixtures/projects'))
     Configuration.stubs(:jobs_app_alpha?).returns(true)
     Rails.application.reload_routes!
+    get projects_path
+    assert :success 
+
+    # generate a CSRF token for redirects
+    doc = Nokogiri::XML(@response.body)
+    @token = doc.xpath("/html/head/meta[@name='csrf-token']/@content").to_s
   end
 
-  test "should get index" do 
+  test "gets index" do 
     get projects_path
     assert_response :success
   end
 
-  test "should get new" do
+  test "successfully creates new project" do
     get new_project_path
     assert_response :success
-  end
 
-  test "should get destroy" do
-    get project_path(:id)
+    params = { project: { name: 'test-project', icon: 'fas://cog' } }
+    header = { 'X-CSRF-Token' => @token }
+
+    post projects_path, params: params, headers: header
+
+    assert_response :redirect
+    follow_redirect!
     assert_response :success
   end
 
-  test "should get edit" do
+  test "unsuccessfully creates new project" do
+    get new_project_path
+    assert_response :success
+
+    params = { project: { name: 'invalid name', icon: 'fas://cog' } }
+    header = { 'X-CSRF-Token' => @token }
+
+    post projects_path, params: params, headers: header
+
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+    # flash needed
+  end
+
+  test "deletes project" do
+    create_project
+
+    header = { 'X-CSRF-Token' => @token }
+
+    delete project_path('test-project'), headers: header
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+  end
+
+  test "update project redirects with success flash" do
+    create_project
+
+    header =  { 'X-CSRF-Token' => @token }
+
+    patch project_path('test-project'), headers: header
+
+    params = { project: { name: 'test-project-update' } }
+    patch project_path('test-project'), headers: header
+    # needs to get flash and ensure redirect still
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+  end
+
+  test "update with invalid redirects with error flash" do
+    create_project
+
+    header =  { 'X-CSRF-Token' => @token }
+
+    params = { project: { name: 'bad test project update' } }
+    patch project_path('test-project'), headers: header
+    # need to get flash too
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+  end
+
+  test "gets edit" do
     # I think the project needs to exist to edit, :dir is missing in template
     Dir.mktmpdir do |dir|
       Project.stubs(dir).returns(Pathname.new("#{dir}/projects/project_1"))
@@ -50,5 +114,15 @@ class ProjectsControllerTest < ActionDispatch::IntegrationTest
 
         assert Dir.exist?("#{dir}/projects")
     end
+  end
+
+  def create_project
+    get new_project_path
+
+    params = { project: { name: 'test-project', icon: 'fas://cog' } }
+    header = { 'X-CSRF-Token' => @token }
+
+    post projects_path, params: params, headers: header
+    follow_redirect!
   end
 end
