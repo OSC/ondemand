1dfdadf1ed2c9c66c4c58d41e4ca01da8fa1bf45
Trey Dockendorf <tdockendorf@osc.edu>
2022-07-20 09:41:07 -0400

    More fixes for ood-portal-generator unit tests

--- ood-portal-generator/spec/update_ood_portal_spec.rb
+++ ood-portal-generator/spec/update_ood_portal_spec.rb
@@ -33,6 +33,7 @@ describe 'update_ood_portal' do
   before(:each) do
     allow(Process).to receive(:uid).and_return(0)
     allow(OodPortalGenerator).to receive(:apache_group).and_return('apache')
+    allow(OodPortalGenerator).to receive(:debian?).and_return(false)
     allow(OodPortalGenerator).to receive(:fqdn).and_return('example.com')
   end
 
