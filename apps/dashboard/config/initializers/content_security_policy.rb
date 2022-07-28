1de02e18a49a9c059d49a508048ee573da10a182
Jeff Ohrstrom <johrstrom@osc.edu>
2022-07-28 14:44:01 -0400

    remove https from default CSP, as we really just want self

--- apps/dashboard/config/initializers/content_security_policy.rb
+++ apps/dashboard/config/initializers/content_security_policy.rb
@@ -5,14 +5,14 @@
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
 Rails.application.config.content_security_policy do |policy|
-  policy.default_src :self, :https
-  policy.font_src    :self, :https, :data
-  policy.img_src     :self, :https, :data
+  policy.default_src :self,
+  policy.font_src    :self, :data
+  policy.img_src     :self, :data
   policy.object_src  :none
-  policy.script_src  :self, :https
-  policy.style_src   :self, :https
+  policy.script_src  :self
+  policy.style_src   :self
   # If you are using webpack-dev-server then specify webpack-dev-server host
-  policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
+  policy.connect_src :self, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
 
   # Specify URI for violation reports
   # policy.report_uri "/csp-violation-report-endpoint"
