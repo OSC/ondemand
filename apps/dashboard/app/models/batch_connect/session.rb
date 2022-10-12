17e09b09485f5750353c8b1728572bdae9d3d5f9
Jeff Ohrstrom <johrstrom@osc.edu>
2022-10-12 12:14:19 -0400

    use SHA1 instead of MD5 for FIPs systems (#2325)

--- apps/dashboard/app/models/batch_connect/session.rb
+++ apps/dashboard/app/models/batch_connect/session.rb
@@ -528,7 +528,7 @@ module BatchConnect
         connect: running? ? connect.to_h : nil,
         time: info.wallclock_time.to_i / 60     # only update every minute
       }
-      Digest::MD5.hexdigest(hsh.to_json)
+      Digest::SHA1.hexdigest(hsh.to_json)
     end
 
     private
