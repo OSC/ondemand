17b330568461e9d8a8c25548d3e400bf8e0aba6a
Luke Weiler <lweiler@osc.edu>
2022-10-14 14:58:53 -0400

    Test w/ OOD_DATAROOT set to nil instead of deleted

--- apps/dashboard/test/config/configuration_singleton_test.rb
+++ apps/dashboard/test/config/configuration_singleton_test.rb
@@ -232,8 +232,7 @@ class ConfigurationSingletonTest < ActiveSupport::TestCase
   end
 
   test "should have default dataroot under home dir if production" do
-    with_modified_env(RAILS_ENV: 'production') do
-      ENV.delete('OOD_DATAROOT')
+    with_modified_env(RAILS_ENV: 'production', OOD_DATAROOT: nil) do
       assert_equal Pathname.new('~/ondemand/data/sys/dashboard').expand_path, ConfigurationSingleton.new.dataroot
     end
   end
@@ -245,15 +244,13 @@ class ConfigurationSingletonTest < ActiveSupport::TestCase
   end
 
   test "can configure portal component of dataroot if production" do
-    with_modified_env(RAILS_ENV: 'production', OOD_PORTAL: 'MY_PORTAL') do
-      ENV.delete('OOD_DATAROOT')
+    with_modified_env(RAILS_ENV: 'production', OOD_DATAROOT: nil, OOD_PORTAL: 'MY_PORTAL') do
       assert_equal Pathname.new('~/MY_PORTAL/data/sys/dashboard').expand_path, ConfigurationSingleton.new.dataroot
     end
   end
 
   test "can configure app token component of dataroot if production" do
-    with_modified_env(RAILS_ENV: 'production', APP_TOKEN: 'MY/APP/TOKEN') do
-      ENV.delete('OOD_DATAROOT')
+    with_modified_env(RAILS_ENV: 'production', APP_TOKEN: 'MY/APP/TOKEN', OOD_DATAROOT: nil) do
       assert_equal Pathname.new('~/ondemand/data/MY/APP/TOKEN').expand_path, ConfigurationSingleton.new.dataroot
     end
   end
