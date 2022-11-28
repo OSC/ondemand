c185b64292f51a884987af6756c4f6cd8685228f
Jeff Ohrstrom <johrstrom@osc.edu>
2022-11-28 12:26:33 -0500

    add rlcone initializer to auto populate rclone remotes (#2371)

---
+++ apps/dashboard/config/initializers/rclone.rb
@@ -0,0 +1,11 @@
+require 'rclone_util'
+
+Rails.application.config.after_initialize do |_|
+  remotes = RcloneUtil.list_remotes.map { |r| FavoritePath.new('', title: r, filesystem: r) }
+
+  OodFilesApp.candidate_favorite_paths.tap do |paths|
+    paths.concat(remotes)
+  end
+rescue => e
+  Rails.logger.error(e.message)
+end
