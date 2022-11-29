0da07bd91a98a26f45b7d59a6bf0e3340f11c2cf
Jeff Ohrstrom <johrstrom@osc.edu>
2022-11-29 09:27:41 -0500

    navconfig to use allowlist to deprecate whitelist (#2380)
    * navconfig to use allowlist to deprecate whitelist
    
    * use barename for all aliases

--- apps/dashboard/app/apps/nav_config.rb
+++ apps/dashboard/app/apps/nav_config.rb
@@ -1,11 +1,18 @@
+# frozen_string_literal: true
+
 #
 # Deprecated.
 # This is now configurable through configuration. See UserConfiguration class.
 class NavConfig
-    class << self
-      attr_accessor :categories, :categories_whitelist
-      alias_method :categories_whitelist?, :categories_whitelist
-    end
-    self.categories = ["Apps", "Files", "Jobs", "Clusters", "Interactive Apps"]
-    self.categories_whitelist = false
+  class << self
+    attr_accessor :categories, :categories_allowlist
+    alias categories_allowlist? categories_allowlist
+
+    alias categories_whitelist categories_allowlist
+    alias categories_whitelist= categories_allowlist=
+    alias categories_whitelist? categories_allowlist?
+  end
+
+  self.categories = ['Apps', 'Files', 'Jobs', 'Clusters', 'Interactive Apps']
+  self.categories_allowlist = false
 end
