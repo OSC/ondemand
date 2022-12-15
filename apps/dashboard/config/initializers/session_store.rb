965190c0ac32ede8712b24e781f3e359134a8765
Jeff Ohrstrom <johrstrom@osc.edu>
2022-12-15 11:41:23 -0500

    use cache store for sessions if you can (#2434)

--- apps/dashboard/config/initializers/session_store.rb
+++ apps/dashboard/config/initializers/session_store.rb
@@ -1,5 +1,13 @@
 # Be sure to restart your server when you modify this file.
 
 if Rails.application.config.session_store.nil?
-  Rails.application.config.session_store :cookie_store, key: '_dashboard_session', secure: Rails.env.production?, same_site: :strict
+  begin
+    dir = "/var/tmp/#{Etc.getlogin}"
+    Dir.mkdir(dir, 0o0700) unless Dir.exist?(dir)
+
+    Rails.application.config.session_store(:cache_store, cache: ActiveSupport::Cache::FileStore.new(dir))
+  rescue StandardError => e
+    warn "Cannot use cache store because of error: #{e.message}"
+    Rails.application.config.session_store(:cookie_store, key: '_dashboard_session', secure: Rails.env.production?, same_site: :strict)
+  end
 end
