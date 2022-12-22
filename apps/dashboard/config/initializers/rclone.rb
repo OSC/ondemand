b373f45e7b1bed6c0f2309d38e933b79b2650c28
Jeff Ohrstrom <johrstrom@osc.edu>
2022-12-22 12:50:54 -0500

    rename remote files config to remote_files_enabled

--- apps/dashboard/config/initializers/rclone.rb
+++ apps/dashboard/config/initializers/rclone.rb
@@ -1,6 +1,8 @@
 require 'rclone_util'
 
 Rails.application.config.after_initialize do |_|
+  break unless Configuration.remote_files_enabled?
+
   remotes = RcloneUtil.list_remotes.map { |r| FavoritePath.new('', title: r, filesystem: r) }
 
   OodFilesApp.candidate_favorite_paths.tap do |paths|
