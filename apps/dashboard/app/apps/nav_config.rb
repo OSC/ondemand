1ffc9d41cd9da4fd860023a28f05b92922ad3ea6
Jeff Ohrstrom <johrstrom@osc.edu>
2023-01-03 13:39:44 -0500

    add deprecation warnings for NavConfig

--- apps/dashboard/app/apps/nav_config.rb
+++ apps/dashboard/app/apps/nav_config.rb
@@ -4,15 +4,37 @@
 # Deprecated.
 # This is now configurable through configuration. See UserConfiguration class.
 class NavConfig
+
   class << self
-    attr_accessor :categories, :categories_allowlist
+
+    DEPRECATION_MSG = "NavConfig is deprecated and will be removed in 3.0. Use the ondemand.d property 'nav_categories' instead."
+    DEFAULT_CATEGORIES = ['Apps', 'Files', 'Jobs', 'Clusters', 'Interactive Apps'].freeze
+    DEFAULT_ALLOWLIST = false
+
+    def categories
+      ActiveSupport::Deprecation.warn(DEPRECATION_MSG) unless Rails.env.test?
+      @categories || DEFAULT_CATEGORIES
+    end
+
+    def categories=(value)
+      ActiveSupport::Deprecation.warn(DEPRECATION_MSG) unless Rails.env.test?
+      @categories = value
+    end
+
+    def categories_allowlist
+      ActiveSupport::Deprecation.warn(DEPRECATION_MSG) unless Rails.env.test?
+      @categories_allowlist || DEFAULT_ALLOWLIST
+    end
+
+    def categories_allowlist=(value)
+      ActiveSupport::Deprecation.warn(DEPRECATION_MSG) unless Rails.env.test?
+      @categories_allowlist = value
+    end
+
     alias categories_allowlist? categories_allowlist
 
     alias categories_whitelist categories_allowlist
     alias categories_whitelist= categories_allowlist=
     alias categories_whitelist? categories_allowlist?
   end
-
-  self.categories = ['Apps', 'Files', 'Jobs', 'Clusters', 'Interactive Apps']
-  self.categories_allowlist = false
 end
