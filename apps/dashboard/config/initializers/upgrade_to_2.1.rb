7df1046187d578ac5c36c9d3195aa4a59c000232
Jeff Ohrstrom <johrstrom@osc.edu>
2023-01-06 11:44:52 -0500

    add initializer to sync cache files from 2.0 to 2.1 (#2442)

---
+++ apps/dashboard/config/initializers/upgrade_to_2.1.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+Rails.application.reloader.to_prepare do
+  # Since https://github.com/OSC/ondemand/pull/1526 all the batch connect cache files
+  # have moved. So, when folks upgrade to 2.1, let's sync these old files so that
+  # they don't lose their cached choices.
+  old_context_files = "#{Configuration.dataroot}/batch_connect/**/*/context.json"
+  cache_root = BatchConnect::Session.cache_root
+
+  Dir.glob(old_context_files).map do |old_file|
+    new_filename = old_file.gsub(%r{.*/batch_connect/}, '').gsub('/context.json', '').gsub('/', '_')
+    new_filename = "#{new_filename}.json"
+
+    new_file = "#{cache_root}/#{new_filename}"
+    if !File.exist?(new_file)
+      FileUtils.cp(old_file, new_file)
+    elsif File.mtime(old_file) > File.mtime(new_file)
+      FileUtils.cp(old_file, new_file)
+    end
+  end
+end
