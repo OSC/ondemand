b748c9a5b37ed840ee488dc005effac99747caed
Jeff Ohrstrom <johrstrom@osc.edu>
2023-01-11 16:49:11 -0500

    start auto_accounts bc option

---
+++ apps/dashboard/app/lib/smart_attributes/attributes/auto_accounts.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module SmartAttributes
+  class AttributeFactory
+    # Build this attribute object. No options are used as this Attribute
+    # is meant to be dynamically generated from the users' unix groups
+    # @param opts [Hash] attribute's options
+    # @return [Attributes::AutoGroups] the attribute object
+    def self.build_auto_accounts(opts = {})
+      # only support for slurm atm and slurm queries all clusters.
+      cluster = Configuration.job_clusters.select(&:slurm?).first
+      accounts = cluster.job_adapter.accounts.map(&:to_s).uniq
+
+      static_opts = {
+        options: accounts
+      }.merge(opts.without(:options).to_h)
+
+      Attributes::AutoAccounts.new('auto_accounts', static_opts)
+    end
+  end
+
+  module Attributes
+    class AutoAccounts < Attribute
+      def widget
+        'select'
+      end
+
+      def label(*)
+        (opts[:label] || 'Account').to_s
+      end
+
+      # Submission hash describing how to submit this attribute
+      # @param fmt [String, nil] formatting of hash
+      # @return [Hash] submission hash
+      def submit(*)
+        { script: { accounting_id: value } }
+      end
+    end
+  end
+end
