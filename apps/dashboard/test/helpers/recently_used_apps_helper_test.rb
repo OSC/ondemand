f230efe132dbc218d2078a1ea272cc7bf7c9b3f1
Aday BA <aday@theagilemonkeys.com>
2023-01-24 18:48:52 +0000

    Added recently used applications widget (#2503)

---
+++ apps/dashboard/test/helpers/recently_used_apps_helper_test.rb
@@ -0,0 +1,133 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class RecentlyUsedAppsHelperTest < ActionView::TestCase
+  include RecentlyUsedAppsHelper
+
+  def setup
+    # Mock ApplicationController setting default values to the apps
+    @sys_apps = []
+    @dev_apps = []
+    @usr_apps = []
+  end
+
+  test 'recently_used_apps should return empty array when cache_root directory has no files' do
+    BatchConnect::Session.stubs({ cache_root: stub({ children: [] }) })
+    result = recently_used_apps
+
+    assert_equal [], result
+  end
+
+  test 'recently_used_apps should return empty array when cache files do not match any application' do
+    cache_files = create_cache_files(['rstudio.json', 'jupyter.json'])
+    BatchConnect::Session.stubs({ cache_root: stub({ children: cache_files }) })
+    @sys_apps = create_batch_connect_apps(['no_match.json', 'other.json'])
+
+    result = recently_used_apps
+
+    assert_equal [], result
+  end
+
+  test 'recently_used_apps should ignore applications that are not batch_connect' do
+    cache_files = create_cache_files(['rstudio.json', 'jupyter.json'])
+    BatchConnect::Session.stubs({ cache_root: stub({ children: cache_files, join: nil }) })
+    @sys_apps = create_batch_connect_apps(['rstudio.json', 'jupyter.json'], batch_connect: false)
+
+    result = recently_used_apps
+
+    assert_equal [], result
+  end
+
+  test 'recently_used_apps should return the applications that matches the cache files' do
+    cache_files = create_cache_files(['rstudio.json', 'jupyter.json'])
+    BatchConnect::Session.stubs({ cache_root: stub({ children: cache_files, join: nil }) })
+    @sys_apps = create_batch_connect_apps(['no_match.json', 'rstudio.json'])
+
+    result = recently_used_apps
+
+    assert_equal 1, result.size
+    assert_equal 'rstudio.json', result[0].cache_file
+  end
+
+  test 'recently_used_apps should set cacheable attribute to matched sys apps to true' do
+    cache_files = create_cache_files(['rstudio.json', 'jupyter.json'])
+    BatchConnect::Session.stubs({ cache_root: stub({ children: cache_files, join: nil }) })
+    @sys_apps = create_batch_connect_apps(['jupyter.json', 'rstudio.json'])
+
+    result = recently_used_apps
+
+    assert_equal 2, result.size
+    result.each do |app|
+      app.build_session_context.each do |attribute|
+        assert_equal true, attribute.cacheable?(false)
+      end
+    end
+  end
+
+  test 'recently_used_apps should return at most 4 apps sorted by most recent cache files' do
+    cache_files = create_cache_files(
+      ['ancient.json',
+       'very_old.json',
+       'old.json',
+       'now.json',
+       'newer.json',
+       'newest.json']
+    )
+    BatchConnect::Session.stubs({ cache_root: stub({ children: cache_files, join: nil }) })
+    @sys_apps = create_batch_connect_apps(['newer.json', 'very_old.json', 'now.json', 'ancient.json', 'newest.json', 'old.json'])
+
+    result = recently_used_apps
+
+    assert_equal 4, result.size
+    assert_equal 'newest.json', result[0].cache_file
+    assert_equal 'newer.json', result[1].cache_file
+    assert_equal 'now.json', result[2].cache_file
+    assert_equal 'old.json', result[3].cache_file
+  end
+
+  test 'recently_used_apps should call update_session_with_cache in all matched applications' do
+    cache_files = create_cache_files(['rstudio.json', 'jupyter.json', 'desktop.json'])
+    BatchConnect::Session.stubs({ cache_root: stub({ children: cache_files, join: nil }) })
+    @sys_apps = create_batch_connect_apps(['rstudio.json', 'jupyter.json', 'desktop.json'])
+    @sys_apps.each { |app| app.expects(:update_session_with_cache) }
+
+    result = recently_used_apps
+
+    assert_equal 3, result.size
+  end
+
+  test 'recently_used_apps_cached should should call Rails.cache with a block' do
+    BatchConnect::Session.stubs({ cache_root: stub({ children: [] }) })
+    Rails.cache.expects(:fetch).with('recently_used_apps', expires_in: 1.hour).with_block_given.returns([])
+    result = recently_used_apps
+
+    assert_equal [], result
+  end
+
+  def create_cache_files(files)
+    files.each_with_index.map do |filename, index|
+      Pathname.new(filename).tap { |pathname| File.stubs(:mtime).with(pathname).returns(Time.now + index.day) }
+    end
+  end
+
+  def create_batch_connect_apps(files, batch_connect: true)
+    attributes = files.map { |filename| SmartAttributes::Attribute.new(filename, { cacheable: false }) }
+    files.map do |filename|
+      BatchConnectAppMock.new.tap do |sys_app|
+        sys_app.send('batch_connect_app?=', batch_connect)
+        sys_app.cache_file = filename
+        sys_app.sub_app_list = [sys_app]
+        sys_app.build_session_context = BatchConnect::SessionContext.new(attributes)
+      end
+    end
+  end
+
+  private
+
+  class BatchConnectAppMock < OpenStruct
+    def update_session_with_cache(session_context, cache_file)
+      # Just stubbing the method
+    end
+  end
+end
---
+++ apps/dashboard/test/integration/recently_used_apps_widget_test.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+
+require 'html_helper'
+require 'test_helper'
+
+class RecentlyUsedAppsWidgetTest < ActionDispatch::IntegrationTest
+  def setup
+    SysRouter.stubs(:base_path).returns(Rails.root.join('test/fixtures/widgets/recently_used_apps/sys'))
+    DevRouter.stubs(:base_path).returns(Rails.root.join('test/fixtures/widgets/recently_used_apps/dev'))
+    UsrRouter.stubs(:base_path).returns(Rails.root.join('test/fixtures/widgets/recently_used_apps/usr'))
+    BatchConnect::Session.stubs(:cache_root).returns(Rails.root.join('test/fixtures/widgets/recently_used_apps'))
+    OodAppkit.stubs(:clusters).returns(OodCore::Clusters.load_file('test/fixtures/config/clusters.d'))
+
+    stub_user_configuration(
+      {
+        dashboard_layout: {
+          rows: [{ columns: [{ width: 12, widgets: ['recently_used_apps'] }] }]
+        }
+      }
+    )
+  end
+
+  test 'should render recently used widget' do
+    get '/'
+
+    assert_select 'div.recently-used-apps-header h4' do |widget_header|
+      assert_equal 1, widget_header.size
+      assert_equal true, widget_header.first.text.include?(I18n.t('dashboard.recently_used_apps_title')),
+                   'Should display the widget title'
+    end
+
+    assert_select "div[data-toggle='launcher-button']", 1
+    assert_select 'form.button_to', 1
+    assert_select 'form.button_to button p.app-title', text: 'Recently Used Sys'
+  end
+
+  test 'recently used apps should render the a launcher with a form with the application cached values' do
+    get '/'
+
+    assert_select "div[data-toggle='launcher-button']", 1
+
+    assert_select 'form.button_to' do |elements|
+      assert_equal 1, elements.size
+      assert_equal batch_connect_session_contexts_path(token: 'sys/bc_desktop'), elements[0]['action']
+      assert_equal 'post', elements[0]['method']
+    end
+
+    recently_used_app = BatchConnect::App.from_token('sys/bc_desktop')
+    app_attributes = recently_used_app.attributes
+    form_values = {}
+    assert_select 'form.button_to input[type="hidden"]' do |elements|
+      elements.each do |input|
+        form_values[input['name']] = input['value']
+      end
+    end
+
+    app_attributes.each do |attribute|
+      assert_equal true, form_values.key?("batch_connect_session_context[#{attribute.id}]"),
+                   "Attribute: #{attribute.id} not in form: #{form_values}"
+    end
+
+    assert_equal 'quick', form_values['batch_connect_session_context[cluster]']
+    assert_equal '100', form_values['batch_connect_session_context[bc_vnc_idle]']
+    assert_equal 'gnome', form_values['batch_connect_session_context[desktop]']
+    assert_equal '10', form_values['batch_connect_session_context[bc_num_hours]']
+    assert_equal '5', form_values['batch_connect_session_context[bc_num_slots]']
+    assert_equal 'big', form_values['batch_connect_session_context[node_type]']
+    assert_equal 'account', form_values['batch_connect_session_context[bc_account]']
+    assert_equal 'queue', form_values['batch_connect_session_context[bc_queue]']
+    assert_equal '10', form_values['batch_connect_session_context[bc_num_hours]']
+    assert_equal '1080x754', form_values['batch_connect_session_context[bc_vnc_resolution]']
+    assert_equal '1', form_values['batch_connect_session_context[bc_email_on_started]']
+  end
+end
