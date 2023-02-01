dd9580c1548d0e0a75cefca88d69c3d39a6ec00f
Jeff Ohrstrom <johrstrom@osc.edu>
2023-02-01 16:12:33 -0500

    use memory store caches (#2532)
    use :memory_store instead of mem cache.

--- apps/dashboard/config/environments/development.rb
+++ apps/dashboard/config/environments/development.rb
@@ -20,15 +20,14 @@ Rails.application.configure do
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
-    config.cache_store = :memory_store
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
-
-    config.cache_store = :null_store
   end
+
+  config.cache_store = :memory_store
 
   config.action_mailer.perform_caching = false
 
