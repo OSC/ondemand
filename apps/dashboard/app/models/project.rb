2537ec89a8366f75c9103aedcf102b90e831e823
Travis Ravert <travert@osc.edu>
2023-02-10 12:15:49 -0500

    There should only be a one single form in a project.

--- apps/dashboard/app/models/project.rb
+++ apps/dashboard/app/models/project.rb
@@ -86,9 +86,7 @@ class Project
   end
 
   def form
-    form_dataroot.children.map do |form_path|
-      Script.new(form_path: form_path.to_s)
-    end
+    File.open(form_dataroot, 'w')
   end
 
   def project_dataroot
