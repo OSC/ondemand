2e2da6ae885b26057fed6ee2a9bbda7652b3c650
Aday BA <aday@theagilemonkeys.com>
2023-02-15 14:31:54 +0000

    Added support for images from /public location (#2577)

--- apps/dashboard/test/helpers/application_helper_test.rb
+++ apps/dashboard/test/helpers/application_helper_test.rb
@@ -67,4 +67,40 @@ class ApplicationHelperTest < ActionView::TestCase
     @user_configuration = stub(:custom_css_files => [''], :public_url => Pathname.new('/public'))
     assert_equal [], custom_css_paths
   end
+
+  test "icon_tag should should render icon tag for known icon schemas" do
+    @user_configuration = stub({ public_url: Pathname.new('/public') })
+    ['fa', 'fas', 'far', 'fab', 'fal'].each do |icon_schema|
+      image_uri = URI("#{icon_schema}://icon_name")
+      html = Nokogiri::HTML(icon_tag(image_uri))
+
+      icon_html = html.at_css('i')
+      assert_equal true, icon_html['class'].include?(icon_schema)
+      assert_equal true, icon_html['class'].include?('icon_name')
+    end
+  end
+
+  test "icon_tag should should render image tag prefixing relative_url_root if image URI does not start with public_url" do
+    @user_configuration = stub({ public_url: Pathname.new('/public') })
+    config.stubs(:relative_url_root).returns('/prefix')
+    image_uri = URI('/path/to/image.png')
+    html = Nokogiri::HTML(icon_tag(image_uri))
+
+    image_html = html.at_css('img')
+    assert_equal 'app-icon', image_html['class']
+    assert_equal image_uri.to_s, image_html['title']
+    assert_equal '/prefix/path/to/image.png', image_html['src']
+  end
+
+  test "icon_tag should should render image tag without prefixing relative_url_root if image URI starts with public_url" do
+    @user_configuration = stub({ public_url: Pathname.new('/public') })
+    config.stubs(:relative_url_root).returns('/prefix')
+    image_uri = URI('/public/path/image.png')
+    html = Nokogiri::HTML(icon_tag(image_uri))
+
+    image_html = html.at_css('img')
+    assert_equal 'app-icon', image_html['class']
+    assert_equal image_uri.to_s, image_html['title']
+    assert_equal image_uri.to_s, image_html['src']
+  end
 end
