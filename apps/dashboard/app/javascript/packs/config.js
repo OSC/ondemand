aae83369cbd2ba487930eb645163a22afcdda5c0
Travis Ravert <travert@osc.edu>
2023-02-16 09:34:49 -0500

    Use config.js to handle js setup for configs and csrf_token (#2550)
    * Use config.js to handle js setup for configs and csrf_token

--- apps/dashboard/app/javascript/packs/config.js
+++ apps/dashboard/app/javascript/packs/config.js
@@ -38,11 +38,36 @@ function navbarHighlight(theme, color) {
     }`;
 }
 
-function getConfigData() {
+function maxFileSize () {
   const cfgData = configData();
 
-  return cfgData;
+  // Check if cfgData['maxFileSize'] is just empty string, 
+  // if so set default of maxFileUpload=10737420000 bytes.
+  if (cfgData['maxFileSize'].length == 0) {
+    return parseInt(10737420000, 10);
+  }
+  else {
+    const maxFileSize = cfgData['maxFileSize'];
+    return parseInt(maxFileSize, 10);
+  }
 }
 
-export { setNavbarColor };
-export { getConfigData};
+function transfersPath() {
+  const cfgData = configData();
+  const transfersPath = cfgData['transfersPath'];
+
+  return transfersPath;
+}
+
+function csrfToken() {
+  const csrf_token = document.querySelector('meta[name="csrf-token"]').content;
+
+  return csrf_token;
+}
+
+export { 
+  setNavbarColor, 
+  maxFileSize, 
+  transfersPath, 
+  csrfToken 
+};
