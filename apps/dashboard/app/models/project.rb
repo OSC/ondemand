b6cedd093047fc277272d6aa6ce0db1b07442b87
Travis Ravert <travert@osc.edu>
2023-02-16 09:35:33 -0500

    Add test and logic to set default icon (#2583)

--- apps/dashboard/app/models/project.rb
+++ apps/dashboard/app/models/project.rb
@@ -100,12 +100,16 @@ class Project
   def valid_form_inputs?(attributes)
     icon_pattern = %r{\Afa[bsrl]://[\w-]+\z}
     name_pattern = /\A[\w-]+\z/
+
     if !attributes[:icon].nil? && !attributes[:icon].match?(icon_pattern)
       errors.add(:icon, :invalid_format, message: 'Icon format invalid or missing')
       false
     elsif !attributes[:name].match?(name_pattern)
       errors.add(:name, :invalid_format, message: 'Name format invalid')
       false
+    elsif attributes[:icon].nil?
+      attributes[:icon] = 'fas://cog'
+      true
     else
       true
     end
