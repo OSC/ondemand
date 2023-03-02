da079c9a8d49302a982891fd7313ceae94eff7ee
Jeff Ohrstrom <johrstrom@osc.edu>
2023-03-02 17:10:10 -0500

    lets just rm it bc its not safe

--- apps/dashboard/app/controllers/files_controller.rb
+++ apps/dashboard/app/controllers/files_controller.rb
@@ -1,6 +1,5 @@
 # The controller for all the files pages /dashboard/files
 class FilesController < ApplicationController
-  # include ActionController::Live
   include ZipTricks::RailsStreaming
 
   def fs
