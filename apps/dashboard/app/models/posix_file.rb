08c1928c682c9cc431478186b68e7f3be5d4a53b
Jeff Ohrstrom <johrstrom@osc.edu>
2023-03-03 13:00:01 -0500

    filter files that have non utf-8 characters in their filenames (#2626)
    Filter files that have non utf-8 characters in their filenames using valid_encoding?

--- apps/dashboard/app/models/posix_file.rb
+++ apps/dashboard/app/models/posix_file.rb
@@ -85,6 +85,10 @@ class PosixFile
   def ls
     path.each_child.map do |child_path|
       PosixFile.stat(child_path)
+    end.select do |stats|
+      valid_encoding = stats[:name].to_s.valid_encoding?
+      Rails.logger.warn("Not showing file '#{stats[:name]}' because it is not a UTF-8 filename.") unless valid_encoding
+      valid_encoding
     end.sort_by { |p| p[:directory] ? 0 : 1 }
   end
 
