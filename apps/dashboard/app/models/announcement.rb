c291db06c5bb643259194f2005a422277e6c40df
Jeff Ohrstrom <johrstrom@osc.edu>
2023-03-09 16:54:27 -0500

    rescue SyntaxErrors in announcements

--- apps/dashboard/app/models/announcement.rb
+++ apps/dashboard/app/models/announcement.rb
@@ -46,8 +46,8 @@ class Announcement
                   {}
                 end
       @opts.symbolize_keys.compact
-    rescue Errno::ENOENT  # File does not exist
-      @opts = {}
+    rescue Errno::ENOENT, SyntaxError # File does not exist or syntax errors
+      @opts = { }
     rescue => e
       Rails.logger.warn "Error parsing announcement file '#{@path}': #{e.message}"
       @opts = {}
