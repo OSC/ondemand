9685ff13f7ab6c964c79534465a4b0a3666a110a
Jeff Ohrstrom <johrstrom@osc.edu>
2023-03-22 17:50:39 -0400

    add some test cases for scripts and fix some bugs (#2691)

--- apps/dashboard/test/system/jobs_app_test.rb
+++ apps/dashboard/test/system/jobs_app_test.rb
@@ -2,11 +2,28 @@
 
 require 'application_system_test_case'
 
+# Tests /projects URL and the associated code paths
 class ProjectsTest < ApplicationSystemTestCase
-
   def setup
+    stub_clusters
+    stub_user
     Configuration.stubs(:jobs_app_alpha).returns(true)
     Rails.application.reload_routes!
+    Open3
+      .stubs(:capture3)
+      .with({}, 'sacctmgr', '-nP', 'show', 'users', 'withassoc', 'format=account,cluster,partition,qos', 'where', 'user=me', { stdin_data: ''})
+      .returns([File.read('test/fixtures/cmd_output/sacctmgr_show_accts.txt'), '', exit_success])
+  end
+
+  def setup_project(dir)
+    OodAppkit.stubs(:dataroot).returns(Pathname.new(dir))
+    proj = 'test-project'
+    icon = 'fas://arrow-right'
+    visit projects_path
+    click_on I18n.t('dashboard.jobs_project_create_new_project_directory')
+    find('#project_name').set(proj)
+    find('#product_icon_select').set(icon)
+    click_on 'Save'
   end
 
   test 'create a new project on fs and display the table entry' do
@@ -106,14 +123,66 @@ class ProjectsTest < ApplicationSystemTestCase
     end
   end
 
-  def setup_project(dir)
-    OodAppkit.stubs(:dataroot).returns(Pathname.new(dir))
-    proj = 'test-project'
-    icon = 'fas://arrow-right'
-    visit projects_path
-    click_on I18n.t('dashboard.jobs_project_create_new_project_directory')
-    find('#project_name').set(proj)
-    find('#product_icon_select').set(icon)
-    click_on 'Save'
+  test 'creating and showing scripts' do
+    Dir.mktmpdir do |dir|
+      setup_project(dir)
+      find('[href="/projects/1"]').click
+      click_on 'New Script'
+      find('#script_title').set('the script title')
+      click_on 'Save'
+      expected_yml = <<~HEREDOC
+        ---
+        title: the script title
+      HEREDOC
+
+      assert_selector('.alert-success', text: "×\nClose\nsucess!")
+      assert_equal(expected_yml, File.read("#{dir}/projects/1/.ondemand/scripts/1.yml"))
+
+      find('[href="/projects/1/scripts/1"]').click
+      assert_selector('h1', text: 'the script title', count: 1)
+    end
+  end
+
+  test 'showing scripts with auto attributes' do
+    Dir.mktmpdir do |dir|
+      setup_project(dir)
+
+      # init some shell scripts
+      script_dir = "#{dir}/projects/1/.ondemand/scripts"
+      `mkdir -p #{script_dir}`
+      `touch #{dir}/my_cool_script.sh`
+      `touch #{dir}/my_cooler_script.bash`
+
+      # now write a new script file
+      yml = {
+        'title' => 'the script title',
+        'form' => [
+          'auto_scripts',
+          'auto_accounts'
+        ],
+        'attributes' => {
+          'auto_scripts' => {
+            'directory' => "#{dir}"
+          }
+        }
+      }
+
+      File.write("#{script_dir}/1.yml", yml.to_yaml)
+
+      find('[href="/projects/1"]').click
+      refresh
+      find('[href="/projects/1/scripts/1"]').click
+      assert_selector('h1', text: 'the script title', count: 1)
+
+      expected_accounts = ['pas1604', 'pas1754', 'pas1871', 'pas2051', 'pde0006', 'pzs0714', 'pzs0715', 'pzs1010',
+                           'pzs1117', 'pzs1118', 'pzs1124'].to_set
+
+      assert_equal(expected_accounts, page.all('#script_auto_accounts option').map(&:value).to_set)
+      assert_equal(["#{dir}/my_cool_script.sh", "#{dir}/my_cooler_script.bash"].to_set,
+                   page.all('#script_auto_scripts option').map(&:value).to_set)
+
+      # clusters are automatically added
+      assert_equal(['owens', 'oakley'].to_set, page.all('#script_cluster option').map(&:value).to_set)
+    end
   end
 end
