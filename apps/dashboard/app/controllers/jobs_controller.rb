d98ba6000eae9809ee6a8f80a2b77cd9b4850f71
Jeff Ohrstrom <johrstrom@osc.edu>
2023-03-30 10:26:39 -0400

    refactor this a bit

--- apps/dashboard/app/controllers/jobs_controller.rb
+++ apps/dashboard/app/controllers/jobs_controller.rb
@@ -1,22 +1,23 @@
-# The controller for activejobs pages /dashboard/activejobs
+# frozen_string_literal: true
+
+# The controller for jobs API /dashboard/jobs
 class JobsController < ApplicationController
   def info
     respond_to do |format|
       format.json do
         cluster = OodAppkit.clusters[info_params[:cluster].to_sym]
-        render_404 if cluster.nil?
+        render :json => {}, :status => 404 if cluster.nil?
 
         job_info = cluster.job_adapter.info(info_params[:id].to_s)
-        info = {
-          state: job_info.status.state
-        }
-        render :json => info
+        render :json => info_to_hash(job_info)
       end
     end
   end
 
-  def render_404
-    render :json => {}, :status => 404
+  def info_to_hash(info)
+    {
+      state: info.status.state
+    }
   end
 
   def info_params
