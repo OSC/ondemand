39c0b1a3523651177248c29f0e7d0aafd16ff7ad
Luke Weiler <lweiler@osc.edu>
2023-04-06 16:22:50 -0400

    Add comments, split changeMap by field

--- apps/dashboard/app/javascript/packs/batch_connect.js
+++ apps/dashboard/app/javascript/packs/batch_connect.js
@@ -2,61 +2,35 @@
 
 /*
 batch_connect.js
-Handles batch connect form logic
+Handles batch connect form logic.
+When a field is changed, update other fields based on limits written in DOM attributes
 
-Dom <option> elements have attributes like 
+Dom <option> elements in the nodeType select field have attributes like 
 data-option-for-cluster-owens="true"
+data-option-for-cluster-pitzer="false"
 or
 data-min-num-cores-for-cluster-owens="1"
+data-max-num-cores-for-cluster-owens="28"
 
-When an option is selected, we handle events based on the data-* attributes and the current values
-If an option has data-option-for-*="true", it will be hidden when the value it specifies is not set. If it is set, it will be shown again
-
-*/
-/* 
-  changeMap - Map that defines how fields are changed when a key is changed
-  Ex: {
-    'cluster': { // When cluster field is changed 
-      'owens': {
-        show: [ // Array of options to show in node type field
-          'gpu',
-          'hugemem',
-          'debug'
-        ],
-        hide: [], // Array of options to hide when
-        'hugemem': {
-          'minCores': 48,
-          'maxCores': 48
-        }
-      },
-      'pitzer': [...]
-    },
-    'nodeType': {
-      'owens/hugemem': {
-        'minCores': 48,
-        'maxCores': 48
-      }
-    }
-  }
+These are processed into changeMaps clusterNodeTypes and nodeTypeNumCores.
+The changeMaps are used to determine what changes should be made after a field is updated.
 */
 
 // Form fields
 const formCluster = document.getElementById('batch_connect_session_context_cluster');
-const formNumCores = document.getElementById('batch_connect_session_context_num_cores');
 const formNodeType = document.getElementById('batch_connect_session_context_node_type');
+const formNumCores = document.getElementById('batch_connect_session_context_num_cores');
 
-let clusterChanges = {};
-let nodeTypeChanges = {};
-let changeMap = {
-  'cluster': {},
-  'nodeType': {}
-};
+// Change maps
+let clusterNodeTypes = {};
+let nodeTypeNumCores = {};
 
 
 // Ensure that the cluster name is in the changeMap
 function addClusterToMap(cluster) {
-  // Create empty sets in changeMap.cluster.hide and changeMap.cluster.show if undefined
-  changeMap.cluster[cluster] ??= {
+  // Create empty sets in clusterNodeTypes[cluster].hide and clusterNodeTypes[cluster].show if undefined
+  clusterNodeTypes[cluster] ??= {
+    // The sets here will contain references to nodeType DOM option elements to hide or show when a given cluster is selected
     'hide': new Set(),
     'show': new Set()
   };
@@ -65,8 +39,8 @@ function addClusterToMap(cluster) {
 // Ensure that the nodeType is in the changeMap
 function addNodeTypeToMap(cluster, nodeType) {
   // Set default min/maxCores if cluster/nodeType pair is undefined
-  changeMap.nodeType[cluster] ??= {};
-  changeMap.nodeType[cluster][nodeType] ??= {
+  nodeTypeNumCores[cluster] ??= {};
+  nodeTypeNumCores[cluster][nodeType] ??= {
     'minCores': 1,
     'maxCores': 100
   };
@@ -74,8 +48,7 @@ function addNodeTypeToMap(cluster, nodeType) {
 
 // Create changeMap from option attributes
 // Select all form fields by getting all elements whose id begins with 'batch_connect_session_context'
-const selectQuery = "[id^='batch_connect_session_context']";
-document.querySelectorAll(selectQuery).forEach((formField) => {
+document.querySelectorAll("[id^='batch_connect_session_context']").forEach((formField) => {
   // If formField specifies a select field, populate map of options to show when cluster is set AND minMaxNodes when nodeType is set
   if (formField.tagName == 'SELECT') {
     const formFieldOptions = formField.children;
@@ -92,32 +65,32 @@ document.querySelectorAll(selectQuery).forEach((formField) => {
           addClusterToMap(cluster);
           if (attr.value == 'true') {
             // If data-option-for-cluster-x='true', add to show set
-            changeMap.cluster[cluster].show.add(option);
+            clusterNodeTypes[cluster].show.add(option);
           } else if (attr.value == 'false') {
             // If data-option-for-cluster-x='false', add to hide set
-            changeMap.cluster[cluster].hide.add(option);
+            clusterNodeTypes[cluster].hide.add(option);
           }
         } else if (attr.name.startsWith('data-min-num-cores-for-cluster-')) {
           // If 'data-min-num-cores-for-cluster-x' attribute is present, 
           const cluster = attr.name.replace('data-min-num-cores-for-cluster-', '');
           // Ensure cluster in changeMap
           addClusterToMap(cluster);
-          // Show option when cluster selected
-          changeMap.cluster[cluster].show.add(option); // May or may not be necessary to add to show here
+          // Add option to set of options to show when cluster selected
+          clusterNodeTypes[cluster].show.add(option);
           // Ensure nodeType in changeMap
           addNodeTypeToMap(cluster, option.value);
           // Set minCores of cluster, nodeType pair
-          changeMap.nodeType[cluster][option.value].minCores = Number(attr.value);
+          nodeTypeNumCores[cluster][option.value].minCores = Number(attr.value);
         } else if (attr.name.startsWith('data-max-num-cores-for-cluster-')) {
           const cluster = attr.name.replace('data-max-num-cores-for-cluster-', '');
           // Ensure cluster in changeMap
           addClusterToMap(cluster);
-          // Show option when cluster selected
-          changeMap.cluster[cluster].show.add(option);
+          // Add option to set of options to show when cluster selected
+          clusterNodeTypes[cluster].show.add(option);
           // Ensure nodeType in changeMap
           addNodeTypeToMap(cluster, option.value);
           // Set maxCores of cluster, nodeType pair
-          changeMap.nodeType[cluster][option.value].maxCores = Number(attr.value);
+          nodeTypeNumCores[cluster][option.value].maxCores = Number(attr.value);
         }
       }
     }
@@ -130,8 +103,9 @@ document.querySelectorAll(selectQuery).forEach((formField) => {
 */
 function updateNumCores() {
   // On nodeType change, set min/max cores
-  formNumCores.min = changeMap.nodeType[formCluster.value][formNodeType.value].minCores;
-  formNumCores.max = changeMap.nodeType[formCluster.value][formNodeType.value].maxCores;
+  formNumCores.min = nodeTypeNumCores[formCluster.value][formNodeType.value].minCores;
+  formNumCores.max = nodeTypeNumCores[formCluster.value][formNodeType.value].maxCores;
+  // If current value out of range, set to closest value in range
   if (formNumCores.value > formNumCores.max) formNumCores.value = formNumCores.max;
   else if (formNumCores.value < formNumCores.min) formNumCores.value = formNumCores.min;
 }
@@ -146,12 +120,12 @@ formNodeType.addEventListener('change', updateNumCores);
 */
 function updateNodeTypes() {
   // Remove 'display:none' from options in show list
-  changeMap.cluster[formCluster.value]['show'].forEach((option) => option.style.display = '' ); 
-  changeMap.cluster[formCluster.value]['hide'].forEach((option) => {
+  clusterNodeTypes[formCluster.value]['show'].forEach((option) => option.style.display = '' ); 
+  clusterNodeTypes[formCluster.value]['hide'].forEach((option) => {
     // Add 'display:none' to options in hide list
     option.style.display = 'none';
     // If currently selected option is now disabled, change to first option in 'show' set
-    if (option.value == formNodeType.value) formNodeType.value = [...changeMap.cluster[formCluster.value]['show']][0].value;
+    if (option.value == formNodeType.value) formNodeType.value = [...clusterNodeTypes[formCluster.value]['show']][0].value;
   });
   // Update numCores based on new cluster and nodeType pair
   updateNumCores();
