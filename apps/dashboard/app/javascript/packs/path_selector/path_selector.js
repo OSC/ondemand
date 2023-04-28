c06534894f65fe903cf4588a7f40e95d589decae
Jeff Ohrstrom <johrstrom@osc.edu>
2023-04-28 15:36:44 -0400

    refactor the path selector to accomidate variable paths.
    The previous implementation of the path selector had ids hardcoded,
    namely working_dir as the field you were trying to select a path for.
    
    This allows for a path selector to be attached to any id. I had to
    refactor a lot of it to sort of simplifiy all the event passing
    back and forth. It also gets rid of sweet alert because we kinda
    want to get rid of it long term anyhow.

---
+++ apps/dashboard/app/javascript/packs/path_selector/path_selector.js
@@ -0,0 +1,35 @@
+'use strict';
+
+import { PathSelectorTable } from "./path_selector_data_table";
+
+export function attachPathSelectors() {
+  $("[data-path-selector='true']").each((_idx, element) => {
+    const query = `#${pathSelectorId(element.id)}`;
+    const modal = $(query).get(0);
+
+    makeTable(modal);
+  });  
+}
+
+function pathSelectorId(id) {
+  return `${id}_path_selector`;
+}
+
+function makeTable(element) {
+  const options = getPathSelectorOptions(element);
+  new PathSelectorTable(options);
+}
+
+function getPathSelectorOptions(element) {
+  options = {};
+
+  options.filesPath       = element.dataset['filesPath'];
+  options.homeDirectory   = element.dataset['homeDirectory'];
+  options.tableId         = element.dataset['tableId'];
+  options.breadcrumbId    = element.dataset['breadcrumbId'];
+  options.selectButtonId  = element.dataset['selectButtonId'];
+  options.inputFieldId    = element.dataset['inputFieldId'];
+  options.modalId         = element.id;
+
+  return options;
+}
