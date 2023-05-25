6c7e947db224d787512fe9ccd4fe5551a17c9ef0
Travis Ravert <travert@osc.edu>
2023-05-25 10:53:31 -0400

    rename job-composer to projects

--- apps/dashboard/config/configuration_singleton.rb
+++ apps/dashboard/config/configuration_singleton.rb
@@ -297,8 +297,8 @@ class ConfigurationSingleton
     can_access_core_app? 'file-editor'
   end
 
-  def can_access_job_composer?
-    can_access_core_app? 'job-composer'
+  def can_access_projects?
+    can_access_core_app? 'projects'
   end
 
   # Maximum file upload size that nginx will allow from clients in bytes
