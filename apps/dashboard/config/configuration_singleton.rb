016448c583721cf7bbb9139c144ce544e7826c34
Jeff Ohrstrom <johrstrom@osc.edu>
2023-08-08 10:29:05 -0400

    sort ondemand.d files by name before loading (#2944)
    Sort ondemand.d files by name before loading them. This guarantees the order of these files so that
    sites can order them appropriately.

--- apps/dashboard/config/configuration_singleton.rb
+++ apps/dashboard/config/configuration_singleton.rb
@@ -387,7 +387,7 @@ class ConfigurationSingleton
 
   def read_config
     files = Pathname.glob(config_directory.join("*.{yml,yaml,yml.erb,yaml.erb}"))
-    files.each_with_object({}) do |f, conf|
+    files.sort.each_with_object({}) do |f, conf|
       begin
         content = ERB.new(f.read, trim_mode: "-").result(binding)
         yml = YAML.safe_load(content, aliases: true) || {}
