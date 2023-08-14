302ce3e0c79a15ff28d83592b1632c705f282d53
Robin Karlsson <61623634+robinkar@users.noreply.github.com>
2023-08-14 16:48:28 +0300

    Show current filesystem in file browser breadcrumb (#2957)
    * Show current filesystem in file browser breadcrumb
    
    * Add test for file browser breadcrumbs

--- apps/dashboard/app/helpers/files_helper.rb
+++ apps/dashboard/app/helpers/files_helper.rb
@@ -1,10 +1,13 @@
 # Helper for /files pages.
 module FilesHelper
-  def path_segment_with_slash(segment, counter, total)
-    # TODO: ucomment if we decide to omit trailing slash on current directory
-    # if counter == 0 || counter == total - 1
+  def path_segment_with_slash(filesystem, segment, counter, total)
+    # TODO: add check for counter == total - 1 if we decide to omit trailing slash on current directory
     if counter == 0
-      segment
+      if filesystem == 'fs'
+        segment
+      else
+        "#{filesystem}: #{segment}"
+      end
     else
       segment + " /"
     end
