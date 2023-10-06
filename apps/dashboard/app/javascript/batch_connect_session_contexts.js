adad0121e0e51efce18268127eb2726b9af167f2
Jeff Ohrstrom <johrstrom@osc.edu>
2023-10-06 12:10:33 -0400

    need a / here

--- apps/dashboard/app/javascript/batch_connect_session_contexts.js
+++ apps/dashboard/app/javascript/batch_connect_session_contexts.js
@@ -4,7 +4,7 @@ import { attachPathSelectors } from './path_selector/path_selector';
 import { prefillTemplatesHandler } from './prefill_templates/prefill_templates';
 import { prefillSubmitHandler } from './prefill_templates/prefill_submit';
 import { isBCDynamicJSEnabled } from './config';
-import { makeChangeHandlers } from '.dynamic_forms';
+import { makeChangeHandlers } from './dynamic_forms';
 
 
 jQuery(function() {
