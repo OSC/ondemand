6e8c4407b150d0da29644525946108d46f2d3fd7
Jeff Ohrstrom <johrstrom@osc.edu>
2023-11-17 15:34:09 -0500

    correctly account for paths with symlinks that dont exist

--- apps/dashboard/app/models/allowlist_policy.rb
+++ apps/dashboard/app/models/allowlist_policy.rb
@@ -40,10 +40,30 @@ class AllowlistPolicy
 
   # call realpath to ensure symlinks are handled
   def real_expanded_path(path)
-    # call realpath to ensure symlinks are resolved
-    Pathname.new(path).expand_path.realpath
-  rescue SystemCallError
-    # path doesn't exist, so we just get absolute version then
+    path = Pathname.new(path).expand_path
+
+    # if it exsists, then you can just call realpath
+    return Pathname.new(path).expand_path.realpath if path.exist?
+
+    real_expanded_path_not_exist(path)
+  end
+
+  # if a path doesn't exist yet, then we have the find the
+  # first parent that _does_ exist and cast _it_ to the realpath
+  # to find what's a symlink and what's not.
+  def real_expanded_path_not_exist(path)
+    first_real_parent = '/'
+    parent_to_replace = '/'
+
+    path.ascend do |parent_path|
+      if parent_path.exist?
+        first_real_parent = parent_path.expand_path.realpath
+        parent_to_replace = parent_path
+        break
+      end
+    end
+
+    path = path.to_s.gsub(parent_to_replace.to_s, first_real_parent.to_s)
     Pathname.new(path).expand_path
   end
 
