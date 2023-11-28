83bd6346cd93a8f847a97dce1505ed0ea61a64ba
Jeff Ohrstrom <johrstrom@osc.edu>
2023-11-28 14:53:10 -0500

    rm unused 3.1 config

--- apps/dashboard/config/configuration_singleton.rb
+++ apps/dashboard/config/configuration_singleton.rb
@@ -63,7 +63,6 @@ class ConfigurationSingleton
       :facl_domain              => nil,
       :auto_groups_filter       => nil,
       :bc_clean_old_dirs_days   => '30',
-      :google_analytics_tag_id  => nil,
       :project_template_dir     => "#{config_root}/projects",
       :default_profile          => nil
     }.freeze
