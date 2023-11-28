cecilialau6776/OOD-Dashboard

Added default_profile setting to allow admins to select the default profile for users (#3200)

config/configuration_singleton.rb
@@ -66,7 +66,8 @@ class ConfigurationSingleton
       :bc_clean_old_dirs_days   => '30',
       :google_analytics_tag_id  => nil,
       :project_template_dir     => "#{config_root}/projects",
-      :rclone_extra_config      => nil
+      :rclone_extra_config      => nil,
+      :default_profile          => nil
     }.freeze
   end
 

OSC/ondemand

apps/dashboard/config/configuration_singleton.rb
@@ -58,12 +58,14 @@ class ConfigurationSingleton
   # @return [Hash] key/value pairs of defaults
   def string_configs
     {
-      :module_file_dir        => nil,
-      :user_settings_file     => '.ood',
-      :facl_domain            => nil,
-      :auto_groups_filter     => nil,
-      :bc_clean_old_dirs_days => '30',
-      :project_template_dir   => "#{config_root}/projects"
+      :module_file_dir          => nil,
+      :user_settings_file       => '.ood',
+      :facl_domain              => nil,
+      :auto_groups_filter       => nil,
+      :bc_clean_old_dirs_days   => '30',
+      :google_analytics_tag_id  => nil,
+      :project_template_dir     => "#{config_root}/projects",
+      :default_profile          => nil
     }.freeze
   end
 