2d38b02be931e11ab714c8b1ab30e14c0733bb84
Aday Bujeda <aday@theagilemonkeys.com>
2024-01-18 22:57:17 +0000

    Fixes to navigation tests after accessibility improvements

--- apps/dashboard/test/integration/custom_help_navigation_test.rb
+++ apps/dashboard/test/integration/custom_help_navigation_test.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'test_helper'
+require 'html_helper'
 
 class CustomHelpNavigationTest < ActionDispatch::IntegrationTest
   test 'should render a custom help navigation menu when help_bar is defined in UserConfiguration' do
@@ -19,7 +20,8 @@ class CustomHelpNavigationTest < ActionDispatch::IntegrationTest
                 url:     '/menu/link',
                 new_tab: false },
               { title:   'Profile Link',
-                profile: 'profile1' }
+                profile: 'profile1',
+                icon:    'fa://user'}
             ] },
           { title: 'Docs Menu',
             links: [
@@ -40,60 +42,52 @@ class CustomHelpNavigationTest < ActionDispatch::IntegrationTest
     assert_response :success
 
     # Check nav menus
-    assert_select '#navbar li.dropdown[title]', 2
+    assert_select dropdown_links, 2
+    assert_select dropdown_link(1), text: 'Help Menu'
+    assert_select dropdown_link(2), text: 'Docs Menu'
 
-    # Check custom "Help Menu"
-    assert_select nav_menu(1) do |menu|
-      assert_select menu.first, 'a', text: 'Help Menu'
-      assert_select menu.first, 'ul' do |menu_items|
-        menu_items.first['class'].include?('dropdown-menu-right')
-      end
-    end
-    assert_select nav_menu_header('Help Menu'), text: 'Help Menu Dropdown Header'
-    assert_select nav_menu_links('Help Menu') do |elements|
-      assert_equal 2, elements.size
-      assert_match(/Menu Link/, elements[0].text)
-      assert_equal '/menu/link', elements[0]['href']
-      assert_nil elements[0]['target']
-      check_icon(elements[0], 'fa-cog')
-    end
+    # Check custom 'Help Menu'
+    menu_items = dropdown_list('Help Menu')
+    assert_equal true, menu_items.first['class'].include?('dropdown-menu-right')
+    menu_links = css_select(menu_items, 'a')
+    assert_equal 2, menu_links.size
+    # Check first expected link
+    assert_match(/Menu Link/, menu_links[0].text)
+    assert_equal '/menu/link', menu_links[0]['href']
+    assert_nil menu_links[0]['target']
+    check_icon(menu_links[0], 'fa-cog')
+    # Check second expected link
+    assert_match(/Profile Link/, menu_links[1].text)
+    assert_equal settings_path("settings[profile]" => "profile1"), menu_links[1]['href']
+    assert_nil menu_links[1]['target']
+    check_icon(menu_links[1], 'fa-user')
 
     # Check custom 'Docs Menu'
-    assert_select nav_menu(2) do |menu|
-      assert_select menu.first, 'a', text: 'Docs Menu'
-      assert_select menu.first, 'ul' do |menu_items|
-        menu_items.first['class'].include?('dropdown-menu-right')
-      end
-    end
-    assert_select nav_menu_header('Docs Menu'), text: 'Docs Menu Dropdown Header'
-    assert_select nav_menu_links('Docs Menu') do |elements|
-      assert_equal 1, elements.size
-      assert_match(/Jupyter Notebook/, elements[0].text)
-      assert_equal '/batch_connect/sys/bc_jupyter/session_contexts/new', elements[0]['href']
-      assert_nil elements[0]['target']
-      check_icon(elements[0], 'fa-gear')
-    end
+    menu_items = dropdown_list('Docs Menu')
+    assert_equal true, menu_items.first['class'].include?('dropdown-menu-right')
+    menu_links = css_select(menu_items, 'a')
+    assert_equal 1, menu_links.size
+    # Check first expected link
+    assert_match(/Jupyter Notebook/, menu_links[0].text)
+    assert_equal '/batch_connect/sys/bc_jupyter/session_contexts/new', menu_links[0]['href']
+    assert_nil menu_links[0]['target']
+    check_icon(menu_links[0], 'fa-gear')
 
     # Check custom link
-    assert_select nav_link('Custom Help Link'), text: 'Custom Help Link'
-    assert_select nav_link('Custom Help Link') do |link|
-      assert_equal '/custom/link', link.first['href']
-      assert_equal '_blank', link.first['target']
-      check_icon(link.first, 'fa-desktop')
-    end
-
+    link = nav_link('Custom Help Link')[0]
+    assert_match /Custom Help Link/, link.text
+    assert_equal '/custom/link', link['href']
+    assert_equal '_blank', link['target']
+    check_icon(link, 'fa-desktop')
     # User
-    assert_select '#navbar .navbar-nav a.nav-link.disabled' do |link|
-      assert_match(/Logged in as/, link.first.text.strip)
-      check_icon(link.first, 'fa-user')
-    end
+    user = nav_link("Logged in as #{CurrentUser.name}")[0]
+    check_icon(user, 'fa-user')
 
     # Log out link
-    assert_select "#navbar .navbar-nav a.nav-link[href='/logout']" do |link|
-      assert_equal '/logout', link.first['href']
-      assert_equal 'Log Out', link.first.text.strip
-      check_icon(link.first, 'fa-sign-out-alt')
-    end
+    logout = nav_link('Log Out')[0]
+    assert_match /Log Out/, logout.text
+    assert_equal '/logout', logout['href']
+    check_icon(logout, 'fa-sign-out-alt')
   end
 
   test 'develop template should not render when Configuration.app_development_enabled? is false' do
@@ -107,31 +101,13 @@ class CustomHelpNavigationTest < ActionDispatch::IntegrationTest
     Configuration.stubs(:app_development_enabled?).returns(true)
     get root_path
     assert_response :success
-    assert_select '#navbar li.dropdown[title]', 1
-    assert_select nav_menu(1) do |menu|
-      assert_select menu.first, 'a', text: 'Develop'
-    end
+    assert_select dropdown_links, 1
+    assert_select dropdown_link(1), text: 'Develop'
 
     Configuration.stubs(:app_development_enabled?).returns(false)
     get root_path
     assert_response :success
-    assert_select '#navbar li.dropdown[title]', 0
-  end
-
-  def nav_menu(order)
-    "#navbar .navbar-nav li.dropdown:nth-of-type(#{order})"
-  end
-
-  def nav_menu_links(title)
-    "#navbar .navbar-nav li.dropdown[title='#{title}'] ul.dropdown-menu a"
-  end
-
-  def nav_menu_header(title)
-    "#navbar .navbar-nav li.dropdown[title='#{title}'] ul.dropdown-menu li.dropdown-header"
-  end
-
-  def nav_link(title)
-    "#navbar .navbar-nav a.nav-link[title='#{title}']"
+    assert_select dropdown_links, 0
   end
 
   def check_icon(parent_element, icon_class)
