7ac4846ba7e3ea1e81cbac9343ee5aad23bc0183
Jeff Ohrstrom <johrstrom@osc.edu>
2024-01-25 09:46:54 -0500

    log a warning when paths are not allowed (#3316)
    log a warning when paths are not allowed.

--- apps/dashboard/app/models/allowlist_policy.rb
+++ apps/dashboard/app/models/allowlist_policy.rb
@@ -28,7 +28,11 @@ class AllowlistPolicy
   # @raises ArgumentError if any allowlist path or permitted? argument
   #         has the form ~user/some/path where user doesn't exist
   def validate!(path)
-    raise AllowlistPolicy::Forbidden, "#{path} does not have an ancestor directory specified in ALLOWLIST_PATH" unless permitted?(path.to_s)
+    return if permitted?(path.to_s)
+
+    msg = "#{path} does not have an ancestor directory specified in ALLOWLIST_PATH"
+    Rails.logger.warn(msg)
+    raise AllowlistPolicy::Forbidden, msg
   end
 
   protected
