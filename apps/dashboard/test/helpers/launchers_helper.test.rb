66e2abaf10df47704a3b8ab01fa93020786217ff
Jeff Ohrstrom <johrstrom@osc.edu>
2024-03-04 13:47:21 -0500

    change this helper test too

--- apps/dashboard/test/helpers/launchers_helper.test.rb
+++ apps/dashboard/test/helpers/launchers_helper.test.rb
@@ -2,13 +2,13 @@
 
 require 'test_helper'
 
-class ScriptsHelperTest < ActionView::TestCase
-  include ScriptsHelper
+class LaunchersHelperTest < ActionView::TestCase
+  include LaunchersHelper
 
-  test 'script_removable_field? should return false for expected fields' do
+  test 'launcher_removable_field? should return false for expected fields' do
     assert_equal true, script_removable_field?('any_field')
 
-    non_removable_fields = ['script_auto_scripts', 'script_auto_batch_clusters']
+    non_removable_fields = ['launcher_auto_scripts', 'launcher_auto_batch_clusters']
     non_removable_fields.each do |field|
       assert_equal false, script_removable_field?(field)
     end
