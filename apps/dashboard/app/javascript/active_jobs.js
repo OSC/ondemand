4ce2491467b5a505aa4676fc190afec2a992f29a
Hazel <hrandquist@osc.edu>
2024-03-14 16:29:06 -0400

    Refactors inline javascript in active_jobs (#3423)
    * WIP - Refactors inline javascript in active_jobs
    
    * Fixes two bugs
    
    * Removes redundant plugin require
    
    * Fixes cluster/filter search functionality
    
    * Adjusts git string quote marks
    
    * Makes active_jobs JS code one file
    
    * Simplifies filter & cluster _id setting

--- apps/dashboard/app/javascript/active_jobs.js
+++ apps/dashboard/app/javascript/active_jobs.js
@@ -1,15 +1,12 @@
 'use strict';
 
 import oboe from 'oboe';
-import dataTableProcessing from 'datatables.net-plugins/api/processing()';
 import { cssBadgeForState, capitalizeFirstLetter } from './utils.js'
 
 window.fetch_table_data = fetch_table_data;
 window.create_datatable = create_datatable;
 window.set_cluster_id = set_cluster_id;
 window.set_filter_id = set_filter_id;
-
-dataTableProcessing(window, jQuery);
 
 var entityMap = {
   '&': '&amp;',
@@ -336,3 +333,53 @@ function set_cluster_id(id) {
 function reload_page() {
     window.location = '?' + get_request_params();
 }
+
+const activeJobsConfig = $('#active_jobs_config')[0].dataset;
+
+var filter_id = activeJobsConfig.filterId;
+var cluster_id = activeJobsConfig.clusterId;
+
+if (filter_id == "null") {
+  filter_id = localStorage.getItem('jobfilter') || activeJobsConfig.defaultFilterId;
+}
+
+if (cluster_id == "null") {
+  cluster_id = localStorage.getItem('jobcluster') || 'all';
+}
+
+var performance_tracking_enabled = false;
+
+function report_performance(){
+  var marks = performance.getEntriesByType('mark');
+  marks.forEach(function(entry){
+    console.log(entry.startTime + "," + entry.name);
+  });
+
+  // hack but only one mark for document ready, and rest are draw times
+  if(marks.length > 1){
+    console.log("version,documentReady,firstDraw,lastDraw");
+    console.log(`${activeJobsConfig.oodVersion},${marks[0].startTime},${marks[1].startTime},${marks.slice(-1)[0].startTime}`);
+  }
+}
+
+if (activeJobsConfig.consoleLogPerformanceReport) {
+  performance_tracking_enabled = true;
+  performance.mark('document ready - build table and make ajax request for jobs');
+}
+
+var table = create_datatable({
+  drawCallback: function(settings){
+    // do a performance mark every time we draw the table (which happens when new records are downloaded)
+    if(performance_tracking_enabled && settings.aoData.length > 0){
+      performance.mark('draw records - ' + settings.aoData.length);
+    }
+  }, base_uri: activeJobsConfig.baseUri});
+
+fetch_table_data(table, {
+  doneCallback: function(){
+    // generate report after done fetching records
+    if(performance_tracking_enabled){
+      setTimeout(report_performance, 2000);
+    }
+  },
+  base_uri: activeJobsConfig.baseUri});
