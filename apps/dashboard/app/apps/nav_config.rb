5fabd9bfe9a655842100a6958aca0d288555acfb
HazelGrant <hrandquist@osc.edu>
2024-04-09 14:43:41 -0400

    Removes NavConfig class `

--- apps/dashboard/app/apps/nav_config.rb
+++
@@ -1,8 +0,0 @@
-class NavConfig
-    class << self
-      attr_accessor :categories, :categories_whitelist
-      alias_method :categories_whitelist?, :categories_whitelist
-    end
-    self.categories = ["Apps", "Files", "Jobs", "Clusters", "Interactive Apps"]
-    self.categories_whitelist = false
-end
