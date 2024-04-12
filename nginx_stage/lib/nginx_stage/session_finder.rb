7dfc9c6c76df670c097c333283513a20a02f99fb
Jeff Ohrstrom <johrstrom@osc.edu>
2024-04-12 10:59:24 -0400

    lint this file

--- nginx_stage/lib/nginx_stage/session_finder.rb
+++ nginx_stage/lib/nginx_stage/session_finder.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 module NginxStage
   module SessionFinder
     def session_count(user)
       `ps  -o cmd -u #{user}`.split("\n").select do |command|
-        command.match?(/Passenger [\w]+App:/)
+        command.match?(/Passenger \w+App:/)
       end.count
     end
   end
