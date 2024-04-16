bbf77956d0325ec282718bf5c8378ea59ccc062e
Jeff Ohrstrom <johrstrom@osc.edu>
2024-04-16 12:22:12 -0400

    add timeout to ps command

--- nginx_stage/lib/nginx_stage/session_finder.rb
+++ nginx_stage/lib/nginx_stage/session_finder.rb
@@ -3,7 +3,7 @@
 module NginxStage
   module SessionFinder
     def session_count(user)
-      `ps -o cmd -u #{user}`.split("\n").select do |command|
+      `timeout 10 ps -o cmd -u #{user}`.split("\n").select do |command|
         command.match?(/Passenger \w+App:/)
       end.count
     end
