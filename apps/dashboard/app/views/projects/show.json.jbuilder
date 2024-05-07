8ed579756aa2db74ebfc6020a19f98ab038c0236
Aday BA <aday@theagilemonkeys.com>
2024-05-07 15:50:42 +0100

    Added support to disable the project size feature (#3531)
    * Added support to disable the project size feature
    
    * Trigger failign CI environment tests
    
    * Trigger CI environment tests

--- apps/dashboard/app/views/projects/show.json.jbuilder
+++ apps/dashboard/app/views/projects/show.json.jbuilder
@@ -3,6 +3,8 @@ json.name @project.name
 json.description @project.description
 json.icon @project.icon
 json.directory @project.directory
-project_size = @project.size
-json.size project_size
-json.human_size number_to_human_size(project_size)
+if Configuration.project_size_enabled
+  project_size = @project.size
+  json.size project_size
+  json.human_size number_to_human_size(project_size)
+end
--- apps/dashboard/test/integration/projects_controller_test.rb
+++ apps/dashboard/test/integration/projects_controller_test.rb
@@ -8,7 +8,7 @@ class ProjectsControllerTest < ActionDispatch::IntegrationTest
     Rails.application.reload_routes!
   end
 
-  test "should get index" do 
+  test "should get index" do
     get projects_path
     assert_response :success
   end
@@ -49,5 +49,42 @@ class ProjectsControllerTest < ActionDispatch::IntegrationTest
 
       assert Dir.exist?("#{dir}/projects")
     end
+  end
+
+  test "gets JSON reponse" do
+    project_dir = Rails.root.join('test/fixtures/projects/json_response')
+    OodAppkit.stubs(:dataroot).returns(project_dir)
+    Project.stubs(:lookup_table).returns({ 'json_response' => project_dir.to_s })
+    Project.any_instance.expects(:size).once.returns(2097152)
+
+    get project_path('json_response', format: :json)
+    assert_response :success
+    json = JSON.parse(@response.body)
+    assert_equal 'json_response', json['id']
+    assert_equal 'JsonResponseName', json['name']
+    assert_equal 'JsonResponseDescription', json['description']
+    assert_equal project_dir.to_s, json['directory']
+    assert_equal 'fas://user', json['icon']
+    assert_equal 2097152, json['size']
+    assert_equal '2 MB', json['human_size']
+  end
+
+  test "project size is not added to JSON reponse when Configuration.project_size_enabled is false" do
+    project_dir = Rails.root.join('test/fixtures/projects/json_response')
+    OodAppkit.stubs(:dataroot).returns(project_dir)
+    Configuration.stubs(:project_size_enabled).returns(false)
+    Project.stubs(:lookup_table).returns({ 'json_response' => project_dir.to_s })
+    Project.any_instance.expects(:size).never
+
+    get project_path('json_response', format: :json)
+    assert_response :success
+    json = JSON.parse(@response.body)
+    assert_equal 'json_response', json['id']
+    assert_equal 'JsonResponseName', json['name']
+    assert_equal 'JsonResponseDescription', json['description']
+    assert_equal project_dir.to_s, json['directory']
+    assert_equal 'fas://user', json['icon']
+    assert_equal false, json.has_key?('size')
+    assert_equal false, json.has_key?('human_size')
   end
 end
