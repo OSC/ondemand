31969424f05b0864734384391ed4eab4c786d2df
Ashton South <asouth@osc.edu>
2024-05-23 12:43:21 -0400

    Systemstatus visual updates (#3561)
    * remove total counts
    
    * resolve helper conflict
    
    * update view
    
    * add whitespace and remove active counts
    
    * change percentage message
    
    * add whitespace to erb file

--- apps/dashboard/app/helpers/system_status_helper.rb
+++ apps/dashboard/app/helpers/system_status_helper.rb
@@ -6,37 +6,24 @@ module SystemStatusHelper
     "#{cluster.metadata.title.titleize} Cluster Status"
   end
 
+  def status_hash(name, active, total)
+    {
+      message: "#{name} Available: #{number_with_delimiter(total - active)}",
+      percent: percent(active, total)
+    }
+  end
+
+  def components_status(job_adapter)
+    cluster_info = job_adapter.cluster_info
+    [
+      status_hash('Nodes', cluster_info.active_nodes, cluster_info.total_nodes),
+      status_hash('Processors', cluster_info.active_processors, cluster_info.total_processors),
+      status_hash('GPUs', cluster_info.active_gpus, cluster_info.total_gpus)
+    ]
+  end
+
   def percent(value, total)
     "#{(value.to_f / total * 100).round 2}%"
-  end
-
-  def generic_status(active, total, name)
-    free = number_with_delimiter(total - active)
-    "#{name} Active: #{number_with_delimiter(active)}/#{number_with_delimiter(total)} (#{free} Free)"
-  end
-
-  def node_status(cluster_info)
-    generic_status(cluster_info.active_nodes, cluster_info.total_nodes, 'Nodes')
-  end
-
-  def node_pct(cluster_info)
-    percent(cluster_info.active_nodes, cluster_info.total_nodes)
-  end
-
-  def processor_status(cluster_info)
-    generic_status(cluster_info.active_processors, cluster_info.total_processors, 'Processors')
-  end
-
-  def processor_pct(cluster_info)
-    percent(cluster_info.active_processors, cluster_info.total_processors)
-  end
-
-  def gpu_status(cluster_info)
-    generic_status(cluster_info.active_gpus, cluster_info.total_gpus, 'GPUs')
-  end
-
-  def gpu_pct(cluster_info)
-    percent(cluster_info.active_gpus, cluster_info.total_gpus)
   end
 
   def active_jobs(job_adapter)
