651ae9fbe6589ec5ea748ff8c0be7e88563769e2
Ashton South <asouth@osc.edu>
2024-06-03 11:03:48 -0400

    refractor systemstatus polling

--- apps/dashboard/app/javascript/system_status.js
+++ apps/dashboard/app/javascript/system_status.js
@@ -1,19 +1,7 @@
 import { statusIndexUrl } from './config'
-import { replaceHTML } from './turbo_shim'
+import { pollAndReplace } from './turbo_shim'
 
-function poll() {
-  pollDelay = 30000; // Probably want a configuration in the future, but 30 seconds for now
-
-  url = statusIndexUrl();
-  fetch(url, { headers: { Accept: "text/vnd.turbo-stream.html" } })
-    .then(response => response.ok ? Promise.resolve(response) : Promise.reject(response.text()))
-    .then((r) => r.text())
-    .then((html) => replaceHTML("system-status", html))
-    .then(setTimeout(poll, pollDelay))
-    .catch((err) => {
-      console.log('Cannot retrieve system status information due to error:');
-      console.log(err);
-    });
-}
-
-jQuery(poll)
+jQuery(() => {
+  pollDelay = 30000;
+  pollAndReplace(statusIndexUrl(), pollDelay, "system-status");
+})
