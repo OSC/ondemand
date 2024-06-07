56e21400adceb49628b5c1ec31355fbfd4f67c1f
Ashton South <asouth@osc.edu>
2024-06-07 09:34:15 -0400

    Systemstatus poll config (#3595)
    * Add system status poll delay configuration
    
    * fix formatting
    
    * use configured poll delay in javascript

--- apps/dashboard/app/javascript/system_status.js
+++ apps/dashboard/app/javascript/system_status.js
@@ -1,7 +1,6 @@
-import { statusIndexUrl } from './config'
+import { statusIndexUrl, statusPollDelay } from './config'
 import { pollAndReplace } from './turbo_shim'
 
 jQuery(() => {
-  pollDelay = 30000;
-  pollAndReplace(statusIndexUrl(), pollDelay, "system-status");
-})
+  pollAndReplace(statusIndexUrl(), statusPollDelay(), "system-status");
+});
