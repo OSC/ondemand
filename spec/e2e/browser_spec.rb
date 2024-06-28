d7a33bd059f66cba95b88d34ce4de8eba7904eb5
Ashton South <asouth@osc.edu>
2024-06-28 12:12:03 -0400

    Revert "update title on test page"
    This reverts commit 368d097ace3ebc64a159e1e18fa0f5f9c08d963d.

--- spec/e2e/browser_spec.rb
+++ spec/e2e/browser_spec.rb
@@ -58,7 +58,7 @@ describe 'OnDemand browser test' do
   it 'redirects /pun/sys/file-editor and preserves the file' do
     browser.goto "#{ctr_base_url}/pun/sys/file-editor/edit/home/ood/.bashrc"
     expect(browser.url).to eq("#{ctr_base_url}/pun/sys/dashboard/files/edit/fs/home/ood/.bashrc")
-    expect(browser.title).to eq('Dashboard - Open OnDemand')
+    expect(browser.title).to eq('File Editor - Open OnDemand - .bashrc')
   end
 
   it 'redirects /pun/sys/files#index' do
