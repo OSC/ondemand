b8b9674a542ca5d4e85dbf6f83261fefde427f07
Jeff Ohrstrom <johrstrom@osc.edu>
2024-08-26 13:52:42 -0400

    refactor how jobs are logged in the project manager (#3740)
    Refactor how jobs are logged in the project manager so that they're not tied to any launcher and instead a part of the project.

---
+++ apps/dashboard/app/models/concerns/job_logger.rb
@@ -0,0 +1,50 @@
+
+module JobLogger
+
+  def upsert_job!(directory, job)
+    existing_jobs = jobs(directory)
+    stored_job = existing_jobs.detect { |j| j.id == job.id && j.cluster == job.cluster }
+
+    if stored_job.nil?
+      new_jobs = (existing_jobs + [job.to_h]).map(&:to_h)
+    else
+      new_jobs = existing_jobs.map(&:to_h)
+      idx = existing_jobs.index(stored_job)
+      new_jobs[idx].merge!(job.to_h) { |_key, old_val, new_val| new_val.nil? ? old_val : new_val }
+    end
+
+    JobLoggerHelper.write_log(directory, new_jobs)
+  end
+
+  # def write_job_log!(directory, jobs)
+  #   JobLoggerHelper.write_log!(directory, jobs)
+  # endd
+
+  def jobs(directory)
+    file = JobLoggerHelper.log_file(directory)
+    begin
+      data = YAML.safe_load(File.read(file.to_s), permitted_classes: [Time]).to_a
+      data.map { |job_data| HpcJob.new(**job_data) }
+    rescue StandardError => e
+      Rails.logger.error("Cannot read job log file '#{file}' due to error: #{e}")
+      []
+    end
+  end
+
+  # helper methods here are located here so that they don't
+  # bleed into the class that uses this module
+  class JobLoggerHelper
+    class << self
+      def log_file(directory)
+        Pathname.new("#{directory}/.ondemand/job_log.yml").tap do |path|
+          FileUtils.touch(path.to_s) unless path.exist?
+        end
+      end
+
+      def write_log(directory, jobs)
+        file = log_file(directory)
+        File.write(file.to_s, jobs.to_yaml)
+      end
+    end
+  end
+end
