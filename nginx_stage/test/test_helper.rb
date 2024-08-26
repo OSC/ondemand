1bfa83b97acd28faff7def5f077fd3925adec869
Jeff Ohrstrom <johrstrom@osc.edu>
2024-08-26 13:05:29 -0400

    rm unused comments

--- nginx_stage/test/test_helper.rb
+++ nginx_stage/test/test_helper.rb
@@ -14,18 +14,3 @@ module TestHelper
     NginxStage::User.any_instance.stubs(:get_groups).returns(['test'])
   end
 end
-
-  # let(:test_user) { "spec" }
-  # let(:test_user_full_name) { "spec@domain.edu" }
-  # let(:test_user_gid) { 1111 }
-
-  # def stub_etc(username)
-  #   etc_stub = {
-  #     :gid => test_user_gid,
-  #     :name => username
-  #   }
-
-  #   allow(Etc).to receive(:getpwnam).with(test_user).and_return(Struct.new(*etc_stub.keys).new(*etc_stub.values))
-  #   allow(Etc).to receive(:getgrgid).with(test_user_gid).and_return(nil)
-  #   allow_any_instance_of(NginxStage::User).to receive(:get_groups).and_return([test_user])
-  # end
