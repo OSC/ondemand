053c212728966d764f2cb6e1e924e2c2e28e13f7
Jeff Ohrstrom <johrstrom@osc.edu>
2024-08-28 12:22:24 -0400

    rm _inline_js.html.erb and mv functionality to pure js

--- apps/dashboard/app/javascript/files/index.js
+++ apps/dashboard/app/javascript/files/index.js
@@ -3,4 +3,8 @@ import {} from './data_table.js';
 import {} from './file_ops.js';
 import {} from './sweet_alert.js';
 import {} from './uppy_ops.js';
+import { setPageLoadState } from './page_load';
 
+addEventListener("DOMContentLoaded", (_event) => {
+  setPageLoadState();
+});
