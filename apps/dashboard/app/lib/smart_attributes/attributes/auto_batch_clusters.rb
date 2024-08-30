724f4e541adfb8669a17779a84281c551fb92eac
Ashton South <asouth@osc.edu>
2024-08-30 11:59:59 -0400

    set data-max-auto-cores for clusters

--- apps/dashboard/app/lib/smart_attributes/attributes/auto_batch_clusters.rb
+++ apps/dashboard/app/lib/smart_attributes/attributes/auto_batch_clusters.rb
@@ -8,7 +8,9 @@ module SmartAttributes
     # @param opts [Hash] attribute's options
     # @return [Attributes::AutoBatchClusters] the attribute object
     def self.build_auto_batch_clusters(opts = {})
-      options = batch_clusters
+      options = cluster_options
+
+      Rails.logger.info("Cluster options: #{options}")
 
       static_opts = {
         options: options
@@ -17,11 +19,19 @@ module SmartAttributes
       Attributes::AutoBatchClusters.new('auto_batch_clusters', static_opts)
     end
 
+    def self.cluster_options
+      batch_clusters.map do |cluster|
+        cluster_string = cluster.id.to_s
+        max_cores = cluster.job_adapter.nodes.max { |a, b| a.procs <=> b.procs }.procs
+        [cluster_string, cluster_string, {'data-max-auto-cores': max_cores}]
+      end.sort
+    end
+
     def self.batch_clusters
       Rails.cache.fetch('script_batch_clusters', expires_in: 4.hours) do
         Configuration.job_clusters.reject do |c|
           reject_cluster?(c)
-        end.map(&:id).map(&:to_s).sort
+        end
       end
     end
 
