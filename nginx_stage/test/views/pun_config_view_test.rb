3c28d711a12c6faa83c9da10df250ad9366f7298
Jeff Ohrstrom <johrstrom@osc.edu>
2024-09-05 14:44:25 -0400

    add test for restart_confirmation

---
+++ nginx_stage/test/views/pun_config_view_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class PunConfigViewTest < Minitest::Test
+  include NginxStage::PunConfigView
+
+  # required for generating the restart_confirmation in the PunConfigView module
+  def app_init_url
+    '/test'
+  end
+
+  def test_something
+    expected = File.read('test/fixtures/restart_confirmation.html')
+    assert_equal(expected, restart_confirmation)
+  end
+end
