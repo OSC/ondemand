b88d45f8630e53056438aba89c37a522bc8ad909
Jeff Ohrstrom <johrstrom@osc.edu>
2024-09-25 09:29:59 -0400

    set the uri to only the uri, disregarding query params

--- mod_ood_proxy/lib/node_proxy.lua
+++ mod_ood_proxy/lib/node_proxy.lua
@@ -34,11 +34,11 @@ function node_proxy_handler(r)
   local conn = {}
   conn.user = user
   conn.server = host .. ":" .. port
-  conn.uri = uri and (r.args and (uri .. "?" .. r.args) or uri) or r.uri
+  conn.uri = uri or r.uri or '/'
 
   -- last ditch effort to ensure that the uri is at least something
   -- because the request-line of an HTTP request _has_ to have something for a URL
-  if conn.uri == nil or conn.uri == '' then
+  if conn.uri == '' then
     conn.uri = '/'
   end
 
