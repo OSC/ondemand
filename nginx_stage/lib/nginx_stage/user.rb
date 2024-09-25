ab63139a8a70d5ee05bbd32464eca333d948c947
guruevi <evi.vanoost@gmail.com>
2024-09-25 10:46:20 -0400

    Allow UID to be returned by the mapper script. (#3795)
    Allow UID to be returned by the mapper script. Using a UID is useful in multi-domain situations where "username" may be different people in different domains.

--- nginx_stage/lib/nginx_stage/user.rb
+++ nginx_stage/lib/nginx_stage/user.rb
@@ -33,21 +33,37 @@ module NginxStage
     # @param user [String] the user name defining this object
     # @raise [ArgumentError] if user or primary group doesn't exist on local system
     def initialize(user)
-      @passwd = Etc.getpwnam user.to_s
+      # See if user is all numbers (potentially uid), regexp is 8% faster than integer conversion
+      # Benchmark: 0.13 microseconds per call (1M cycles)
+      if user.match?(/\A\d+\z/)
+        # The user is composed of all numbers, (numeric string)
+        #  10 microseconds per call if it matches
+        # 203 microseconds per call if it doesn't match (only happens if username is all numbers but not a uid)
+        begin
+          @passwd = Etc.getpwuid(user.to_i)
+        rescue ArgumentError
+          # We got a number as a username but we failed the lookup, fallthrough to the string lookup
+        end
+      end
+
+      # Variable is not set, so the user is a string
+      unless @passwd
+        # Benchmark: 13 microseconds per call (1M cycles)
+        @passwd = Etc.getpwnam(user)
+        if name.to_s != user.to_s
+          err_msg = <<~HEREDOC
+            Username '#{user}' is being mapped to '#{name}' in SSSD and they don't match.
+            Users with domain names cannot be mapped correctly. If '#{name}' still has the
+            domain in it you'll need to set SSSD's full_name_format to '%1$s'.
+  
+            See https://github.com/OSC/ondemand/issues/1759 for more details.
+          HEREDOC
+
+          raise StandardError, err_msg
+        end
+      end
       @group = Etc.getgrgid gid
       @groups = get_groups
-
-      if name.to_s != user.to_s
-        err_msg = <<~HEREDOC
-          Username '#{user}' is being mapped to '#{name}' in SSSD and they don't match.
-          Users with domain names cannot be mapped correctly. If '#{name}' still has the
-          domain in it you'll need to set SSSD's full_name_format to '%1$s'.
-
-          See https://github.com/OSC/ondemand/issues/1759 for more details.
-        HEREDOC
-
-        raise StandardError, err_msg
-      end
     end
 
     # User's primary group name
@@ -79,7 +95,16 @@ module NginxStage
       # Use `id` to get list of groups as the /etc/group file can give
       # erroneous results
       def get_groups
-        `id -nG #{name}`.split(' ')
+        # Group names can contain spaces, prevent "domain users" people from being added to the "users" group
+        # We retrieve GIDs and convert to names (or GID)
+        `id -G #{name}`.split(' ').map(&:to_i).map do |gid|
+          begin
+            Etc.getgrgid(gid).name
+          rescue ArgumentError
+            # Still return the GID as a string if the group doesn't exist
+            gid.to_s
+          end
+        end
       end
   end
 end
