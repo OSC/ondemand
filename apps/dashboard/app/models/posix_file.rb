35b0dabf93cfa80cfe63ebc50787332a854443c7
Jeff Ohrstrom <johrstrom@osc.edu>
2024-10-02 15:10:41 -0400

    allow empty directories to be downloaded

--- apps/dashboard/app/models/posix_file.rb
+++ apps/dashboard/app/models/posix_file.rb
@@ -185,8 +185,6 @@ class PosixFile
           error = I18n.t('dashboard.files_directory_size_calculation_error')
         elsif size.to_i > download_directory_size_limit
           error = I18n.t('dashboard.files_directory_too_large', download_directory_size_limit: download_directory_size_limit)
-        elsif size.to_i == 0
-          error = I18n.t('dashboard.files_directory_download_size_0', cmd: "timeout 10s du -cbs #{path.to_s}")
         else
           can_download = true
         end
