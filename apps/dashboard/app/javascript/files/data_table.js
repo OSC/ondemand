2318cf7a1f34aba6be88b7159cc0393c72ccc90a
HazelGrant <hrandquist@osc.edu>
2024-12-13 08:28:04 -0500

    Small refactor

--- apps/dashboard/app/javascript/files/data_table.js
+++ apps/dashboard/app/javascript/files/data_table.js
@@ -223,7 +223,7 @@ class DataTable {
                     orderable: false,
                     defaultContent: '<input type="checkbox">',
                     render: (data, type, row, meta) => {
-                        return "<input type='checkbox' data-dl-url='" + row.download_url + "'>";
+                        return `<input type='checkbox' data-dl-url='${row.download_url}'>`;
                     }
                 },
                 { data: 'type', render: (data, type, row, meta) => data == 'd' ? '<span title="directory" class="fa fa-folder" style="color: gold"><span class="sr-only"> dir</span></span>' : '<span title="file" class="fa fa-file" style="color: lightgrey"><span class="sr-only"> file</span></span>' }, // type
