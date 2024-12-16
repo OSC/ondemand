d49db55764a857c115fd71e4cbfcfe5e8ef5c3ec
HazelGrant <hrandquist@osc.edu>
2024-12-16 09:17:00 -0500

    Removes check on test

--- apps/dashboard/test/system/remote_files_test.rb
+++ apps/dashboard/test/system/remote_files_test.rb
@@ -319,17 +319,7 @@ class RemoteFilesTest < ApplicationSystemTestCase
         attach_file 'files[]', src_file, visible: false, match: :first
         find('.uppy-StatusBar-actionBtn--upload', wait: MAX_WAIT).click
 
-      # TODO: Investigate why this upload is failing the first time in the first place.
-       begin
-          find('tbody a', exact_text: File.basename(src_file), wait: MAX_WAIT)
-        rescue
-          find('#upload-btn').click
-          find('.uppy-Dashboard-AddFiles', wait: MAX_WAIT)
-          attach_file 'files[]', src_file, visible: false, match: :first
-          find('.uppy-StatusBar-actionBtn--upload', wait: MAX_WAIT).click
-
-          find('tbody a', exact_text: File.basename(src_file), wait: MAX_WAIT)
-        end
+        find('tbody a', exact_text: File.basename(src_file), wait: MAX_WAIT)
       end
     end
   end
