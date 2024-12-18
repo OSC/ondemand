4a660787463788a8131248c9b5dd30ac690ff269
HazelGrant <hrandquist@osc.edu>
2024-12-18 09:03:38 -0500

    Find #tloading_spinner first

--- apps/dashboard/test/system/remote_files_test.rb
+++ apps/dashboard/test/system/remote_files_test.rb
@@ -318,6 +318,7 @@ class RemoteFilesTest < ApplicationSystemTestCase
         src_file = 'test/fixtures/files/upload/hello-world.c'
         attach_file 'files[]', src_file, visible: false, match: :first
         find('.uppy-StatusBar-actionBtn--upload', wait: MAX_WAIT).click
+        find("#tloading_spinner")
         find('tbody a', exact_text: File.basename(src_file), wait: MAX_WAIT)
       end
     end
