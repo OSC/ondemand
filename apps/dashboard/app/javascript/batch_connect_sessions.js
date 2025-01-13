5729fc386e2f396f4a70944532d58888214a0ba6
ahmed-mgd <aabdelmageed@osc.edu>
2025-01-13 14:22:30 -0500

    Move session status updates to batch_connect_sessions.js

--- apps/dashboard/app/javascript/batch_connect_sessions.js
+++ apps/dashboard/app/javascript/batch_connect_sessions.js
@@ -4,6 +4,36 @@ import { bcIndexUrl, bcPollDelay } from './config';
 import { bindFullPageSpinnerEvent } from './utils';
 import { pollAndReplace } from './turbo_shim';
 
+/**
+ * Alert screen reader when the status of an interactive session changes.
+ */
+
+const sessionStatusMap = new Map();
+
+function updateLiveRegion(message) {
+  const liveRegion = document.getElementById("sr-live-region");
+  liveRegion.textContent = message;
+}
+
+function checkStatusChanges() {
+  const sessionCards = document.querySelectorAll('.card.session-panel');
+  
+  sessionCards.forEach((card) => {
+    const sessionId = card.querySelector('[id^="id_link"]').textContent;
+    const newStatus = card.querySelector('.status').textContent;
+    if (sessionStatusMap.has(sessionId)) {
+      const oldStatus = sessionStatusMap.get(sessionId);
+      if (oldStatus !== newStatus) {
+        sessionStatusMap.set(sessionId, newStatus);
+        const sessionTitle = card.querySelector('.card_title').textContent
+        updateLiveRegion(`${sessionTitle} session status changed to ${newStatus}`);
+      }
+    } else {
+      sessionStatusMap.set(sessionId, newStatus);
+    }
+  });
+}
+
 function settingKey(id) {
   return id + '_value';
 }
@@ -36,6 +66,9 @@ window.tryUpdateSetting = tryUpdateSetting;
 
 jQuery(function (){
   if ($('#batch_connect_sessions').length) {
-    pollAndReplace(bcIndexUrl(), bcPollDelay(), "batch_connect_sessions", bindFullPageSpinnerEvent);
+    pollAndReplace(bcIndexUrl(), bcPollDelay(), "batch_connect_sessions", () => {
+      bindFullPageSpinnerEvent();
+      checkStatusChanges();
+    });
   }
 });
