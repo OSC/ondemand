11f845b5ad7ea32c9cfc37c278afdedf544739bf
Greg Buchanan <gbuchanan@osc.edu>
2025-01-15 11:01:43 -0500

    Format update.

--- apps/dashboard/test/models/project_manifest_test.rb
+++ apps/dashboard/test/models/project_manifest_test.rb
@@ -2,23 +2,23 @@ require 'test_helper'
 
 class ProjectManifestTest < ActiveSupport::TestCase
 
-test 'ProjectManifest writes files entries' do
-  opts = { files: ['file1', 'file2'] }
+  test 'ProjectManifest writes files entries' do
+    opts = { files: ['file1', 'file2'] }
 
-  valid_manifest = <<~HEREDOC
-    ---
-    files:
-    - file1
-    - file2
-  HEREDOC
+    valid_manifest = <<~HEREDOC
+      ---
+      files:
+      - file1
+      - file2
+    HEREDOC
 
-  manifest = ProjectManifest.new(opts)
+    manifest = ProjectManifest.new(opts)
 
-  Dir.mktmpdir do |dir|
-    path = dir << '/manifest.yml'
-    manifest.save(path)
+    Dir.mktmpdir do |dir|
+      path = dir << '/manifest.yml'
+      manifest.save(path)
 
-    assert_equal valid_manifest, File.read(path)
+      assert_equal valid_manifest, File.read(path)
+    end
   end
-end
 end
