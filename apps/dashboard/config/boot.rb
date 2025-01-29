5b82842f855ba4e24c02bfb0b18b186de4133015
Jeff Ohrstrom <johrstrom@osc.edu>
2025-01-29 12:57:37 -0500

    reset Gemfile and require logger in boot.rb

--- apps/dashboard/config/boot.rb
+++ apps/dashboard/config/boot.rb
@@ -6,6 +6,9 @@ ENV['PATH'] = "#{File.join(File.expand_path('..', __dir__), 'tmp', 'node_modules
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
+# TODO: we remove this once we upgrade to Rails 7.1
+require 'logger'
+
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 
 # load dotenv files before "before_configuration" callback
