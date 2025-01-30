9337928ab38a18c824bc2e8e502f29bb055870b9
Jeff Ohrstrom <johrstrom@osc.edu>
2025-01-30 15:10:12 -0500

    Manual update (#4108)
    Upgrade dependencies manually and require 'logger' before booting up.

--- apps/dashboard/config/boot.rb
+++ apps/dashboard/config/boot.rb
@@ -8,6 +8,9 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 
+# TODO: we remove this once we upgrade to Rails 7.1
+require 'logger'
+
 # load dotenv files before "before_configuration" callback
 require File.expand_path('configuration_singleton', __dir__)
 
--- apps/myjobs/config/boot.rb
+++ apps/myjobs/config/boot.rb
@@ -2,6 +2,9 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 
+# TODO: we remove this once we upgrade to Rails 7.1
+require 'logger'
+
 # load dotenv files before "before_configuration" callback
 require_relative 'configuration_singleton'
 
