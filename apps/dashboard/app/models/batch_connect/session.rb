10ad12744de3919c336223006d61538b0a4b3752
Travis Ravert <travert@osc.edu>
2025-03-26 19:24:24 -0400

    Add conversion for min_memory and use match to handle regex.

--- apps/dashboard/app/models/batch_connect/session.rb
+++ apps/dashboard/app/models/batch_connect/session.rb
@@ -436,32 +436,29 @@ module BatchConnect
     # The min_memory value from Job::OodCore::Info.native
     # @return [Float] of job's memory in GB's
     def min_memory
-      # Assumes output will be in GB's
+      return nil if info.native[:min_memory].empty?
+
+      # Comes in as raw megabyte count e.g. 4556M 
       raw_info = info.native[:min_memory]
 
-      # Guard if no min_memory available.
-      return nil if raw_info.empty?
+      memory_match =  /(\d+)([KMGTP])/.match(raw_info)
+      # grab the raw value and raw unit from the raw info
+      value = memory_match[1]
+      unit = memory_match[2]
 
-      memory_match =  raw_info
-      when /(\d+)([KMGTP])/
-        # grab the raw value and raw unit from the raw info
-        raw_value = Regexp.last_match(1).to_f
-        raw_unit = Regexp.last_match(2)
+      memory_conversions = {
+        K: 1.0 / 1_000_000,
+        M: 1.0 / 1_000,
+        G: 1.0,
+        T: 1_000.0,
+        P: 1_000_000.0,
+      }
 
-        memory_conversions = {
-          K: 1.0 / 1_000_000,
-          M: 1.0 / 1_000,
-          G: 1.0,
-          T: 1_000.0,
-          P: 1_000_000.0,
-        }
+      # convert the memory letter representation to its float value.
+      memory_multiplier = memory_conversions[unit.to_sym]
 
-        # convert the memory letter representation to its float value.
-        memory_multiplier = memory_conversions[raw_unit.to_sym]
-        
-        # Return the converted the value for display in GBs.
-        (raw_value * memory_multiplier).round(1)
-      end
+      # Return the converted the value for display in GBs.
+      (value * memory_multiplier).round(1)
     end
 
     # Whether this session is persisted to the database
