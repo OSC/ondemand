b53aeb05708177498a26412bd3398374ac99b42e
Harshit Soora <soora.iitkgp@gmail.com>
2025-04-21 13:42:23 -0400

    Added project icon as button instead of list

--- apps/dashboard/app/javascript/projects_import.js
+++ apps/dashboard/app/javascript/projects_import.js
@@ -5,14 +5,3 @@ import { attachPathSelectors }  from './path_selector/path_selector';
 jQuery(function() {
   attachPathSelectors();
 });
-
-document.addEventListener('DOMContentLoaded', function () {
-  const inputField = document.getElementById('project_directory');
-  const dropdownMenu = document.getElementById('directory_dropdown_menu');
-
-  dropdownMenu.addEventListener('click', function (e) {
-    if (e.target && e.target.matches('a.dropdown-item')) {
-      inputField.value = e.target.getAttribute('data-value');
-    }
-  });
-});
