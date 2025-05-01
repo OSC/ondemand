b106b30838ea0692b90256be583efce0253f2d26
ahmed-mgd <aabdelmageed@osc.edu>
2025-05-01 12:57:19 -0400

    Parse module description

--- apps/dashboard/app/models/hpc_module.rb
+++ apps/dashboard/app/models/hpc_module.rb
@@ -3,6 +3,11 @@
 # HpcModule is a class representing a module you'd see on an HPC system.
 class HpcModule
   class << self
+    def all
+      Configuration.job_clusters.map do |cluster|
+        all(cluster.id)
+      end.uniq(&:to_s).sort_by(&:to_s)
+    end
     def all(cluster)
       Rails.cache.fetch("modules_#{cluster}", expires_in: 24.hours) do
         file = "#{Configuration.module_file_dir}/#{cluster}.json"
@@ -10,7 +15,11 @@ class HpcModule
           begin
             JSON.parse(File.read(file)).map do |name, spider_output|
               spider_output.map do |_, mod|
-                HpcModule.new(name, version: mod['Version'], hidden: mod['hidden'])
+                desc =
+                  mod['whatis']&.find { |line| line.downcase.start_with?('short description') }&.split(':', 2)&.last&.strip ||
+                  mod['help']&.strip ||
+                  "No description available"
+                HpcModule.new(name, version: mod['Version'], description: desc, hidden: mod['hidden'])
               end
             end.flatten.uniq.reject(&:hidden?)
           rescue StandardError => e
@@ -31,14 +40,16 @@ class HpcModule
     end
   end
 
-  attr_reader :name, :version, :hidden
+  attr_reader :name, :version, :description, :hidden
   attr_accessor :cluster
   alias hidden? hidden
 
-  def initialize(name, version: nil, hidden: false)
+  def initialize(name, version: nil, description: nil, hidden: false)
     @name = name
     @version = version.to_s if version
+    @description = description
     @hidden = hidden
+    Rails.logger.debug("HpcModule: #{name} version: #{version} description: #{description}")
   end
 
   def to_s
