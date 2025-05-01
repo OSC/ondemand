5c4232458a3775d15fda7b48eeacf8dbb5b9b8eb
ahmed-mgd <aabdelmageed@osc.edu>
2025-05-01 15:21:03 -0400

    Wrap filtering logic to ensure DOM is ready

--- apps/dashboard/app/javascript/module_browser.js
+++ apps/dashboard/app/javascript/module_browser.js
@@ -1,14 +1,16 @@
-$(document).on('input change', '#module_search, #cluster_filter', function () {
-  const searchQuery = $('#module_search').val().toLowerCase();
-  const selectedCluster = $('#cluster_filter').val();
+jQuery(function (){
+  $(document).on('input change', '#module_search, #cluster_filter', function () {
+    const searchQuery = $('#module_search').val().toLowerCase();
+    const selectedCluster = $('#cluster_filter').val();
 
-  $('[data-name][data-clusters]').each(function () {
-    const name = $(this).data('name');
-    const clusters = $(this).data('clusters').split(',');
+    $('[data-name][data-clusters]').each(function () {
+      const name = $(this).data('name');
+      const clusters = $(this).data('clusters').split(',');
 
-    const searchMatches = name.includes(searchQuery);
-    const clusterMatches = !selectedCluster || clusters.includes(selectedCluster);
+      const searchMatches = name.includes(searchQuery);
+      const clusterMatches = !selectedCluster || clusters.includes(selectedCluster);
 
-    $(this).toggle(searchMatches && clusterMatches);
+      $(this).toggle(searchMatches && clusterMatches);
+    });
   });
 });
