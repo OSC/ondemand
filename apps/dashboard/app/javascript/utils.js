2145651f7ee46058bf27219f277b01103035b72f
Jeff Ohrstrom <johrstrom@osc.edu>
2025-05-15 11:58:59 -0400

    Spin instead of sweet alert load (#4353)
    Provide a full page spinner to indicate the files app is working/loading instead of using SweetAlert.
    
    This is work to get rid of SweetAlert eventually.

--- apps/dashboard/app/javascript/utils.js
+++ apps/dashboard/app/javascript/utils.js
@@ -1,4 +1,5 @@
 import {analyticsPath} from "./config";
+import { ariaNotify } from './aria_live_notify';
 
 export function cssBadgeForState(state){
   switch (state) {
@@ -45,6 +46,18 @@ export function today() {
 function showSpinner() {
   $('body').addClass('modal-open');
   $('#full-page-spinner').removeClass('d-none');
+}
+
+export function pageSpin() {
+  const ele = document.getElementById('full_page_spinner');
+  ele.classList.remove('d-none');
+  ariaNotify('Loading.');
+}
+
+export function stopPageSpin() {
+  const ele = document.getElementById('full_page_spinner');
+  ele.classList.add('d-none');
+  ariaNotify('Loading complete.');
 }
 
 export function bindFullPageSpinnerEvent() {
