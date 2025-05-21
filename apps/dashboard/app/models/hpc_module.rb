ce3194e9369edf3f6094ed91f71a83c9c835eb73
ahmed-mgd <aabdelmageed@osc.edu>
2025-05-21 11:46:25 -0400

    Refactor HpcModule to return all modules across clusters

--- apps/dashboard/app/models/hpc_module.rb
+++ apps/dashboard/app/models/hpc_module.rb
@@ -3,23 +3,29 @@
 # HpcModule is a class representing a module you'd see on an HPC system.
 class HpcModule
   class << self
-    def all(cluster)
-      Rails.cache.fetch("modules_#{cluster}", expires_in: 24.hours) do
-        file = "#{Configuration.module_file_dir}/#{cluster}.json"
-        if File.file?(file) && File.readable?(file)
-          begin
-            JSON.parse(File.read(file)).map do |name, spider_output|
-              spider_output.map do |_, mod|
-                HpcModule.new(name, version: mod['Version'], dependencies: mod['parentAA'], hidden: mod['hidden'])
-              end
-            end.flatten.uniq.reject(&:hidden?)
-          rescue StandardError => e
-            Rails.logger.warn("Did not read #{file} correctly because #{e.class}:#{e.message}")
+    def all(cluster = nil)
+      if cluster
+        Rails.cache.fetch("modules_#{cluster}", expires_in: 24.hours) do
+          file = "#{Configuration.module_file_dir}/#{cluster}.json"
+          if File.file?(file) && File.readable?(file)
+            begin
+              JSON.parse(File.read(file)).map do |name, spider_output|
+                spider_output.map do |_, mod|
+                  HpcModule.new(name, version: mod['Version'], dependencies: mod['parentAA'], cluster: cluster, hidden: mod['hidden'])
+                end
+              end.flatten.uniq.reject(&:hidden?)
+            rescue StandardError => e
+              Rails.logger.warn("Did not read #{file} correctly because #{e.class}:#{e.message}")
+              []
+            end
+          else
+            Rails.logger.warn("File #{file} is unreadable.")
             []
           end
-        else
-          Rails.logger.warn("File #{file} is unreadable.")
-          []
+        end
+      else
+        Configuration.job_clusters.flat_map do |cluster|
+          all(cluster.id)
         end
       end
     end
@@ -31,14 +37,14 @@ class HpcModule
     end
   end
 
-  attr_reader :name, :version, :dependencies, :hidden
-  attr_accessor :cluster
+  attr_reader :name, :version, :dependencies, :cluster, :hidden
   alias hidden? hidden
 
-  def initialize(name, version: nil, dependencies: nil, hidden: false)
+  def initialize(name, version: nil, dependencies: nil, cluster: nil, hidden: false)
     @name = name
     @version = version.to_s if version
     @dependencies = dependencies
+    @cluster = cluster
     @hidden = hidden
   end
 
