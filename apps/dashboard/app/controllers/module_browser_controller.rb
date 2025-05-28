59ea7c37754ddba339778f8d7b8ab60c93088a91
Ahmed Abdelmageed <111192682+ahmed-mgd@users.noreply.github.com>
2025-05-28 12:43:09 -0400

    Enhance module browser UI (#4386)
    * Add metadata info to view and debounce search
    
    * Improve styling and trim search term

--- apps/dashboard/app/controllers/module_browser_controller.rb
+++ apps/dashboard/app/controllers/module_browser_controller.rb
@@ -5,5 +5,6 @@ class ModuleBrowserController < ApplicationController
     # FIXME: select clusters from Configuration.job_clusters
     @clusters = OodAppkit.clusters.select(&:job_allow?).reject { |c| c.metadata.hidden }
     @modules = HpcModule.all.group_by(&:name).sort_by { |name, _| name.to_s.titleize }
+    @modules_last_updated = helpers.modules_last_updated
   end
 end
