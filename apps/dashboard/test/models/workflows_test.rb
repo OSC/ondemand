88a88d377749e0bf9b81bf9f76f231e13248bc3f
Harshit Soora <soora.iitkgp@gmail.com>
2025-06-15 18:20:33 -0400

    Added skeleton for the launcher workflow class
    Updated workflow path from .workflow to workflows/workflow.yml following convention in PM
    
    Updated as per review comments
    
    Converted project_dir as argument instead of class variable
    
    Left one bug, fixed now
    
    Minor fix
    
    Moved workflows from new page to project page
    
    Cleaned the PR, remove lookup workflow.yml and no need to save project_id in <workflow-id>.yml file and few typos
    
    Added support to pick launcher subset
    
    Added simplistic UI for selecting edges among launchers on show page of workflow
    
    UI is working and send the edge list to backedn
    
    Added DAG class, later use this inside the submit
    
    Code is compiling, still working on correctness
    
    Tested working, next will add some unit test, need to add a mehtod to identify if it is a loop
    
    Added error handling and cycle check
    
    Fixed one typo
    
    Updated as per review comments and tested again
    
    Updated helper to model, error handling and camel case
    
    Added test case for workflow class

---
+++ apps/dashboard/test/models/workflows_test.rb
@@ -0,0 +1,135 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class WorkflowsTest < ActiveSupport::TestCase
+  test 'create empty workflow' do
+    workflow = Workflow.new
+
+    assert_nil workflow.id
+    assert_nil workflow.project_dir
+    assert_nil workflow.name
+    assert_nil workflow.description
+    assert_nil workflow.created_at
+    assert_equal [], workflow.launcher_ids
+    assert_equal [], workflow.source_ids
+    assert_equal [], workflow.target_ids
+  end
+
+  test 'create workflow validation' do
+    Dir.mktmpdir do |tmp|
+      # add error in workflow save if project_dir is not valid
+      # workflow = Workflow.new({})
+      # assert_not workflow.save
+      # assert_equal 1, workflow.errors.size
+      # assert_not workflow.errors[:name].empty?
+
+      invalid_directory = tmp
+      workflow = Workflow.new({ name: 'test', project_dir: invalid_directory.to_s})
+
+      assert workflow.save
+      assert_equal 0, workflow.errors.size
+    end
+  end
+
+  test 'creates workflow defaults' do
+    stub_du
+    Dir.mktmpdir do |tmp|
+      project_dir = Pathname.new(tmp)
+      workflow = create_workflow(name: 'MyLocalName', description: 'MyLocalDescription', project_dir: project_dir)
+
+      # Check id and directory defaults
+      assert_not_nil workflow.id
+      assert_equal "#{project_dir}/.ondemand/workflows", Workflow.workflow_dir(project_dir).to_s
+    end
+  end
+
+  test 'creates workflow' do
+    stub_du
+    Dir.mktmpdir do |tmp|
+      project_dir = Pathname.new(tmp)
+      workflow_id = Workflow.next_id
+      workflow = create_workflow(id: workflow_id, name: 'MyLocalName', description: 'MyLocalDescription', project_dir: project_dir)
+
+      assert workflow.errors.inspect
+      assert Dir.entries("#{project_dir}/.ondemand/workflows").include?("#{workflow_id}.yml")
+      assert_equal workflow_id, workflow.id
+      assert_equal 'MyLocalName', workflow.name
+      assert_equal 'MyLocalDescription', workflow.description
+    end
+  end
+
+  test 'creates manifest file in .ondemand/workflows config directory' do
+    Dir.mktmpdir do |tmp|
+      project_dir = Pathname.new(tmp)
+      workflow = create_workflow(id: "test-#{Workflow.next_id}", project_dir: project_dir)
+
+      assert workflow.errors.inspect
+      manifest_file = Pathname.new("#{project_dir}/.ondemand/workflows/#{workflow.id}.yml")
+      assert_equal manifest_file, workflow.manifest_file
+      assert File.file?(manifest_file)
+
+      manifest_data = YAML.safe_load(File.read(manifest_file), permitted_classes: [Pathname], aliases: true)
+
+      assert_equal workflow.id, manifest_data["id"]
+      assert_equal "test-workflow", manifest_data["name"]
+      assert_equal "description", manifest_data["description"]
+      assert_equal project_dir, manifest_data["project_dir"]
+    end
+  end
+
+  test 'deletes workflow' do
+    Dir.mktmpdir do |tmp|
+      project_dir = Pathname.new(tmp)
+      workflow = create_workflow(project_dir: project_dir)
+
+      assert Dir.entries("#{project_dir}/.ondemand/workflows/").include?("#{workflow.id}.yml")
+
+      workflow.destroy!
+      assert_not Dir.entries("#{project_dir}/.ondemand/workflows/").include?("#{workflow.id}.yml")
+    end
+  end
+
+  # Enable when you have added update method to Workflow model 
+  # test 'update workflow manifest file' do
+  #   Dir.mktmpdir do |tmp|
+  #     project_dir = Pathname.new(tmp)
+  #     workflow = create_workflow(id: "test-#{Workflow.next_id}", project_dir: project_dir)
+
+  #     name          = 'test-workflow-2'
+  #     description   = 'my test workflow'
+  #     test_attributes = { name: name, description: description }
+
+  #     assert workflow.update(test_attributes)
+  #     assert File.exist?(workflow.manifest_file)
+
+  #     manifest_data = YAML.safe_load(File.read(manifest_file), permitted_classes: [Pathname], aliases: true)
+
+  #     assert_equal workflow.id, manifest_data["id"]
+  #     assert_equal name, manifest_data["name"]
+  #     assert_equal description, manifest_data["description"]
+  #     assert_equal project_dir, manifest_data["project_dir"]
+  #   end
+  # end
+
+  # test 'update workflow only updates name, and description' do
+  #   Dir.mktmpdir do |tmp|
+  #     project_dir = Pathname.new(tmp)
+  #     workflow = create_workflow(project_dir: project_dir)
+  #     old_id = workflow.id
+
+  #     assert workflow.update({ id: 'updated', name: 'updated', description: 'updated'})
+  #     assert_equal 'updated', workflow.name
+  #     assert_equal 'updated', workflow.description
+  #     assert_equal old_id, workflow.id
+  #   end
+  # end
+
+  def create_workflow(id: nil, name: 'test-workflow', description: 'description', project_dir: nil, launcher_ids: [], source_ids: [], target_ids: [])
+    attrs = { name: name, id: id, description: description, project_dir: project_dir, launcher_ids: launcher_ids, source_ids: source_ids, target_ids: target_ids}
+    workflow = Workflow.new(attrs)
+    assert workflow.save
+
+    workflow
+  end
+end
