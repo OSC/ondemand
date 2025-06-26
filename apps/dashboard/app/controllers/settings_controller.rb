ddbf2ddba8eb3319e4e18c1e85ef36f4e9401a58
Aday BA <aday@theagilemonkeys.com>
2025-06-26 17:41:47 +0100

    Added support to redirect back for dismissible announcements (#4427)
    Added support to redirect back for dismissible announcements.

--- apps/dashboard/app/controllers/settings_controller.rb
+++ apps/dashboard/app/controllers/settings_controller.rb
@@ -12,7 +12,13 @@ class SettingsController < ApplicationController
 
     logger.info "settings: updated user settings to: #{new_settings}"
     respond_to do |format|
-      format.html { redirect_to root_url, notice: I18n.t('dashboard.settings_updated') }
+      format.html do
+        if back_param == 'true'
+          redirect_back allow_other_host: false, fallback_location: root_url, notice: I18n.t('dashboard.settings_updated')
+        else
+          redirect_to root_url, notice: I18n.t('dashboard.settings_updated')
+        end
+      end
       format.json { head :no_content }
     end
   end
@@ -21,6 +27,10 @@ class SettingsController < ApplicationController
 
   def settings_param
     params.require(:settings).permit(ALLOWED_SETTINGS) if params[:settings].present?
+  end
+
+  def back_param
+    params.permit(:back)[:back]
   end
 
   def read_settings(params)
