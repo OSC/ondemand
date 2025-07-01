7311ebd61e2b66c3171e1ce05f49daf5138020db
Jeff Ohrstrom <johrstrom@osc.edu>
2025-07-01 09:22:49 -0400

    set this to false and defer the refactor

--- apps/dashboard/config/environments/test.rb
+++ apps/dashboard/config/environments/test.rb
@@ -56,8 +56,8 @@ Rails.application.configure do
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
 
-  # Raise error when a before_action's only/except options reference missing actions
-  config.action_controller.raise_on_missing_callback_actions = true
+  # do not raise error when a before_action's only/except options reference missing actions
+  config.action_controller.raise_on_missing_callback_actions = false
 
   config.active_job.queue_adapter = ActiveJob::QueueAdapters::AsyncAdapter.new(min_threads: 1, max_threads: 1)
 
