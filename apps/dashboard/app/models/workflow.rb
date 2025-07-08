6f02f091e61841b4a26c169773e0e09bff599a18
Harshit Soora <soora.iitkgp@gmail.com>
2025-07-08 10:48:08 -0400

    Cleaned the PR, remove lookup workflow.yml and no need to save project_id in <workflow-id>.yml file and few typos

--- apps/dashboard/app/models/workflow.rb
+++ apps/dashboard/app/models/workflow.rb
@@ -4,57 +4,45 @@ class Workflow
   include ActiveModel::Model
 
   class << self
-    def workflows_file(project_dir)
-      workflows_dir = Pathname.new("#{project_dir}/.ondemand/workflows")
-      # TODO: Later use <workflow_id>.yml to get list of all workflows
-      file = workflows_dir.join("workflow.yml")
-
-      FileUtils.mkdir_p(workflows_dir)
-      FileUtils.touch(workflows_dir.join("workflow.yml")) unless file.exist?
-
-      file
+    def workflow_dir(project_dir)
+      dir = Pathname.new("#{project_dir}/.ondemand/workflows")
+      FileUtils.mkdir_p(dir) unless dir.exist?
+      dir
     end
 
-    def all(project_id)
-      project = Project.find(project_id)
-      project_dir = project.directory
-      f = File.read(workflows_file(project_dir))
-      YAML.safe_load(f).to_h.map do |id, workflow_name|
-        Workflow.new({ id: id, name: workflow_name })
-      end
-    rescue StandardError, Exception => e
-      Rails.logger.warn("cannot read #{project_dir}/.ondemand/workflows/workflow.yml due to error #{e}")
-      {}
+    def find(id, project_dir)
+      file = "#{workflow_dir(project_dir)}/#{id}.yml"
+      Workflow.from_yaml(file, project_dir)
+    end
+
+    def all(project_dir)
+      Dir.glob("#{workflow_dir(project_dir)}/*.yml").map do |file|
+        Workflow.from_yaml(file, project_dir)
+      end.compact.sort_by { |s| s.created_at }
+    end
+
+    def from_yaml(file, project_dir)
+      contents = File.read(file)
+      opts = YAML.safe_load(contents).deep_symbolize_keys
+      new(opts)
+    rescue StandardError, Errno::ENOENT => e
+      Rails.logger.warn("Did not find workflow due to error #{e}")
+      nil
     end
 
     def next_id
       SecureRandom.alphanumeric(8).downcase
     end
-
-    def find(project_id, id)
-      project = Project.find(project_id)
-      project_dir = project.directory
-      f = File.read(workflows_file(project_dir))
-      opts = YAML.safe_load(f).to_h.select do |id, workflow_name|
-        id == id.to_s
-      end.map do |id, workflow_name|
-        { id: id, name: workflow_name, project_id: project_id}
-      end.first
-      return nil if opts.nil?
-
-      Workflow.new(opts)
-    end
   end
-  
-  attr_reader :id, :name, :description, :project_id, :project_dir
+
+  attr_reader :id, :name, :description, :project_dir, :created_at
 
   def initialize(attributes = {})
     @id = attributes[:id]
     @name = attributes[:name]
     @description = attributes[:description]
-    @project_id = attributes[:project_id]
-    project = Project.find(@project_id)
-    @project_dir = project.directory unless project.nil?
+    @project_dir = attributes[:project_dir]
+    @created_at = attributes[:created_at]
   end
 
   def to_h
@@ -62,16 +50,17 @@ class Workflow
       :id => id,
       :name => name,
       :description => description,
+      :created_at => created_at,
+      :project_dir => project_dir,
     }
   end
 
   def save
     return false unless valid?(:create)
 
-    # SET DEFAULTS
+    @created_at = Time.now.to_i if @created_at.nil?
     @id = Workflow.next_id if id.blank?
-
-    add_to_workflow(:save) && save_manifest(:save)
+    save_manifest(:save)
   end
 
   def save_manifest(operation)
@@ -85,39 +74,17 @@ class Workflow
     false
   end
 
-  def add_to_workflow(operation)
-    f = File.read(Workflow.workflows_file(project_dir))
-    new_table = YAML.safe_load(f).to_h.merge(Hash[id, name.to_s])
-    File.write(Workflow.workflows_file(project_dir), new_table.to_yaml)
-    true
-  rescue StandardError => e
-    errors.add(operation, "Cannot update workflow lookup file with error #{e.class}:#{e.message}")
-    false
-  end
-
   def collect_errors
     errors.map(&:message).join(', ')
   end
 
   def destroy!
-    remove_from_lookup
     FileUtils.remove_entry(manifest_file, true)
     true
   end
 
-  def remove_from_lookup
-    f = File.read(Workflow.workflows_file(project_dir))
-    new_table = YAML.safe_load(f).except(id)
-    File.write(Workflow.workflows_file(project_dir), new_table.to_yaml)
-    true
-  rescue StandardError => e
-    errors.add(:update, "Cannot update lookup file with error #{e.class}:#{e.message}")
-    false
-  end
-
   def manifest_file
-    workflows_dir = Pathname.new("#{@project_dir}/.ondemand/workflows")
-    file = workflows_dir.join("#{@id}.yml")
+    Workflow.workflow_dir(@project_dir).join("#{@id}.yml")
   end
 
   # TODO: Add logic to save the DAG relation between launchers like array of <launcher #1, launcher #2>
