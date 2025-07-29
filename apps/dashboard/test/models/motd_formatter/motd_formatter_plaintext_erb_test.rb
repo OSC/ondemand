6d310e7ebaa4f6c9fba31ef0512b057694669e2b
Braeden Singleton <bsingleton@ascend-rw01.ten.osc.edu>
2025-07-29 13:27:01 -0400

    refactored motd_formatter tests

--- apps/dashboard/test/models/motd_formatter/motd_formatter_plaintext_erb_test.rb
+++ apps/dashboard/test/models/motd_formatter/motd_formatter_plaintext_erb_test.rb
@@ -1,55 +1,41 @@
 require 'test_helper'
 
 class MotdFormatter::PlaintextErbTest < ActiveSupport::TestCase
-  include MotdFormatter
   test "plaintext-formatter-erb returns a valid motd file when given a valid motd file" do
     path = "#{Rails.root}/test/fixtures/files/motd_valid"
-    motd_file = MotdFile.new(path)
-    formatted_motd = MotdFormatterPlaintextErb.new(motd_file)
-    expected_file = File.open(path).read
+    with_modified_env({ 'MOTD_FORMAT': "text_erb", 'MOTD_PATH': path }) do
+      expected_file = File.open(path).read
 
-    assert_equal expected_file, formatted_motd.content
+      assert_equal expected_file, MotdFile.new.formatter.content
+    end
   end
 
   test "plaintext-formatter-erb returns an empty string when given an empty motd file" do
-    path = "#{Rails.root}/test/fixtures/files/motd_empty"
-    motd_file = MotdFile.new(path)
-    formatted_motd = MotdFormatterPlaintextErb.new(motd_file)
-
-    assert_equal '', formatted_motd.content
+    with_modified_env({ 'MOTD_FORMAT': "text_erb", 'MOTD_PATH': "#{Rails.root}/test/fixtures/files/motd_empty" }) do
+      assert_equal '', MotdFile.new.formatter.content
+    end
   end
 
   test "plaintext-formatter-erb throws a standard error when given an invalid motd erb file" do
-    path = "#{Rails.root}/test/fixtures/files/motd_erb_standard_error"
-    motd_file = MotdFile.new(path)
-    
-    assert_raises(Exception) {
-      MotdFormatterPlaintextErb.new(motd_file)
-    }
+    with_modified_env({ 'MOTD_FORMAT': "text_erb", 'MOTD_PATH': "#{Rails.root}/test/fixtures/files/motd_erb_standard_error" }) do
+      assert_raises(Exception) {
+        MotdFile.new.formatter
+      }
+    end
   end
 
   test "plaintext-formatter-erb returns a valid motd erb file when given a valid motd erb file" do
-    path = "#{Rails.root}/test/fixtures/files/motd_valid_erb"
-    motd_file = MotdFile.new(path)
-    formatted_motd = MotdFormatterPlaintextErb.new(motd_file)
-    expected_file = "\nWelcome to the Ohio Supercomputer Center!\n"
-    
-    assert_equal expected_file, formatted_motd.content
+    with_modified_env({ 'MOTD_FORMAT': "text_erb", 'MOTD_PATH': "#{Rails.root}/test/fixtures/files/motd_valid_erb" }) do
+      expected_file = "\nWelcome to the Ohio Supercomputer Center!\n"
+      
+      assert_equal expected_file, MotdFile.new.formatter.content
+    end
   end
   
-  test "plaintext-formatter-erb returns an empty string when given a missing file" do
-    path = "#{Rails.root}/test/fixtures/files/motd_missing"
-    motd_file = MotdFile.new(path)
-    formatted_motd = MotdFormatterPlaintextErb.new(motd_file)
-
-    assert_equal '', formatted_motd.content
-  end
-
-  test "plaintext-formatter-erb returns an empty string when given nil" do
-    motd_file = nil
-    formatted_motd = MotdFormatterPlaintextErb.new(motd_file)
-
-    assert_not_nil formatted_motd.content
+  test "MotdFile.formatter returns nil when given a missing file" do
+    with_modified_env({ 'MOTD_FORMAT': "text_erb", 'MOTD_PATH': "#{Rails.root}/test/fixtures/files/motd_missing" }) do
+      assert_nil MotdFile.new.formatter
+    end
   end
 end
 
