3364cb0f2fff12a0a7f5860a08846900bd76c80b
Braeden Singleton <bsingleton@osc.edu>
2025-08-18 16:37:10 -0400

    change under_dotfile? to is_dotfile?

--- apps/myjobs/test/models/workflow_file_test.rb
+++ apps/myjobs/test/models/workflow_file_test.rb
@@ -1,10 +1,16 @@
 require 'test_helper'
 
 class WorkflowFileTest < ActiveSupport::TestCase
-  test "it recognizes hidden files" do
-    assert WorkflowFile.new(
+  test "is dotfile does not detect hidden folders" do
+    refute WorkflowFile.new(
         Pathname.new('/home/johrstrom/.wine/drive_c'),
         Pathname.new('/home/johrstrom/ondemand/and/so/on')
-    ).under_dotfile?
+    ).is_dotfile?
   end
+
+  test "is dotfile detects hidden files" do 
+    assert WorkflowFile.new(
+      Pathname.new('/home/johrstrom/wine/.drive_c'),
+      Pathname.new('/home/johrstrom/ondemand/and/so/on')
+  ).is_dotfile?
 end
