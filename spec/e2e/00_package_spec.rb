c966c64feb3bbf93c4ab90450f36488e318b2d75
Trey Dockendorf <tdockendorf@osc.edu>
2025-10-23 16:25:04 -0400

    Add debugging

--- spec/e2e/00_package_spec.rb
+++ spec/e2e/00_package_spec.rb
@@ -108,6 +108,9 @@ describe 'OnDemand installed with packages' do
     it { is_expected.to be_grouped_into('root') }
   end
 
+  it 'dumps gems with world write' do
+    on hosts, "find #{ood_gems_path} -perm /002 -exec ls -la {} \\;"
+  end
   describe command("find #{ood_gems_path} -perm /002") do
     its(:stdout) { is_expected.to be_empty }
   end
