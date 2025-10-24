98365e146a7f70afac59c43f2e781adc76fd2b4d
Harshit Soora <soora.iitkgp@gmail.com>
2025-10-24 11:56:52 -0400

    Interactive UI for workflows (#4604)
    * displays launchers in workflow as an interactive DAG
    
    * allows addition and deletion of both launchers and edges connecting launchers
    
    * added dag class in client side js to detect any cyclic dependency immediately
    
    * uses grid logic to keep each launcher separated in a fixed spot, not allowing another to take its place.
    
    * uses scroll feature to allow the whole graph to be viewed and modified regardless of zoom/screen size
    
    * has custom zoom feature to accommodate various graph sizes, with a max size of 32x32 launchers
    
    * expands click area around edges to make edge selection easier

---
+++ apps/dashboard/app/javascript/dag.js
@@ -0,0 +1,78 @@
+// This class will help us to detect if any new edge can lead to cycle or not
+// Thus we can alert user to avoid that edge thus resolving cycle issue on UI
+
+// Directed Acyclic Graph
+export class DAG {
+  #launcher_list;
+  #adjacency_list;
+  #visited;
+  #stack;
+  #has_cycle;
+
+  constructor() {
+    this.#launcher_list = new Set();
+    this.#adjacency_list = {};
+  }
+
+  addEdge(fromId, toId) {
+    if (!this.#launcher_list.has(fromId)) {
+      this.#launcher_list.add(fromId);
+    }
+
+    if (!this.#launcher_list.has(toId)) {
+      this.#launcher_list.add(toId);
+    }
+
+    if (!this.#adjacency_list[fromId]) {
+      this.#adjacency_list[fromId] = [];
+    }
+    this.#adjacency_list[fromId].push(toId);
+
+    this.#has_cycle = false;
+    this.#visited = new Set();
+    this.#stack = new Set();
+    this.#launcher_list.forEach(l => this.#detectCycle(l));
+
+    // Remove the added edge from the adjacency list if cycle detected
+    if (this.#has_cycle === true) {
+      this.#adjacency_list[fromId].pop();
+    }
+  }
+
+  removeEdge(fromId, toId) {
+    if (!this.#adjacency_list[fromId]) return;
+
+    if (this.#adjacency_list[fromId].includes(toId)) {
+      this.#adjacency_list[fromId] = this.#adjacency_list[fromId].filter(x => x !== toId);
+    }
+  }
+
+  removeLauncher(id) {
+    this.#launcher_list.delete(id);
+    delete this.#adjacency_list[id];
+
+    for (const from in this.#adjacency_list) {
+      this.#adjacency_list[from] = this.#adjacency_list[from].filter(x => x !== id);
+    }
+  }
+
+  hasCycle() {
+    return this.#has_cycle;
+  }
+
+  // Basic dfs on graph to find a cycle
+  #detectCycle(id) {
+    if (this.#stack.has(id)) {
+      this.#has_cycle = true;
+      return;
+    }
+    if (this.#visited.has(id)) return;
+
+    this.#stack.add(id);
+    this.#visited.add(id);
+    for (const l of this.#adjacency_list[id] || []) {
+      this.#detectCycle(l);
+    }
+    this.#stack.delete(id);
+  }
+}
