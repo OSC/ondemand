9bdcf6c06ff11b819b1845e7428dcda81d3cc28c
Braeden Singleton <bsingleton@osc.edu>
2025-11-06 10:24:13 -0500

    Initialize directory structure with project (#4750)
    * refactor directory preparation so that everything is initialized with the project
    
    * modify workflow directory and logfile methods to return pathnames without file creation
    
    * create necessary files in tests

--- apps/dashboard/app/models/project.rb
+++ apps/dashboard/app/models/project.rb
@@ -302,6 +302,10 @@ class Project
   def make_dir
     project_dataroot.mkpath         unless project_dataroot.exist?
     configuration_directory.mkpath  unless configuration_directory.exist?
+    workflow_directory = Workflow.workflow_dir(project_dataroot)
+    workflow_directory.mkpath unless workflow_directory.exist?
+    logfile_path = JobLogger::JobLoggerHelper.log_file(project_dataroot)
+    FileUtils.touch(logfile_path.to_s) unless logfile_path.exist?
     true
   rescue StandardError => e
     errors.add(:save, "Failed to make directory: #{e.message}")
