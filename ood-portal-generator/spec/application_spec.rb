3b3cee7beec1e0feb235dfb650a16f90003b904c
Neil Prockter <mrgum@users.noreply.github.com>
2025-11-07 18:39:14 +0000

    enhanced register options (#4763)
    enhanced register options adding register_path, register_method and register_method_options to ood_portal.yml.

--- ood-portal-generator/spec/application_spec.rb
+++ ood-portal-generator/spec/application_spec.rb
@@ -200,6 +200,18 @@ describe OodPortalGenerator::Application do
       test_generate('input/auth.yml', 'output/auth_deb.conf')
     end
 
+    it 'generates defaults for register' do
+      test_generate('input/register-defaults.yml', 'output/register-defaults.conf')
+    end
+
+    it 'generates scriptalias for register' do
+      test_generate('input/register-scriptalias.yml', 'output/register-scriptalias.conf')
+    end
+
+    it 'generates wsgiscriptalias for register' do
+      test_generate('input/register-wsgiscriptalias.yml', 'output/register-wsgiscriptalias.conf')
+    end
+
     context 'dex' do
       let(:config_dir) do
         Dir.mktmpdir
