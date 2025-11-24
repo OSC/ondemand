8c8cd00245ec2c41cf1e89a2c419ac445422b67d
Braeden Singleton <bsingleton@osc.edu>
2025-11-24 01:37:09 -0500

    enable path selector

--- apps/dashboard/app/javascript/projects_index.js
+++ apps/dashboard/app/javascript/projects_index.js
@@ -2,6 +2,16 @@
 
 import { attachPathSelectors }  from './path_selector/path_selector';
 
-jQuery(function() {
+function onFirstFrameRender(event) {
+  const frame = event.target;
+  if (!(frame instanceof HTMLElement)) return;
+  if (frame.id !== 'import_pane') return;
+
   attachPathSelectors();
+
+  document.removeEventListener('turbo:frame-render', onFirstFrameRender);
+}
+
+jQuery(function() {
+  document.addEventListener('turbo:frame-render', onFirstFrameRender);
 });
