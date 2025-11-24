1afab8cb15362fc0ee58da4b82b043ca4dbdab70
Braeden Singleton <bsingleton@osc.edu>
2025-11-24 16:14:58 -0500

    protect turbo_frame from redirects

--- apps/dashboard/app/controllers/projects_controller.rb
+++ apps/dashboard/app/controllers/projects_controller.rb
@@ -113,10 +113,10 @@ class ProjectsController < ApplicationController
       if Project.import_to_lookup(@project)
         redirect_to projects_path, notice: I18n.t('dashboard.jobs_project_imported')
       else
-        redirect_to project_import_path, alert: @project.errors.full_messages.join('. ')
+        redirect_to projects_path, alert: @project.errors.full_messages.join('. ')
       end
     else
-      redirect_to project_import_path, alert: @project.errors.full_messages.join('. ')
+      redirect_to projects_path, alert: @project.errors.full_messages.join('. ')
     end
   end
 
