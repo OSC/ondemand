c56c28abdc64f32d3d7158c0336ee4f1d3c7e7da
Jeff Ohrstrom <johrstrom@osc.edu>
2025-12-18 16:32:21 -0500

    disable shared settings when appropriate

--- apps/dashboard/app/javascript/projects_new.js
+++ apps/dashboard/app/javascript/projects_new.js
@@ -1,10 +1,17 @@
 'use strict';
 
 import { attachPathSelectors }  from './path_selector/path_selector';
+import { userHome } from './config';
+
+// cache this just so we don't have to keep parsing the DOM to retrieve it.
+const home = userHome();
 
 jQuery(function() {
   $("#project_template").on('change', (event) => templateChange(event));
+  $("#project_directory").on('input', (event) => toggleSharedSettings(event.target.value));
   attachPathSelectors();
+
+  toggleSharedSettings(document.getElementById('project_directory').value);
 });
 
 function templateChange(event) {
@@ -21,4 +28,13 @@ function templateChange(event) {
   $("#project_description").val(description);
   $("#product_icon_select").val(icon);
   $("#product_icon_select").trigger('change');
+}
+
+function toggleSharedSettings(value) {
+  const owner = document.getElementById('project_group_owner');
+  const setgid = document.getElementById('project_setgid');
+  const isDisabled = value.startsWith(home) || value === "";
+
+  owner.disabled = isDisabled;
+  setgid.disabled = isDisabled;
 }
