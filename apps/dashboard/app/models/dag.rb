a0f50a4dbb22830d8eb82d0e5afd6e5d2841e3b8
Harshit Soora <soora.iitkgp@gmail.com>
2026-01-05 18:31:06 -0500

    Added support to get order in subgraph of the original workflow

--- apps/dashboard/app/models/dag.rb
+++ apps/dashboard/app/models/dag.rb
@@ -1,16 +1,20 @@
 # Directed Acyclic Graph class used by workflows
 
 class Dag
-  attr_reader :size, :index, :dependency, :adj_mat, :order, :has_cycle
+  attr_reader :size, :index, :dependency, :adj_mat, :order, :has_cycle, :start_launcher
 
   def initialize(attributes = {})
     @size = attributes[:launcher_ids].size
     @index = attributes[:launcher_ids].each_with_index.to_h
+    if attributes.key?(:start_launcher)
+      @start_launcher = attributes[:start_launcher]
+    else
+      @start_launcher = nil
+    end 
 
-    create_dependency_list(attributes[:source_ids], attributes[:target_ids], attributes[:launcher_ids])
     create_adjacency_matrix(attributes[:source_ids], attributes[:target_ids], attributes[:launcher_ids])
-
     topological_sort(attributes[:launcher_ids])
+    create_dependency_list(attributes[:source_ids], attributes[:target_ids], attributes[:launcher_ids])
   end
 
   # This will be use to do Depth-First-Search on graph
@@ -21,10 +25,10 @@ class Dag
     m = to_ids.size
     for i in 0...m
       next if from_ids[i].nil? || to_ids[i].nil?
-      next unless launcher_ids.include?(from_ids[i]) && launcher_ids.include?(to_ids[i])
+      next unless launcher_ids.include?(from_ids[i]) || launcher_ids.include?(to_ids[i])
 
-      u = index[from_ids[i]]
-      v = index[to_ids[i]]
+      u = @index[from_ids[i]]
+      v = @index[to_ids[i]]
       @adj_mat[u][v] = true
     end
   end
@@ -37,6 +41,7 @@ class Dag
     for i in 0...m
       next if from_ids[i].nil? || to_ids[i].nil?
       next unless launcher_ids.include?(from_ids[i]) && launcher_ids.include?(to_ids[i])
+      next unless @order.include?(from_ids[i]) && @order.include?(to_ids[i])
 
       @dependency[to_ids[i]] ||= []
       @dependency[to_ids[i]] << from_ids[i]
@@ -50,7 +55,7 @@ class Dag
     stack[parent] = true
 
     for child in 0...@size
-      if adj_mat[parent][child]
+      if @adj_mat[parent][child]
         if stack[child]
           @has_cycle = true  # To detect cycle
           return
@@ -60,7 +65,7 @@ class Dag
     end
 
     # Append the launcher_id in order is there is no other launcher dependent on it
-    order.unshift(launcher_ids[parent])
+    @order.unshift(launcher_ids[parent])
     stack[parent] = false
   end
 
@@ -70,9 +75,13 @@ class Dag
     visited = Array.new(@size, false)
     stack = Array.new(@size, false)
 
-    for i in 0...@size
-      dfs(i, visited, stack, launcher_ids) unless visited[i]
-      break if @has_cycle
+    if !@start_launcher.nil?
+      dfs(@index[@start_launcher], visited, stack, launcher_ids)
+    else
+      for i in 0...@size
+        dfs(i, visited, stack, launcher_ids) unless visited[i]
+        break if @has_cycle
+      end
     end
   end
 
