38a2f69111f073f0690a245bc52021460436982f
Harshit Soora <soora.iitkgp@gmail.com>
2026-01-05 18:27:53 -0500

    Undo my changes for dag

--- apps/dashboard/app/models/dag.rb
+++ apps/dashboard/app/models/dag.rb
@@ -1,20 +1,16 @@
 # Directed Acyclic Graph class used by workflows
 
 class Dag
-  attr_reader :size, :index, :dependency, :adj_mat, :order, :has_cycle, :start_launcher
+  attr_reader :size, :index, :dependency, :adj_mat, :order, :has_cycle
 
   def initialize(attributes = {})
     @size = attributes[:launcher_ids].size
     @index = attributes[:launcher_ids].each_with_index.to_h
-    if attributes.key?(:start_launcher)
-      @start_launcher = attributes[:start_launcher]
-    else
-      @start_launcher = nil
-    end
 
-    create_adjacency_matrix(attributes[:source_ids], attributes[:target_ids], attributes[:launcher_ids])
-    topological_sort(attributes[:launcher_ids])
     create_dependency_list(attributes[:source_ids], attributes[:target_ids], attributes[:launcher_ids])
+    create_adjacency_matrix(attributes[:source_ids], attributes[:target_ids], attributes[:launcher_ids])
+
+    topological_sort(attributes[:launcher_ids])
   end
 
   # This will be use to do Depth-First-Search on graph
@@ -27,8 +23,8 @@ class Dag
       next if from_ids[i].nil? || to_ids[i].nil?
       next unless launcher_ids.include?(from_ids[i]) && launcher_ids.include?(to_ids[i])
 
-      u = @index[from_ids[i]]
-      v = @index[to_ids[i]]
+      u = index[from_ids[i]]
+      v = index[to_ids[i]]
       @adj_mat[u][v] = true
     end
   end
@@ -41,7 +37,6 @@ class Dag
     for i in 0...m
       next if from_ids[i].nil? || to_ids[i].nil?
       next unless launcher_ids.include?(from_ids[i]) && launcher_ids.include?(to_ids[i])
-      next unless @order.include?(from_ids[i]) && @order.include?(to_ids[i])
 
       @dependency[to_ids[i]] ||= []
       @dependency[to_ids[i]] << from_ids[i]
@@ -55,7 +50,7 @@ class Dag
     stack[parent] = true
 
     for child in 0...@size
-      if @adj_mat[parent][child]
+      if adj_mat[parent][child]
         if stack[child]
           @has_cycle = true  # To detect cycle
           return
@@ -65,7 +60,7 @@ class Dag
     end
 
     # Append the launcher_id in order is there is no other launcher dependent on it
-    @order.unshift(launcher_ids[parent])
+    order.unshift(launcher_ids[parent])
     stack[parent] = false
   end
 
@@ -75,13 +70,9 @@ class Dag
     visited = Array.new(@size, false)
     stack = Array.new(@size, false)
 
-    if !@start_launcher.nil?
-      dfs(@index[@start_launcher], visited, stack, launcher_ids)
-    else
-      for i in 0...@size
-        dfs(i, visited, stack, launcher_ids) unless visited[i]
-        break if @has_cycle
-      end
+    for i in 0...@size
+      dfs(i, visited, stack, launcher_ids) unless visited[i]
+      break if @has_cycle
     end
   end
 
