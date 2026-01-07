3ca185fa60223137916fb8766474196bd4a12240
Jeff Ohrstrom <johrstrom@osc.edu>
2026-01-07 10:21:01 -0500

    fix this typo

--- apps/myjobs/app/helpers/pages_helper.rb
+++ apps/myjobs/app/helpers/pages_helper.rb
@@ -5,6 +5,6 @@ module PagesHelper
   #
   # @return [String, nil] The title of the cluster or nil if unassigned
   def cluster_title(cluster_key)
-    OODClusters[cluster_key].titlex if cluster_key && OODClusters[cluster_key]
+    OODClusters[cluster_key].title if cluster_key && OODClusters[cluster_key]
   end
 end
