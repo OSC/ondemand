fede99a53eef8672f37aaadff05888fa9e9d3544
Harshit Soora <soora.iitkgp@gmail.com>
2026-01-08 10:22:49 -0500

    Support to get subgraph order in DAG class (#4918)
    * Added support to get subgraph of the original workflow
    
    * Added tests for partial workflows

--- apps/dashboard/app/models/dag.rb
+++ apps/dashboard/app/models/dag.rb
@@ -1,22 +1,26 @@
 # Directed Acyclic Graph class used by workflows
 
 class Dag
-  attr_reader :size, :index, :dependency, :adj_mat, :order, :has_cycle
+  attr_reader :size, :index, :dependency, :adj_mat, :order, :has_cycle, :start_launcher
 
   def initialize(attributes = {})
     @size = attributes[:launcher_ids].size
     @index = attributes[:launcher_ids].each_with_index.to_h
+    if attributes.key?(:start_launcher)
+      @start_launcher = attributes[:start_launcher]
+    else
+      @start_launcher = nil
+    end 
 
-    create_dependency_list(attributes[:source_ids], attributes[:target_ids], attributes[:launcher_ids])
     create_adjacency_matrix(attributes[:source_ids], attributes[:target_ids], attributes[:launcher_ids])
-
     topological_sort(attributes[:launcher_ids])
+    create_dependency_list(attributes[:source_ids], attributes[:target_ids], attributes[:launcher_ids])
   end
 
   # This will be use to do Depth-First-Search on graph
   # Save edges are integer representing index of launcher_ids
   def create_adjacency_matrix(from_ids, to_ids, launcher_ids)
-    @adj_mat = Array.new(@size) { Array.new(@size, false) }
+    @adj_mat = Array.new(size) { Array.new(size, false) }
 
     m = to_ids.size
     for i in 0...m
@@ -25,7 +29,7 @@ class Dag
 
       u = index[from_ids[i]]
       v = index[to_ids[i]]
-      @adj_mat[u][v] = true
+      adj_mat[u][v] = true
     end
   end
 
@@ -37,9 +41,10 @@ class Dag
     for i in 0...m
       next if from_ids[i].nil? || to_ids[i].nil?
       next unless launcher_ids.include?(from_ids[i]) && launcher_ids.include?(to_ids[i])
+      next unless order.include?(from_ids[i]) && order.include?(to_ids[i])
 
-      @dependency[to_ids[i]] ||= []
-      @dependency[to_ids[i]] << from_ids[i]
+      dependency[to_ids[i]] ||= []
+      dependency[to_ids[i]] << from_ids[i]
     end
   end
 
@@ -49,7 +54,7 @@ class Dag
     visited[parent] = true
     stack[parent] = true
 
-    for child in 0...@size
+    for child in 0...size
       if adj_mat[parent][child]
         if stack[child]
           @has_cycle = true  # To detect cycle
@@ -67,12 +72,16 @@ class Dag
   def topological_sort(launcher_ids)
     @order = []
     @has_cycle = false
-    visited = Array.new(@size, false)
-    stack = Array.new(@size, false)
+    visited = Array.new(size, false)
+    stack = Array.new(size, false)
 
-    for i in 0...@size
-      dfs(i, visited, stack, launcher_ids) unless visited[i]
-      break if @has_cycle
+    if !start_launcher.nil?
+      dfs(index[start_launcher], visited, stack, launcher_ids)
+    else
+      for i in 0...size
+        dfs(i, visited, stack, launcher_ids) unless visited[i]
+        break if has_cycle
+      end
     end
   end
 
