b44fdde6e0e0509b659872653791d75fe6073965
Jeff Ohrstrom <johrstrom@osc.edu>
2026-01-28 13:47:38 -0500

    Revert "fix icons by setting x_sendfile_header (#5007)" (#5028)
    This reverts commit 611be2399b2cc0cd1bd09e3d7a6aafdfa82eaa30.

--- apps/dashboard/config/initializers/send_file_headers.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# The FilesController serves files itself by unsetting this header
-# and using 'config/initializers/validate_send_files.rb' to validate files it
-# serves.  The AppsController also uses send_file to serve icons that may not be
-# on the allowlist.  So we add this configuration to allow the AppsController
-# to serve app icons through Nginx.  No other controller should be using send_file
-# outside these 2 use cases.
-Rails.application.config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
