9252e6b6b9c5cef6bccc6630642ad3ec45ffc984
Braeden Singleton <bsingleton@osc.edu>
2026-03-18 12:05:37 -0400

    merge 7.2 configs

--- apps/dashboard/config/initializers/content_security_policy.rb
+++ apps/dashboard/config/initializers/content_security_policy.rb
@@ -1,13 +1,12 @@
-# frozen_string_literal: true
-
 # Be sure to restart your server when you modify this file.
 
 # Define an application-wide content security policy
-# For further information see the following documentation
-# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
+# See the Securing Rails Applications Guide for more information:
+# https://guides.rubyonrails.org/security.html#content-security-policy-header
 
 if Configuration.csp_enabled?
-  Rails.application.config.content_security_policy do |policy|
+  Rails.application.configure do
+    config.content_security_policy do |policy|
     policy.default_src :self
     policy.font_src    :self, :data
     policy.img_src     :self, :data
@@ -18,18 +17,17 @@ if Configuration.csp_enabled?
 
     # Specify URI for violation reports
     # policy.report_uri "/csp-violation-report-endpoint"
+    # If you are using UJS then enable automatic nonce generation
+    config.content_security_policy_nonce_generator = ->(_request) { SecureRandom.base64(16) }
+
+    # Set the nonce only to specific directives
+    config.content_security_policy_nonce_directives = ['script-src', 'style-src']
+    
+    # Report CSP violations to a specified URI
+    # For further information see the following documentation:
+    # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
+    if Configuration.csp_enabled?
+      config.content_security_policy_report_only = Configuration.csp_report_only
+    end
   end
-end
-
-# If you are using UJS then enable automatic nonce generation
-Rails.application.config.content_security_policy_nonce_generator = ->(_request) { SecureRandom.base64(16) }
-
-# Set the nonce only to specific directives
-Rails.application.config.content_security_policy_nonce_directives = ['script-src', 'style-src']
-
-# Report CSP violations to a specified URI
-# For further information see the following documentation:
-# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
-if Configuration.csp_enabled?
-  Rails.application.config.content_security_policy_report_only = Configuration.csp_report_only
 end
