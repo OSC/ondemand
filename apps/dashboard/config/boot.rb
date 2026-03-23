1cd97279a7509e1e2eedbb62e4433304d761a41a
Braeden Singleton <bsingleton@osc.edu>
2026-03-23 13:47:48 -0400

    reset strings and string literal declarations

--- apps/dashboard/config/boot.rb
+++ apps/dashboard/config/boot.rb
@@ -1,24 +1,24 @@
 # frozen_string_literal: true
 
 # favor tmp/node_modules/yarn/bin/yarnpkg
-ENV["PATH"] = "#{File.join(File.expand_path("..", __dir__), "tmp", "node_modules", "yarn", "bin")}:#{ENV["PATH"]}"
+ENV['PATH'] = "#{File.join(File.expand_path('..', __dir__), 'tmp', 'node_modules', 'yarn', 'bin')}:#{ENV['PATH']}"
 
 # Set up gems listed in the Gemfile.
-ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
-require "bundler/setup" # Set up gems listed in the Gemfile.
+require 'bundler/setup' # Set up gems listed in the Gemfile.
 
 # TODO: we remove this once we upgrade to Rails 7.1
-require "logger"
+require 'logger'
 
 # load dotenv files before "before_configuration" callback
-require File.expand_path("configuration_singleton", __dir__)
+require File.expand_path('configuration_singleton', __dir__)
 
 # global instance to access and use
 Configuration = ConfigurationSingleton.new
 
 # set defaults to address OodAppkit.dataroot issue
-ENV["OOD_DATAROOT"] = Configuration.dataroot.to_s
+ENV['OOD_DATAROOT'] = Configuration.dataroot.to_s
 
 # Rails 5.2.3 suggests adding bootsnap (https://github.com/Shopify/bootsnap)
 # which writes to /tmp as it does not appear to write to a user-namespaced
