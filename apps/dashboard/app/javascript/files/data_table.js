46ba6c9b256a6d5d57dd6663a1aefe5b53289c24
bstepanovski <bstepanovski@osc.edu>
2026-04-10 16:32:26 -0400

    Update files app page title to include directory (fix #2078) (#5252)
    * set page title on initial load
    
    * update page title on each table reload

--- apps/dashboard/app/javascript/files/data_table.js
+++ apps/dashboard/app/javascript/files/data_table.js
@@ -303,8 +303,11 @@ class DataTable {
                 $('#select_all').trigger();
             }
 
-	    $(`${CONTENTID}_caption`).text(`Contents of directory ${data.path}`);
-	    ariaNotify(`navigated to ${data.path}`);
+        $(`${CONTENTID}_caption`).text(`Contents of directory ${data.path}`);
+        ariaNotify(`navigated to ${data.path}`);
+            if (data.page_title) {
+                document.title = data.page_title;
+            }
 
             let result = await Promise.resolve(data);
             $('td input[type=checkbox]').on('keypress', function(event) {
