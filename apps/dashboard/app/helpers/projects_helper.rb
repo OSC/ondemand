9938e6f1c8369198400636a48d9b0285f4759b57
Braeden Singleton <bsingleton@osc.edu>
2026-04-22 17:09:48 -0400

    make help text keyboard-focusable

--- apps/dashboard/app/helpers/projects_helper.rb
+++ apps/dashboard/app/helpers/projects_helper.rb
@@ -53,6 +53,6 @@ module ProjectsHelper
   end
 
   def form_label_tooltip(content)
-    "<i class='fa fa-question-circle vertical-align-middle' data-bs-toggle='tooltip' data-bs-placement='right' title='#{content}'></i>"
+    "<i class='fa fa-question-circle vertical-align-middle' data-bs-toggle='tooltip' data-bs-placement='right' title='#{content}' tabindex='0'></i>"
   end
 end
