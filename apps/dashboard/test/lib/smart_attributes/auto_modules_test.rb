cc536e5d8f569ebfed8deb5e8a00361ecd816dfa
bstepanovski <bstepanovski@osc.edu>
2026-05-28 16:45:31 -0500

    Normalize dynamic batch connect tokens with underscores (#5462)
    * Normalize dynamic batch connect tokens with underscores
    
    * Add unit test for hyphenated data-option-for cluster handling
    
    * Add system test for dashed cluster names in dynamic form

--- apps/dashboard/test/lib/smart_attributes/auto_modules_test.rb
+++ apps/dashboard/test/lib/smart_attributes/auto_modules_test.rb
@@ -41,6 +41,18 @@ class SmartAttributes::AutoModulesTest < ActiveSupport::TestCase
     end
   end
 
+  test 'data-option-for-cluster uses hyphens for cluster ids with underscores' do
+    with_modified_env({ OOD_MODULE_FILE_DIR: fixture_dir }) do
+      attribute = SmartAttributes::AttributeFactory.build('auto_modules_R', {})
+      Configuration.stubs(:job_clusters).returns([stub(id: 'x-nextgen_ascend')])
+      HpcModule.stubs(:all_versions).returns([stub(on_cluster?: false, version: '1.0', to_s: 'R/1.0')])
+
+      data = attribute.send(:hpc_versions).first[2]
+      assert data.key?('data-option-for-cluster-x-nextgen-ascend')
+      refute data.key?('data-option-for-cluster-x-nextgen_ascend')
+    end
+  end
+
   test 'can filter modules when given an array' do
     with_modified_env({ OOD_MODULE_FILE_DIR: fixture_dir }) do
       attribute = SmartAttributes::AttributeFactory.build('auto_modules_R', { filter: [/Eliminate/, /GetRidOfMeToo/] })
