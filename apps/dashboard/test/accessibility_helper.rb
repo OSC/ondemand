26580637a40004b65c868c0ee6ee74be5d2614d5
Braeden Singleton <bsingleton@osc.edu>
2026-06-15 13:30:19 -0400

    Update luminance computation threshold

--- apps/dashboard/test/accessibility_helper.rb
+++ apps/dashboard/test/accessibility_helper.rb
@@ -5,7 +5,7 @@ class ActiveSupport::TestCase
       // Matches step 1 under Tests/Procedure at https://www.w3.org/WAI/WCAG22/Techniques/general/G17
       const [rs, gs, bs] = [r, g, b].map(c => {
         c /= 255;
-        return c <= 0.03928 ? c / 12.92 : Math.pow((c + 0.055) / 1.055, 2.4);
+        return c <= 0.04045 ? c / 12.92 : Math.pow((c + 0.055) / 1.055, 2.4);
       });
       return 0.2126 * rs + 0.7152 * gs + 0.0722 * bs;
     }
