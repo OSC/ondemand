11d894f05ba57cb82b828808a5b69f5a0f0b2961
Jeff Ohrstrom <johrstrom@osc.edu>
2026-06-22 13:56:07 -0400

    fix this test case and use ()

--- apps/dashboard/test/helpers/app_helper_test.rb
+++ apps/dashboard/test/helpers/app_helper_test.rb
@@ -4,30 +4,30 @@ require 'test_helper'
 
 class AppHelperTest < ActionView::TestCase
   test 'recent_settings escapes label and value to prevent XSS' do
-    attr = stub(
+    attrib = stub(
       display?: true,
       label:    '<script>alert("xss")</script>',
       value:    '"><img src=x onerror=alert(1)>'
     )
-    app = stub(attributes: [attr])
+    app = stub(attributes: [attrib])
 
     result = recent_settings(app)
 
-    assert_not_nil result
+    assert_not_nil(result)
     assert_no_match(/<script>/, result)
-    assert_no_match(/onerror/, result)
-    assert_includes result, '&lt;script&gt;'
-    assert_includes result, '&quot;'
+    assert_no_match(/<img/, result)
+    assert_includes(result, '&lt;script&gt;')
+    assert_includes(result, '&quot;')
   end
 
   test 'recent_settings returns nil when no displayable attributes' do
     app = stub(attributes: [stub(display?: false)])
-    assert_nil recent_settings(app)
+    assert_nil(recent_settings(app))
   end
 
   test 'recent_settings returns nil when attributes list is empty' do
     app = stub(attributes: [])
-    assert_nil recent_settings(app)
+    assert_nil(recent_settings(app))
   end
 
   test 'recent_settings renders safe label and value unchanged' do
@@ -36,7 +36,7 @@ class AppHelperTest < ActionView::TestCase
 
     result = recent_settings(app)
 
-    assert_includes result, 'Number of cores'
-    assert_includes result, '4'
+    assert_includes(result, 'Number of cores')
+    assert_includes(result, '4')
   end
 end
