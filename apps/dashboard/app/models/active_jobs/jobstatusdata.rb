9c9925d3964b17b4c013c08b576ce8e6abd2da6a
adhati09 <144935273+adhati09@users.noreply.github.com>
2026-07-24 13:49:57 -0400

    Fixed indentation of attr_writer after reverting it back

--- apps/dashboard/app/models/active_jobs/jobstatusdata.rb
+++ apps/dashboard/app/models/active_jobs/jobstatusdata.rb
@@ -357,8 +357,7 @@ module ActiveJobs
         node_info_array.map { |n| n.name }
       end
 
-      # Change this line back to the original order (:gpus before :cput, :mem):
-attr_writer :pbsid, :jobname, :username, :account, :status, :cluster, :cluster_title, :nodes, :starttime, :walltime, :walltime_used, :submit_args, :output_path, :nodect, :ppn, :total_cpu, :queue, :gpus, :cput, :mem, :vmem, :shell_url, :file_explorer_url, :extended_available, :native_attribs, :error
+      attr_writer :pbsid, :jobname, :username, :account, :status, :cluster, :cluster_title, :nodes, :starttime, :walltime, :walltime_used, :submit_args, :output_path, :nodect, :ppn, :total_cpu, :queue, :gpus, :cput, :mem, :vmem, :shell_url, :file_explorer_url, :extended_available, :native_attribs, :error
 
   end
 end
