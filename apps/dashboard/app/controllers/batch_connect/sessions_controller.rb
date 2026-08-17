c7d4b843b560051c1ddd0bb01f7f78e4adbc04c8
Aditya <adhati09@gmail.com>
2026-08-17 11:57:52 -0400

    fix(dashboard): remove unintended changes from batch connect sessions_controller

--- apps/dashboard/app/controllers/batch_connect/sessions_controller.rb
+++ apps/dashboard/app/controllers/batch_connect/sessions_controller.rb
@@ -1,30 +1,13 @@
-#The controller for active batch connect sessions
+# The controller for active batch connect sessions.
 class BatchConnect::SessionsController < ApplicationController
   include BatchConnectConcern
   include UserSettingStore
 
-  rescue_from BatchConnect::Session::InvalidDbRoot do |exception|
-    respond_to do |format|
-      format.html do
-        redirect_back allow_other_host: false,
-                      fallback_location: batch_connect_sessions_url,
-                      alert: exception.message
-      end
-      format.json { render json: { error: exception.message }, status: :internal_server_error }
-    end
-  end
-
   # GET /batch_connect/sessions
   # GET /batch_connect/sessions.json
   def index
-    begin
-      @sessions = BatchConnect::Session.all
-      @sessions.each(&:update_cache_completed!)
-    rescue BatchConnect::Session::InvalidDbRoot => e
-      Rails.logger.error("BatchConnect db_root error: #{e.message}")
-      flash.now[:alert] = e.message
-      @sessions = []
-    end
+    @sessions = BatchConnect::Session.all
+    @sessions.each(&:update_cache_completed!)
 
     set_app_groups
     set_saved_settings
@@ -68,26 +51,25 @@ class BatchConnect::SessionsController < ApplicationController
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_session
+      @session = BatchConnect::Session.find(params[:id])
+    end
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_session
-    @session = BatchConnect::Session.find(params[:id])
-  end
+    # Set list of app lists for navigation
+    def set_app_groups
+      @sys_app_groups = bc_sys_app_groups
+      @usr_app_groups = bc_usr_app_groups
+      @dev_app_groups = bc_dev_app_groups
+      @apps_menu_group = bc_custom_apps_group
+    end
 
-  # Set list of app lists for navigation
-  def set_app_groups
-    @sys_app_groups = bc_sys_app_groups
-    @usr_app_groups = bc_usr_app_groups
-    @dev_app_groups = bc_dev_app_groups
-    @apps_menu_group = bc_custom_apps_group
-  end
+    # Set the all the saved settings to render the navigation
+    def set_saved_settings
+      @bc_saved_settings = all_bc_templates
+    end
 
-  # Set the all the saved settings to render the navigation
-  def set_saved_settings
-    @bc_saved_settings = all_bc_templates
-  end
-
-  def delete_session_panel?
-    params[:delete] ? params[:delete] == 'true' : true
-  end
+    def delete_session_panel?
+      params[:delete] ? params[:delete] == 'true' : true
+    end
 end
