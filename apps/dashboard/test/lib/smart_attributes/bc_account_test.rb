3f6f388203e004785942a03976b6c2bb4e5e79b5
sxiao <sxiao@osc.edu>
2026-08-28 16:26:30 -0400

    feat: add test cases for bc_account.rb widget

---
+++ apps/dashboard/test/lib/smart_attributes/bc_account_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+module SmartAttributes
+  class BcAccountTest < ActiveSupport::TestCase
+    test "widget should default to text_field" do
+      attribute = SmartAttributes::AttributeFactory.build_bc_account({})
+      assert_equal "text_field", attribute.widget
+    end
+
+    test "widget should be select" do
+      attribute = SmartAttributes::AttributeFactory.build_bc_account({ widget: "select" })
+      assert_equal "select", attribute.widget
+    end
+  end
+end
