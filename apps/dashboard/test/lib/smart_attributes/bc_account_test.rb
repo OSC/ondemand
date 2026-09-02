ff0c0f05ffda65ad7db024607450209c980c2a17
sxiao <sxiao@osc.edu>
2026-09-02 13:09:59 -0400

    feat: add test cases for label and submit methods

--- apps/dashboard/test/lib/smart_attributes/bc_account_test.rb
+++ apps/dashboard/test/lib/smart_attributes/bc_account_test.rb
@@ -2,14 +2,18 @@ require 'test_helper'
 
 module SmartAttributes
   class BcAccountTest < ActiveSupport::TestCase
-    test "widget should default to text_field" do
+    test "widget default" do
       attribute = SmartAttributes::AttributeFactory.build_bc_account({})
       assert_equal("text_field", attribute.widget)
+      assert_equal("Account", attribute.label)
+      assert_equal({ script: { accounting_id: nil } }, attribute.submit)
     end
 
     test "widget should be select" do
-      attribute = SmartAttributes::AttributeFactory.build_bc_account({ widget: "select" })
+      attribute = SmartAttributes::AttributeFactory.build_bc_account({ widget: "select", label: "label", value: "value" })
       assert_equal("select", attribute.widget)
+      assert_equal("label", attribute.label)
+      assert_equal({ script: { accounting_id: "value" } }, attribute.submit)
     end
   end
 end
