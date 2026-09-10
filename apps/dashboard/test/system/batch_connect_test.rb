29403cdbcefdbe89b30cd90a4f750d20108ffeea
Braeden Singleton <bsingleton@osc.edu>
2026-09-10 12:54:23 -0400

    finish test on auto_batch_clusters and auto_cores

--- apps/dashboard/test/system/batch_connect_test.rb
+++ apps/dashboard/test/system/batch_connect_test.rb
@@ -2751,8 +2751,7 @@ class BatchConnectTest < ApplicationSystemTestCase
     Dir.mktmpdir do |dir|
       "#{dir}/app".tap { |d| Dir.mkdir(d) }
       SysRouter.stubs(:base_path).returns(Pathname.new(dir))
-      stub_scontrol
-      stub_sacctmgr
+      stub_sinfo
       stub_git("#{dir}/app")
 
       form = <<~HEREDOC
@@ -2763,11 +2762,13 @@ class BatchConnectTest < ApplicationSystemTestCase
 
       Pathname.new("#{dir}/app/").join('form.yml').write(form)
       visit new_batch_connect_session_context_url('sys/app')
-      raise StandardError
-    end
-  end
+      
+      assert_equal('oakley', find_value('auto_batch_clusters'))
+      assert_equal(80, find_max('auto_cores'))
 
-  test 'auto_cores are queue aware' do
+      select('owens', from: bc_ele_id('auto_batch_clusters'))
+      assert_equal(48, find_max('auto_cores'))
+    end
   end
 
   test 'path_selector works' do
