80280ec5abe4c9d7de61a528309eff807836bc2d
Jeff Ohrstrom <johrstrom@osc.edu>
2026-09-11 10:26:57 -0400

    reset dynamic forms

--- apps/dashboard/app/javascript/dynamic_forms.js
+++ apps/dashboard/app/javascript/dynamic_forms.js
@@ -859,10 +859,12 @@ function idFromToken(str) {
 
   // you matched multiple things. For example you're searching for
   // ClusterFilesystem and matched against both 'Cluster' and 'ClusterFilesystem'.
-  // The correrct element id is the longer one.
+  // The correrct element id ends with cluster_filesystem.
   } else if(elements.length > 1) {
-    let longest = elements.reduce((a, b) => a.length > b.length ? a : b);
-    return longest;
+    const snake_case_str = snakeCaseWords(str);
+    return elements.filter((element) => {
+      return element.endsWith(snake_case_str);
+    })[0];
   }
 }
 
