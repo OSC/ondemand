b13e4620edda446108ae324e3598808e4f4bdf34
GlazerMann <kurt.glaesemann@pnnl.gov>
2026-09-18 09:03:07 -0500

    Update pun_pre_hook_spec.rb

--- spec/e2e/pun_pre_hook_spec.rb
+++ spec/e2e/pun_pre_hook_spec.rb
@@ -7,24 +7,6 @@ describe 'Pun Pre Hook' do
     @browser ||= new_browser
   end
 
-  def clean_nginx
-    on hosts, <<~SH
-      /opt/ood/nginx_stage/sbin/nginx_stage nginx_clean --force || true
-      rm -rf /var/run/ondemand-nginx/deleted_user
-      rm -f /var/lib/ondemand-nginx/config/puns/deleted_user.secret_key_base.txt
-
-      for i in $(seq 1 30); do
-        if [ ! -S /var/run/ondemand-nginx/ood/passenger.sock ]; then
-          exit 0
-        fi
-        sleep 1
-      done
-
-      echo 'passenger.sock still exists after nginx cleanup' >&2
-      exit 1
-    SH
-  end
-
   before(:all) do
     clean_nginx
     on hosts, 'mkdir -p /opt/hooks'
@@ -34,13 +16,11 @@ describe 'Pun Pre Hook' do
     restart_dex
   end
 
-  before do
-    clean_nginx
-  end
-
   after do
-    browser.close
-    clean_nginx
+    if @browser
+      @browser.close
+      clean_nginx
+    end
   end
 
   context 'pre hook crash' do
@@ -50,8 +30,6 @@ describe 'Pun Pre Hook' do
 
     it 'does not crash login when pre hook crashes' do
       browser_login(browser)
-      browser.goto ctr_base_url
-      sleep 1 # give it time to login
       expect(browser.title).to eq('Dashboard - Open OnDemand')
     end
 
@@ -67,7 +45,6 @@ describe 'Pun Pre Hook' do
 
     it 'does not crash login when pre hook is misconfigured' do
       browser_login(browser)
-      browser.goto ctr_base_url
       expect(browser.title).to eq('Dashboard - Open OnDemand')
     end
   end
