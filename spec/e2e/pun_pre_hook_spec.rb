af00f7921a9cec5f61655f9cb856360bb1873aaf
GlazerMann <kurt.glaesemann@pnnl.gov>
2026-09-18 08:56:20 -0500

    whitespace

--- spec/e2e/pun_pre_hook_spec.rb
+++ spec/e2e/pun_pre_hook_spec.rb
@@ -51,7 +51,7 @@ describe 'Pun Pre Hook' do
     it 'does not crash login when pre hook crashes' do
       browser_login(browser)
       browser.goto ctr_base_url
-      sleep 1  # give it time to login
+      sleep 1 # give it time to login
       expect(browser.title).to eq('Dashboard - Open OnDemand')
     end
 
