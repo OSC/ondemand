3b84a76bcd4d90e00415e5fc97e4e1232e1b0053
GlazerMann <kurt.glaesemann@pnnl.gov>
2026-09-19 12:11:46 -0500

    Refine comments in PunConfigGenerator class

--- nginx_stage/lib/nginx_stage/generators/pun_config_generator.rb
+++ nginx_stage/lib/nginx_stage/generators/pun_config_generator.rb
@@ -7,8 +7,11 @@ module NginxStage
   class PunConfigGenerator < Generator
     desc 'Generate a new per-user nginx config and process'
 
-    # Serialize the complete PUN initialization sequence, including directory
-    # setup, secret/config generation, pre-hooks, and process startup.
+    # Serialize the complete PUN initialization sequence, including:
+    # directory setup
+    # secret/config generation
+    # pre-hooks
+    # process startup.
     def invoke
       with_pun_restart_lock(user: user) { super }
     end
