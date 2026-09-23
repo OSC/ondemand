7449a86458f3445dc37dd3796c628e55da8a7531
Braeden Singleton <bsingleton@osc.edu>
2026-09-23 18:08:01 -0400

    Fix indentation in file_ops.js

--- apps/dashboard/app/javascript/files/file_ops.js
+++ apps/dashboard/app/javascript/files/file_ops.js
@@ -450,7 +450,7 @@ class FileOps {
       const finalUrl = iframe.contentWindow.location.href;
       if (finalUrl !== downloadUrl) {
         window.location.href = baseUrl;
-	document.body.removeChild(iframe);
+		document.body.removeChild(iframe);
       }
     };
 
